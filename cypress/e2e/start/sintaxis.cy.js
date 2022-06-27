3a85c0dbd4ed4301aff6ddf769c1a78946a0d1bf
Ely <91127281+elytest@users.noreply.github.com>
2022-06-27 03:06:52 -0300

    sintaxis ejemplo

--- cypress/e2e/start/sintaxis.cy.js
+++ cypress/e2e/start/sintaxis.cy.js
@@ -1,5 +1,19 @@
-describe('empty spec', () => {
-	it('passes', () => {
+describe('US # | TS # | TC#: aquí se escribe el escenario de prueba', () =>
+{
+	it('Aquí empiezas escribiendo una Precondición o más', () =>
+	{
 		cy.visit('https://example.cypress.io')
+	})
+	it('Aquí puedes escribir un Paso de Acción', () =>
+	{
+		cy.get('.home-list a').first().click()
+	})
+	it('Aquí puedes escribir otro Paso de Acción', () =>
+	{
+		cy.get("#query-btn").click()
+	})
+	it("Aquí puedes escribir un Resultado Esperado de este Caso", () =>
+	{
+		cy.get('.query-btn').should('contain', 'Button')
 	})
 })
