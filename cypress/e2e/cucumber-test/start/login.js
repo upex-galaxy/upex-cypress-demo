150f2bd3c7200270da323b3e946755ac841af035
Elyer Mald <elyerars@gmail.com>
2022-07-12 05:09:34 -0300

    new guide hoverover

--- cypress/e2e/cucumber-test/start/login.js
+++ cypress/e2e/cucumber-test/start/login.js
@@ -24,9 +24,21 @@ describe("Login to Orange CRM Website", () =>
     })
 })
 
+
+
+
+//________________________________________________________________________
 // Comando predeterminado para que no ocurran errores de excepciones:
 Cypress.on('uncaught:exception', (err, runnable) => {
 	// returning false here prevents Cypress from
 	// failing the test
 	return false
 })
+// Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
+const origLog = Cypress.log
+Cypress.log = function (opts, ...other) {
+	if (opts.displayName === 'xhr'|| opts.displayName === 'fetch' && opts.url.startsWith('https://')) {
+		return
+	}
+	return origLog(opts, ...other)
+}
--- cypress/e2e/test/sintaxis.cy.js
+++ cypress/e2e/test/sintaxis.cy.js
@@ -2,19 +2,22 @@ describe('US # | TS # | TC#: aquí se escribe el escenario de prueba', () =>
 {
 	it('Aquí empiezas escribiendo una Precondición o más', () =>
 	{
-		cy.visit('https://example.cypress.io')
+		cy.visit('https://example.cypress.io') // Esto es un Comando de Acción directa
 	})
 	it('Aquí puedes escribir un Paso de Acción', () =>
 	{
-		cy.get('.home-list a').first()
-			.click()
+		cy.get('[href="/commands/querying"]') // Sintaxis Comando Get de Selector de Atributos
+			.eq(2) // Esto es un método opcional
+			.click() // Esto es un método de Interacción
 	})
 	it('Aquí puedes escribir otro Paso de Acción', () =>
 	{
-		cy.get("#query-btn").click()
+		cy.get("#query-btn") // Sintaxis Comando Get de Selector de IDs
+			.click() // Esto es un método de Interacción
 	})
 	it("Aquí puedes escribir un Resultado Esperado de este Caso", () =>
 	{
-		cy.get('.query-btn').should('contain', 'Button')
+		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
+			.should('contain', 'Button') // Assertion BDD para validar resultado esperado (de muchas formas)
 	})
 })
