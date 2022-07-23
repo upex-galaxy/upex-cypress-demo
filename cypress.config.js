107901872debef04bbcc347b4cde5562dd8d6fb8
Elyer Mald <elyerars@gmail.com>
2022-07-23 02:46:21 -0300

    YML Cucumber Final Test

--- cypress.config.js
+++ cypress.config.js
@@ -13,20 +13,6 @@ module.exports = defineConfig({
 		setupNodeEvents(on, config) {
 			return require('./cypress/plugins/index.js')(on, config)
 		},
-		setupNodeEvents(on, config) {
-			on('after:spec', (spec, results) => {
-				if (results && results.video) {
-					// Do we have failures for any retry attempts?
-					const failures = _.some(results.tests, (test) => {
-						return _.some(test.attempts, {state: 'failed'})
-					})
-					if (!failures) {
-						// delete the video if the spec passed and no tests retried
-						return del(results.video)
-					}
-				}
-			})
-		},
 		specPattern: ['**/*.feature', 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'],
 		baseUrl: 'https://www.saucedemo.com/',
 	},
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,9 +15,21 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+
+// Record Videos only on failed tests.
 module.exports = (on, config) => {
-	// `on` is used to hook into various events Cypress emits
-	// `config` is the resolved Cypress config
+	on('after:spec', (spec, results) => {
+				if (results && results.video) {
+					// Do we have failures for any retry attempts?
+					const failures = _.some(results.tests, (test) => {
+						return _.some(test.attempts, {state: 'failed'})
+					})
+					if (!failures) {
+						// delete the video if the spec passed and no tests retried
+						return del(results.video)
+					}
+				}
+			})
 }
 
 //For Cypress file Download
