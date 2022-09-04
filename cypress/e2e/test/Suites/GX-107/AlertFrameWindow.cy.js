4eaf33811d4e3204698d7b80b0533b894f9f74a9
fercuis <feranando.j.masci@gmail.com##>
2022-09-04 18:24:06 -0300

    cambios actualizados, tests aun no corridos, falta terminar los scriptis de prueba

---
+++ cypress/e2e/test/Suites/GX-107/AlertFrameWindow.cy.js
@@ -0,0 +1,64 @@
+describe('US 107 : ToolsQA | Alert-Frame-Window | Browser Windows', () => {
+    before('Precondition', () => {
+        cy.visit('https://demoqa.com/browser-windows')
+        cy.url().should('contain', 'browser-windows')
+        }) // tutorial info by https://testersdock.com/cypress-new-window/
+        // https://github.com/cypress-io/cypress-example-recipes/tree/master/examples/testing-dom__tab-handling-links
+    
+    describe('US 107 | TS 261 | TC01: Validate handling a new tab window by clicking on the button named "New Tab"', () => {
+        it('Validate a new browser tab window to click the button named "New Tab', ()=> {
+            cy.get('#tabButton')
+                .invoke('removeAttr', 'btn btn-primary').click()
+            cy.visit('https://demoqa.com/sample')
+                cy.url().should('contain', 'sample')
+            cy.get('h1')
+                .should('have.text', 'This is a sample page')  //  browser tab 
+                                
+        })
+    })
+    describe('US 107 | TS 261 | TC02: Validate to select button named "New Window"', () => {
+        it('Validate handling a new pop-up window by clicking on the button named New Window', ()=>  {
+            cy.window().then((win) => {
+            cy.stub(win, 'open', url => {
+                win.location.href = 'https://demoqa.com/sample';
+            }).as("popup")
+        })
+        cy.get('button').click()
+        cy.get('@popup')
+            .should("be.called")
+        cy.get('h1')
+            .should('have.text', 'This is a sample page')
+        })
+    })
+
+    describe('US 107 | TS 261 | TC03: Validate to select button named "New Window Message"', () => {
+        it('Validate handling a new pop-up window message by clicking on the button named New Window', ()=>  {
+            cy.window().then((win) => {
+            cy.stub(win, 'open', url => {
+                win.location.href = 'https://demoqa.com/sample';
+            }).as("popup")
+        })
+        cy.get('button').click()
+        cy.get('@popup')
+            .should("be.called")
+        cy.get('body')
+            .should('have.text', 'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.')
+        })
+    })
+})
+
+
+// Command predeterminado para que no ocurran errores de excepciones: //
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
+// Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
+const origLog = Cypress.log
+Cypress.log = function (opts, ...other) {
+    if (opts.displayName === 'xhr' || opts.displayName === 'fetch' && opts.url.startsWith('https://')) {
+        return
+    }
+    return origLog(opts, ...other)
+}
