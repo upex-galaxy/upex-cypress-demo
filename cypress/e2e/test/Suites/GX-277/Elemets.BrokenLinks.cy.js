69b484508fb57570acf33407885b265c0018e871
LauraMontano <lauramotano3414@gmail.com>
2022-09-12 21:42:41 -0300

    US finalizada con exito, no estoy segura si se resolvio el TC4 de forma correcta

--- cypress/e2e/test/Suites/GX-277/Elemets.BrokenLinks.cy.js
+++ cypress/e2e/test/Suites/GX-277/Elemets.BrokenLinks.cy.js
@@ -1,5 +1,47 @@
-const { And } = require("@badeball/cypress-cucumber-preprocessor")
-const { get } = require("cypress/types/lodash")
+describe('GX277 | ToolsQA | Elements | Broken Links Images', () => {
+    beforeEach("Ingreso a elementos ", () => {
+        cy.fixture("DOM/toolsqa/Elements/BrokenLinks.Page").then((the) => { 
+            cy.visit(the.url.Base)
+            cy.url()
+                .should('include', '/broken')
+        })    
+    })
+    it('US 277 | TS 287 | TC1: Validation of a valid image.', () => {
+        cy.fixture("DOM/toolsqa/Elements/BrokenLinks.Page").then((the) => { 
+            cy.get(the.image.valid)
+                .should(([ img ]) => {
+                    expect(img.naturalWidth).not.equal(0)
+                })
+        })
+    })
+    it('US 277 | TS 287 | TC2: Validation of an invalid image.', () => {
+        cy.fixture("DOM/toolsqa/Elements/BrokenLinks.Page").then((the) => {
+            cy.get(the.image.broken)
+                .should(([ img ]) => {
+                    expect(img.naturalWidth).to.equal(0)
+                })
+        })
+    })
+    it('US 277 | TS 287 | TC3: Validation of a valid link.', () => { 
+        cy.fixture("DOM/toolsqa/Elements/BrokenLinks.Page").then((the) =>
+        {
+            cy.contains(the.link.valid)
+                .click()
+            cy.url()
+                .should('include', the.url.valid)
+        })
+    })
+    it('US 277 | TS 287 | TC4: Validation of an invalid link.', () => { 
+        cy.fixture("DOM/toolsqa/Elements/BrokenLinks.Page").then((the) =>
+        {
+            cy.contains(the.link.invalid)
+                .click()
+            cy.url()
+                .should('include', the.url.invalid)
+        })
+    })
+})
+
 
 // Comando predeterminado para que no ocurran errores de excepciones:
 Cypress.on('uncaught:exception', (err, runnable) => {
@@ -15,43 +57,3 @@ Cypress.log = function (opts, ...other) {
 	}
 	return origLog(opts, ...other)
 }
-
-describe('GX277 | ToolsQA | Elements | Broken Links Images', () => {
-    before("Ingreso a elementos ",() =>{
-        cy.visit("https://demoqa.com/broken")
-    })
-    it('US 277 | TS 287 | TC1: Validation of a valid image.', () => {
-        cy.fixture("DOM/Elements/BrokenLinks.Page.json").then((the) => { 
-            cy.get(the.image.valid)
-            .should(([ img ]) => {
-                expect(img.naturalWidth).not.equal(0)
-            })
-        })
-    })
-    it('US 277 | TS 287 | TC1: Validation of an invalid image.', () => {
-        cy.fixture("DOM/Elements/BrokenLinks.Page.json").then((the) => {
-            cy.get(the.image.broken)
-            .should(([ img ]) => {
-                expect(img.naturalWidth).to.equal(0)
-            })
-        })
-    })
-    it('US 277 | TS 287 | TC1: Validation of a valid link.', () => { 
-        cy.fixture("DOM/Elements/BrokenLinks.Page.json").then((the) =>
-        {
-            cy.get(the.link.valid)
-                .click()
-            cy.url()
-            .should('include', the.validUrl)
-        })
-    })
-    it('US 277 | TS 287 | TC1: Validation of an invalid link.', () => { 
-        cy.fixture("DOM/Elements/BrokenLinks.Page.json").then((the) =>
-        {
-            cy.get(the.link.invalid)
-                .click()
-            cy.url()
-            .should('include', the.invalidUrl)
-        })
-    })
-})
