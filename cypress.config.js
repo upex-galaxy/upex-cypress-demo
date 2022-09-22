e6f0551946a9049ddcb8da7aa6f6e8e335a7df91
Juan Francisco Volpe <juanvolpe87@gmail.com>
2022-09-22 03:04:09 -0300

    GX-36 Test (Elements.FileUploadDownload) ALL TEST PASSED / Se instalaron dos dependencias avaladas por Ely

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,6 @@
-const {defineConfig} = require('cypress')
+const { defineConfig } = require('cypress')
+const { downloadFile } = require('cypress-downloadfile/lib/addPlugin')
+const { isFileExist, findFiles } = require('cy-verify-downloads');
 
 module.exports = defineConfig({
 	projectId: '',
@@ -19,8 +21,10 @@ module.exports = defineConfig({
 		// We've imported your old cypress plugins here.
 		// You may want to clean this up later by importing these.
 		setupNodeEvents(on, config) {
-			return require('./cypress/plugins/index.js')(on, config)
-		},
+			on('task', { downloadFile })
+			on('task', { isFileExist, findFiles })
+			return require('./cypress/plugins/index.js')(on, config)			
+		},		
 		specPattern: ['**/*.feature', 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'],
 		baseUrl: 'https://demoqa.com/',
 	},
