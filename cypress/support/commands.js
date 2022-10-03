f809296f8801f5f9e76e3c0fcb897999cce765ef
Ely <elyermad@gmail.com>
2022-10-03 06:10:07 -0300

    new changes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,63 +11,6 @@ import 'cypress-file-upload';
 require('@4tw/cypress-drag-drop')
 
 
-Cypress.Commands.add("Login", () =>
-{
-    cy.fixture("DOM/sauce/login.Page").then((the) =>
-        {
-            cy.get(the.input.username).type(the.data.user)
-            .should("have.value", the.data.user)
-        
-            cy.get(the.input.password).type(the.data.password)
-            .should("have.value", the.data.password)
-        
-            cy.contains("Login").click()
-        })
-})
-Cypress.Commands.add("CustomLogin", (user,password) =>
-{
-    cy.fixture("DOM/sauce/login.Page").then((the) =>
-        {
-            cy.get(the.input.username).type(user)
-            .should("have.value", user)
-        
-            cy.get(the.input.password).type(password)
-            .should("have.value", password)
-        
-            cy.contains("Login").click()
-        })
-})
-Cypress.Commands.add("ErrorCard", () =>
-{
-    cy.fixture("DOM/sauce/login.Page").then((the) =>
-        {
-            cy.get(the.error)
-                .should("be.visible")
-                .and("contain.text",
-                    "Epic sadface: Username and password do not match any user in this service")
-        })
-})
-Cypress.Commands.add("DragDrop", (obj, X, Y) =>
-{
-    cy.get(obj)
-        .move({ deltaX: X, deltaY: Y })
-    cy.get(obj)
-        .should("have.attr", "style",`position: relative; left: ${X}px; top: ${Y}px;`)
-})
-Cypress.Commands.add("DragDropX", (obj, X, Y) =>
-{
-    cy.get(obj)
-        .move({ deltaX: X, deltaY: Y })
-    cy.get(obj)
-        .should("have.attr", "style",`position: relative; left: ${X}px; top: 0px;`)
-})
-Cypress.Commands.add("DragDropY", (obj, X, Y) =>
-{
-    cy.get(obj)
-        .move({ deltaX: X, deltaY: Y })
-    cy.get(obj)
-        .should("have.attr", "style",`position: relative; left: 0px; top: ${Y}px;`)
-})
 
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
