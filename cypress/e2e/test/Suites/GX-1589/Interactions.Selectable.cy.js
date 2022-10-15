e9031e9a6af1d20471f0862a8171f768e405d841
ZwitterKaneda <smentek@gmail.com>
2022-10-15 09:58:00 -0300

    Scrit for the anti publicity

--- cypress/e2e/test/Suites/GX-1589/Interactions.Selectable.cy.js
+++ cypress/e2e/test/Suites/GX-1589/Interactions.Selectable.cy.js
@@ -23,4 +23,19 @@ describe('US GX-1589 | TS: ✅ToolsQA | Interactions | Selectable', () => {
         selectablegridpage.numbersClicks();
     });
 
+    // Comando predeterminado para que no ocurran errores de excepciones proporcionado por Upex-Galaxy:
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+    })
+    // Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
+    const origLog = Cypress.log
+    Cypress.log = function (opts, ...other) {
+        if (opts.displayName === 'xhr' || opts.displayName === 'fetch' && opts.url.startsWith('https://')) {
+            return
+        }
+        return origLog(opts, ...other)
+    }
 });
