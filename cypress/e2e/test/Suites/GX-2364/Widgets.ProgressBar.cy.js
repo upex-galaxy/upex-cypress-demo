c0e177ebf1ee9ee6f3e9b45c233daa012be48ec4
Nico950 <nicotest950@gmail.com>
2022-11-03 19:22:14 -0300

    GX-2364 All done, test passed

--- cypress/e2e/test/Suites/GX-2364/Widgets.ProgressBar.cy.js
+++ cypress/e2e/test/Suites/GX-2364/Widgets.ProgressBar.cy.js
@@ -1,34 +1,24 @@
-const random = Math.floor(Math.random() * 5000) + 1000
-
 describe('ToolsQA|Widgets|ProgressBar', () => {
-	beforeEach('Precondición: Ir a la página de check-box', () => {
+	beforeEach('Precondition: Go to progress-bar page', () => {
 		cy.visit('https://demoqa.com/progress-bar')
 		cy.url().should('contain', 'progress-bar')
 	})
 
-	xit('2365 | TC01: Validate if the progress bar is working correctly', function () {
+	it('2365 | TC01: Validate if the progress bar is working correctly 2', function () {
+		const random = Math.floor(Math.random() * 5000) + 1000 //randoms seconds
 		cy.get('#startStopButton').click()
 		cy.wait(random)
 		cy.get('#startStopButton').click()
-		cy.get('[aria-valuenow]').invoke('attr', 'aria-valuenow').as('targetValue')
-		cy.get('#progressBar').then($textValue => {
-			const textValue = $textValue.text()
-			cy.log(textValue)
-			cy.get('@targetValue').should('match', textValue)	
-			//expect('targetValue').to.be.attr(textValue)
-		})
-	})
-	it('2365 | TC02: Validate if the progress bar is working correctly 2', function () {
-		cy.get('#startStopButton').click()
-		cy.wait(random)
-		cy.get('#startStopButton').click()
-		cy.get('[aria-valuenow]').invoke('attr', 'aria-valuenow').then(
-			targetValue => cy.log(targetValue)
+		cy.get('[aria-valuenow]').invoke('attr', 'aria-valuenow').then((targetValue) => 
+			cy.get('#progressBar').invoke('text').then((textValue) =>
+				expect(targetValue).eq(textValue.replace("%", ""))
+			)
 		)
-		cy.get('#progressBar').invoke('text').then(
-			textValue => cy.log(textValue)
-		)
-		//cy.get(targetValue).contains(textValue)
+		cy.get('#startStopButton').click()
+		cy.wait(7000)
+		cy.get('#resetButton').should('exist')
+		cy.get('#startStopButton').should('not.exist')
+
 	})
 })
 
