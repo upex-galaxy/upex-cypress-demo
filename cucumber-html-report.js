99e8fe5ec234302ef17d50b2a4597e7a20651810
Ely <elyermad@gmail.com>
2022-11-14 01:30:28 -0300

    new yml file to regression test

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -13,15 +13,5 @@ report.generate({
             name: 'ubuntu',
             version: '16.04'
         }
-    },
-    customData: {
-        title: 'Run info',
-        data: [
-            {label: 'Project', value: 'Custom project'},
-            {label: 'Release', value: '1.2.3'},
-            {label: 'Cycle', value: 'B11221.34321'},
-            {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
-            {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
-        ]
     }
 });
