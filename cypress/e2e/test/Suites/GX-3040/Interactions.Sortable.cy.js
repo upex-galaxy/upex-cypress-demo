78028ccd40a6e98ca7821819e8a6c7c7d114abcb
icastrillon <ivetcastril@gmai.com>
2022-11-24 01:29:54 -0500

    GX-3040-modificaciones gid-falta-validartextos

--- cypress/e2e/test/Suites/GX-3040/Interactions.Sortable.cy.js
+++ cypress/e2e/test/Suites/GX-3040/Interactions.Sortable.cy.js
@@ -1,7 +1,7 @@
-import {SelectablePage} from "../../../../support/Pages/GX-3040/SelectablePage"
+import {SortablePage} from "../../../../support/Pages/GX-3040/SortablePage"
 
 describe('ToolsQA | Interactions | Sortable', () => {
-	const selectablepage = new SelectablePage();
+	const sortablepage = new SortablePage();
 	
 	beforeEach('Precondition: Go to the page', () => {
 		cy.visit('https://demoqa.com/sortable')
@@ -10,83 +10,65 @@ describe('ToolsQA | Interactions | Sortable', () => {
 	
 	xit('3040|TC1:Validar que se visualice correctamente las pestañas “Lista” y “Grid”',()=> {
         //visualizar las pestañas losa y grid
-		selectablepage.validarContenidoLista().contains("List")
-		selectablepage.validarContenidoGrid().contains("Grid")
+		sortablepage.validarContenidoLista().contains("List")
+		sortablepage.validarContenidoGrid().contains("Grid")
 		
 		//verificar que la pestaña Lista esta por defecto 
-		selectablepage.validarContenidoLista().should('have.class', 'active')
-		selectablepage.validarContenidoGrid().should('not.have.class', 'active')
+		sortablepage.validarContenidoLista().should('have.class', 'active')
+		sortablepage.validarContenidoGrid().should('not.have.class', 'active')
 		
 		//Validar que la aplicación muestre una pestaña a la vez.
-        selectablepage.validarContenidoGrid().click().should('be.visible');
-		selectablepage.validarContenidoGrid().should('have.class', 'active')
-		selectablepage.validarContenidoLista().should('not.have.class', 'active')
+        sortablepage.validarContenidoGrid().click().should('be.visible');
+		sortablepage.validarContenidoGrid().should('have.class', 'active')
+		sortablepage.validarContenidoLista().should('not.have.class', 'active')
        
-		selectablepage.validarContenidoLista().click().should('be.visible');
-		selectablepage.validarContenidoLista().should('have.class', 'active')
-		selectablepage.validarContenidoGrid().should('not.have.class', 'active')
+		sortablepage.validarContenidoLista().click().should('be.visible');
+		sortablepage.validarContenidoLista().should('have.class', 'active')
+		sortablepage.validarContenidoGrid().should('not.have.class', 'active')
 	}) 
 
-    it('3040|TC2:Validar el comportamiento al arrastar un elemento en la lista de items', ()=> {
+    xit('3040|TC2:Validar el comportamiento al arrastar un elemento en la lista de items', ()=> {
 	//	const dataTransfer = new DataTransfer();
-
-		//validar la cantidad de elementos 
-	cy.get(".vertical-list-container>.list-group-item").should('have.length',6)
-	//Validar la pestaña “lista“ muestre por default  los items del 1 al 9.	selectablepage.
-	selectablepage.validarItemsLista().contains("One")
-	selectablepage.validarItemsLista().contains("Two")
-	selectablepage.validarItemsLista().contains("Three")
-	selectablepage.validarItemsLista().contains("Four")
-	selectablepage.validarItemsLista().contains("Five")
-	selectablepage.validarItemsLista().contains("Six")
+		sortablepage.validarItemsLista1();
+		sortablepage.validarItemsLista2();
+		sortablepage.validarItemsLista3();
+		sortablepage.validarItemsLista4();
+		sortablepage.validarItemsLista5();
+		sortablepage.validarItemsLista6();	
+  //validar la cantidad de elementos 
+	sortablepage.validarItems().should('have.length',6)
 	//Validar la pestaña “lista “ cuando se arraste un elemento entre otros elementos de la lista debe permanecer en el orden seleccionado y debe permitir colocar el elemento en cualquier orden
-		
-	cy.get('.vertical-list-container>.list-group-item').first()
-	   .trigger('mousedown',  { button: 0 }, { force: true })
-	cy.get(".vertical-list-container>.list-group-item").first().next().click()
-	   .trigger("mouseup", { force: true });
-
-		cy.get('.vertical-list-container>.list-group-item').contains("Three")
-		.trigger('mousedown',  { button: 0 }, { force: true })
-		cy.get(".vertical-list-container>.list-group-item").contains("Three").next().click()
-		.trigger("mouseup", { force: true });  
-
-	//	Validar el orden
-	selectablepage.validarItemsLista().contains("Two")
-	selectablepage.validarItemsLista().contains("One")
-	selectablepage.validarItemsLista().contains("Four")
-	selectablepage.validarItemsLista().contains("Three")
-	selectablepage.validarItemsLista().contains("Six")
-	selectablepage.validarItemsLista().contains("Five")
-	})
+	 sortablepage.moverItems();
 })
-  
-
-    xit('3040|TC3:Validar el comportamiento al arrastar un elemento en el grid de items',()=> {
-   
+    it('3040|TC3:Validar el comportamiento al arrastar un elemento en el grid de items',()=> { 
 	//GX-3040|TC6:Validar la pestaña “grid“ muestre por default  los items del 1 al 9 ordenado de arriba hacia abajo.
 
+		sortablepage.validarContenidoGrid().click().should('be.visible');	
+		//sortablepage.validarListGrid().should('have.length',9)
+		sortablepage.moverGrid();
+    
 	//Validar la pestaña “grid“ muestre por defaul una cuadricula del 1 al 9 ordenado de izquierda a derecha.
 	
 	})      
       
 
-//})
+  // Comando predeterminado para que no ocurran errores de excepciones proporcionado por Upex-Galaxy:
 
-
-
-//________________________________________________________________________
-// Comando predeterminado para que no ocurran errores de excepciones:
-Cypress.on('uncaught:exception', (err, runnable) => {
-	//returning false here prevents Cypress from
+  Cypress.on('uncaught:exception', (err, runnable) => {
+	// returning false here prevents Cypress from
 	// failing the test
 	return false
 })
 // Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
 const origLog = Cypress.log
 Cypress.log = function (opts, ...other) {
-	if (opts.displayName === 'xhr' || (opts.displayName === 'fetch' && opts.url.startsWith('https://'))) {
+	if (opts.displayName === 'xhr' || opts.displayName === 'fetch' && opts.url.startsWith('https://')) {
 		return
 	}
 	return origLog(opts, ...other)
 }
+
+})
+
+
+
---
+++ cypress/support/Pages/GX-3040/SortablePage.js
@@ -0,0 +1,146 @@
+export class SortablePage {
+    constructor(){
+        this.list="#demo-tab-list"
+        this.grid="#demo-tab-grid"
+        this.gridList=".create-grid"
+        this.itemList1=".vertical-list-container > :nth-child(1)"
+        this.itemList2=".vertical-list-container > :nth-child(2)"
+        this.itemList3=".vertical-list-container > :nth-child(3)"
+        this.itemList4=".vertical-list-container > :nth-child(4)"
+        this.itemList5=".vertical-list-container > :nth-child(5)"
+        this.itemList6=".vertical-list-container > :nth-child(6)"
+        this.items=".vertical-list-container>.list-group-item"
+
+        this.itemGri1=".create-grid>:nth-child(1)"
+        this.itemGri2=".create-grid>:nth-child(2)"
+        this.itemGri3=".create-grid>:nth-child(3)"
+        this.itemGri4=".create-grid>:nth-child(4)"
+        this.itemGri5=".create-grid>:nth-child(5)"
+        this.itemGri6=".create-grid>:nth-child(6)"
+        this.itemGri7=".create-grid>:nth-child(7)"
+        this.itemGri8=".create-grid>:nth-child(8)"
+        this.itemGri9=".create-grid>:nth-child(9)"
+
+    }
+    
+
+validarContenidoLista(){
+   return cy.get(this.list)
+}
+validarContenidoGrid(){
+    return cy.get(this.grid)
+}
+
+validarItemsLista1(){
+    return cy.get(this.itemList1).contains("One")
+}
+validarItemsLista2(){
+    return cy.get(this.itemList2).contains("Two")
+}
+validarItemsLista3(){
+    return cy.get(this.itemList3).contains("Three")
+}
+
+validarItemsLista4(){
+    return cy.get(this.itemList4).contains("Four")
+}
+
+validarItemsLista5(){
+    return cy.get(this.itemList5).contains("Five")
+}
+
+validarItemsLista6(){
+    return cy.get(this.itemList6).contains("Six")
+}
+
+validarItems(){
+    return cy.get(this.items)
+}
+
+validarListGrid(){
+   return cy.get(this.gridList)
+}
+moverItems() {
+    cy.get(this.itemList1)
+    .trigger('mousedown',  { button: 0 }, { force: true })
+ cy.get(this.itemList1).next().click()
+    .trigger("mouseup", { force: true });
+
+     cy.get(this.itemList2)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+     cy.get(this.itemList2).next().click()
+     .trigger("mouseup", { force: true });  
+
+     cy.get(this.itemList3)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+     cy.get(this.itemList3).next().click()
+     .trigger("mouseup", { force: true });  
+
+     cy.get(this.itemList4)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+     cy.get(this.itemList1).next().click()
+     .trigger("mouseup", { force: true });  
+
+     cy.get(this.itemList5)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+     cy.get(this.itemList2).next().click()
+     .trigger("mouseup", { force: true });  
+
+     cy.get(this.itemList6)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+     cy.get(this.itemList3).next().click()
+     .trigger("mouseup", { force: true });  
+
+    };
+
+moverGrid(){
+    
+	cy.get(this.itemGri1)
+    .trigger('mousedown',  { button: 0 }, { force: true })
+     cy.get(this.itemGri2).next().click()
+      .trigger("mouseup", { force: true });
+ 
+     cy.get(this.itemGri2)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+  cy.get(this.itemGri3).next().click()
+     .trigger("mouseup", { force: true });
+ 
+     cy.get(this.itemGri3)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+  cy.get(this.itemGri4).next().click()
+     .trigger("mouseup", { force: true });
+ 
+     cy.get(this.itemGri4)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+  cy.get(this.itemGri5).next().click()
+     .trigger("mouseup", { force: true });
+ 
+     cy.get(this.itemGri5)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+  cy.get(this.itemGri6).next().click()
+     .trigger("mouseup", { force: true });
+ 
+     cy.get(this.itemGri6)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+  cy.get(this.itemGri7).next().click()
+     .trigger("mouseup", { force: true });
+ 
+     cy.get(this.itemGri7)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+  cy.get(this.itemGri8).next().click()
+     .trigger("mouseup", { force: true });
+ 
+     cy.get(this.itemGri8)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+  cy.get(this.itemGri1).next().click()
+     .trigger("mouseup", { force: true });
+ 
+     cy.get(this.itemGri9)
+     .trigger('mousedown',  { button: 0 }, { force: true })
+  cy.get(this.itemGri2).click()
+   .trigger("mouseup", { force: true });
+ 
+ 
+}
+
+};
