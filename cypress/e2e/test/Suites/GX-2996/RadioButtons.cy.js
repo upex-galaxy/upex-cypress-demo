704fbbd9085b037feaa8cdac3a6cd8b856b9ef09
Luis Diaz <diazldev@gmail.com>
2022-11-24 10:45:53 -0300

    GX-2996 TC Radio Buttons

--- cypress/e2e/test/Suites/GX-2996/RadioButtons.cy.js
+++ cypress/e2e/test/Suites/GX-2996/RadioButtons.cy.js
@@ -1,4 +1,28 @@
-describe("",() => {
+describe("GX-2996 | ToolsQA | Elements | Radio Buttons",() => {
+	beforeEach("Test the Radio button", () => {
+
+		cy.visit("https://demoqa.com/radio-button")
+        cy.url().should("contain","radio-button")
+	
+	})
+    it("2996 | TC:1 Validar poder selccionar RB Yes | Validar que fue selccionado |",()=>{
+		cy.contains("Do you like the site?").should("be.visible")
+		cy.get("[type='radio']").eq(0).click({force:true})
+			.should("be.checked")
+		cy.get(".mt-3").should("have.text", "You have selected Yes")
+    })
+	it("2996 | TC:2 Validar poder selccionar RB Impressive | Validar que fue selccionado |",()=>{
+		cy.get("[type='radio']").eq(1).click({force:true})
+			.should("be.checked")
+		cy.get(".mt-3").should("have.text", "You have selected Impressive")
+    })
+	it.only("2996 | TC:1 Validar poder selccionar RB yes | Validar que fue selccionado |",()=>{
+
+		cy.get("[type='radio']").eq(2).click({force:true})
+			.should("be.disabled")
+		cy.get(".mt-3").should("not.exist")
+    })
+	
 
 })
 
