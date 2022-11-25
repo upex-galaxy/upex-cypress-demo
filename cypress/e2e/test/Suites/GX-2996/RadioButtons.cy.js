b858e9d9f064821d50dae2e0062f33cfe8e8b785
Luis Diaz <diazldev@gmail.com>
2022-11-25 09:20:32 -0300

    GX-2996 Corrección de TC

--- cypress/e2e/test/Suites/GX-2996/RadioButtons.cy.js
+++ cypress/e2e/test/Suites/GX-2996/RadioButtons.cy.js
@@ -5,18 +5,18 @@ describe("GX-2996 | ToolsQA | Elements | Radio Buttons",() => {
         cy.url().should("contain","radio-button")
 	
 	})
-    it("2996 | TC:1 Validar poder selccionar RB Yes | Validar que fue selccionado |",()=>{
+    it("2996 | TC:1 Validar poder seleccionar RB Yes | Validar que fue seleccionado |",()=>{
 		cy.contains("Do you like the site?").should("be.visible")
 		cy.get("[type='radio']").eq(0).click({force:true})
 			.should("be.checked")
 		cy.get(".mt-3").should("have.text", "You have selected Yes")
     })
-	it("2996 | TC:2 Validar poder selccionar RB Impressive | Validar que fue selccionado |",()=>{
+	it("2996 | TC:2 Validar poder seleccionar RB Impressive | Validar que fue seleccionado |",()=>{
 		cy.get("[type='radio']").eq(1).click({force:true})
 			.should("be.checked")
 		cy.get(".mt-3").should("have.text", "You have selected Impressive")
     })
-	it("2996 | TC:1 Validar no poder selccionar RB No | Validar que no fue selccionado |",()=>{
+	it("2996 | TC:1 Validar no poder seleccionar RB No | Validar que no fue seleccionado |",()=>{
 
 		cy.get("[type='radio']").eq(2).click({force:true})
 			.should("be.disabled")
