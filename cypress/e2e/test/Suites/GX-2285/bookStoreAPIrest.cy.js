85008f31ad32137feac9ac74d105c32e3474ac5b
ZwitterKaneda <smentek@gmail.com>
2022-11-25 03:20:42 -0300

    GX-2285 Actualización: Se resolvió el problema de autorización necesaria para poder realizar peticiones DELETE. Queda por resolver la selección de libros aleatorios para poder borrar. Quedará pendiente la modificación de un libro aleatorio mediante PUT.

--- cypress/e2e/test/Suites/GX-2285/bookStoreAPIrest.cy.js
+++ cypress/e2e/test/Suites/GX-2285/bookStoreAPIrest.cy.js
@@ -1,25 +1,96 @@
-describe('✅BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-DELETE)', () => {
-   
-   
-    it('2294 | TC1:  Validar por backend la eliminación de un libro de la tienda', () => {
-        cy.request({
-            method: 'POST', url: 'https://demoqa.com/Account/v1/User',
-            body: {userName: "kaneharu", password: "8Miau679"}
-        })
-                // cy.api({
-        //     method: 'GET',
-        //     url: 'https://demoqa.com/BookStore/v1/Books',
-        // })
-        //     .then(({ body }) => {
-        //         cy.log(body)
-        //     })
-        // cy.api({
-        //     method: 'DELETE',
-        //     url: 'https://demoqa.com/BookStore/v1/Books?UserId=KaneHaru',
-        //     qs: {
-        //         isbn: '9781449325862',
-        //     }
-        // })
+const userNameID = "Kanesme"
+const passwordID = '3K$M1o2yZ'
 
-    })
+describe('✅BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-DELETE)', () => {
+
+	it('2294 | TC1:  Validar por backend la eliminación de un libro de la tienda', () => {
+		//Creación de usuario
+		cy.api({
+			method: 'GET',
+			url: 'https://demoqa.com/BookStore/v1/Books'
+		}).then(({body})=>{
+			
+			const bookRandom = Math.floor(Math.random() * body.books.lenght);
+			cy.log(bookRandom)
+			const miau2 = body.books[3].isbn
+			cy.log(miau2)
+		})
+		const miau = "cisco miauuuu"
+		cy.api({
+			method: 'POST',
+			url: 'https://demoqa.com/Account/v1/User',
+			body: {
+				userName: userNameID,
+				password: passwordID,
+			},
+		}).then(({body}) => {
+			// //Generando Token (al pedo porque no se utiliza)
+			cy.log(miau)
+			const userID = body.userID
+			// cy.log(userID)
+			// cy.api({
+			// 	method: 'POST',
+			// 	url: 'https://demoqa.com/Account/v1/GenerateToken',
+			// 	body: {
+			// 		userName: userNameID,
+			// 		password: passwordID,
+			// 	},
+			// })
+			// 	.then(({body}) => {
+			// 		// Autorizando Usuario (?)
+			// 		const token = body.token
+			// 		cy.api({
+			// 			method: 'POST',
+			// 			url: 'https://demoqa.com/Account/v1/Authorized',
+			// 			body: {
+			// 				userName: userNameID,
+			// 				password: passwordID,
+			// 			},
+			// 		}).then((response) => {
+			// 			cy.log(response)
+			// 		})
+			// 	}).then(()=>{
+					cy.api({
+						    method: 'DELETE',
+						    url: `https://demoqa.com/BookStore/v1/Books?UserId=${userID}`,
+							auth: {
+								username: userNameID,
+								password: passwordID,
+							},
+						    body: {
+						        isbn: '9781449325862',
+						    }
+				})
+				.then(() => {
+					
+					cy.api({
+						method: 'DELETE',
+						url: `https://demoqa.com/Account/v1/User/${userID}`,
+						auth: {
+							username: userNameID,
+							password: passwordID,
+						},
+						body: {
+							userId: userID,
+						},
+					})
+				})
+		})
+	})
 })
+// })
+
+// cy.api({
+//     method: 'GET',
+//     url: 'https://demoqa.com/BookStore/v1/Books',
+// })
+//     .then(({ body }) => {
+//         cy.log(body)
+//     })
+// cy.api({
+//     method: 'DELETE',
+//     url: 'https://demoqa.com/BookStore/v1/Books?UserId=KaneHaru',
+//     qs: {
+//         isbn: '9781449325862',
+//     }
+// })
