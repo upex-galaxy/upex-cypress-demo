d281ccbb7291dbd52f55336e60caec0fd0523717
ZwitterKaneda <smentek@gmail.com>
2022-11-25 23:28:08 -0300

    GX-2285: Actualización, se complicó con los datos a ingresar para realizar la petición PUT, nuevo bloqueo.

--- cypress/e2e/test/Suites/GX-2285/bookStoreAPIrest.cy.js
+++ cypress/e2e/test/Suites/GX-2285/bookStoreAPIrest.cy.js
@@ -1,21 +1,23 @@
-const userNameID = "Kanesme"
+const userNameID = "CiscoMiau5"
 const passwordID = '3K$M1o2yZ'
 
 describe('✅BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-DELETE)', () => {
 
-	it('2294 | TC1:  Validar por backend la eliminación de un libro de la tienda', () => {
+	it('2294 | TC1:  Validar por backend la eliminación de un libro aleatorio de la tienda', () => {
 		//Creación de usuario
+		let book
 		cy.api({
 			method: 'GET',
 			url: 'https://demoqa.com/BookStore/v1/Books'
 		}).then(({body})=>{
 			
-			const bookRandom = Math.floor(Math.random() * body.books.lenght);
+			//Libro aleatorio
+			const bookRandom = Math.floor(Math.random() * 8);
 			cy.log(bookRandom)
-			const miau2 = body.books[3].isbn
-			cy.log(miau2)
+			const book = body.books[bookRandom].isbn
+			
 		})
-		const miau = "cisco miauuuu"
+		
 		cy.api({
 			method: 'POST',
 			url: 'https://demoqa.com/Account/v1/User',
@@ -24,33 +26,9 @@ describe('✅BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-D
 				password: passwordID,
 			},
 		}).then(({body}) => {
-			// //Generando Token (al pedo porque no se utiliza)
-			cy.log(miau)
+			
 			const userID = body.userID
-			// cy.log(userID)
-			// cy.api({
-			// 	method: 'POST',
-			// 	url: 'https://demoqa.com/Account/v1/GenerateToken',
-			// 	body: {
-			// 		userName: userNameID,
-			// 		password: passwordID,
-			// 	},
-			// })
-			// 	.then(({body}) => {
-			// 		// Autorizando Usuario (?)
-			// 		const token = body.token
-			// 		cy.api({
-			// 			method: 'POST',
-			// 			url: 'https://demoqa.com/Account/v1/Authorized',
-			// 			body: {
-			// 				userName: userNameID,
-			// 				password: passwordID,
-			// 			},
-			// 		}).then((response) => {
-			// 			cy.log(response)
-			// 		})
-			// 	}).then(()=>{
-					cy.api({
+								cy.api({
 						    method: 'DELETE',
 						    url: `https://demoqa.com/BookStore/v1/Books?UserId=${userID}`,
 							auth: {
@@ -58,10 +36,10 @@ describe('✅BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-D
 								password: passwordID,
 							},
 						    body: {
-						        isbn: '9781449325862',
+						        isbn: book,
 						    }
 				})
-				.then(() => {
+				.then((respuesta) => {
 					
 					cy.api({
 						method: 'DELETE',
@@ -74,23 +52,68 @@ describe('✅BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-D
 							userId: userID,
 						},
 					})
+
+				}).then((respuesta)=>{expect(respuesta.status).equal(204)})
+		})
+	})
+	xit('2294 | TC2:  Validar por backend la modificación de un libro aleatorio de la tienda', () => {
+		//Creación de usuario
+		let book
+		cy.api({
+			method: 'GET',
+			url: 'https://demoqa.com/BookStore/v1/Books'
+		}).then(({body})=>{
+			
+			//Libro aleatorio
+			const bookRandom = Math.floor(Math.random() * 8);
+			cy.log(bookRandom)
+			const book = body.books[bookRandom].isbn
+			
+		})
+		
+		cy.api({
+			method: 'POST',
+			url: 'https://demoqa.com/Account/v1/User',
+			body: {
+				userName: userNameID,
+				password: passwordID,
+			},
+		}).then(({body}) => {
+			
+			const userID = body.userID
+								cy.api({
+						    method: 'PUT',
+						    url: `https://demoqa.com/BookStore/v1/Books/${book}`,
+							auth: {
+								username: userNameID,
+								password: passwordID,
+							},
+							qs: {
+								username: userNameID,
+						        isbn: book
+								
+						    },
+						    body: {
+								username: userNameID,
+						        isbn: book,
+								
+						    }
 				})
+				.then((respuesta) => {
+					
+					cy.api({
+						method: 'DELETE',
+						url: `https://demoqa.com/Account/v1/User/${userID}`,
+						auth: {
+							username: userNameID,
+							password: passwordID,
+						},
+						body: {
+							userId: userID,
+						},
+					})
+
+				}).then((respuesta)=>{expect(respuesta.status).equal(204)})
 		})
 	})
 })
-// })
-
-// cy.api({
-//     method: 'GET',
-//     url: 'https://demoqa.com/BookStore/v1/Books',
-// })
-//     .then(({ body }) => {
-//         cy.log(body)
-//     })
-// cy.api({
-//     method: 'DELETE',
-//     url: 'https://demoqa.com/BookStore/v1/Books?UserId=KaneHaru',
-//     qs: {
-//         isbn: '9781449325862',
-//     }
-// })
