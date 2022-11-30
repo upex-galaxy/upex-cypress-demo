c4bf241efcdcf96c7499c52d8c7cfc4994c4109f
ZwitterKaneda <smentek@gmail.com>
2022-11-30 02:14:03 -0300

    GX-2285 Finalizando con el proyecto. Se utilizó guia de API Aplication creada por @Ely. Se realizaron modificaciones, eliminación de libro almacenado en perfil de usuario. Eliminación de cuenta de usuario. (OOS)Se agregó la eliminación de usuario para no generar posible error al correr indifinidamente el test.

--- cypress/e2e/test/Suites/GX-2285/bookStoreAPIrest.cy.js
+++ cypress/e2e/test/Suites/GX-2285/bookStoreAPIrest.cy.js
@@ -1,119 +1,231 @@
-const userNameID = "CiscoMiau5"
-const passwordID = '3K$M1o2yZ'
+const the = require('../../../../fixtures/DOM/toolsqa/BookStoreApplications/GX-2285/userID.Page.json')
+const username = 'DiazDaniel'
+const password = '3K$M1o2yZ'
 
-describe('✅BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-DELETE)', () => {
-
-	it('2294 | TC1:  Validar por backend la eliminación de un libro aleatorio de la tienda', () => {
-		//Creación de usuario
-		let book
-		cy.api({
-			method: 'GET',
-			url: 'https://demoqa.com/BookStore/v1/Books'
-		}).then(({body})=>{
-			
-			//Libro aleatorio
-			const bookRandom = Math.floor(Math.random() * 8);
-			cy.log(bookRandom)
-			const book = body.books[bookRandom].isbn
-			
-		})
-		
+describe(the.Test.US, () => {
+	before(() => {
+		// CREAR USUARIO:
 		cy.api({
 			method: 'POST',
-			url: 'https://demoqa.com/Account/v1/User',
+			url: the.Account.createUser,
 			body: {
-				userName: userNameID,
-				password: passwordID,
+				userName: username,
+				password: password,
 			},
-		}).then(({body}) => {
-			
-			const userID = body.userID
-								cy.api({
-						    method: 'DELETE',
-						    url: `https://demoqa.com/BookStore/v1/Books?UserId=${userID}`,
-							auth: {
-								username: userNameID,
-								password: passwordID,
-							},
-						    body: {
-						        isbn: book,
-						    }
-				})
-				.then((respuesta) => {
-					
-					cy.api({
-						method: 'DELETE',
-						url: `https://demoqa.com/Account/v1/User/${userID}`,
-						auth: {
-							username: userNameID,
-							password: passwordID,
-						},
-						body: {
-							userId: userID,
-						},
-					})
+		}).then((response) => {
+			expect(response.status).to.equal(201)
+			expect(response.body.username).to.be.equal(username)
+			userID = response.body.userID
+			cy.log(userID)
 
-				}).then((respuesta)=>{expect(respuesta.status).equal(204)})
+			// GENERAR TOKEN:
+			cy.api({
+				method: 'POST',
+				url: the.Account.generateToken,
+				body: {
+					userName: username,
+					password: password,
+				},
+			}).then(({body}) => {
+				expect(body.token).to.be.an('string')
+				expect(body.status).to.be.equal('Success')
+				expect(body.result).to.be.equal('User authorized successfully.')
+
+				// GET TOKEN AND SAVE IT:
+				token = body.token
+				cy.log(token)
+
+				// VERIFICAR AUTH:
+				cy.api({
+					method: 'POST',
+					url: the.Account.verifyAuth,
+					body: {
+						userName: username,
+						password: password,
+					},
+				}).then(({body}) => {
+					expect(body).to.be.an('boolean')
+					expect(body).to.be.true
+
+					// OBTENER COLLECTION:
+					cy.api({
+						url: the.Profile.GetCollection + userID,
+						auth: {
+							bearer: token,
+							userName: username,
+							password: password,
+						},
+					}).then(({body}) => {
+						expect(body.books).to.be.empty
+					})
+					// GET ONE BOOK FROM STORE:
+					cy.api({
+						url: the.Store.AllBooks,
+					}).then(({body}) => {
+						expect(body.books.length).to.be.greaterThan(1)
+
+						let random = () => Math.floor(Math.random() * body.books.length - 1) + 1
+						const forStore = random()
+						let forUser = random()
+						while (forStore == forUser) {
+							forUser = random()
+						}
+						const storeBook = body.books[forStore].isbn
+						cy.log(`This is the Book I want: ${storeBook}`)
+						const userBook = body.books[forUser].isbn
+						cy.log(`This is the Book I will get: ${userBook}`)
+
+						// TAKE A BOOK FROM STORE:
+						cy.api({
+							method: 'POST',
+							url: the.Store.TakeBook,
+							auth: {
+								bearer: token,
+							},
+							body: {
+								userId: userID,
+								collectionOfIsbns: [
+									{
+										isbn: userBook,
+									},
+								],
+							},
+						}).then(({body}) => {
+							cy.log(`This one should be my current Book in Collection:${userBook}`)
+							const obj = body.books.find(({isbn}) => isbn === userBook)
+							cy.log(obj)
+							expect(obj).include({
+								isbn: userBook,
+							})
+							cy.log(`I Sent ${userBook} and I received Response body: ${obj.isbn}`)
+						})
+
+						cy.log('SAVE VARIABLES IN FIXTURE FILES')
+						cy.writeFile('cypress/fixtures/DOM/toolsqa/BookStoreApplications/GX-2285/userData.Page.json', {
+							userID: userID,
+							token: token,
+							storeIsbn: storeBook,
+							userIsbn: userBook,
+						})
+					})
+				})
+			})
 		})
 	})
-	xit('2294 | TC2:  Validar por backend la modificación de un libro aleatorio de la tienda', () => {
-		//Creación de usuario
-		let book
-		cy.api({
-			method: 'GET',
-			url: 'https://demoqa.com/BookStore/v1/Books'
-		}).then(({body})=>{
-			
-			//Libro aleatorio
-			const bookRandom = Math.floor(Math.random() * 8);
-			cy.log(bookRandom)
-			const book = body.books[bookRandom].isbn
-			
-		})
-		
-		cy.api({
-			method: 'POST',
-			url: 'https://demoqa.com/Account/v1/User',
-			body: {
-				userName: userNameID,
-				password: passwordID,
-			},
-		}).then(({body}) => {
-			
-			const userID = body.userID
-								cy.api({
-						    method: 'PUT',
-						    url: `https://demoqa.com/BookStore/v1/Books/${book}`,
-							auth: {
-								username: userNameID,
-								password: passwordID,
-							},
-							qs: {
-								username: userNameID,
-						        isbn: book
-								
-						    },
-						    body: {
-								username: userNameID,
-						        isbn: book,
-								
-						    }
-				})
-				.then((respuesta) => {
-					
-					cy.api({
-						method: 'DELETE',
-						url: `https://demoqa.com/Account/v1/User/${userID}`,
-						auth: {
-							username: userNameID,
-							password: passwordID,
-						},
-						body: {
-							userId: userID,
-						},
-					})
+	it(the.Test.TC2, () => {
+		cy.fixture('DOM/toolsqa/BookStoreApplications/GX-2285/userData.Page').then((data) => {
+			// VERIFICAR AUTH
+			cy.api({
+				method: 'POST',
+				url: the.Account.verifyAuth,
+				body: {
+					userName: username,
+					password: password,
+				},
+			}).then(({body}) => {
+				expect(body).to.be.an('boolean')
+				expect(body).to.be.true
 
-				}).then((respuesta)=>{expect(respuesta.status).equal(204)})
+				// OBTENER LA COLLECTION DEBERÍA HABER UN LIBRO:
+				cy.api({
+					url: the.Profile.GetCollection + data.userID,
+					auth: {
+						bearer: data.token,
+						userName: username,
+						password: password,
+					},
+				}).then(({body}) => {
+					expect(body.books).to.be.not.empty
+					const desiredBook = data.storeIsbn
+					const currentBook = data.userIsbn
+					const books = body.books.map(({isbn}) => isbn)
+					expect(currentBook).to.be.oneOf(books)
+					expect(desiredBook).to.not.be.oneOf(books)
+				})
+
+				// TC: CAMBIAR EL LIBRO DE COLLECTION:
+				cy.api({
+					method: 'PUT',
+					url: the.Store.SwitchBook + data.userIsbn, // This is my current Book in Collection
+					auth: {
+						bearer: data.token,
+					},
+					body: {
+						userId: data.userID,
+						isbn: data.storeIsbn, // The Store's Book I want to change with my current Book
+					},
+				}).then((response) => {
+					expect(response.status).to.equal(200)
+					const updatedBook = data.storeIsbn
+					const removedBook = data.userIsbn
+					const books = response.body.books.map(({isbn}) => isbn)
+					expect(updatedBook).to.be.oneOf(books)
+					expect(removedBook).to.not.be.oneOf(books)
+				})
+			})
+		})
+	})
+	it(the.Test.TC1, () => {
+		cy.fixture('DOM/toolsqa/BookStoreApplications/GX-2285/userData.Page').then((data) => {
+			// VERIFICAR AUTH
+			cy.api({
+				method: 'POST',
+				url: the.Account.verifyAuth,
+				body: {
+					userName: username,
+					password: password,
+				},
+			}).then(({body}) => {
+				expect(body).to.be.an('boolean')
+				expect(body).to.be.true
+				// OBTENER LA COLLECTION DEBERÍA HABER UN LIBRO:
+				cy.api({
+					url: the.Profile.GetCollection + data.userID,
+					auth: {
+						bearer: data.token,
+						userName: username,
+						password: password,
+					},
+				}).then(({body}) => {
+					expect(body.books).to.be.not.empty
+					const currentBook = data.storeIsbn
+					const books = body.books.map(({isbn}) => isbn)
+					expect(currentBook).to.be.oneOf(books)
+				})
+				//Borrar Libro de la colección
+				cy.api({
+					method: 'DELETE',
+					url: the.Store.DeleteBook + data.userID,
+					auth: {
+						bearer: data.token,
+						username: username,
+						password: password,
+					},
+					body: {
+						isbn: data.storeIsbn,
+					},
+				}).then((response) => {
+					expect(response.status).to.equal(204)
+				})
+			})
+		})
+	})
+	after(() => {
+		cy.fixture('DOM/toolsqa/BookStoreApplications/GX-2285/userData.Page').then((data) => {
+			//Eliminación de Usuario
+			cy.api({
+				method: 'DELETE',
+				url: the.Account.DeleteUser + data.userID,
+				auth: {
+					username: username,
+					password: password,
+				},
+				body: {
+					userId: userID,
+				},
+			}).then((respuesta) => {
+				expect(respuesta.status).equal(204)
+			})
 		})
 	})
 })
