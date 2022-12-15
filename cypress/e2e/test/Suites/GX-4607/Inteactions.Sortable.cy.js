25aa77d706278abbae65d057ef2727c181c24092
gero <goya.geronimo@gmail.com>
2022-12-15 14:46:30 -0800

    primeros avances + command  para drag&drop

---
+++ cypress/e2e/test/Suites/GX-4607/Inteactions.Sortable.cy.js
@@ -0,0 +1,107 @@
+describe('GX-4607 ToolsQA | Inteactions | Sortable', () => {
+	beforeEach(() => {
+		cy.gotoSortablePage()
+	})
+
+
+	it('4607 | TC1: The tab “List” must be opened by default showing the contained list of items.', () => {
+		cy.get('demo-tab-list').should('be.visible')
+		cy.get('demo-tab-grid').should('be.visible')
+	});
+	
+	it('4607 | TC2: The tabs List and “Gird” must be showing by default.', () => {
+		cy.get('a[id="demo-tab-list"]').should('have.attr', 'aria-selected', 'true')
+		cy.get('a[id="demo-tab-grid"]').should('have.attr', 'aria-selected', 'false')
+		//cy.get('demo-tab-grid').should('be.visible')
+	});
+
+	it('4607 | TC3: Only one tab can be displayed at once. ', () => {
+		if (cy.get('a[id="demo-tab-list"]').should('have.attr', 'aria-selected', 'true')) {
+			cy.get('a[id="demo-tab-grid"]').should('have.attr', 'aria-selected', 'false')
+		} else {
+			cy.get('a[id="demo-tab-list"]').should('have.attr', 'aria-selected', 'false')
+		}
+	});
+
+	it('4607 | TC4: Default order of LIST items', () => {
+
+		cy.get('#demo-tabpane-list > div > div:nth-child(1)')
+		.invoke('text').should('eq','One')
+		cy.get('#demo-tabpane-list > div > div:nth-child(2)').invoke('text').should('eq','Two')
+		cy.get('#demo-tabpane-list > div > div:nth-child(3)').invoke('text').should('eq','Three')
+		cy.get('#demo-tabpane-list > div > div:nth-child(4)').invoke('text').should('eq','Four')
+		cy.get('#demo-tabpane-list > div > div:nth-child(5)').invoke('text').should('eq','Five')
+		cy.get('#demo-tabpane-list > div > div:nth-child(6)').invoke('text').should('eq','Six')
+
+	});	
+
+	it.only('4607 | TC5: Drag an element of the LIST and verify new location', () => {
+	//	cy.get('#demo-tabpane-list > div > div:nth-child(1)')
+/*	let listItem = cy.get('#demo-tabpane-list').within(()=>{ cy.get('div[class="list-group-item list-group-item-action"]').invoke('text')})
+		console.log("ITEM----->" + listItem[0])
+		*/
+
+		cy.get('div[class="vertical-list-container mt-4"]').within(() =>{
+			cy.get('div[class="list-group-item list-group-item-action"]').eq(5).invoke('text').should('eq', 'Six')
+
+		})
+	});	
+
+
+	it('4607 | TC6: Default order of GRID items', () => {
+		cy.get('a[id="demo-tab-grid"]').click()
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(1)').invoke('text').should('eq','One')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(2)').invoke('text').should('eq','Two')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(3)').invoke('text').should('eq','Three')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(4)').invoke('text').should('eq','Four')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(5)').invoke('text').should('eq','Five')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(6)').invoke('text').should('eq','Six')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(7)').invoke('text').should('eq','Seven')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(8)').invoke('text').should('eq','Eight')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(9)').invoke('text').should('eq','Nine')
+	});
+
+	it('4607 | TC7: Drag and drop a random element to another location', () => {
+		/*
+		const dataTransfer = new DataTransfer()
+		cy.get('.create-grid').contains('One').trigger('dragstart', {
+			dataTransfer
+		})
+		
+		cy.get('.create-grid').contains('Nine').trigger('drop', {
+			dataTransfer
+		})
+		
+		//div[class="create-grid"][div[class="list-group-item list-group-item-action"]
+		*/
+		cy.get('a[id="demo-tab-grid"]').click()
+
+		cy.moveFromTo('#demo-tabpane-grid > div > div > div:nth-child(9)', '#demo-tabpane-grid > div > div > div:nth-child(1)')
+		cy.get('#demo-tabpane-grid > div > div > div:nth-child(1)').invoke('text').should('eq', 'Nine')
+		
+		/*cy.get('#demo-tabpane-grid > div > div > div:nth-child(9)').drag('#demo-tabpane-grid > div > div > div:nth-child(1)', {force: true}).then((success) =>{
+			assert.isTrue(success)
+		})
+*/
+
+
+    })
+    
+  
+	
+	//________________________________________________________________________
+	// Comando predeterminado para que no ocurran errores de excepciones:
+	Cypress.on('uncaught:exception', (err, runnable) => {
+		// returning false here prevents Cypress from
+		// failing the test
+		return false
+	})
+	// Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
+	const origLog = Cypress.log
+	Cypress.log = function (opts, ...other) {
+		if (opts.displayName === 'xhr' || (opts.displayName === 'fetch' && opts.url.startsWith('https://'))) {
+			return
+		}
+		return origLog(opts, ...other)
+	}
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -640,6 +640,13 @@ Cypress.Commands.add("gotoButtonsPage", ()=>
 	
 })
 
+Cypress.Commands.add("gotoSortablePage", ()=>
+{
+	cy.visit('https://demoqa.com/sortable')
+	cy.url().should('contain', 'sortable')
+	
+})
+
 Cypress.Commands.add("signin", (username, password) =>
 {
     {
@@ -754,3 +761,49 @@ Cypress.Commands.add("validateSelectFile",()=>
     cy.get('input[type=file]').selectFile('sampleFile.jpeg');
     cy.get('#uploadedFilePath').should('contain.text','sampleFile.jpeg');
 })
+
+Cypress.Commands.add('moveFromTo', (subject, target) => {
+    Cypress.log({
+        name: 'DRAGNDROP',
+        message: `Dragging element ${subject} to ${target}`,
+        consoleProps: () => {
+            return {
+                subject: subject,
+                target: target
+            };
+        }
+    });
+    const BUTTON_INDEX = 0;
+    const SLOPPY_CLICK_THRESHOLD = 10;
+    cy.get(target)
+        .first()
+        .then($target => {
+            let coordsDrop = $target[0].getBoundingClientRect();
+            cy.get(subject)
+                .first()
+                .then(subject => {
+                    const coordsDrag = subject[0].getBoundingClientRect();
+                    cy.wrap(subject)
+                        .trigger('mousedown', {
+                            button: BUTTON_INDEX,
+                            clientX: coordsDrag.x,
+                            clientY: coordsDrag.y,
+                            force: true
+                        })
+                        .trigger('mousemove', {
+                            button: BUTTON_INDEX,
+                            clientX: coordsDrag.x + SLOPPY_CLICK_THRESHOLD,
+                            clientY: coordsDrag.y,
+                            force: true
+                        });
+                    cy.get('body')
+                        .trigger('mousemove', {
+                            button: BUTTON_INDEX,
+                            clientX: coordsDrop.x,
+                            clientY: coordsDrop.y,
+                            force: true            
+                        })
+                        .trigger('mouseup');
+                });
+        });
+});
