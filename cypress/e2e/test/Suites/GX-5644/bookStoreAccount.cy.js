0802b53aa5ee5e2914c3811d81967d723860b5d8
QAZebass <ze.basszender@gmail.com>
2022-12-27 16:11:16 -0300

    GX-5644 Actualizacion CI-buildTest

--- cypress/e2e/test/Suites/GX-5644/bookStoreAccount.cy.js
+++ cypress/e2e/test/Suites/GX-5644/bookStoreAccount.cy.js
@@ -1,3 +1,8 @@
 describe('GX-5644 ✅BookStore | Account | Crear, Obtener y Eliminar Usuario (POST-GET-DELETE)',()=>{
 
+    it('5645 | TC1: Validate that the user registers successfully',()=>{
+        cy.visit('/');
+    })
+
+
 })
