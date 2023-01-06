d15617cafc317d154213aea79286fb74ddc78e1d
gero <goya.geronimo@gmail.com>
2023-01-05 20:09:22 -0800

    GX-5770 Se agregam mas TCs

--- cypress/e2e/test/Suites/GX-5770/Widgets.DatePicker.cy.js
+++ cypress/e2e/test/Suites/GX-5770/Widgets.DatePicker.cy.js
@@ -1,17 +1,16 @@
 
-const { datePickerPage } = require('../../../../support/Pages/GX-5770/datePickerPage')
+import {datePickerPage} from '@pages/GX-5770/datePickerPage.js'
+
 
 describe('GX-5770 | ToolsQA | Widgets | Date Picker', () => {
-	//const datepickerPage = new DatePickerPage()
 
 	let today = new Date()
 	let currentDay = new Date()
 
-	// Calculate Previous Month 
 	currentDay.setMonth(currentDay.getMonth() - 1)
 	const previousMonth = currentDay.toLocaleString('default', {month: 'long'})
 	// Calculate Next Month
-	currentDay.setMonth(currentDay.getMonth()+2)
+	currentDay.setMonth(currentDay.getMonth() + 2)
 	const nextMonth = currentDay.toLocaleString('default', {month: 'long'})
 
 	let dd = String(today.getDate()).padStart(2, '0')
@@ -21,11 +20,8 @@ describe('GX-5770 | ToolsQA | Widgets | Date Picker', () => {
 	let hour = today.getHours()
 	let mins = today.getMinutes()
 	const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-	// Get the name of the current month
 	let monthName = months[today.getMonth()]
-	// Get current day in format mm/dd/yyyy
 	currentDay = mm + '/' + dd + '/' + yyyy
-	// Get current time in format XX:XX AM/PM
 	currentTime = dateTOAMORPM(today)
 	function dateTOAMORPM() {
 		var AMPM = hour >= 12 ? 'PM' : 'AM'
@@ -43,20 +39,31 @@ describe('GX-5770 | ToolsQA | Widgets | Date Picker', () => {
 
 	it('5772 | TC1: Date Picker 1 and 2 must display the current Date (2 different formats)', () => {
 		cy.log(`Date: ${currentDay} Time: ${currentTime} Month Name: ${monthName} Previous Month: ${previousMonth} Next Month: ${nextMonth}`) //debugging
+		datePickerPage.get
+			.datePicker1()
+			.invoke('val')
+			.should('match', /\d\d\/\d\d\/\d\d\d\d/i)
+		cy.log(`FORMAT HAS BEEN VERIFIED ON DATE PICKER 1`)
+		datePickerPage.get.datePicker1().invoke('val').should('eq', `${currentDay}`)
+		cy.log(`CURRENT DAY HAS BEEN VERIFIED ON DATE PICKER 1`)
+		datePickerPage.get.datePicker2().invoke('val').should('eq', `${monthName} ${dd2}, ${yyyy} ${currentTime}`) // Aveces falla porque durante la ejecucion cambia de minuto
+		cy.log(`CURRENT DATE AND FORMAT HAVE BEEN VERIFIED ON DATE PICKER 2`)
 
-		datePickerPage.verifyFormatOnDatePicker1()
-		datePickerPage.verifyCurrentDayOnDatePicker1(currentDay)
-		datePickerPage.verifyDatePicker2(monthName, dd2, yyyy, currentTime)
-
-		cy.log(`Previous Month is:  ${previousMonth}`)
 	})
 
 	it('5772 | TC2: Date Picker 1 - Month Selection - Left arrow', () => {
-		cy.log(`Current Month: ${monthName}`)
-		cy.log(`Previous Month: ${previousMonth}`)
 		datePickerPage.clickOnDatePicker1()
 		datePickerPage.clickPreviousMonthOnDatePicker1()
-		datePickerPage.verifyPreviousMonthOnDatePicker1(previousMonth, yyyy)
+		cy.log(`Current Month: ${monthName} Previous Month: ${previousMonth}`).then(() => {
+			if (previousMonth == 'December') {
+				datePickerPage.get
+					.selectedMonthPicker1()
+					.invoke('text')
+					.should('eq', `${previousMonth} ${yyyy - 1}`)
+			} else {
+				cy.contains(`${previousMonth} ${yyyy}`)
+			}
+		})
 	})
 
 	it('5772 | TC3: Date Picker 1 - Month Selection - Right arrow', () => {
@@ -64,22 +71,79 @@ describe('GX-5770 | ToolsQA | Widgets | Date Picker', () => {
 		cy.log(`Previous Month: ${nextMonth}`)
 		datePickerPage.clickOnDatePicker1()
 		datePickerPage.clickNextMonthOnDatePicker1()
-		datePickerPage.verifyNextMonthOnDatePicker1(nextMonth, yyyy)
+		cy.then(() => {
+			if (nextMonth == 'January') {
+				datePickerPage.get
+					.selectedMonthPicker1()
+					.invoke('text')
+					.should('eq', `${nextMonth} ${yyyy + 1}`)
+			} else {
+				datePickerPage.get.selectedMonthPicker1().invoke('text').should('eq', `${nextMonth} ${yyyy}`)
+			}
+		})
 	})
 
 	it('5772 | TC4: Date Picker 1 - Month Selection', () => {
 		datePickerPage.clickOnDatePicker1()
-		datePickerPage.verifyMonthsOnDatePicker1(months)
+		cy.then(() => {
+			for (let index = 0; index < months.length; index++) {
+				const nameOfMonth = months[index]
+				datePickerPage.get
+					.monthSelect()
+					.select(nameOfMonth)
+					.then(() => {
+						cy.get(`select.react-datepicker__month-select option:selected`).should('have.text', nameOfMonth)
+					})
+				cy.log(`The selected month is:  ${nameOfMonth}`)
+			}
+		})
 	})
 
-	it('5772 | TC5: Date Picker 1 - Year Selection', () => {	
+	it('5772 | TC5: Date Picker 1 - Year Selection', () => {
 		datePickerPage.clickOnDatePicker1()
-		datePickerPage.verifyYearsOnDatePicker1()
+		cy.then(() =>{
+			for (let index = 1980; index < 2025; index++) {
+				index = index.toString()
+				datePickerPage.get.yearSelectPicker1().select(index).invoke('val').should('eq', index)
+			}
+		})
 	})
 
+	it('5772 | TC6: Date Picker 1 - Current date - The day selected background color is blue', () => {
+		datePickerPage.clickOnDatePicker1()
+		datePickerPage.get.daySelectedPicker1().should('have.css', 'background-color', 'rgb(33, 107, 165)')
+	})
+
+	it('5772 | TC7: Date Picker 2 - Year selection - The selected year is marked with a √', () => {
+		datePickerPage.clickOnDatePicker2()
+		datePickerPage.get.yearArrowPicker2().click()
+		datePickerPage.get.yearSelectedPicker2().invoke('text').should('eq','✓')
+	})
+
+	it('5772 | TC8: Date Picker 2 - Month Selection', () => {
+		datePickerPage.clickOnDatePicker2()
+		cy.then(() => {
+			for (let index = 0; index < months.length; index++) {
+				const nameOfMonth = months[index]
+				datePickerPage.get.monthSelecDownArrow2().click()
+				datePickerPage.get.monthSelect2options().eq(index).click()
+				datePickerPage.get.selectedMonthPicker2().invoke('text').should('eq',`${nameOfMonth} ${yyyy}`)
+				cy.log(`The selected month is:  ${nameOfMonth}`)
+			}
+		})
+	})
+
+	it('5772 | TC9: Date Picker 2 - The selected month is marked with a √', () => {
+		datePickerPage.clickOnDatePicker2()
+		cy.then(() => {
+
+				datePickerPage.get.monthSelecDownArrow2().click({ force: true })
+				datePickerPage.get.monthWithCheckMarkPicker2()
+				.children('span').invoke('text').should('eq','✓')
+			
+		})
+	})
 	//
-
-
 })
 
 //----------------------------------------------------------
--- cypress/support/Pages/GX-5770/datePickerPage.js
+++ cypress/support/Pages/GX-5770/datePickerPage.js
@@ -6,44 +6,27 @@ class DatePickerPage {
 		monthSelect: () => cy.get('.react-datepicker__month-select'),
 		previuosMonth: () => cy.get('.react-datepicker__navigation--previous'),
 		nextMonth: () => cy.get('button[class$="react-datepicker__navigation--next"]'),
-		selectedMonthDatePicker1: () => cy.get('div[class$="hasMonthDropdown"]'),
+		selectedMonthPicker1: () => cy.get('div[class$="hasMonthDropdown"]'),
 		yearSelectPicker1: () => cy.get('.react-datepicker__year-select'),
-	}
+		daySelectedPicker1: () => cy.get('div[class$="react-datepicker__day--today"]'),
 
-	verifyFormatOnDatePicker1() {
-		this.get.datePicker1()
-			.invoke('val')
-			.should('match', /\d\d\/\d\d\/\d\d\d\d/i)
-		cy.log(`FORMAT HAS BEEN VERIFIED ON DATE PICKER 1`)
-	}
+		yearArrowPicker2: () => cy.get('.react-datepicker__year-read-view--selected-year'),
+		yearSelectedPicker2: () => cy.get('.react-datepicker__year-option--selected'),
 
-	verifyCurrentDayOnDatePicker1(day) {
-		this.get.datePicker1().invoke('val').should('eq', `${day}`)
-		cy.log(`CURRENT DAY HAS BEEN VERIFIED ON DATE PICKER 1`)
-	}
+		
+		
+		monthSelecDownArrow2: () => cy.get('.react-datepicker__month-read-view--down-arrow'),
+		selectedMonthPicker2: () => cy.get('div[class$="react-datepicker__current-month--hasMonthDropdown"]'),
+		monthSelect2options: () => cy.get('.react-datepicker__month-option'),
+		monthWithCheckMarkPicker2: () => cy.get('div[class$="react-datepicker__month-option--selected_month"]')
 
-	verifyDatePicker2(monthName, dd2, yyyy, currentTime) {
-		this.get.datePicker2().invoke('val').should('eq', `${monthName} ${dd2}, ${yyyy} ${currentTime}`)
-		cy.log(`CURRENT DATE AND FORMAT HAVE BEEN VERIFIED ON DATE PICKER 2`)
 	}
 
 	clickOnDatePicker1() {
 		this.get.datePicker1().click()
 	}
-
-	verifyMonthsOnDatePicker1(months) {
-		for (let index = 0; index < months.length; index++) {
-			const nameOfMonth = months[index]
-			this.get.monthSelect().select(index)
-			this.get
-				.monthSelect()
-				.select(nameOfMonth)
-				.then(() => {
-					cy.get(`select.react-datepicker__month-select option:selected`).should('have.text', nameOfMonth)
-				})
-
-			cy.log(`The selected month is:  ${nameOfMonth}`)
-		}
+	clickOnDatePicker2() {
+		this.get.datePicker2().click()
 	}
 
 	clickPreviousMonthOnDatePicker1() {
@@ -54,33 +37,7 @@ class DatePickerPage {
 		this.get.nextMonth().click()
 	}
 
-	verifyPreviousMonthOnDatePicker1(previous, year) {
-		if (previous == 'December') {
-			this.get.selectedMonthDatePicker1()
-				.invoke('text')
-				.should('eq', `${previous} ${year - 1}`)
-		} else {
-			cy.contains(`${previous} ${year}`)
-		}
-	}
-
-	verifyNextMonthOnDatePicker1(next, year) {
-		if (next == 'January') {
-			this.get.selectedMonthDatePicker1()
-				.invoke('text')
-				.should('eq', `${next} ${year + 1}`)
-		} else {
-			this.get.selectedMonthDatePicker1().invoke('text').should('eq', `${next} ${year}`)
-		}
-	}
-
-	verifyYearsOnDatePicker1() {
-
-		for (let index = 1980; index < 2025; index++) {
-			index = index.toString()
-			this.get.yearSelectPicker1().select(index).invoke('val').should('eq', index)
-		}
-	}
+	
 }
 
 export const datePickerPage = new DatePickerPage()
