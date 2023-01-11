6160815e94b43c70a50adf3e8cfdcbd71009db8b
Lucas Casco <lucassebaacasco@gmail.com>
2023-01-11 17:46:10 -0300

    GX-6340 modificacion de test

--- cypress/e2e/Tests/Elements/GX-6340-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-6340-Buttons.cy.js
@@ -1,42 +1,33 @@
-
-  describe('ToolsQA | Elements | Buttons', () => {
-    it('click in buttons', () => {
-      //cy.visit('https://example.cypress.io')
+describe('ToolsQA | Elements | Buttons', () => {
+    beforeEach(()=>{
       cy.visit('https://demoqa.com/buttons')
-      
+    })
+
+    it('6341 | TC1:   validar doble click en boton “Double Click Me“',() => {
+    
       //doble click
       cy.get('#doubleClickBtn').dblclick()
       const dblclickmsn = "You have done a double click"
       cy.get('#doubleClickMessage').should('have.text',dblclickmsn)
+    })
 
+    it('6341  | TC2:  validar click derecho en boton “Right Click Me“',() => {
       //click derecho
       cy.get('#rightClickBtn').rightclick()
       const rightclickmsn = "You have done a right click"
       cy.get('#rightClickMessage').should('have.text',rightclickmsn)
-      
+    })
     
+    it('6341  | TC3:  validar click en boton “Click Me“',() => {
       //cy.get('button[type="button"]').eq(3).click()
       cy.get('button').last().click()
       const clickmsn = "You have done a dynamic click"
       cy.get('#dynamicClickMessage').should('have.text',clickmsn)
-
-
     })
-  })
+})
     
-  
-  
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  Cypress.on('uncaught:exception', (err, runnable) => {
+Cypress.on('uncaught:exception', (err, runnable) => {
         // returning false here prevents Cypress from
         // failing the test
         return false
