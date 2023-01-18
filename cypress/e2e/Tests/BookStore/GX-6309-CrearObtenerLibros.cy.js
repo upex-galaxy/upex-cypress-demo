9e79d5c8191b4ae5917a955f0da562bf9bd5befb
Paola Rod <pao.alejandra1992@gmail.com>
2023-01-18 10:10:30 -0300

    GX-6309: se anexa fixture y validaciones en TC

--- cypress/e2e/Tests/BookStore/GX-6309-CrearObtenerLibros.cy.js
+++ cypress/e2e/Tests/BookStore/GX-6309-CrearObtenerLibros.cy.js
@@ -1,118 +1,132 @@
-import {seeBook} from '@pages/SeeBook.Page.js'
-
 describe('✅BookStore | Grid | Crear y Obtener Libros de la Tienda (POST-GET)', () => {
 	let isbn, userID, token
 
 	before('precondicion: usuario debe estar logueado', () => {
-		cy.api({
-			method: 'POST',
-			url: 'https://demoqa.com/Account/v1/User',
-			body: {
-				userName: 'Petthrti',
-				password: 'Pepe123*'
-			},
-		}).then((response) => {
-			expect(response.status).to.eq(201)
-			userID = response.body.userID
+		cy.fixture('data/createObtenerLibros').then((the) => {
 			cy.api({
 				method: 'POST',
-				url: 'https://demoqa.com/Account/v1/GenerateToken',
+				url: the.url.createUser,
+				failOnStatusCode: false,
 				body: {
-					userName: 'Petthrti',
-					password: 'Pepe123*'
+					userName: the.username,
+					password: the.password,
+				},
+			}).then((response) => {
+				expect(response.status).to.eq(201)
+				expect(response.body.username).to.be.equal(the.username)
+				userID = response.body.userID
+			})
+
+            cy.visit('https://demoqa.com/login')
+            cy.get('#userName').type(the.username)
+            cy.get('#password').type(the.username)
+
+			cy.api({
+				method: 'POST',
+				url: the.url.generateToken,
+				failOnStatusCode: false,
+				body: {
+					userName: the.username,
+					password: the.password,
 				},
 			}).then((response) => {
 				expect(response.status).to.eq(200)
+				expect(response.body.status).to.be.equal('Success')
+				expect(response.body.result).to.be.equal('User authorized successfully.')
 				token = response.body.token
-				cy.log(`token: ${token}`)
-                cy.log(`UserID: ${userID}`)
 				cy.api({
 					method: 'POST',
-					url: 'https://demoqa.com/Account/v1/Authorized',
+					url: the.url.authorized,
 					body: {
-						userName: 'Petthrti',
-						password: 'Pepe123*'
+						userName: the.username,
+						password: the.password,
 					},
 				}).then((response) => {
 					expect(response.status).to.eq(200)
-                    expect(response.body).to.be.an('boolean').and.eq(true)
+					expect(response.body).to.be.an('boolean').and.eq(true)
 				})
 			})
 		})
 	})
 
 	it('6310 | TC1: Validar visualizar un libro de la tienda', () => {
-		cy.api({
-			method: 'GET',
-			url: 'https://demoqa.com/BookStore/v1/Books',
-		}).then((response) => {
-			expect(response.status).to.eq(200)
-			isbn = response.body.books[1].isbn
+		cy.fixture('data/createObtenerLibros').then((the) => {
 			cy.api({
 				method: 'GET',
-				url: 'https://demoqa.com/BookStore/v1/Book',
-				qs: {
-					ISBN: isbn,
-				},
+				url: the.url.getBooks,
 			}).then((response) => {
 				expect(response.status).to.eq(200)
+				isbn = response.body.books[1].isbn
+				cy.api({
+					method: 'GET',
+					url: the.url.getBook,
+					qs: {
+						ISBN: isbn,
+					},
+				}).then((response) => {
+					expect(response.status).to.eq(200)
+					expect(response.body.isbn).to.be.equal(isbn)
+				})
 			})
 		})
 	})
 
 	it('6310 | TC2: Validar guardar libro en su coleccion', () => {
-		cy.api({
-			method: 'POST',
-			url: 'https://demoqa.com/BookStore/v1/Books',
-            auth: {
-                bearer: token
-			},
-			body: {
-				userId: userID,
-				collectionOfIsbns: [
-					{
-						isbn: isbn,
-					},
-				],
-			},
-		}).then((response) => {
-			expect(response.status).to.eq(201)
+		cy.fixture('data/createObtenerLibros').then((the) => {
+			cy.api({
+				method: 'POST',
+				url: the.url.saveBook,
+				auth: {
+					bearer: token,
+				},
+				body: {
+					userId: userID,
+					collectionOfIsbns: [
+						{
+							isbn: isbn,
+						},
+					],
+				},
+			}).then((response) => {
+				expect(response.status).to.eq(201)
+				expect(response.body.books[0].isbn).to.be.equal(isbn)
+			})
 		})
 	})
 
 	it('6310 | TC3: Validar no poder guardar un libro no existente en la coleccion', () => {
-		cy.api({
-			method: 'POST',
-			url: 'https://demoqa.com/BookStore/v1/Books',
-			failOnStatusCode: false,
-			headers: {
-				authorization: 'Basic YXNkOlBlcGUxMjMq',
-			},
-			body: {
-				userId: userID,
-				collectionOfIsbns: [
-					{
-						isbn: isbn,
-					},
-				],
-			},
-		}).then((response) => {
-			expect(response.status).to.eq(401)
+		cy.fixture('data/createObtenerLibros').then((the) => {
+			cy.api({
+				method: 'POST',
+				url: the.url.saveBook,
+				failOnStatusCode: false,
+				body: {
+					userId: userID,
+					collectionOfIsbns: [
+						{
+							isbn: the.noExistBook,
+						},
+					],
+				},
+			}).then((response) => {
+				expect(response.status).to.eq(401)
+				expect(response.body.message).to.be.equal('User not authorized!')
+			})
 		})
 	})
 
-    after('borra usuario', () => {
-        cy.api({
+	after('borra usuario', () => {
+		cy.api({
 			method: 'DELETE',
 			url: `https://demoqa.com/Account/v1/User/${userID}`,
-            auth: {
-                bearer: token
+			auth: {
+				bearer: token,
 			},
-            body: {
-				userId: userID
-			}
+			body: {
+				userId: userID,
+			},
 		}).then((response) => {
 			expect(response.status).to.eq(204)
 		})
-    })
+	})
 })
