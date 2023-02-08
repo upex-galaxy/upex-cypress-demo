01e996b421609ad82d96a6ee9ce717b8f24d66d8
QAZebass <ze.basszender@gmail.com>
2023-02-08 01:01:09 -0300

    GX2-624 Test case 1

--- cypress/e2e/Tests/ToolsQA-Forms/Interactions/GX2-624 ToolsQA-Draggable.cy.js
+++ cypress/e2e/Tests/ToolsQA-Forms/Interactions/GX2-624 ToolsQA-Draggable.cy.js
@@ -1,12 +1,36 @@
 describe('US GX2-624 | TS: ✅ToolsQA | Interactions | Dragabble',()=>{
 
-    it('GX2-625 | TC1: Validate that the user can dragg the box “drag me” anywhere',()=>{
-        
+    beforeEach('Precondition',()=>{
         cy.visit('https://demoqa.com/dragabble')
-        cy.get('[id="draggableExample-tab-simple"]').should('exist')
-        cy.get('[id="dragBox"]').should('exist')
-
+    })
+    
+    it('GX2-625 | TC1: Validate that the user can dragg the box “drag me” anywhere',()=>{
+        let Xcoord= Cypress._.random(1, 500)
+        let Ycoord= Cypress._.random(1, 340)
         
+        cy.get('[id="draggableExample-tab-simple"]').should('exist')
+        cy.get('[id="dragBox"]').then(($button)=>{
+            let rect= $button[0].getBoundingClientRect();
+
+            cy.wrap($button)
+            .trigger('mousedown',{
+                which: 1,
+                pageX: rect.left,
+                pageY: rect.top,
+                force: true
+            })
+            .trigger('mousemove',{
+                pageX: rect.left + Xcoord,
+                pageY: rect.top + Ycoord,
+                force: true
+            })
+            .trigger('mouseup',{
+                which: 1,
+                force: true
+            })
+        cy.wrap($button).should('have.css', "left", `${Xcoord}px`) 
+        cy.wrap($button).should('have.css', "top", `${Ycoord}px`)
+        })
     })
 })
 Cypress.on('uncaught:exception', (err, runnable) => {
