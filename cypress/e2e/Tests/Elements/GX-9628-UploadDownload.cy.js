36e58bf369130b44e67bf09d87d049ced9882d22
LilibethCamico <argcamico@gmail.com>
2023-02-24 19:47:38 -0300

    GX-9628: POM para ambos TCS-Add Data in fixture-relativePath Add image in fixture. All Test Passed

--- cypress/e2e/Tests/Elements/GX-9628-UploadDownload.cy.js
+++ cypress/e2e/Tests/Elements/GX-9628-UploadDownload.cy.js
@@ -1,21 +1,31 @@
 //importar el Page
-import { elements } from '@pages/uploadDownload.Page.js';
+import { elements } from '@pages/uploadDownload.Page.js'
 
-describe("ToolsQA | Elements | Upload and Download", ()=>{
-    beforeEach("Precondition: Be located in Upload and Download", ()=>{
-        cy.visit('https://demoqa.com/upload-download');
-        cy.url().should("contain", "download");
-    })
-    it("9629 | TC1: Validar download in tho download PC folder.", ()=>{
-        elements.clickDownload();
-        cy.should('have.attr','href');
-        cy.readFile('cypress/downloads/sampleFile.jpeg').should('exist');
+describe('ToolsQA | Elements | Upload and Download', () => {
+	beforeEach('Precondition: Be located in Upload and Download', () => {
+		cy.visit('https://demoqa.com/upload-download')
+		cy.url().should('contain', 'download')
+	})
+	it('9629 | TC1: Validar download in tho download PC folder.', () => {
+        //Modificado con POM y Feature(Data para los relative Path):
+		cy.fixture('data/downloadUpload').then((the) => {
+			elements.clickDownload()
+			cy.should('have.attr', 'href')
+			//cy.readFile('cypress/downloads/sampleFile.jpeg').should('exist');
+			cy.readFile(the.relativePathDownloadFile).should('exist')
+		})
+	})
+	it('9629 | TC2: Validar uploaded from PC File Explorer.', () => {
+		//cy.get('#uploadFile').selectFile('cypress/fixtures/images/upexlogo.png');
+		//cy.get('#uploadedFilePath').should("contain", "upexlogo");
 
-    })
-    it("9629 | TC2: Validar uploaded from PC File Explorer.", ()=>{
-        cy.get('#uploadFile').selectFile('cypress/fixtures/images/upexlogo.png');
-        cy.get('#uploadedFilePath').should("contain", "upexlogo");
-    })
+		//Modificado con POM y Feature(Data para los relative Path):
+		cy.fixture('data/downloadUpload').then((the) => {
+			elements.clickUploaded()
+			elements.selectUploadedFile(the.relativePathUploadFile)
+			elements.get.generatedFilePath().should('contain', 'Business-Analyst')
+		})
+	})
 })
 Cypress.on('uncaught:exception', (err, runnable) => {
 	return false
@@ -23,7 +33,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 // Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
 const origLog = Cypress.log
 Cypress.log = function (opts, ...other) {
-	if (opts.displayName === 'xhr'|| opts.displayName === 'fetch' && opts.url.startsWith('https://')) {
+	if (opts.displayName === 'xhr' || (opts.displayName === 'fetch' && opts.url.startsWith('https://'))) {
 		return
 	}
 	return origLog(opts, ...other)
--- cypress/support/pages/uploadDownload.Page.js
+++ cypress/support/pages/uploadDownload.Page.js
@@ -1,13 +1,20 @@
 class Elements {
     get = {
         buttonDownload: ()=> cy.get('#downloadButton'),
-        inputUploadedFile: ()=> cy.get('#uploadFile')
+        buttonUploadedFile: ()=> cy.get('#uploadFile'),
+        generatedFilePath: ()=> cy.get('#uploadedFilePath')
     }
     clickDownload(){
         this.get.buttonDownload().click()
     }
-    clickUploadedFile(){
-        this.get.inputUploadedFile().click()
+
+    clickUploaded(){
+        this.get.buttonUploadedFile().click()
+    }
+
+
+    selectUploadedFile(pathR){
+        this.get.buttonUploadedFile().selectFile(pathR);
     }
 }
 export const elements = new Elements();
