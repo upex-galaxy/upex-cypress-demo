948682e57851c3123e75ae265107dc9c03fb891b
EDDMAY <eddmay2015@gmail.com>
2023-03-11 17:17:00 -0300

    GX-10497 primer commit

---
+++ cypress/e2e/Tests/Elements/GX-10497-Buttons.cy.js
@@ -0,0 +1,5 @@
+describe('✅ToolsQA | Elements | Buttons', () => {
+	it('10498 | TC1: Validar que al hacer doble click en el botón “Double click me“ me parezca un mensaje de verificación.', () => {
+		cy.visit('https://demoqa.com/buttons');
+	});
+});
