5d858574ddfa8c09d6d7a5b47d59f66b095d3024
EDDMAY <eddmay2015@gmail.com>
2023-03-11 18:38:27 -0300

    GX-10497 -finalice test

--- cypress/e2e/Tests/Elements/GX-10497-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-10497-Buttons.cy.js
@@ -1,5 +1,20 @@
 describe('✅ToolsQA | Elements | Buttons', () => {
-	it('10498 | TC1: Validar que al hacer doble click en el botón “Double click me“ me parezca un mensaje de verificación.', () => {
+	beforeEach(() => {
 		cy.visit('https://demoqa.com/buttons');
 	});
+	it('10498 | TC1: Validar que al hacer doble click en el botón “Double click me“ me parezca un mensaje de verificación.', () => {
+		cy.get('#doubleClickBtn').dblclick();
+		cy.get('#doubleClickMessage').should('have.text', 'You have done a double click');
+	});
+	it('10498 | TC2: Validar hacer click derecho en el botón “Right  click me“ me aparezca un mensaje de verificación', () => {
+		cy.get('#rightClickBtn').rightclick();
+		cy.get('#rightClickMessage').should('have.text', 'You have done a right click');
+	});
+	it('10498 | TC3: Validar hacer click en el botón “Click me” me aparezca un mensaje de verificación.', () => {
+		cy.get('[type="button"]').eq(3).click();
+		cy.get('#dynamicClickMessage').should('have.text', 'You have done a dynamic click');
+	});
 });
+
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
