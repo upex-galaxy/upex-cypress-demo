c5d67b7a41b1b2de070da60950a69a3ee3b6f4f9
LilibethCamico <argcamico@gmail.com>
2023-03-23 18:59:09 -0300

    GX2-990: Some modifications in the tests, pass without having implemented POM

--- cypress/support/pages/checkBox.Page.js
+++ cypress/support/pages/checkBox.Page.js
@@ -1,6 +1,11 @@
 class Checkbox {
+	get = {
+		buttonCheck: () => cy.get('input[type="checkbox"]'),
+	};
 
-
-
+	//Actions methods
+	clickCheck() {
+		this.get.buttonCheck().click({ force: true });
+	}
 }
 export const checkBox = new Checkbox();
