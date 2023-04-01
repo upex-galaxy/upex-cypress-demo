947783c7612ebc7671434db4b237a6269cd2fc24
leonardo <leonardo.millan011@gmail.com>
2023-04-01 15:55:24 -0400

    GX2-1662 Avances

--- cypress/e2e/Tests/Elements/GX2-1662-webTable.cy.js
+++ cypress/e2e/Tests/Elements/GX2-1662-webTable.cy.js
@@ -14,6 +14,7 @@ describe('GX2-1662', () => {
 		WebTables.typeUserEmail('Valid@email.com');
 		WebTables.typeDepartment('Legal');
 		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('not.exist');
 		WebTables.get.dataTable().then(dataTable => {
 			expect(dataTable).to.contain('ValidfirstName', 'ValidLastName', '27', '1000', 'Valid@email.com');
 		});
@@ -28,6 +29,7 @@ describe('GX2-1662', () => {
 		WebTables.typeUserEmail('Valid@email.com');
 		WebTables.typeDepartment('Legal');
 		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
 		WebTables.get.firstName().then(FistnameForm => {
 			expect(FistnameForm).to.have.css('border-color', 'rgb(220, 53, 69)');
 		});
@@ -42,11 +44,12 @@ describe('GX2-1662', () => {
 		WebTables.typeUserEmail('Valid@email.com');
 		WebTables.typeDepartment('Legal');
 		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
 		WebTables.get.firstName().then(FistnameForm => {
 			expect(FistnameForm).to.have.css('border-color', 'rgb(220, 53, 69)');
 		});
 	});
-	it.only('TC4: Validate No registration with invalid first name (empty)', () => {
+	it('TC4: Validate No registration with invalid first name (empty)', () => {
 		WebTables.addNewPeople();
 		WebTables.registrationSubmit();
 		WebTables.typeLastname('ValidLastName');
@@ -55,11 +58,228 @@ describe('GX2-1662', () => {
 		WebTables.typeUserEmail('Valid@email.com');
 		WebTables.typeDepartment('Legal');
 		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
 		WebTables.get.firstName().then(FistnameForm => {
 			expect(FistnameForm).to.have.css('border-color', 'rgb(220, 53, 69)');
 		});
 	});
+	it.skip('TC5: Validate No registration with invalid last name (numbers)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('12314512');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('1000');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.lastName().then(lastnameForm => {
+			expect(lastnameForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it.skip('TC6: Validate No registration with invalid last name (special characters)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('#$%@#!@');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('1000');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.lastName().then(lastnameForm => {
+			expect(lastnameForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC7: Validate No registration with invalid last name (empty)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('1000');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.lastName().then(lastnameForm => {
+			expect(lastnameForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC8: Validate No registration with invalid email', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('1000');
+		WebTables.typeUserEmail('InvalidEmail');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.userEmail().then(emailForm => {
+			expect(emailForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC9: Validate No registration with email (empty)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('1000');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.userEmail().then(emailForm => {
+			expect(emailForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC10: Validate No registration with invalid age (text)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('Invalid');
+		WebTables.typeSalary('1000');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.userAge().then(userAgeForm => {
+			expect(userAgeForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC11: Validate No registration with invalid age (special characters)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('$$$');
+		WebTables.typeSalary('1000');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.userAge().then(userAgeForm => {
+			expect(userAgeForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC12: Validate No registration with invalid age (empty)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeSalary('1000');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.userAge().then(userAgeForm => {
+			expect(userAgeForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC13: Validate No registration with invalid salary (text)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('Invalid');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.salary().then(salaryForm => {
+			expect(salaryForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC14: Validate No registration with invalid salary (special characters)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('#$%#');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.salary().then(salaryForm => {
+			expect(salaryForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC15: Validate No registration with invalid salary (empty)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('27');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('Legal');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.salary().then(salaryForm => {
+			expect(salaryForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it.skip('TC16: Validate No registration with invalid deparment (numbers)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('Invalid');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('31124');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.department().then(deparmentForm => {
+			expect(deparmentForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it.skip('TC17: Validate No registration with invalid deparment (special characters)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('Invalid');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.typeDepartment('#@%#!');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.department().then(deparmentForm => {
+			expect(deparmentForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it('TC18: Validate No registration with invalid deparment (empty)', () => {
+		WebTables.addNewPeople();
+		WebTables.registrationSubmit();
+		WebTables.typeFirstname('ValidFirstName');
+		WebTables.typeLastname('ValidLastName');
+		WebTables.typeUserAge('27');
+		WebTables.typeSalary('Invalid');
+		WebTables.typeUserEmail('Valid@email.com');
+		WebTables.registrationSubmit();
+		WebTables.get.registerForm().should('be.visible');
+		WebTables.get.department().then(deparmentForm => {
+			expect(deparmentForm).to.have.css('border-color', 'rgb(220, 53, 69)');
+		});
+	});
+	it.only('TC19: Validate list of coincidences when use the search bar', () => {
+		WebTables.addMultiplesUsers();
+		WebTables.get.firstRowTable().then(FirstRow => {
+			expect(FirstRow).not.contain('Maria');
+		});
+		WebTables.typeSearchBox('Maria');
+		WebTables.get.firstRowTable().then(FirstRow => {
+			expect(FirstRow).to.contain('Maria');
+		});
+	});
 });
+
 import { WebTables } from '@pages/webtables.Page';
 import { removeLogs } from '@helper/RemoveLogs';
 removeLogs();
--- cypress/support/pages/webtables.Page.js
+++ cypress/support/pages/webtables.Page.js
@@ -9,6 +9,10 @@ class webTables {
 		department: () => cy.get('#department'),
 		registrationSubmit: () => cy.get('#submit'),
 		dataTable: () => cy.get('[class*=rt-tbody]'),
+		registerForm: () => cy.get('[class*=modal-content]'),
+		searchBox: () => cy.get('#searchBox'),
+		rowsTable: () => cy.get('[class*=rt-tr-group]'),
+		firstRowTable: () => cy.get('[class*=rt-tr-group]').eq(0),
 	};
 	addNewPeople() {
 		this.get.addButton().click();
@@ -33,6 +37,67 @@ class webTables {
 	}
 	typeDepartment(text) {
 		this.get.department().type(text);
+	}
+	addMultiplesUsers() {
+		this.get.addButton().click();
+		this.get.firstName().type('Pedro');
+		this.get.lastName().type('Martinez');
+		this.get.userAge().type('22');
+		this.get.userEmail().type('pedromartinez152@gmail.com');
+		this.get.salary().type('1400');
+		this.get.department().type('Insurance');
+		this.get.registrationSubmit().click();
+		//d
+		this.get.addButton().click();
+		this.get.firstName().type('Maria');
+		this.get.lastName().type('Guzman');
+		this.get.userAge().type('28');
+		this.get.userEmail().type('Mariaguzman123@gmail.com');
+		this.get.salary().type('2000');
+		this.get.department().type('legal');
+		this.get.registrationSubmit().click();
+		//d
+		this.get.addButton().click();
+		this.get.firstName().type('Jose');
+		this.get.lastName().type('Perez');
+		this.get.userAge().type('21');
+		this.get.userEmail().type('JosePe@gmail.com');
+		this.get.salary().type('2211');
+		this.get.department().type('legal');
+		this.get.registrationSubmit().click();
+		//d
+		this.get.addButton().click();
+		this.get.firstName().type('Juan');
+		this.get.lastName().type('Hernandez');
+		this.get.userAge().type('23');
+		this.get.userEmail().type('juanhernandez16@gmail.com');
+		this.get.salary().type('2500');
+		this.get.department().type('Insurance');
+		this.get.registrationSubmit().click();
+		//d
+		this.get.addButton().click();
+		this.get.firstName().type('Joel');
+		this.get.lastName().type('Perdomo');
+		this.get.userAge().type('19');
+		this.get.userEmail().type('joelPerdomo@gmail.com');
+		this.get.salary().type('1400');
+		this.get.department().type('legal');
+		this.get.registrationSubmit().click();
+		//d
+		this.get.addButton().click();
+		this.get.firstName().type('Flor');
+		this.get.lastName().type('Bolivar');
+		this.get.userAge().type('30');
+		this.get.userEmail().type('florbolivar1@gmail.com');
+		this.get.salary().type('2500');
+		this.get.department().type('Insurance');
+		this.get.registrationSubmit().click();
+	}
+	typeSearchBox(text) {
+		this.get.searchBox().type(text);
+	}
+	firstRowOnTable() {
+		this.get.rowsTable().eq(0);
 	}
 }
 export const WebTables = new webTables();
