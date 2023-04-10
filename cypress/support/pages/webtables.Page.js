dfeea58824196d8904b4af63fb6e2c922e0bc0b2
leonardo <leonardo.millan011@gmail.com>
2023-04-10 09:02:37 -0400

    GX2-1662 update

--- cypress/support/pages/webtables.Page.js
+++ cypress/support/pages/webtables.Page.js
@@ -1,5 +1,10 @@
+import { faker } from '@faker-js/faker';
+let Num = [];
+
 class webTables {
 	get = {
+		rowsInTable: () => cy.get('[role="rowgroup"] [role="row"]'),
+		filledRow: () => cy.get('[role="rowgroup"] [role="row"]:not([class*=padRow])'),
 		addButton: () => cy.get('#addNewRecordButton'),
 		firstName: () => cy.get('#firstName'),
 		lastName: () => cy.get('#lastName'),
@@ -9,95 +14,136 @@ class webTables {
 		department: () => cy.get('#department'),
 		registrationSubmit: () => cy.get('#submit'),
 		dataTable: () => cy.get('[class*=rt-tbody]'),
-		registerForm: () => cy.get('[class*=modal-content]'),
+		registerForm: () => cy.get('#registration-form-modal'),
 		searchBox: () => cy.get('#searchBox'),
 		rowsTable: () => cy.get('[class*=rt-tr-group]'),
-		firstRowTable: () => cy.get('[class*=rt-tr-group]').eq(0),
+		cells: () => cy.get('[role="gridcell"]'),
+		pageSizeOptions: () => cy.get('[class*=pageSizeOptions]'),
+		buttonNext: () => cy.contains('.-btn', 'Next'),
+		buttonPrevious: () => cy.contains('.-btn', 'Previous'),
+		selected: '',
 	};
-	addNewPeople() {
-		this.get.addButton().click();
+	getRandomValueInColumn(nameColumn) {
+		let columnValues = this.getColumnValuesByName(nameColumn);
+		let value = [];
+		cy.get(columnValues)
+			.its('length')
+			.then(row => {
+				let num = Math.floor(Math.random() * (row - 1) + 1);
+				Num.push(num);
+				value.push(columnValues[Num]);
+			});
+
+		return value;
 	}
-	registrationSubmit() {
+
+	getColumnValuesByName(nameColumn) {
+		let columnValues = [];
+		cy.contains('.rt-th', nameColumn)
+			.invoke('index')
+			.then(rowIndex => {
+				this.get.filledRow().each(row => {
+					cy.wrap(row).within(() => {
+						this.get
+							.cells()
+							.eq(rowIndex)
+							.then(cell => {
+								columnValues.push(cell.text().toLowerCase());
+							});
+					});
+				});
+			});
+
+		return columnValues;
+	}
+
+	addUser({ firstName, lastName, userEmail, Age, salary, department }) {
+		this.get.addButton().click();
+		firstName && this.get.firstName().type(firstName);
+		lastName && this.get.lastName().type(lastName);
+		Age && this.get.userAge().type(Age);
+		userEmail && this.get.userEmail().type(userEmail);
+		salary && this.get.salary().type(salary);
+		department && this.get.department().type(department);
 		this.get.registrationSubmit().click();
 	}
-	typeFirstname(text) {
-		this.get.firstName().type(text);
-	}
-	typeLastname(text) {
-		this.get.lastName().type(text);
-	}
-	typeUserEmail(text) {
-		this.get.userEmail().type(text);
-	}
-	typeUserAge(text) {
-		this.get.userAge().type(text);
-	}
-	typeSalary(text) {
-		this.get.salary().type(text);
-	}
-	typeDepartment(text) {
-		this.get.department().type(text);
-	}
-	addMultiplesUsers() {
-		this.get.addButton().click();
-		this.get.firstName().type('Pedro');
-		this.get.lastName().type('Martinez');
-		this.get.userAge().type('22');
-		this.get.userEmail().type('pedromartinez152@gmail.com');
-		this.get.salary().type('1400');
-		this.get.department().type('Insurance');
-		this.get.registrationSubmit().click();
-		//d
-		this.get.addButton().click();
-		this.get.firstName().type('Maria');
-		this.get.lastName().type('Guzman');
-		this.get.userAge().type('28');
-		this.get.userEmail().type('Mariaguzman123@gmail.com');
-		this.get.salary().type('2000');
-		this.get.department().type('legal');
-		this.get.registrationSubmit().click();
-		//d
-		this.get.addButton().click();
-		this.get.firstName().type('Jose');
-		this.get.lastName().type('Perez');
-		this.get.userAge().type('21');
-		this.get.userEmail().type('JosePe@gmail.com');
-		this.get.salary().type('2211');
-		this.get.department().type('legal');
-		this.get.registrationSubmit().click();
-		//d
-		this.get.addButton().click();
-		this.get.firstName().type('Juan');
-		this.get.lastName().type('Hernandez');
-		this.get.userAge().type('23');
-		this.get.userEmail().type('juanhernandez16@gmail.com');
-		this.get.salary().type('2500');
-		this.get.department().type('Insurance');
-		this.get.registrationSubmit().click();
-		//d
-		this.get.addButton().click();
-		this.get.firstName().type('Joel');
-		this.get.lastName().type('Perdomo');
-		this.get.userAge().type('19');
-		this.get.userEmail().type('joelPerdomo@gmail.com');
-		this.get.salary().type('1400');
-		this.get.department().type('legal');
-		this.get.registrationSubmit().click();
-		//d
-		this.get.addButton().click();
-		this.get.firstName().type('Flor');
-		this.get.lastName().type('Bolivar');
-		this.get.userAge().type('30');
-		this.get.userEmail().type('florbolivar1@gmail.com');
-		this.get.salary().type('2500');
-		this.get.department().type('Insurance');
-		this.get.registrationSubmit().click();
+	addMultiplesUsers({ amountOfusers }) {
+		for (let index = 0; index <= amountOfusers; index++) {
+			const randomFirstName = faker.name.firstName();
+			const randomLastName = faker.name.lastName();
+			const randomUserEmail = faker.internet.email();
+			const randomUserAge = faker.datatype.number({ max: 100 });
+			const randomUserSalary = faker.datatype.number({ min: 500 });
+			const randomDepartment = faker.commerce.department();
+
+			this.addUser({
+				firstName: randomFirstName,
+				lastName: randomLastName,
+				userEmail: randomUserEmail,
+				Age: randomUserAge,
+				salary: randomUserSalary,
+				department: randomDepartment,
+			});
+		}
 	}
 	typeSearchBox(text) {
 		this.get.searchBox().type(text);
 	}
-	firstRowOnTable() {
-		this.get.rowsTable().eq(0);
+	clickOnHeaderCell(type) {
+		cy.contains('.rt-resizable-header-content', type).click({ force: true });
+	}
+
+	clickOnEditButtonByRow({ row }) {
+		const rowIndex = row - 1;
+		cy.get('[class*=rt-table]').within(() => {
+			cy.get('div')
+				.parent('[role="gridcell"]')
+				.eq(rowIndex)
+				.each(() => {
+					cy.get('[title="Edit"]').eq(rowIndex).click();
+				});
+		});
+	}
+	clickOnDeleteButtonByRow({ row }) {
+		const rowIndex = row - 1;
+		cy.get('[class*=rt-table]').within(() => {
+			cy.get('div')
+				.parent('[role="gridcell"]')
+				.eq(rowIndex)
+				.each(() => {
+					cy.get('[title="Delete"]').eq(rowIndex).click();
+				});
+		});
+	}
+	getUserDataByRow({ row }) {
+		const rowIndex = row - 1;
+		const userData = [];
+		this.get
+			.filledRow()
+			.eq(rowIndex)
+			.each(cell => {
+				cy.wrap(cell).within(() => {
+					this.get.cells().each(cellText => {
+						userData.push(cellText.text());
+					});
+				});
+			});
+		return userData;
+	}
+	getDataTable() {
+		const userData = [];
+		this.get.filledRow().each(row => {
+			cy.wrap(row).within(() => {
+				this.get.cells().each(cell => {
+					userData.push(cell.text());
+				});
+			});
+		});
+		return userData;
+	}
+	selectRowForPage({ amountOfRows }) {
+		this.get.pageSizeOptions().children().select(amountOfRows);
 	}
 }
+
 export const WebTables = new webTables();
