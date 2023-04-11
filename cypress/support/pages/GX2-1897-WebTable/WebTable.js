fbcc54c0b7383886f642a29988f200c56885f14f
QAZebass <ze.basszender@gmail.com>
2023-04-11 15:45:48 -0300

    GX2-1897: TC2 added, one command was created for user sign in and the Page was updated.

--- cypress/support/pages/GX2-1897-WebTable/WebTable.js
+++ cypress/support/pages/GX2-1897-WebTable/WebTable.js
@@ -2,9 +2,37 @@ class WebTable {
 	elements = {
 		addbutton: () => cy.get('[id="addNewRecordButton"]'),
 		modal: () => cy.get('[class="modal-content"]'),
+		nameInput: () => cy.get('[id="firstName"]'),
+		lasnameInput: () => cy.get('[id="lastName"]'),
+		mailInput: () => cy.get('[id="userEmail"]'),
+		ageInput: () => cy.get('[id="age"]'),
+		salaryInput: () => cy.get('[id="salary"]'),
+		departmentInput: () => cy.get('[id="department"]'),
+		submitbutton: () => cy.get('[id="submit"]'),
 	};
 	clickAdd() {
 		this.elements.addbutton().click();
+	}
+	typeName(name) {
+		this.elements.nameInput().type(name);
+	}
+	typeLastname(lastname) {
+		this.elements.lasnameInput().type(lastname);
+	}
+	typeMail(email) {
+		this.elements.mailInput().type(email);
+	}
+	typeAge(age) {
+		this.elements.ageInput().type(age);
+	}
+	typeSalary(salary) {
+		this.elements.salaryInput().type(salary);
+	}
+	typeDepartment(department) {
+		this.elements.departmentInput().type(department);
+	}
+	clickSubmit() {
+		this.elements.submitbutton().click();
 	}
 }
 export const webtable = new WebTable();
