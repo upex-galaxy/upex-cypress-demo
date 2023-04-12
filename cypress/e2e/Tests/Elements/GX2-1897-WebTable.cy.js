0a2e222c0a0eb929d2edf70a854bec313eed6ef5
QAZebass <ze.basszender@gmail.com>
2023-04-12 17:44:06 -0300

    Tests up to number 6 were added along with modification to its corresponding Page

--- cypress/e2e/Tests/Elements/GX2-1897-WebTable.cy.js
+++ cypress/e2e/Tests/Elements/GX2-1897-WebTable.cy.js
@@ -1,6 +1,6 @@
 import { webtable } from '@pages/GX2-1897-WebTable/WebTable';
 import { faker } from '@faker-js/faker';
-
+const users = [];
 describe('GX2-1897-✅-tools-qa-elements-web-table', () => {
 	beforeEach('Precondition', () => {
 		cy.visit('https://demoqa.com/webtables');
@@ -31,6 +31,117 @@ describe('GX2-1897-✅-tools-qa-elements-web-table', () => {
 		webtable.clickAdd();
 		cy.UserSignIn(info.name, info.surname, info.mail, info.age, info.salary, info.department);
 		webtable.clickSubmit();
+	});
+
+	it('GX2-1898 | TC3: Validate that a user can be deleted with the “trash” icon', () => {
+		cy.get('div.rt-tr.-odd')
+			.eq(1)
+			.children()
+			.each($el => {
+				cy.wrap($el)
+					.invoke('text')
+					.then(info => {
+						cy.wrap(info).should('exist');
+					});
+			});
+		webtable.clickonDelete3();
+		cy.get('div.rt-tr.-odd')
+			.eq(1)
+			.children()
+			.each($el => {
+				cy.wrap($el)
+					.invoke('text')
+					.then(info => {
+						cy.wrap(info).should('not.contain', 'text');
+					});
+			});
+	});
+	it('GX2-1898 | TC4: Validate that the user can search for a specific name in the table using the searchbox', () => {
+		for (let i = 0; i <= 4; i++) {
+			const rn = Cypress._.random(10, 100);
+			const info = {
+				name: faker.name.firstName(),
+				surname: faker.name.lastName(),
+				mail: faker.internet.email(),
+				age: Cypress._.random(18, 65),
+				salary: rn * 1000,
+				department: faker.company.bs(),
+			};
+			users.push(info.name);
+			webtable.clickAdd();
+			cy.UserSignIn(info.name, info.surname, info.mail, info.age, info.salary, info.department);
+			webtable.clickSubmit();
+		}
+		const rn = Cypress._.random(0, 3);
+		webtable.elements.searchbox().should('exist');
+		const selectedName = users[rn];
+		cy.log(selectedName);
+		webtable.typeFirst2letters(selectedName);
+		cy.get('div.rt-tr.-odd')
+			.first()
+			.children()
+			.first()
+			.invoke('text')
+			.then(name => {
+				expect(name).to.equal(selectedName);
+			});
+	});
+	it('GX2-1898 | TC5: Validate that the user can edit information with the “pen” icon.', () => {
+		let namebefore;
+		let nameafter;
+		cy.get('div.rt-tr.-odd')
+			.first()
+			.children()
+			.first()
+			.invoke('text')
+			.then(name => {
+				namebefore = name;
+				cy.wrap(name).should('exist');
+
+				webtable.clickEdit1();
+				webtable.elements.nameInput().clear();
+				webtable.elements.nameInput().type(faker.name.firstName());
+				webtable.clickSubmit();
+				cy.get('div.rt-tr.-odd')
+					.first()
+					.children()
+					.first()
+					.invoke('text')
+					.then(name => {
+						nameafter = name;
+						expect(namebefore).not.to.equal(nameafter);
+					});
+			});
+	});
+	it('GX2-1898 | TC6: Validate that the user go to the following page by clicking the “next” button.', () => {
+		webtable.elements.nextbutton().should('not.be.enabled');
+		cy.get('[class="-pageInfo"]')
+			.invoke('text')
+			.then(pageinfo => {
+				expect(pageinfo).to.exist;
+			});
+		for (let i = 0; i <= 7; i++) {
+			const rn = Cypress._.random(10, 100);
+			const info = {
+				name: faker.name.firstName(),
+				surname: faker.name.lastName(),
+				mail: faker.internet.email(),
+				age: Cypress._.random(18, 65),
+				salary: rn * 1000,
+				department: faker.company.bs(),
+			};
+			users.push(info.name);
+			webtable.clickAdd();
+			cy.UserSignIn(info.name, info.surname, info.mail, info.age, info.salary, info.department);
+			webtable.clickSubmit();
+		}
+		cy.get('[class="-pageInfo"]')
+			.invoke('text')
+			.then(pageinfo => {
+				expect(pageinfo).to.exist;
+			});
+		webtable.elements.nextbutton().should('not.be.disabled');
+		webtable.clickNext();
 	});
 });
 
