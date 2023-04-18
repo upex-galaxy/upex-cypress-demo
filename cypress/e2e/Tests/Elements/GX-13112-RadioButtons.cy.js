6ecfc36873966787f74d37377ddc84aecc73d94c
agusnes <agusnespral@gmail.com>
2023-04-18 17:07:43 -0300

    segundo commit

---
+++ cypress/e2e/Tests/Elements/GX-13112-RadioButtons.cy.js
@@ -0,0 +1,33 @@
+describe('TC Fixture - RadioButtons', () => {
+	beforeEach('Precondición: Usuario situado en seccion radio buttons', () => {
+		cy.visit('https://demoqa.com/');
+		cy.viewport(1920, 1080);
+		cy.fixture('DOM/radioButtons.Page').then(the => {
+			cy.contains(the.elements).click();
+			cy.contains(the.radio).click();
+			cy.url().should('contain', the.radioButtonURL);
+		});
+	});
+
+	it('US 13113  | TC#1: Validar seleccionar radio button "Yes"', () => {
+		cy.fixture('DOM/radioButtons.Page').then(the => {
+			cy.get(the.radioButtons.yesButton).check({ force: true }).should('be.checked');
+			cy.get(the.flag).contains(the.flagMessage);
+			cy.get(the.flagText).contains(the.flagMessageOption.yes);
+		});
+	});
+
+	it('US 13112  | TC#2: Validar seleccionar radio button "Impressive"', () => {
+		cy.fixture('DOM/radioButtons.Page').then(the => {
+			cy.get(the.radioButtons.impressiveButton).check({ force: true }).should('be.checked');
+			cy.get(the.flag).contains(the.flagMessage);
+			cy.get(the.flagText).should('have.text', the.flagMessageOption.impressive);
+		});
+	});
+
+	it('US 13112  | TC#3: Validar radio button "no" está disabled', () => {
+		cy.fixture('DOM/radioButtons.Page').then(the => {
+			cy.get(the.noDisabled).should('be.disabled');
+		});
+	});
+});
