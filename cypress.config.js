fa500c674ec90594a0e7a63afb8e9f2977b3dc2b
Nicolas <113467174+Nico950@users.noreply.github.com>
2023-04-24 17:59:52 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -60,7 +60,7 @@ export default defineConfig({
 			signUp: 'https://coderbyte.com/sl', // what does this url here?
 			radioButton: '/radio-button',
 			textBox: 'text-box',
-      selectMenu: '/select-menu',
+                        selectMenu: '/select-menu',
 			selectable: '/selectable',
 		},
 		user: {
