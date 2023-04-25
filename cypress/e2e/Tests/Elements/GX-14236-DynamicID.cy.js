a158e5c849dfbe349dc322b7082b2a3f40abc7d7
Berni Galati <galatib47@gmail.com>
2023-04-25 18:37:32 -0300

    GX-14236 | Code optimization

--- cypress/e2e/Tests/Elements/GX-14236-DynamicID.cy.js
+++ cypress/e2e/Tests/Elements/GX-14236-DynamicID.cy.js
@@ -1,34 +1,37 @@
 import { removeLogs } from '@helper/RemoveLogs';
 removeLogs();
 
-import { URL, TC, select } from '../Elements/DynamicId.Pages.js';
+import { e } from '@pages/ElementDynamicId.Page.js';
 
-//vars
+const timeValue = 5000;
+
+const URL = 'https://demoqa.com/dynamic-properties';
+
 describe('✅ US GX-14236 | TS: ✅ToolsQA | Elements | Dynamic Properties', () => {
 	beforeEach(() => {
 		cy.visit(URL);
 	});
 	it('14237 | TC1: Validar existencia de elemento con texto “this text has..” a través de <texto>, al cargar la página', () => {
-		cy.xpath('//p["This text has random Id"]').should('be.visible');
+		e.readText();
 	});
 
 	it('14237 | TC2: Validar existencia de elemento con texto “Will enabled 5 seconds” y que se encuentre DESHABILITADO', () => {
-		cy.get('#enableAfter').should('be.disabled');
+		e.disabled();
 	});
 
 	it('14237 | TC3: Validar que elemento con texto  “Will enabled 5 seconds” se encuentre HABILITADO después de 5 segundos transcurridos desde carga de la pagina', () => {
-		cy.wait(5000);
-		cy.get('#enableAfter').should('be.enabled');
+		e.wait(timeValue);
+		e.enabled();
 	});
 
 	it('14237 | TC4: Validar cambio de color del elemento con texto  “Change color” después de los 5 segundos transcurridos desde carga de la página', () => {
-		cy.get('[class="mt-4 btn btn-primary"]').contains('Color Change');
-		cy.wait(5000);
-		cy.get('[class="mt-4 text-danger btn btn-primary"]').contains('Color Change');
+		e.whiteClass();
+		e.wait(timeValue);
+		e.dangerClass();
 	});
 
 	it('14237 | TC5: Validar existencia de nuevo elemento con texto “visible after 5 seconds” después de los 5 segundos transcurridos desde de carga de la página', () => {
-		cy.wait(5000);
-		cy.get('#visibleAfter').should('be.visible');
+		e.wait(timeValue);
+		e.visibleAfter();
 	});
 });
---
+++ cypress/support/pages/ElementDynamicId.Page.js
@@ -0,0 +1,76 @@
+//vars
+const randomID = '//p["This text has random Id"]';
+const enableAfter = '#enableAfter';
+const whiteClass = '[class="mt-4 btn btn-primary"]';
+const dangerClass = '[class="mt-4 text-danger btn btn-primary"]';
+const visibleAfter = '#visibleAfter';
+const disabled = 'be.disabled';
+const enabled = 'be.enabled';
+const visible = 'be.visible';
+const text = 'Color Change';
+const timeValue = 5000;
+
+class Elements {
+	get = {
+		randomId: () => cy.xpath(randomID),
+		enableAfter: () => cy.get(enableAfter),
+		whiteClass: () => cy.get(whiteClass),
+		dangerClass: () => cy.get(dangerClass),
+		visibleAfter: () => cy.get(visibleAfter),
+		wait: () => cy.wait(timeValue),
+	};
+
+	enabled() {
+		this.get.enableAfter().should(enabled);
+	}
+
+	disabled() {
+		this.get.enableAfter().should(disabled);
+	}
+
+	readText() {
+		this.get.randomId().should(visible);
+	}
+
+	whiteClass() {
+		this.get.whiteClass().contains(text);
+	}
+
+	dangerClass() {
+		this.get.dangerClass().contains(text);
+	}
+
+	visibleAfter() {
+		this.get.visibleAfter().should(visible);
+	}
+
+	wait() {
+		this.get.wait();
+	}
+}
+
+export const e = new Elements();
+
+class TC {
+	n01() {
+		e.readText().should(visible);
+	}
+
+	n02() {
+		e.disabled().should(disabled);
+	}
+	n03() {
+		e.wait();
+		e.enabled();
+	}
+	n04() {
+		e.whiteClass().contains(text);
+		e.wait();
+		e.dangerClass().contains(text);
+	}
+	n05() {
+		e.wait();
+		e.visibleAfter().should(visible);
+	}
+}
+export const tc = new TC();
