cd0976de83cb9d539ea76d5270f9c821b32e5fe2
VickyNovarese <mariavirginianovarese@gmail.com>
2023-04-27 00:07:39 -0300

    GX-13556 Primer Commit, se creo el primer archivo de test, se creo el CI y se guardó la doc

---
+++ cypress/e2e/Tests/Elements/GX-13556-RadioButtons.cy.js
@@ -0,0 +1,15 @@
+describe('This is your test project title', () => {
+	beforeEach(() => {
+		// runs before every it() test block
+	});
+
+	context('This is your test suite title', () => {
+		// -- Start: Cypress Tests --
+		it('This is your test case one title', () => {
+			// Write your test case one here
+		});
+		it('This is your test case two title', () => {
+			// Write your test case two here
+		});
+	});
+});
