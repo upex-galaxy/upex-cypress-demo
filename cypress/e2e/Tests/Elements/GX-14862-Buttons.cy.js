16f30caa7d547f66c3615b1a329986bda685fe32
Hdzluis10 <hdzluis10@github.com>
2023-04-27 23:05:40 -0600

    GX-14862 Adding elements to POM and enhancing code for TCs

--- cypress/e2e/Tests/Elements/GX-14862-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-14862-Buttons.cy.js
@@ -2,7 +2,7 @@ import { Clicking } from '../../../support/pages/clickButtons.Page';
 
 const page = Cypress.env('endpoint').buttons;
 
-// const fixture1 from "../../"
+// const fixture from "../../../fixtures/DOM/MessageBtn.json";
 
 describe('GX-14862 | ToolsQA | Elements | Buttons', () => {
 	beforeEach('Usuario debe estar en la pagina de element/buttons', () => {
@@ -12,17 +12,20 @@ describe('GX-14862 | ToolsQA | Elements | Buttons', () => {
 
 	it('14863 | TC1: Validate message “You have done a double click“ should be displayed when double click in the Double Click Me button', () => {
 		Clicking.PerformDoubleClick();
-		cy.get('#doubleClickMessage').should('have.text', 'You have done a double click'); //obtain the element where the message is displayed and assert
+		Clicking.getMessage2Click();
+		cy.should('have.text', 'You have done a double click').and('be.visible');
 	});
 
 	it('14863 | TC2: Validate message “You have done a right click“ should be displayed when right click in the Right Click Me button', () => {
 		Clicking.DoRightClick();
-		cy.get('#rightClickMessage').should('include.text', 'right'); //obtain the element where the message is displayed and assert
+		Clicking.getRightClickMsg();
+		cy.should('include.text', 'right'); //obtain the element where the message is displayed and assert
 	});
 
 	it('14863 | TC3: Validate message “You have done a Dynamic click“ should be displayed when click in the Click Me button', () => {
 		Clicking.SoloClick();
-		cy.get('#dynamicClickMessage').should('exist').and('contain', 'You have done a dynamic click'); //obtain the element where the message is displayed and assert
+		Clicking.getClickMsg();
+		cy.should('exist').and('contain', 'You have done a dynamic click'); //obtain the element where the message is displayed and assert
 	});
 });
 
--- cypress/support/pages/clickButtons.Page.js
+++ cypress/support/pages/clickButtons.Page.js
@@ -4,6 +4,9 @@ class clickButtons {
 		doubleClick: () => cy.get('#doubleClickBtn'),
 		RightClick: () => cy.get('#rightClickBtn'),
 		OnlyClick: () => cy.get('[type="button"]').eq(3),
+		Message2Click: () => cy.get('#doubleClickMessage'),
+		MessageRightClick: () => cy.get('#rightClickMessage'),
+		Message1Click: () => cy.get('#dynamicClickMessage'),
 	};
 
 	//Functions or Methods
@@ -17,6 +20,16 @@ class clickButtons {
 
 	SoloClick() {
 		this.get.OnlyClick().click(); //Obtain the element and just click on it.
+	}
+
+	getMessage2Click() {
+		this.get.Message2Click();
+	}
+	getRightClickMsg() {
+		this.get.MessageRightClick();
+	}
+	getClickMsg() {
+		this.get.Message1Click();
 	}
 }
 
