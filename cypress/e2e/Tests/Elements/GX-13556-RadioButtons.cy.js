e85b24ce5a4f6d7ec818fae604969de6f095236a
VickyNovarese <mariavirginianovarese@gmail.com>
2023-05-01 11:34:40 -0300

    GX-13556 Se corrieron todas las pruebas y todas pasaron.

--- cypress/e2e/Tests/Elements/GX-13556-RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-13556-RadioButtons.cy.js
@@ -1,15 +1,20 @@
-describe('This is your test project title', () => {
-	beforeEach(() => {
-		// runs before every it() test block
-	});
+import { removeLogs } from '@helper/RemoveLogs';
 
-	context('This is your test suite title', () => {
-		// -- Start: Cypress Tests --
-		it('This is your test case one title', () => {
-			// Write your test case one here
-		});
-		it('This is your test case two title', () => {
-			// Write your test case two here
-		});
+describe('GX-13556 | ToolsQA | Elements | Radio Buttons', () => {
+	beforeEach('El usuario debe estar en la página', () => {
+		cy.visit('https://demoqa.com/radio-button'); //runs before every it() test block
+		cy.url().should('contain', 'radio-button');
 	});
+	it('13557 | Validar Yes Button', () => {
+		cy.get('#yesRadio').click({ force: true }).should('have.attr', 'type', 'radio');
+		cy.contains('You have selected ').children().should('have.text', 'Yes');
+	});
+	it('13557 | Validar Impressive Button', () => {
+		cy.get('#impressiveRadio').click({ force: true }).should('have.attr', 'type', 'radio');
+		cy.contains('You have selected ').children().should('have.text', 'Impressive');
+	});
+	//it('13557 | Validar impressive button', () => {
+	// Write your test case two here
+	//});
 });
+removeLogs();
