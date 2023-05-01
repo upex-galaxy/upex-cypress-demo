5baf35649c7bb2c54fe0245b4a0a3e1130637a8b
apscecilia <apscecilia@gmail.com>
2023-04-30 21:43:29 -0300

    GX-2200-se crea metodos para TC1-TC2-TC3 y TC4

--- cypress/e2e/Tests/Interactions/GX2-2200-Dragabble.cy.js
+++ cypress/e2e/Tests/Interactions/GX2-2200-Dragabble.cy.js
@@ -1,58 +1,68 @@
 import { removeLogs } from '@helper/RemoveLogs';
+import { dragabble } from '@pages/Dragabble.Page';
+
 
 describe('✅ToolsQA | Interactions | Dragabble', () => {
 	beforeEach('Precondition: be located in the page', () => {
+		//cy.visit('/dragabble');
 		cy.visit('/dragabble');
 		cy.url().should('contain', 'dragabble');
 	});
 
 	it('2201 | TC1: Validate box area  can be dragged as desired in any direction on the Simple tab', () => {
+		dragabble.dragBoxInRandomDirection();
+	});
+	
+	it('2201 | TC2: Validate boxes area move only on the axi X on the Axis Restricted tab', () => {
+		dragabble.selectAxisRestrictedTab();
+		dragabble.elements.restrictedXBox().should('have.css', 'top', '0px');
+		dragabble.draggableBoxRestrictedToXAxi();
+
+		// cy.get('@coord2').then((onlyX) => {
+		// 	const { xcoord } = onlyX;
+		// 	dragabble.elements.onlyXSelect().should('have.css', 'left', `${xcoord}px`);
+		// });
+
+	});
+	it('2201 | TC3: Validate boxes area move only on the axi Y on the Axis Restricted tab', () => {
+		dragabble.selectAxisRestrictedTab();
+		dragabble.elements.restrictedYBox().should('have.css', 'left', '0px');
+		dragabble.draggableBoxRestrictedToYAxi();
+
+		
 
 	});
 	
-	it('2201 | TC2: Validate boxes area move only on the axis indicated on the Axis Restricted tab', () => {
+	it('2201 | TC4: Validate box can not be dragged out of the delimited area of action on the Container Restricted tab', () => {
+		dragabble.selectContainerRestrictedTab();
+		dragabble.elements.containmentWrapper()
+			.should('exist')
+			//.and('have.text', "I'm contained within the box") // check if the box with the text is displayed
+			.and('have.css', 'border', '1.6px solid rgb(204, 204, 204)');
+		dragabble.elements.moveContainedBox()
+			.should('exist')
+			.should('be.visible');
+		dragabble.containerBoxRestricted();
 
 	});
 	
-	it('2201 | TC3: Validate box can not be dragged out of the delimited area of action on the Container Restricted tab', () => {
+	// it('2201 | TC5: Validate text of box can not be dragged out of the delimited area of action on the Container Restricted tab', () => {
 
-	});
-	
-	it('2201 | TC4: Validate text of box can not be dragged out of the delimited area of action on the Container Restricted tab', () => {
+	// });
 
-	});
+	// it('2201 | TC6: Validate cursor must stick to the center of the box if “I will always stick to the center” box is dragged on the Cursor Style tab', () => {
 
-	it('2201 | TC5: Validate cursor must stick to the center of the box if “I will always stick to the center” box is dragged on the Cursor Style tab', () => {
+	// });
 
-	});
+	// it('2201 | TC7: Validate the cursor will stick to the top left outside of the box and the cursor icon changes to a "+" if dashed box "My cursor is at top left" is displayed on the "Cursor Style" tab', () => {
 
-	it('2201 | TC6: Validate the cursor will stick to the top left outside of the box and the cursor icon changes to a "+" if dashed box "My cursor is at top left" is displayed on the "Cursor Style" tab', () => {
+	// });
+	// it('2201 | TC8: Validate the cursor will stick to the bottom center of the box and the cursor hovering icon changes back to the default cursor ico if "My cursor is at bottom" box is dragged on the "Cursor Style" tab', () => {
 
-	});
-	it('2201 | TC7: Validate the cursor will stick to the bottom center of the box and the cursor hovering icon changes back to the default cursor ico if "My cursor is at bottom" box is dragged on the "Cursor Style" tab', () => {
-
-	});
+	// });
 
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 removeLogs();
--- cypress/support/pages/Dragabble.Page.js
+++ cypress/support/pages/Dragabble.Page.js
@@ -1,18 +1,79 @@
 class Dragabble {
 	elements = {
-		tabSimple: () => cy.get('#draggableExample-tab-simple'),
-		tabAxisRestricted: () => cy.get('#draggableExample-tab-axisRestriction'),
-		tabContainerRestricted: () => cy.get('#draggableExample-tab-containerRestriction'),
-		tabCursorStyle: () => cy.get('#draggableExample-tab-cursorStyle'),
-		dragBox: () => cy.get('#dragBox'),
-		restrictedX: () => cy.get('#restrictedX'),
-		restrictedY: () => cy.get('#restrictedY'),
-		containerBox: () => cy.get('#containmentWrapper'),
-		containerText: () => cy.get('.draggableExample-tabpane-containerRestriction'),
-		cursorTopLeft: () => cy.get('#cursorTopLeft'),
-		cursorCenter: () => cy.get('#cursorTopLeft'),
-		cursorBottom: () => cy.get ('#cursorBottom'),
+		simpleTabClick: () => cy.get('#draggableExample-tab-simple'),
+		axisRestrictedTabClick: () => cy.get('#draggableExample-tab-axisRestriction'),
+		containerRestrictedTabClick: () => cy.get('#draggableExample-tab-containerRestriction'),
+		cursorStyleTabClick: () => cy.get('#draggableExample-tab-cursorStyle'),
+		draggableBoxSelect: () => cy.get('#dragBox'),
+		draggableElementSelect: () => cy.get('#draggableExample-tabpane-axisRestriction'),
+		restrictedXBox: () => cy.get('#restrictedX'),
+		restrictedYBox: () => cy.get('#restrictedY'),
+		containmentWrapper: () => cy.get('#containmentWrapper'),
+		moveContainedBox: () => cy.get('#containmentWrapper .draggable'),
+
+		containedTextSelect: () => cy.get('.draggableExample-tabpane-containerRestriction'),
+		cursorTopLeftSelect: () => cy.get('#cursorTopLeft'),
+		cursorCenterSelect: () => cy.get('#cursorTopLeft'),
+		cursorBottomSelect: () => cy.get ('#cursorBottom'),
 
 	};
+	MIN = -30;
+	MAX = 400;	
+
+	dragBoxInRandomDirection() {
+		for (let i = 0; i < 2; i++) {
+			const randomX = Cypress._.random(this.MIN,this.MAX);
+			const randomY = Cypress._.random(this.MIN,this.MAX);
+			this.elements.draggableBoxSelect().move({ deltaX: randomX, deltaY: randomY });
+		}
+	}
+
+	selectAxisRestrictedTab() {
+		this.elements.axisRestrictedTabClick().click();
+	}
+  
+	draggableBoxRestrictedToXAxi() {
+		this.elements.restrictedXBox().then(($box) => {
+			const initialCoords = $box[0].getBoundingClientRect();
+			
+			for (let i = 0; i < 2; i++) {
+				const randomX = Cypress._.random(this.MIN, this.MAX);
+				this.elements.restrictedXBox().move({ deltaX: randomX, deltaY: initialCoords.top });
+			}
+		});
+
+	}
+	draggableBoxRestrictedToYAxi() {
+		this.elements.restrictedYBox().then(($box) => {
+			const initialCoords = $box[0].getBoundingClientRect();
+			
+			for (let i = 0; i < 3; i++) {
+				const randomY = Cypress._.random(this.MIN, this.MAX);
+				this.elements.restrictedYBox().move({ deltaX: initialCoords.left, deltaY: randomY,  });
+			}
+		});
+
+	}
+	selectContainerRestrictedTab() {
+		this.elements.containerRestrictedTabClick().click();
+	}
+	containerBoxRestricted() {
+		const wrapper = this.elements.containmentWrapper();
+		this.elements.moveContainedBox().then(($box) => {
+			const initialCoords = $box[0].getBoundingClientRect();
+			const containerCoords = wrapper[0].getBoundingClientRect();
+
+			const minX = containerCoords.left - initialCoords.left;
+			const maxX = containerCoords.width - initialCoords.width + minX;
+			const minY = containerCoords.top - initialCoords.top;
+			const maxY = containerCoords.height - initialCoords.height + minY;
+
+			const randomX = Cypress._.random(minX, maxX);
+			const randomY = Cypress._.random(minY, maxY);
+
+			this.elements.moveContainedBox().move({ x: randomX, y: randomY });
+
+		});
+	}
 }
-export const dragable = new Dragabble();
+export const dragabble = new Dragabble();
