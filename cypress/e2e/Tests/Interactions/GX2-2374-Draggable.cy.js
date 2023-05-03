33f929a30869d3f248b848a7b469aae9d3c09947
Hdzluis10 <hdzluis10@github.com>
2023-05-02 20:02:26 -0600

    GX-2374 TC1 done missing assertion, pending rest of the test cases as of today

--- cypress/e2e/Tests/Interactions/GX2-2374-Draggable.cy.js
+++ cypress/e2e/Tests/Interactions/GX2-2374-Draggable.cy.js
@@ -1,1 +1,15 @@
-describe('ToolsQA | Interactions | Dragabble', () => {});
+import { dragMeSimple } from '@pages/Draggable.Page';
+
+describe('ToolsQA | Interactions | Dragabble', () => {
+	beforeEach('Should be in the DemoQA page for Dragabble', () => {
+		cy.visit('https://demoqa.com/dragabble');
+		cy.contains('Dragabble').should('be.visible');
+		cy.url().should('contain', 'dragabble');
+	});
+
+	it('2375 | TC1: Validate drag the Drag Me box at any direction for the Simple Tab', () => {
+		dragMeSimple.simpleDrag();
+	});
+});
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
--- cypress/support/pages/Draggable.Page.js
+++ cypress/support/pages/Draggable.Page.js
@@ -1,7 +1,21 @@
 class simple {
 	get = {
 		dragMe: () => cy.get('#dragBox'),
+		dragSection: () => cy.get('[class="col-12 mt-4 col-md-6"]'),
 	};
+
+	simpleDrag() {
+		let left = Cypress._.random(1, 460);
+		let top = Cypress._.random(1, 460);
+		cy.log(left, top);
+
+		this.get.dragSection();
+		this.get
+			.dragMe()
+			.trigger('mousedown', 'center', { which: 1, pageX: left, pageY: top })
+			.trigger('mousemove', { which: 1, clientX: left, clientY: top })
+			.trigger('mouseup', { force: true });
+	}
 }
 
 export const dragMeSimple = new simple();
