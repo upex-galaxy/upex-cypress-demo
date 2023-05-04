7651568f49b787c06685a95bce4924a884c5d7a2
Hdzluis10 <hdzluis10@github.com>
2023-05-04 13:28:05 -0600

    GX-2374 TC1 completed, adding another class for Axis Restricted test on POM file

--- cypress/e2e/Tests/Interactions/GX2-2374-Draggable.cy.js
+++ cypress/e2e/Tests/Interactions/GX2-2374-Draggable.cy.js
@@ -1,4 +1,5 @@
 import { dragMeSimple } from '@pages/Draggable.Page';
+import { dragAxisRestricted } from '@pages/Draggable.Page';
 
 describe('ToolsQA | Interactions | Dragabble', () => {
 	beforeEach('Should be in the DemoQA page for Dragabble', () => {
@@ -9,6 +10,12 @@ describe('ToolsQA | Interactions | Dragabble', () => {
 
 	it('2375 | TC1: Validate drag the Drag Me box at any direction for the Simple Tab', () => {
 		dragMeSimple.simpleDrag();
+
+		cy.should('have.text', 'Drag me');
+	});
+	it('2375 | TC2: Validate drag the Only X box with random range for X axis only for the Axis Restricted Tab', () => {
+		dragAxisRestricted.GetAxisTab();
+		cy.should('have.attr', 'aria-selected', 'true');
 	});
 });
 import { removeLogs } from '@helper/RemoveLogs';
--- cypress/support/pages/Draggable.Page.js
+++ cypress/support/pages/Draggable.Page.js
@@ -1,15 +1,20 @@
 class simple {
 	get = {
 		dragMe: () => cy.get('#dragBox'),
-		dragSection: () => cy.get('[class="col-12 mt-4 col-md-6"]'),
+		dragTab: () => cy.get('#draggableExample-tab-simple'),
+		dragSection: () => cy.get('#tab-content'),
 	};
 
+	CheckTab() {
+		return this.elements.dragTab();
+	}
+
 	simpleDrag() {
-		let left = Cypress._.random(1, 460);
-		let top = Cypress._.random(1, 460);
+		let left = Cypress._.random(1, 100); //Coord was taken from Content Box only
+		let top = Cypress._.random(1, 40); //Coord was taken from Content Box only
 		cy.log(left, top);
 
-		this.get.dragSection();
+		// this.get.dragSection();
 		this.get
 			.dragMe()
 			.trigger('mousedown', 'center', { which: 1, pageX: left, pageY: top })
@@ -18,4 +23,17 @@ class simple {
 	}
 }
 
+class AxisRestricted {
+	get = {
+		AxisTab: () => cy.get('#draggableExample-tab-axisRestriction'),
+		RestrictedX: () => cy.get('#restrictedX'),
+		RestrictedY: () => cy.get('#RestrictedY'),
+	};
+
+	GetAxisTab() {
+		this.get.AxisTab().click();
+	}
+}
+
 export const dragMeSimple = new simple();
+export const dragAxisRestricted = new AxisRestricted();
