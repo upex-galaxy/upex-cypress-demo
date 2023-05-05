067fae5b242e3a3703f9d84ad858c36ad5c9a997
Hdzluis10 <hdzluis10@github.com>
2023-05-05 09:43:10 -0600

    GX-2374 Still working on TC's and POM file

--- cypress/e2e/Tests/Interactions/GX2-2374-Draggable.cy.js
+++ cypress/e2e/Tests/Interactions/GX2-2374-Draggable.cy.js
@@ -1,4 +1,4 @@
-import { dragMeSimple } from '@pages/Draggable.Page';
+import { dragContainerRestricted, dragMeSimple } from '@pages/Draggable.Page';
 import { dragAxisRestricted } from '@pages/Draggable.Page';
 
 describe('ToolsQA | Interactions | Dragabble', () => {
@@ -16,7 +16,27 @@ describe('ToolsQA | Interactions | Dragabble', () => {
 	it('2375 | TC2: Validate drag the Only X box with random range for X axis only for the Axis Restricted Tab', () => {
 		dragAxisRestricted.GetAxisTab();
 		cy.should('have.attr', 'aria-selected', 'true');
+
+		dragAxisRestricted.moveOnlyX();
+
+		cy.should('have.text', 'Only X');
+	});
+	it('2375 | TC3: Validate drag the Only Y box with random range for Y axis only for the Axis Restricted Tab', () => {
+		dragAxisRestricted.GetAxisTab();
+		cy.should('have.attr', 'aria-selected', 'true');
+
+		dragAxisRestricted.moveOnlyY();
+
+		cy.should('have.text', 'Only Y');
+	});
+
+	it.only('2375 | TC4: Validate drag the Im contained within the box box in the delimited area of action', () => {
+		dragContainerRestricted.getContainerRestricted();
+		cy.should('have.attr', 'aria-selected', 'true');
+
+		dragContainerRestricted.moveOnlyWithinTheBox();
 	});
 });
+
 import { removeLogs } from '@helper/RemoveLogs';
 removeLogs();
--- cypress/support/pages/Draggable.Page.js
+++ cypress/support/pages/Draggable.Page.js
@@ -14,26 +14,71 @@ class simple {
 		let top = Cypress._.random(1, 40); //Coord was taken from Content Box only
 		cy.log(left, top);
 
-		// this.get.dragSection();
 		this.get
-			.dragMe()
-			.trigger('mousedown', 'center', { which: 1, pageX: left, pageY: top })
-			.trigger('mousemove', { which: 1, clientX: left, clientY: top })
-			.trigger('mouseup', { force: true });
+			.dragMe() // obtener el elemento a arrastrar
+			.trigger('mousedown', 'center', { which: 1, pageX: left, pageY: top }) //hace click en el elemento
+			.trigger('mousemove', { which: 1, clientX: left, clientY: top }) //Mueve el elemento a las coordenadas asignadas
+			.trigger('mouseup', { force: true }); //suelta el click después de arrastrar el elemento a las coordenadas
 	}
 }
 
 class AxisRestricted {
 	get = {
 		AxisTab: () => cy.get('#draggableExample-tab-axisRestriction'),
-		RestrictedX: () => cy.get('#restrictedX'),
-		RestrictedY: () => cy.get('#RestrictedY'),
+		OnlyX: () => cy.get('#restrictedX'),
+		OnlyY: () => cy.get('#restrictedY'),
 	};
 
 	GetAxisTab() {
 		this.get.AxisTab().click();
 	}
+
+	moveOnlyX() {
+		let left = Cypress._.random(0, 100);
+		this.get
+			.OnlyX()
+			.trigger('mousedown', { which: 1, pageX: left, pageY: 0 })
+			.trigger('mousemove', { which: 1, clientX: left, ClientY: 0 })
+			.trigger('mouseup', { force: true });
+	}
+	moveOnlyY() {
+		let top = Cypress._.random(0, 40);
+		this.get
+			.OnlyY()
+			.trigger('mousedown', { which: 1, pageX: 0, pageY: top })
+			.trigger('mousemove', { which: 1, clientX: 0, ClientY: top })
+			.trigger('mouseup', { force: true });
+	}
+}
+
+class containerRestricted {
+	get = {
+		containerTab: () => cy.get('#draggableExample-tab-containerRestriction'),
+		withinTheBox: () => cy.get('.ui-widget-content.ui-draggable.ui-draggable-handle'),
+		containmentWrapper: () => cy.get('#containmentWrapper'),
+	};
+	getContainerRestricted() {
+		this.get.containerTab().click();
+	}
+	moveOnlyWithinTheBox() {
+		let left = Cypress._.random(1, 382); //Coord was taken from Content Box only
+		let top = Cypress._.random(1, 107); //Coord was taken from Content Box only
+		cy.log(left, top);
+
+		this.get
+			.withinTheBox()
+			.trigger('mousedown', { which: 1, PageX: left, pageY: top })
+			.trigger('mousemove', { which: 1, clientX: left, clientY: top })
+			.trigger('mouseup', { force: true });
+
+		// this.get.containmentWrapper().then($wrapper => {
+		// 	const containerCoords = $wrapper[0].getBoundingClientRect();
+		// 	const containerCoordsFinal = $wrapper[0].getBoundingClientRect();
+		// 	expect(containerCoords).not.equal(containerCoordsFinal);
+		// });
+	}
 }
 
 export const dragMeSimple = new simple();
 export const dragAxisRestricted = new AxisRestricted();
+export const dragContainerRestricted = new containerRestricted();
