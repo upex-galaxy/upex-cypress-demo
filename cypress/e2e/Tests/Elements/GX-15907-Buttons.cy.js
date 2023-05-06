fe6482caf5666c8231acd1806a6bc25789676534
Barby <barbaravaz29@gmail.com>
2023-05-06 17:23:51 -0300

    .

--- cypress/e2e/Tests/Elements/GX-15907-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-15907-Buttons.cy.js
@@ -1,24 +1,22 @@
 describe('GX-15907 | ToolsQA | Elements | Buttons', () => {
 	beforeEach('Visitar pagina demo QA', () => {
 		cy.visit('https://demoqa.com/buttons');
-		
 	});
 
 	it('15908 | TC1 Validar doble click en btn "Double Click Me"', () => {
 		cy.get('#doubleClickBtn').dblclick();
 		cy.contains('You have done a double click').should('exist');
-		cy.wait(4000);
+		cy.wait(3000);
 	});
 	it('15908 | TC2 Validar  click en btn "Right Click"', () => {
-		cy.get('#rightClickBtn').rightClick();
+		cy.get('#rightClickBtn').rightclick();
 		cy.contains('You have done a right click').should('exist');
-		cy.wait(4000);
+		cy.wait(3000);
 	});
 	it('15908 | TC3 Validar  click en btn "Click Me"', () => {
-		cy.get('.btn.btn-primary').eq(3).click();
-
+		cy.get('.btn.btn-primary').eq(2).click();
 		cy.contains('You have done a dynamic click').should('be.visible');
-		cy.wait(4000);
+		cy.wait(3000);
 	});
 });
 import { removeLogs } from '@helper/RemoveLogs';
