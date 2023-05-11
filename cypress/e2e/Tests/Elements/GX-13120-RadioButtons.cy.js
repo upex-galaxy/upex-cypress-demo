dcb20f09c845481871503ae41c8f01e952425da4
Belén <belleloweryart@gmail.com>
2023-05-11 11:04:40 -0300

    GX-13120

---
+++ cypress/e2e/Tests/Elements/GX-13120-RadioButtons.cy.js
@@ -0,0 +1,20 @@
+import { removeLogs } from '@helper/RemoveLogs';
+
+describe('GX-13120 ToolsQA | Elements | Radio Buttons', () => {
+	beforeEach('Precondición: Usuario debe estar situado en el endpoint Radio Buttons de Tools QA', () => {
+		cy.visit('https://demoqa.com/radio-button');
+	}),
+		it('13121 | TC#1: Validar que se visualice  “You have selected ” + buttonName" cuando se presiona el RB Yes', () => {
+			cy.get('#yesRadio').click({ force: true });
+			cy.get('.mt-3').should('contain.text', 'You have selected Yes');
+		});
+
+	it('13121 | TC:2 Validar que se visualice  “You have selected ” + buttonName" cuando se presiona el RB Impressive.', () => {
+		cy.get('#impressiveRadio').click({ force: true });
+		cy.get('.mt-3').should('contain.text', 'You have selected Impressive');
+	});
+
+	it('13121 | TC:2 Validar que no sea posible seleccionar el RB No cuando se hace click.', () => {
+		cy.get('#noRadio').should('be.disabled');
+	});
+});
