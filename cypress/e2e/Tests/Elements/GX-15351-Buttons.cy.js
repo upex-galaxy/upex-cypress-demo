a6b80fefb536c3b7773a7a38a38fb264c64b510c
mjorabdo <jorabdo1991@gmail.com>
2023-05-12 09:48:13 -0500

    GX-15351 | cambiando titulo

--- cypress/e2e/Tests/Elements/GX-15351-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-15351-Buttons.cy.js
@@ -5,34 +5,23 @@ removeLogs();
 import { Clicking } from '@pages/clickButtons.Page';
 
 describe('GX-15351 | ToolsQA | Elements | Buttons', () => {
-
-    
-
 	beforeEach('User should visit the page Elements/Buttons', () => {
 		cy.visit('https://demoqa.com/buttons');
 		cy.url().should('contains', 'button');
 	});
 
-	
-	it('US #15351 | 15358 | TC1:  Validate “Double Click” button is clicked', () => {
+	it('15358 | TC1:  Validate “Double Click” button is clicked', () => {
 		Clicking.PerformDoubleClick();
 		Clicking.getMessage2Click().should('have.text', 'You have done a double click');
 	});
 
-	it('US #15351 | 15358 | TC2:  Validate  “Right Click” button is clicked', () => {
-		Clicking.DoRightClick();		
+	it('15358 | TC2:  Validate  “Right Click” button is clicked', () => {
+		Clicking.DoRightClick();
 		Clicking.getRightClickMsg().should('have.text', 'You have done a right click');
 	});
 
-	it('US #15351 | 15358 | TC3:  Validate  “Click” button is clicked', () => {
+	it('15358 | TC3:  Validate  “Click” button is clicked', () => {
 		Clicking.SoloClick();
 		Clicking.getClickMsg().should('have.text', 'You have done a dynamic click');
 	});
-
-	
-
-	
 });
-
-
-
