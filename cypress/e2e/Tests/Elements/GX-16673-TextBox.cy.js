d78ea5b46590b2b7bfa906d5f3a4bcb5f7237257
María Belén Labolita Salvatiera <123251578+mblslowery@users.noreply.github.com>
2023-05-12 14:05:46 -0300

    Delete GX-16673-TextBox.cy.js

--- cypress/e2e/Tests/Elements/GX-16673-TextBox.cy.js
+++
@@ -1,9 +0,0 @@
-describe('GX-16673 | TS: ✅ToolsQA | Elements | Text Box: Fill form and Submit', () => {
-	beforeEach('PRC: User must be sited into Text Box Page', () => {
-		cy.visit('https://demoqa.com/text-box'),
-			cy.url('contain', 'text box'),
-			it('16674 | TC:1 Validate submit the form when every input is filled', () => {
-				cy.get;
-			});
-	});
-});
