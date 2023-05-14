696fe44b7e6ed3a1cf516765693879196d816577
agusnes <agusnespral@gmail.com>
2023-05-14 10:39:08 -0300

    GX2-2578 se trabaja sobre los restantes TC, en total 13

--- cypress/e2e/Tests/Widgets/GX2-2578-Tools-qa-widgets-date-picker.cy.js
+++ cypress/e2e/Tests/Widgets/GX2-2578-Tools-qa-widgets-date-picker.cy.js
@@ -27,15 +27,16 @@ describe('', () => {
 
 	it('TC3: Validate list of years is correct (1900-2100) in the select date component', () => {
 		datePicker.clickYearFirst();
-
 		let years = [];
 		for (let i = 1900; i <= 2100; i++) {
 			years.push(i);
 		}
 
-		for (let i = 0; i <= 200; i++) {
-			datePicker.get.yearFirst().select(i).should('have.value', years[i]);
-		}
+		let lengthYears = years.length;
+
+		yearRandom = Cypress._.random(0, lengthYears - 1);
+
+		datePicker.get.yearFirst().select(yearRandom).should('have.value', years[yearRandom]);
 	});
 
 	it('TC4: Validate list of months of the year is correct (January-December) in the select date component', () => {
@@ -48,32 +49,17 @@ describe('', () => {
 	});
 
 	it('TC5: Validate left arrow of the month pagination goes to the previous month in the select date component', () => {
-		let monthFinal;
-		let monthInit;
-		//get actual month
-		datePicker.get.firstDateInput().click();
-		datePicker.get
-			.monthFirstSelected()
-			.invoke('val')
-			.then(value => {
-				monthInit = parseInt(value);
-			});
+		datePicker.validatePreviousMonth();
 
-		datePicker.clickPreviousMonth();
-
-		//validates after click left arrow the month goes previous month
-		datePicker.get
-			.monthFirstSelected()
-			.invoke('val')
-			.then(value => {
-				monthFinal = parseInt(value);
-				if (monthInit == 0) {
-					//if month is january (0) compare with december (11)
-					expect(monthFinal).to.equal(11);
-				} else {
-					expect(monthFinal).to.equal(monthInit - 1);
-				}
-			});
+		datePicker.get.monthFirstSelected().then(function () {
+			//only works with traditional function not ()=>
+			if (this.monthInit == 0) {
+				//if month is january (0) compare with december (11)
+				expect(this.monthFinal).to.equal(11);
+			} else {
+				expect(this.monthFinal).to.equal(this.monthInit - 1);
+			}
+		});
 	});
 
 	it('TC6: Validate right arrow of the month pagination goes to the next month in the select date component', () => {
@@ -110,11 +96,8 @@ describe('', () => {
 		datePicker.get.daySelectedFirst().should('have.css', 'background-color', 'rgb(33, 107, 165)');
 	});
 
-	it('TC8: Validate format of input date is ${Month} ${Day} , ${Year} ${Time} ${acronym for time} in the date and time component', () => {
+	it('TC8: Validate format of input date is ${Month} ${Day}, ${Year} ${Time} ${acronym for time} in the date and time component', () => {
 		datePicker.secondInputDate();
-
-		//let dayjs = require('dayjs');
-		//let actualDate = dayjs().format('MMMM DD, YYYY hh:mm A');
 
 		cy.readFile('../../fixtures/data/datePicker.json').then(the => {
 			expect(the.varToday).to.match(
@@ -123,7 +106,7 @@ describe('', () => {
 		});
 	});
 
-	it.only('TC9: Validate acronym for time is AM for morning and PM for the rest of the day in the date and time component', () => {
+	it('TC9: Validate acronym for time is AM for morning and PM for the rest of the day in the date and time component', () => {
 		//seleccionar el elemento horas
 		datePicker.enterSecondInput();
 		let timeArray = [];
@@ -145,6 +128,39 @@ describe('', () => {
 			} else {
 				datePicker.get.secondDateInput().invoke('val').should('contain', 'PM');
 			}
+		});
+	});
+
+	it('TC10: Validate default date is current date in the the date and time component', () => {
+		datePicker.secondInputDate();
+
+		let dayjs = require('dayjs');
+		let actualDate = dayjs().format('MMMM DD, YYYY h:mm A');
+
+		cy.readFile('../../fixtures/data/datePicker.json').then(the => {
+			expect(actualDate).to.be.equal(the.varToday);
+		});
+	});
+
+	it('TC11: Validate the selected year is marked with a √ in the the date and time component', () => {
+		datePicker.selectRandomYear();
+
+		datePicker.get.selectYear().click();
+		datePicker.get.yearsDropdown().children("[class$='selected_year']").should('contain', '✓');
+	});
+
+	it('TC12: Validate the selected month is marked with a √ in the the date and time component', () => {
+		datePicker.selectRandomMonth();
+		datePicker.get.monthDropdown().click().siblings().find("[class$='selected_month']").should('contain', '✓');
+	});
+
+	it('TC13: Validate select time randomly in the range of 00:00 to 23:45 in the the date and time component', () => {
+		datePicker.selectRandomTime();
+
+		datePicker.get.secondDateInput().click();
+
+		cy.get('@a').then(a => {
+			cy.get('.react-datepicker__time-list').find("[class$='selected']").should('contain', a);
 		});
 	});
 });
--- cypress/support/pages/datePicker2578.Page.js
+++ cypress/support/pages/datePicker2578.Page.js
@@ -1,3 +1,5 @@
+import { datepicker } from './DatePicker.Page';
+
 class DatePicker {
 	get = {
 		firstDateInput: () => cy.get("[id = 'datePickerMonthYearInput']"),
@@ -14,6 +16,13 @@ class DatePicker {
 		daySelectedFirst: () => cy.get('div[class*="selected"]'),
 		timeOptions: () => cy.get('.react-datepicker__time-list li'),
 		timeSelect: () => cy.get('.react-datepicker__time-list'),
+		selectYear: () => cy.get('[class=react-datepicker__year-read-view]'),
+		yearsDropdown: () => cy.get('[class$=year-dropdown]'),
+		monthDropdown: () => cy.get("[class= 'react-datepicker__month-read-view']"),
+		onlyMonths: () => cy.get("[class= 'react-datepicker__month-dropdown'] :not(span)"),
+		onlyYears: () => cy.get('[class$=year-dropdown] [class*=year-option]:not(:first-child):not(:last-child)'),
+		monthSelected: () => cy.get("[class$='selected_month']"),
+		yearSelected: () => cy.get("[class$='selected_year']"),
 	};
 
 	firstInputDate() {
@@ -48,6 +57,15 @@ class DatePicker {
 		this.get.monthFirst().select(0);
 	}
 
+	validatePreviousMonth() {
+		this.get.firstDateInput().click();
+		this.get.monthFirstSelected().invoke('val').then(parseInt).as('monthInit');
+
+		this.get.previousMonth().click();
+
+		this.get.monthFirstSelected().invoke('val').then(parseInt).as('monthFinal');
+	}
+
 	clickPreviousMonth() {
 		this.get.firstDateInput().click();
 		this.get.previousMonth().click();
@@ -60,6 +78,95 @@ class DatePicker {
 
 	selectTime() {
 		this.get.timeOptions();
+	}
+
+	validateMonth() {
+		this.get.secondDateInput().click();
+		this.get.monthDropdown().click();
+
+		let monthArray = [];
+		let monthArrayLength;
+		let randomMonth;
+		this.get.onlyMonths().then($monthList => {
+			$monthList.each($monthList => {
+				monthArray.push($monthList);
+			});
+
+			monthArrayLength = monthArray.length;
+			randomMonth = Cypress._.random(0, monthArrayLength - 1);
+
+			this.get.onlyMonths().eq(randomMonth).click();
+		});
+		cy.log(monthArray);
+	}
+
+	selectRandomMonth() {
+		datePicker.get.secondDateInput().click();
+		datePicker.get.monthDropdown().click();
+
+		let monthArray = [];
+
+		datePicker.get.onlyMonths().then($monthList => {
+			$monthList.each((index, $monthList) => {
+				monthArray.push($monthList);
+			});
+			let monthArrayLength = monthArray.length;
+			let randomMonth = Cypress._.random(0, monthArrayLength - 1);
+
+			this.get.onlyMonths().eq(randomMonth).click();
+		});
+	}
+
+	selectRandomYear() {
+		this.get.secondDateInput().click();
+		this.get.selectYear().click();
+		this.get.yearsDropdown();
+
+		let yearsArray = [];
+
+		this.get.onlyYears().then($yearsList => {
+			$yearsList.each((index, $yearsList) => {
+				yearsArray.push($yearsList);
+			});
+			let yearsArrayLength = yearsArray.length;
+			let randomYears = Cypress._.random(0, yearsArrayLength - 1);
+
+			this.get.onlyYears().eq(randomYears).click();
+		});
+	}
+
+	selectRandomTime() {
+		this.get.secondDateInput().click();
+		let arrayHours = [];
+		let lengthArrayHours;
+		let random;
+		let selectedHour;
+		cy.get('.react-datepicker__time-list li')
+			.as('listHours')
+			.each($hours => {
+				const hora = $hours.text();
+				arrayHours.push(hora);
+			})
+			.then(arrayHoras => {
+				lengthArrayHours = arrayHoras.length;
+				random = Cypress._.random(0, lengthArrayHours - 1);
+
+				cy.get('@listHours').eq(random).invoke('text').as('a');
+				cy.get('@listHours').eq(random).click();
+
+				// otra forma de hacerlo, que continua en el commentario de abajo
+				// .then($selectedHour => {
+				// 	selectedHour = $selectedHour.text();
+				// 	Cypress.env('random', random);
+				// 	cy.log(selectedHour);
+				//});
+				//ACA PRUEBO USANDO UN cy.then (no anda con el alias) pero si no lo uso al llamar la env("random") random no esta definido aun
+				// cy.then(() => {
+				// 	const envRandom = Cypress.env('random');
+				// 	datePicker.get.secondDateInput().click();
+				// 	cy.get('@listHours').eq(random).should('have.text', selectedHour);
+				// });
+			});
 	}
 }
 
