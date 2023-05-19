4514ba497810ef35c48e4fb1fefcb0031e3d4586
LilibethCamico <argcamico@gmail.com>
2023-05-18 21:08:02 -0300

    GX-16807 modified TC1 and TC2 passed

--- cypress/e2e/Tests/Elements/GX-16807-BrokenLinksImages.cy.js
+++ cypress/e2e/Tests/Elements/GX-16807-BrokenLinksImages.cy.js
@@ -7,68 +7,45 @@ describe('ToolsQA | Elements | Broken Links Images', () => {
 		cy.url().should('contain', brokenLinkImgPage);
 	});
 	// -- Start: Cypress Tests --
-	it('16808 | Validate that the Tools QA logo must be displayed correctly', () => {
-		imagePage.selectParagraphIndex(0).then($i => {
-			const indexText = $i.text();
-			expect($i).to.contain(indexText);
-			expect(indexText).eq('Valid image');
+	it.skip('16808 | Validate that the Tools QA logo must be displayed correctly', () => {
+		imagePage.getNameTag(0).then($text => {
+			expect($text).eq('Valid image');
 		});
-		imagePage.selectImgIndex(0).then($src => {
-			expect($src).exist;
-			const imgName = $src.split('/').pop();
-			expect(imgName).to.equal('Toolsqa.jpg');
-
-			imagePage.getImageDimensions($src).then(dimensions => {
-				cy.log(dimensions);
-				const imageWidth = dimensions.width;
-				const imageHeight = dimensions.height;
-				const message = dimensions.message;
-				/*cy.log(imageWidth);
-				cy.log(imageHeight);
-				cy.log(message);*/
-
-				// Realizar aserciones con las dimensiones de la imagen
-				expect(imageWidth).to.equal(347);
-				expect(imageHeight).to.equal(100);
-
-				// Realizar aserciones con el mensaje de éxito
-				expect(message).to.equal('The Tools QA logo is displayed with the correct dimensions');
-			});
+		imagePage.getNameImg(0).then(name => {
+			// Realizar aserciones para extraer el nombre de la img
+			expect(name).to.equal('Toolsqa.jpg');
+		});
+		imagePage.getPropertyImg(0).then($img => {
+			// Realizar aserciones con las dimensiones de la imagen(Valida)
+			const { width, height } = $img;
+			/*expect(width).to.be.greaterThan(0);
+			expect(height).to.be.greaterThan(0);*/
+			expect(width).to.equal(347);
+			expect(height).to.equal(100);
 		});
 	});
-	it('16808 | Validate that a broken image icon should appear', () => {
-		imagePage.selectParagraphIndex(1).then($i => {
-			const indexText = $i.text();
-			expect($i).to.contain(indexText);
-			expect(indexText).eq('Broken image');
+	it.skip('16808 | Validate that a broken image icon should appear', () => {
+		imagePage.getNameTag(1).then($text => {
+			expect($text).eq('Broken image');
 		});
-		imagePage.selectImgIndex(1).then($src => {
-			expect($src).exist;
-			const imgName = $src.split('/').pop();
-			expect(imgName).to.equal('Toolsqa_1.jpg');
-
-			imagePage.getImageDimensions($src).then(dimensions => {
-				const imageWidth = dimensions.width;
-				const imageHeight = dimensions.height;
-				const message = dimensions.message;
-				cy.log(imageWidth);
-				cy.log(imageHeight);
-				cy.log(message);
-
-				// Realizar aserciones con las dimensiones de la imagen
-				//expect(imageWidth).to.equal(347);
-				//expect(imageHeight).to.equal(100);
-
-				// Realizar aserciones con el mensaje de éxito
-				//expect(message).to.equal('The Tooling QA logo is NOT shown with the correct dimensions');
-			});
+		imagePage.getNameImg(1).then(name => {
+			expect(name).to.equal('Toolsqa_1.jpg');
+		});
+		imagePage.getPropertyImg(1).then($img => {
+			// Realizar aserciones con las propiedades de la img(rota)
+			const { width, height } = $img;
+			expect(width).to.equal(0);
+			expect(height).to.equal(0);
 		});
 	});
 	it('16808 | Validate that valid link must be working correctly', () => {
-		imagePage.selectParagraphIndex(2).then($i => {
-			const indexText = $i.text();
-			expect($i).to.contain(indexText);
-			expect(indexText).eq('Valid Link');
+		imagePage.getNameTag(2).then($text => {
+			expect($text).eq('Valid Link');
+		});
+	});
+	it.skip('16808 | Validate that invalid link must go to a webpage showing a 500 status code', () => {
+		imagePage.getNameTag(3).then($text => {
+			expect($text).eq('Broken Link');
 		});
 	});
 });
--- cypress/support/pages/BrokenLinksImages.Page.js
+++ cypress/support/pages/BrokenLinksImages.Page.js
@@ -1,17 +1,48 @@
 class brokenLinksImages {
 	get = {
-		getText: () => cy.get('.col-md-6>div>p'),
-		getSrc: () => cy.get('.col-md-6>div>img'),
+		getText: () => cy.get('.row>.col-md-6>div>p'),
+		getSrc: () => cy.get('.row>.col-md-6>div>img'),
+		clickValid: () => cy.get('.row>.col-md-6>a'),
 	};
-	//Method: select an element(Paragraph)
-	selectParagraphIndex(i) {
-		return this.get.getText().eq(i);
+	//Method: and return the text of the label
+	getNameTag(i) {
+		return this.get
+			.getText()
+			.eq(i)
+			.then($i => {
+				const text = $i.text();
+				return text;
+			});
 	}
-	//Method: get src for image
-	selectImgIndex(i) {
-		return this.get.getSrc().eq(i).invoke('attr', 'src');
+	//Method: get and return after split image name
+	getNameImg(i) {
+		return this.get
+			.getSrc()
+			.eq(i)
+			.invoke('attr', 'src')
+			.then($src => {
+				expect($src).exist;
+				const imgName = $src.split('/').pop();
+				return imgName;
+			});
 	}
-	//Method: To get the dimensions of the image and send messages (Success or Fail)
+	//Method: get and return image properties(width, height)
+	getPropertyImg(i) {
+		return this.get
+			.getSrc()
+			.eq(i)
+			.then($img => {
+				const width = $img[0].naturalWidth;
+				const height = $img[0].naturalHeight;
+				return { width, height };
+			});
+	}
+	clickRedirectLinkValid(i) {
+		this.get.clickValid().eq(i).click();
+		cy.url().should('contain', 'demoqa');
+		cy.wait(1000);
+	}
+	/*Method: To get the dimensions of the image and send messages (Success or Fail)
 	getImageDimensions(src) {
 		return new Promise((resolve, reject) => {
 			const image = new Image();
@@ -20,17 +51,13 @@ class brokenLinksImages {
 				const imageWidth = this.width;
 				const imageHeight = this.height;
 
-				/*cy.log(imageWidth);
-				cy.log(imageHeight);*/
-
 				resolve({ width: imageWidth, height: imageHeight, message: 'The Tools QA logo is displayed with the correct dimensions' });
 			};
 			image.onerror = function () {
 				reject(new Error('The Tooling QA logo is NOT shown with the correct dimensions'));
 			};
 			image.src = src;
-			//cy.log(src);
 		});
-	}
+	}*/
 }
 export const imagePage = new brokenLinksImages();
