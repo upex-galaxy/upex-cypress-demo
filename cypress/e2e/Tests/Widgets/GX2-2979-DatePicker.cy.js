523378177ee6a53a1757de8f10c442584ee4cb13
mjorabdo <jorabdo1991@gmail.com>
2023-05-22 12:37:32 -0500

    GX2-2979 | TC creados y codigo modificado

--- cypress/e2e/Tests/Widgets/GX2-2979-DatePicker.cy.js
+++ cypress/e2e/Tests/Widgets/GX2-2979-DatePicker.cy.js
@@ -1,10 +1,14 @@
 import { removeLogs } from '@helper/RemoveLogs';
 import { datePicker } from '@pages/GX2-2979-DatePicker/datePickerPage';
-removeLogs();
 import dayjs from 'dayjs';
+removeLogs();
 
 describe('GX2-2979 | ToolsQA | Widgets | Date Picker', () => {
 	let data;
+	const actualMonth = dayjs().get('month');
+	const actualDay = dayjs().format('MM/DD/YYYY');
+	const actualDateAndTime = dayjs().format('MMM DD, YYYY h:mm A');
+
 	before('fixture', () => {
 		cy.fixture('data/datePicker').then(dato => {
 			data = dato;
@@ -13,17 +17,37 @@ describe('GX2-2979 | ToolsQA | Widgets | Date Picker', () => {
 	beforeEach('User must be situated in the website', () => {
 		cy.visit('https://demoqa.com/date-picker');
 	});
-	it('2980 | TC1:Validate current day ', () => {});
-	it('2980 | TC2:Validate the input day format is ${Month}/${Day}/${Year}', () => {});
-	it('2980 | TC3:Validate the list of years (1900-2100)', () => {
-		datePicker.get.datePicker().click();
-		datePicker.clicklistOfYear();
-		datePicker.checkValuesYear();
+	it('2980 | TC1: Validate the default value of the date is the current day ', () => {
+		datePicker.getDatePickerValue().then(() => {
+			expect(Cypress.env('date')).to.be.equal(actualDay);
+		});
 	});
-	it('2980 | TC4:Validate the list of months in the year (January-December)', () => {
-		datePicker.get.datePicker().click();
+	it('2980 | TC2:Validate the input day format is ${Month}/${Day}/${Year}', () => {
+		datePicker.getDatePickerValue().then(() => {
+			expect(Cypress.env('date')).to.match(/^\d{2}\/\d{2}\/\d{4}$/);
+		});
+	});
+	it('2980 | TC3:Validate the list of years (1900-2100)', () => {
+		let arrayYear = [];
+		datePicker.clickSelectDatePicker();
 		datePicker.clicklistOfYear();
-		cy.get('.react-datepicker__month-select').within(() => {
+		datePicker.get.yearInput().within(() => {
+			cy.get('option').each(($option, index) => {
+				cy.wrap($option)
+					.invoke('text')
+					.should('be.a', 'string')
+					.then(parseInt)
+					.should('be.a', 'number')
+					.then(valor => {
+						arrayYear.push(valor);
+						expect(arrayYear[index]).to.be.equal(1900 + index);
+					});
+			});
+		});
+	});
+	it('2980 | TC4:Validate the list of months in the year (January-December) / (Selected Date)', () => {
+		datePicker.clickSelectDatePicker();
+		datePicker.get.monthInput().within(() => {
 			cy.get('option').each(($option, index) => {
 				cy.wrap($option)
 					.invoke('text')
@@ -33,6 +57,103 @@ describe('GX2-2979 | ToolsQA | Widgets | Date Picker', () => {
 			});
 		});
 	});
-	it('2980 | TC4:Validate in the month pagination the arrows button', () => {});
-	it('2980 | TC4:Validate the background color blue when the day is selected', () => {});
+	it('2980 | TC5:Validate the left arrow button goes to the next month (Selected Date)', () => {
+		datePicker.clickSelectDatePicker();
+		datePicker.clickPreviousMonthDate().then(() => {
+			if (actualMonth === 0) {
+				expect(data.months[Cypress.env('text2')]).to.be.equal(data.months[11]);
+			} else {
+				expect(data.months[Cypress.env('text2')]).to.be.equal(data.months[actualMonth - 1]);
+			}
+		});
+	});
+	it('2980 | TC6:Validate the right arrow button goes to the next month (Selected Date)', () => {
+		datePicker.clickSelectDatePicker();
+		datePicker.clickNextMonthDate().then(() => {
+			if (actualMonth === 0) {
+				expect(data.months[Cypress.env('text2')]).to.be.equal(data.months[11]);
+			} else {
+				expect(data.months[Cypress.env('text2')]).to.be.equal(data.months[actualMonth + 1]);
+			}
+		});
+	});
+	it('2980 | TC7: Validate the input day format is ${Month} ${Day} , ${Year} ${Time} ${acronym for time}', () => {
+		datePicker.getDateAndTimePickerValue().then(() => {
+			expect(Cypress.env('dateAndTime')).to.match(/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{1,2}, \d{4} \d{1,2}:\d{2} (AM|PM)/);
+		});
+	});
+	it('2980 | TC8: Validate the default value of the date and time is the current day ', () => {
+		datePicker.getDateAndTimePickerValue().should('be.equal', actualDateAndTime);
+	});
+	it('2980 | TC9: Validate the selected year is marked with a √', () => {
+		datePicker.clickDateAndTimePicker();
+		datePicker.clickYearList();
+		datePicker.get.yearSelected().should('exist').and('contain', '✓');
+	});
+	it('2980 | TC10:Validate list of months in the year (January-December) and the selected month is marked with a √', () => {
+		datePicker.clickDateAndTimePicker();
+		datePicker.clickMonthDropdown().within(() => {
+			datePicker.get
+				.monthOptions()
+				.each(($option, index) => {
+					cy.wrap($option)
+						.invoke('text')
+						.then(text => {
+							let splitText = text.split(' ')[0].replace(/✓/g, '');
+							expect(splitText).to.be.equal(data.months[index]);
+						});
+				})
+				.then(() => {
+					datePicker.get.selectedMonth().should('contain', '✓');
+				});
+		});
+	});
+	it('2980 | TC11:Validate the left arrow button goes to the next month (Date and Time)', () => {
+		datePicker.clickDateAndTimePicker();
+		datePicker.clickPreviousMonthDT().then(() => {
+			if (actualMonth === 0) {
+				expect(Cypress.env('text')).to.be.equal(data.months[11]);
+			} else {
+				expect(Cypress.env('text')).to.be.equal(data.months[actualMonth - 1]);
+			}
+		});
+	});
+	it('2980 | TC12:Validate the right arrow button goes to the next month (Date and Time)', () => {
+		datePicker.clickDateAndTimePicker();
+		datePicker.clickNextMonthDT().then(() => {
+			if (actualMonth === 0) {
+				expect(Cypress.env('text')).to.be.equal(data.months[11]);
+			} else {
+				expect(Cypress.env('text')).to.be.equal(data.months[actualMonth + 1]);
+			}
+		});
+	});
+	it('2980 | TC13: Validate the selected day background color is blue in "Selected Date" and "Date and Time" Input', () => {
+		datePicker.clickSelectDatePicker().then(() => {
+			datePicker.get.daySelected().should('have.css', 'background-color').and('eq', 'rgb(33, 107, 165)');
+		});
+		datePicker.clickDateAndTimePicker().then(() => {
+			datePicker.get.daySelected().should('have.css', 'background-color').and('eq', 'rgb(33, 107, 165)');
+		});
+	});
+	it('2980 | TC14: Validate the time selected background color is blue (Date and Time)', () => {
+		datePicker.clickDateAndTimePicker();
+		datePicker.clickTimeBoxList();
+		datePicker.clickDateAndTimePicker();
+		datePicker.get.timeSelected().should('have.css', 'background-color').and('eq', 'rgb(33, 107, 165)');
+	});
+	it('2980 | TC15: Validate the time range is from 00:00 to 23:45', () => {
+		let arrayTime = [];
+		datePicker.clickDateAndTimePicker();
+		datePicker.get.timeBoxList().within(() => {
+			cy.get('li').each(($li, index) => {
+				cy.wrap($li)
+					.invoke('text')
+					.then(valor => {
+						arrayTime.push(valor);
+						expect(arrayTime[index]).to.be.equal(arrayTime[index]);
+					});
+			});
+		});
+	});
 });
--- cypress/support/pages/GX2-2979-DatePicker/datePickerPage.js
+++ cypress/support/pages/GX2-2979-DatePicker/datePickerPage.js
@@ -1,27 +1,121 @@
+import dayjs from 'dayjs';
 class DatePicker {
 	get = {
-		datePicker: () => cy.get('#datePickerMonthYearInput'),
+		selectDatePicker: () => cy.get('#datePickerMonthYearInput'),
 		yearInput: () => cy.get('select[class="react-datepicker__year-select"]'),
+		monthInput: () => cy.get('.react-datepicker__month-select'),
+		previousMonth: () => cy.contains('button', 'Previous Month'),
+		nextMonth: () => cy.contains('button', 'Next Month'),
+		dateAndTimePicker: () => cy.get('#dateAndTimePickerInput'),
+		yearList: () => cy.get('.react-datepicker__year-read-view'),
+		yearSelected: () => cy.get('.react-datepicker__year-option--selected'),
+		monthDropdown: () => cy.get('[class^="react-datepicker__month-dropdown-container"]'),
+		monthOptions: () => cy.get('.react-datepicker__month-option'),
+		selectedMonth: () => cy.get('[class*="react-datepicker__month-option--selected_month"]'),
+		printedMonth: () => cy.get('.react-datepicker__month-read-view--selected-month'),
+		daySelected: () => cy.get('[class*="react-datepicker__day--selected "]'),
+		timeBoxList: () => cy.get('.react-datepicker__time-box > ul >li'),
+		timeSelected: () => cy.get('[class*="react-datepicker__time-list-item--selected"]'),
 	};
 
-	clicklistOfYear() {
-		cy.get('[class="react-datepicker__year-select"').trigger('click');
+	clickSelectDatePicker() {
+		return this.get.selectDatePicker().click({ force: true });
 	}
-	checkValuesYear() {
-		let arrayYear = [];
-		cy.get('.react-datepicker__year-select').within(() => {
-			cy.get('option').each(($option, index) => {
-				cy.wrap($option)
-					.invoke('text')
-					.should('be.a', 'string')
-					.then(parseInt)
-					.should('be.a', 'number')
-					.then(valor => {
-						arrayYear.push(valor);
-						expect(array[index]).to.be.equal(1900 + index);
+	clicklistOfYear() {
+		this.get.yearInput().trigger('click');
+	}
+
+	getDatePickerValue() {
+		return this.get
+			.selectDatePicker()
+			.invoke('val')
+			.then($valor => {
+				Cypress.env('date', $valor);
+			});
+	}
+	getDateAndTimePickerValue() {
+		return this.get
+			.dateAndTimePicker()
+			.invoke('val')
+			.then($valor => {
+				Cypress.env('dateAndTime', $valor);
+			});
+	}
+	clickDateAndTimePicker() {
+		this.get.dateAndTimePicker().click({ force: true });
+	}
+	clickYearList() {
+		this.get.yearList().click();
+	}
+	clickMonthDropdown() {
+		return this.get.monthDropdown().click();
+	}
+	clickTimeBoxList() {
+		this.get.timeBoxList().eq(5).click();
+	}
+	clickPreviousMonthDate() {
+		return datePicker.get
+			.previousMonth()
+			.click({ force: true })
+			.then(() => {
+				cy.get('.react-datepicker__month-select')
+					.invoke('val')
+					.then($valor2 => {
+						cy.wrap($valor2)
+							// el valor = 3 ... data.mon
+							.should('be.a', 'string')
+							.then(parseInt)
+							.should('be.a', 'number')
+							.then($texto2 => {
+								Cypress.env('text2', $texto2);
+							});
 					});
 			});
-		});
+	}
+	clickNextMonthDate() {
+		return datePicker.get
+			.nextMonth()
+			.click({ force: true })
+			.then(() => {
+				cy.get('.react-datepicker__month-select')
+					.invoke('val')
+					.then($valor2 => {
+						cy.wrap($valor2)
+							// el valor = 3 ... data.mon
+							.should('be.a', 'string')
+							.then(parseInt)
+							.should('be.a', 'number')
+							.then($texto2 => {
+								Cypress.env('text2', $texto2);
+							});
+					});
+			});
+	}
+	clickNextMonthDT() {
+		return datePicker.get
+			.nextMonth()
+			.click({ force: true })
+			.then(() => {
+				datePicker.get
+					.printedMonth()
+					.invoke('text')
+					.then($texto => {
+						Cypress.env('text', $texto);
+					});
+			});
+	}
+	clickPreviousMonthDT() {
+		return datePicker.get
+			.previousMonth()
+			.click({ force: true })
+			.then(() => {
+				datePicker.get
+					.printedMonth()
+					.invoke('text')
+					.then($texto => {
+						Cypress.env('text', $texto);
+					});
+			});
 	}
 }
 
