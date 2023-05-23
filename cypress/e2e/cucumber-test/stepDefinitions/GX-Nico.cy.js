033d9b84ca64f99ca01ef1c550aa4f8e36f7ea1f
Nico950 <nicotest950@gmail.com>
2023-05-23 11:16:05 -0300

    2062 lest go

--- cypress/e2e/cucumber-test/stepDefinitions/GX-Nico.cy.js
+++
@@ -1,9 +0,0 @@
-describe("prueba", () => {
-    beforeEach(() =>{
-      cy.visit('https://demoqa.com/webtables')
-    }) 
-  
-    it('test', () => {
-        cy.get('.rt-tbody').should('exist')
-    })
-})
