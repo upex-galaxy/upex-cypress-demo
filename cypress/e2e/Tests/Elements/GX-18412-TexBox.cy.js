136562e32aa126906a482a718c924a2b10b49bf5
Barby <barbaravaz29@gmail.com>
2023-05-24 22:36:19 -0300

    3eroooooooooooo

--- cypress/e2e/Tests/Elements/GX-18412-TexBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-18412-TexBox.cy.js
@@ -3,3 +3,18 @@ describe('ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 		cy.visit('https://demoqa.com/text-box');
 	});
 });
+
+it('GX-18413 | TC1: Validar completar formulario con todos los campos completos.', () => {
+	cy.fixture('data/aca va el archivo de fixture nuestro json').then(the => {
+		cy.get(the.input.FullName).type(the.data.FullName);
+		cy.get(the.input.Email).type(the.data.Email);
+		cy.get(the.input.CurrentAddress).type(the.data.CurrentAddress);
+		cy.get(the.input.PermanentAddress).type(the.data.PermanentAddress);
+		cy.get(the.SubmitButton).click();
+
+		cy.get(the.assertions.FullName).should('be.visible');
+		cy.get(the.assertions.Email).should('be.visible');
+		cy.get(the.assertions.CurrentAddress).should('be.visible');
+		cy.get(the.assertions.PermanentAddress).should('be.visible');
+	});
+});
