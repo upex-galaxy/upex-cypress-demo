f7b96095f898fb4c08f21353b8623c661b2c6bd5
Barby <barbaravaz29@gmail.com>
2023-05-24 21:28:17 -0300

    empezando con mis validaciones, primer commit de mi US

---
+++ cypress/e2e/Tests/Elements/GX-18412-TexBox.cy.js
@@ -0,0 +1,5 @@
+describe('ToolsQA | Elements | Text Box: Fill form and Submit', () => {
+	beforeEach('Estar situados en ToolQA TExt-box', () => {
+		cy.visit('https://demoqa.com/text-box');
+	});
+});
