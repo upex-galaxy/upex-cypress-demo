967f88e9dd1feb035c32afb25bd0dc93db7de2c4
Barby <barbaravaz29@gmail.com>
2023-05-25 15:35:52 -0300

    7 casos

--- cypress/e2e/Tests/Elements/GX-18412-TexBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-18412-TexBox.cy.js
@@ -1,20 +1,43 @@
-describe('ToolsQA | Elements | Text Box: Fill form and Submit', () => {
-	beforeEach('Estar situados en ToolQA TExt-box', () => {
+describe('GX 18412 : ToolsQA | Elements | Text Box: Fill form and Submit', () => {
+	beforeEach('Precondición: estar situados en toolQA text box', () => {
 		cy.visit('https://demoqa.com/text-box');
 	});
-});
+	it('18413 | TC1: Validar completar formulario con todos los campos completos', () => {
+		cy.get('#userName').type('Barby');
+		cy.get('#userEmail').type('bar@gmail.com');
+		cy.get('#currentAddress').type('Av Congreso');
+		cy.get('#permanentAddress').type('Villa Urquiza 5234');
+		cy.get('#submit').click();
+	});
 
-it('GX-18413 | TC1: Validar completar formulario con todos los campos completos.', () => {
-	cy.fixture('data/aca va el archivo de fixture nuestro json').then(the => {
-		cy.get(the.input.FullName).type(the.data.FullName);
-		cy.get(the.input.Email).type(the.data.Email);
-		cy.get(the.input.CurrentAddress).type(the.data.CurrentAddress);
-		cy.get(the.input.PermanentAddress).type(the.data.PermanentAddress);
-		cy.get(the.SubmitButton).click();
+	it('18413 | TC2: Validar completar formulario con todos los campos vacíos', () => {
+		cy.get('#submit').click();
+	});
 
-		cy.get(the.assertions.FullName).should('be.visible');
-		cy.get(the.assertions.Email).should('be.visible');
-		cy.get(the.assertions.CurrentAddress).should('be.visible');
-		cy.get(the.assertions.PermanentAddress).should('be.visible');
+	it('18413 | TC3: Validar completar formulario con Full Name vacio, Email: maqueta correcta, Current Address: vacío, Permanent Address: vacío', () => {
+		cy.get('#userEmail').type('bar@gmail.com');
+		cy.get('#submit').click();
+	});
+
+	it('18413 | TC4: Validar completar formulario con Full Name vacio, Email: maqueta incorrecta , Current Address: vacío, Permanent Address: vacío.', () => {
+		cy.get('#userName').type('Barby');
+		cy.get('#userEmail').type('bar@gmail.+');
+
+		cy.get('#submit').click();
+	});
+	it('18413 | TC5: Validar completar formulario con Full Name vacio, Email: maqueta correcta, Current Address: vacío, Permanent Address: vacío', () => {
+		cy.get('#userEmail').type('bar@gmail.com');
+		cy.get('#submit').click();
+	});
+	it('18413 | TC6: Validar completar formulario con Full Name completo, Email: maqueta correcta, Current Address: vacío, Permanent Address: vacío.', () => {
+		cy.get('#userName').type('Barby');
+		cy.get('#userEmail').type('bar@gmail.com');
+		cy.get('#submit').click();
+	});
+	it('18413 | TC7: Validar completar formulario con Full Name completo, Email: maqueta correcta, Current Address: completo, Permanent Address: vacío.', () => {
+		cy.get('#userName').type('Barby');
+		cy.get('#userEmail').type('bar@gmail.com');
+		cy.get('#currentAddress').type('Av Congreso');
+		cy.get('#submit').click();
 	});
 });
