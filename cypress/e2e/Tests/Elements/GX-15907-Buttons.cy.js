d797544f09f49e255633b1901f449de75f2464d3
Barby <barbaravaz29@gmail.com>
2023-05-25 18:17:25 -0300

    terminados los TC

--- cypress/e2e/Tests/Elements/GX-15907-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-15907-Buttons.cy.js
@@ -6,17 +6,14 @@ describe('GX-15907 | ToolsQA | Elements | Buttons', () => {
 	it('15908 | TC1 Validar doble click en btn "Double Click Me"', () => {
 		cy.get('#doubleClickBtn').dblclick();
 		cy.contains('You have done a double click').should('exist');
-		cy.wait(3000);
 	});
-	it('15908 | TC2 Validar  click en btn "Right Click"', () => {
+	it('15908 | TC2 Validar click con boton derecho en btn "Right Click"', () => {
 		cy.get('#rightClickBtn').rightclick();
 		cy.contains('You have done a right click').should('exist');
-		cy.wait(3000);
 	});
-	it('15908 | TC3 Validar  click en btn "Click Me"', () => {
+	it('15908 | TC3 Validar click en btn "Click Me"', () => {
 		cy.get('.btn.btn-primary').eq(2).click();
 		cy.contains('You have done a dynamic click').should('be.visible');
-		cy.wait(3000);
 	});
 });
 import { removeLogs } from '@helper/RemoveLogs';
