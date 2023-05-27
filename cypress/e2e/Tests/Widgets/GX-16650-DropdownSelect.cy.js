7030a4286e24f6eb50f4bfb30a0c8f53aedbb64c
VickyNovarese <mariavirginianovarese@gmail.com>
2023-05-26 21:41:21 -0300

    GX-16650-✅-tools-qa-widgets-dropdown-select-menu

--- cypress/e2e/Tests/Widgets/GX-16650-DropdownSelect.cy.js
+++ cypress/e2e/Tests/Widgets/GX-16650-DropdownSelect.cy.js
@@ -1,17 +1,17 @@
 const { removeLogs } = require('@helper/RemoveLogs');
 
-describe('This is your test project title', () => {
+describe('GX-16650|✅ToolsQA | Widgets | Dropdown - Select Menu', () => {
 	beforeEach(() => {
 		cy.visit('https://demoqa.com/select-menu');
 	});
 
-	it('Validar seleccionar una opción group 1', () => {
+	it('Validar seleccionar una opción del select value', () => {
 		cy.get('.css-tlfecz-indicatorContainer').eq(0).click();
 		cy.get('#react-select-2-group-0-heading').click(); //.should('contain', 'Group 1, option 1');
 		cy.should('contain', 'Group');
 		//.should()
 	});
-	it('Validar seleccionar una opción select one', () => {
+	it('Validar seleccionar una opción del select one', () => {
 		cy.get('.css-tlfecz-indicatorContainer').eq(1).click();
 		cy.get('#react-select-3-option-0-1').click(); //.should('contain', 'Group 1, option 1');
 		cy.get('.css-1uccc91-singleValue').contains('Mr.').should('be.visible');
@@ -23,13 +23,22 @@ describe('This is your test project title', () => {
 		cy.get('select option[value="8"]').should('be.selected').should('be.visible'); //cy.contains('Indigo').should('be.visible');
 		// Write your test case two here
 	});
-	it('Validar seleccionar varias opciones del drop down', () => {
+	it('Validar seleccionar varias opciones del Multiselect drop down', () => {
 		cy.get('.css-tlfecz-indicatorContainer').eq(2).click();
-		// Write your test case two here
+
+		cy.get('#react-select-4-option-0').click();
+		cy.get('#react-select-4-option-1').click();
+		cy.get('#react-select-4-option-2').click();
+		cy.get('#react-select-4-option-3').click();
+		//cy.get('.css-2b097c-container').should('be.visible');
+		cy.get('.css-12jo7m5').should('be.visible');
+		cy.get('.css-1gl4k7y').should('be.visible');
 	});
-	it('Validar seleccionar varias opciones del option button select.', () => {
-		// Write your test case two herValidar seleccionar varias opciones del option button select. two title', () => {
-		// Write your test case two here
+	it('Validar seleccionar varias opciones del stantard multiselect', () => {
+		cy.get('#cars').select('volvo').should('be.visible');
+		cy.get('#cars').select('saab').should('be.visible');
+		cy.get('#cars').select('opel').should('be.visible');
+		cy.get('#cars').select('audi').should('be.visible');
 	});
 });
 removeLogs();
