25b345fc0ef9b2f5cf3d45597a2c4b97b9ad9e8d
Barby <barbaravaz29@gmail.com>
2023-06-05 10:40:42 -0300

    GX-19275 modifique linea duplicada en CI

--- cypress/e2e/Tests/Elements/GX-19275-Dynamic.cy.js
+++ cypress/e2e/Tests/Elements/GX-19275-Dynamic.cy.js
@@ -4,7 +4,6 @@ removeLogs();
 describe('GX-19275: ToolsQA | Elements | Dynamic Properties', () => {
 	beforeEach('Precondición: Usuario debe estar situado en Dynamic properties page', () => {
 		cy.visit('/dynamic-properties');
-		cy.url().should('contain', 'demoqa');
 	});
 
 	it('19276 | TC1: Validar elemento de texto', () => {
@@ -13,7 +12,7 @@ describe('GX-19275: ToolsQA | Elements | Dynamic Properties', () => {
 			.should('have.attr', 'id'); // Validar que el elemento anterior tiene un atributo "id"
 	});
 
-	it('19276 | TC2: Validar habilitación del elemento después de 5 segundos', () => {
+	it.only('19276 | TC2: Validar habilitación del elemento después de 5 segundos', () => {
 		cy.get('button#enableAfter.mt-4.btn.btn-primary').click(); // Interactuar con el botón
 		cy.get('button#enableAfter.mt-4.btn.btn-primary').should('be.disabled', { timeout: 5000 }); // Verificar que el botón esté inicialmente deshabilitado
 		cy.get('button#enableAfter.mt-4.btn.btn-primary').should('be.enabled'); // Verificar que el botón esté habilitado después de 5 segundos
@@ -25,7 +24,7 @@ describe('GX-19275: ToolsQA | Elements | Dynamic Properties', () => {
 		cy.get('.mt-4.text-danger.btn.btn-primary').contains('Color Change'); // Validar cambio de color del elemento
 	});
 
-	it('19276 | TC4: Validar visibilidad del elemento después de 5 segundos ', () => {
+	it.only('19276 | TC4: Validar visibilidad del elemento después de 5 segundos ', () => {
 		cy.get('#visibleAfter').should('exist'); // Verificar que el elemento esté presente en el DOM
 		cy.get('#visibleAfter').should('not.be.visible', { timeout: 5000 }); // Verificar que el elemento no esté visible
 		cy.get('#visibleAfter').should('be.visible'); // Validar visibilidad del elemento después de 5 segundos
