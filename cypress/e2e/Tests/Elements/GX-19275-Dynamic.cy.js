d03c809d913e94c8776c176c35387d7fd48d2bac
Barby <barbaravaz29@gmail.com>
2023-06-05 00:07:04 -0300

    GX-19275 actualicé repo local

--- cypress/e2e/Tests/Elements/GX-19275-Dynamic.cy.js
+++ cypress/e2e/Tests/Elements/GX-19275-Dynamic.cy.js
@@ -1,40 +1,46 @@
 describe('GX-19275: ToolsQA | Elements | Dynamic Properties', () => {
 	beforeEach('Precondición: Usuario debe estar situado en Dynamic properties page', () => {
 		cy.visit('https://demoqa.com/dynamic-properties');
-		cy.url().should('contain', 'dynamic');
+	});
+
+	it('19276 | TC1: Validar elemento de texto', () => {
+		cy.get('#enableAfter') // Seleccionar el elemento que debe estar después
+			.prev() // Seleccionar el elemento anterior
+			.should('have.attr', 'id'); // Validar que el elemento anterior tiene un atributo "id"
+	});
+
+	it('19276 | TC2: Validar habilitación del elemento después de 5 segundos', () => {
+		cy.get('#enableAfter').click();
+		cy.get('#enableAfter').should('not.be.enabled'); //Validar elemento no activo
+		cy.wait(5000); // Esperar 5 segundos
+		cy.get('#enableAfter').should('be.enabled'); // Validar elemento activo después de 5 segundos
+	});
+
+	it('19276 | TC3: Validar cambio de color del elemento despues de 5 segundos', () => {
+		cy.get('.mt-4.btn.btn-primary').contains('Color Change'); //color inicial
+		cy.wait(5000); // Esperar 5 segundos
+		cy.get('.mt-4.text-danger.btn.btn-primary').contains('Color Change'); // Validar cambio de color del elemento
+	});
+
+	it.only('19276 | TC4: Validar visibilidad del elemento después de 5 segundos ', () => {
+		cy.get('#visibleAfter').should('not.be.visible'); // Validar no visibilidad del elemento antes de 5 segundos
+		cy.wait(5000); // Esperar 5 segundos
+		cy.get('#visibleAfter').should('be.visible'); // Validar visibilidad del elemento después de 5 segundos
 	});
 });
 
-it.only('19276 | TC1: Validar elemento de texto', () => {
-	// Validar obtención de elemento con ID dinámico sin usar "texto"
-	cy.get('#enableAfter') // Seleccionar el elemento que debe estar después
-		.prev() // Seleccionar el elemento anterior
-		.should('have.attr', 'id'); // Validar que el elemento anterior tiene un atributo "id"
+//________________________________________________________________________
+// Comando predeterminado para que no ocurran errores de excepciones:
+Cypress.on('uncaught:exception', () => {
+	// returning false here prevents Cypress from
+	// failing the test
+	return false;
 });
-
-it.only('19276 | TC2: Validar que el botón "Will enable 5 Sec" no se puede hacer clic antes de 5 segundos', () => {
-	cy.get('#enableAfter').should('not.be.enabled'); // Validar habilitación del elemento después de 5 segundos
-});
-
-it('19276 | TC3: Validar que el botón "Will enable 5 Sec" se puede hacer clic después de 5 segundos', () => {
-	cy.wait(5000); // Esperar 5 segundos
-	cy.get('#enableAfter').should('be.enabled'); // Validar habilitación del elemento después de 5 segundos
-});
-
-it('19276 | TC4: Validar que el botón "Color Change" tiene texto de color blanco antes de 5 segundos', () => {
-	cy.get('#colorChange').should('have.css', 'color', 'rgb(0, 0, 0)'); // Validar cambio de color del elemento
-});
-
-it('19276 | TC5: Validar que el botón "Color Change" tiene texto de color rojo después de 5 segundos', () => {
-	cy.wait(5000); // Esperar 5 segundos
-	cy.get('#colorChange').should('have.css', 'color', 'rgb(255, 0, 0)'); // Validar cambio de color del elemento
-});
-
-it('19276 | TC6: Validar que el botón "Visible after 5 sec" no es visible antes de 5 segundos', () => {
-	cy.get('#visibleAfter').should('not.be.visible'); // Validar visibilidad del elemento después de 5 segundos
-});
-
-it('19276 | TC7: Validar que el botón "Visible after 5 sec" es visible después de 5 segundos', () => {
-	cy.wait(5000); // Esperar 5 segundos
-	cy.get('#visibleAfter').should('be.visible'); // Validar visibilidad del elemento después de 5 segundos
-});
+// Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
+const origLog = Cypress.log;
+Cypress.log = function (opts, ...other) {
+	if (opts.displayName === 'xhr' || (opts.displayName === 'fetch' && opts.url.startsWith('https://'))) {
+		return;
+	}
+	return origLog(opts, ...other);
+};
