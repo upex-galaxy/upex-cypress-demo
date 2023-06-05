80764a8091c75b59af0ee12e9f5a439f5952375e
Barby <barbaravaz29@gmail.com>
2023-06-05 09:31:32 -0300

    GX-19275 se clonó nuevamente repo por actualizacion de release

---
+++ cypress/e2e/Tests/Elements/GX-19275-Dynamic.cy.js
@@ -0,0 +1,35 @@
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
+
+describe('GX-19275: ToolsQA | Elements | Dynamic Properties', () => {
+	beforeEach('Precondición: Usuario debe estar situado en Dynamic properties page', () => {
+		cy.visit('/dynamic-properties');
+		cy.url().should('contain', 'demoqa');
+	});
+
+	it('19276 | TC1: Validar elemento de texto', () => {
+		cy.get('#enableAfter') // Seleccionar el elemento que debe estar después
+			.prev() // Seleccionar el elemento anterior
+			.should('have.attr', 'id'); // Validar que el elemento anterior tiene un atributo "id"
+	});
+
+	it.only('19276 | TC2: Validar habilitación del elemento después de 5 segundos', () => {
+		cy.get('#enableAfter').should('exist');
+		cy.get('#enableAfter').should('be.disabled'); // Verificar que el botón esté inicialmente deshabilitado
+		cy.get('#enableAfter').click(); // Interactuar con el botón
+		cy.get('#enableAfter').should('not.be.enabled', { timeout: 5000 }); // Verificar que el botón esté deshabilitado después de la interacción
+		cy.get('#enableAfter').should('be.enabled'); // Validar elemento activo después de 5 segundos
+	});
+
+	it('19276 | TC3: Validar cambio de color del elemento después de 5 segundos', () => {
+		cy.get('.mt-4.btn.btn-primary').contains('Color Change'); //color inicial
+		cy.wait(5000); // Esperar 5 segundos
+		cy.get('.mt-4.text-danger.btn.btn-primary').contains('Color Change'); // Validar cambio de color del elemento
+	});
+
+	it('19276 | TC4: Validar visibilidad del elemento después de 5 segundos ', () => {
+		cy.get('#visibleAfter').should('exist'); // Verificar que el elemento esté presente en el DOM
+		cy.get('#visibleAfter').should('not.be.visible', { timeout: 5000 }); // Verificar que el elemento no esté visible
+		cy.get('#visibleAfter').should('be.visible'); // Validar visibilidad del elemento después de 5 segundos
+	});
+});
