f19eb2f2063a75a0c75296de7ecc2080c07b8be8
@alberto.r <alberto.r@tugerente.com>
2023-06-11 09:50:32 -0600

    GX-19713 Happy path completado con calendario y carga de imagen

--- cypress/e2e/Tests/Suites/GX-19713-practiceForm.cy.js
+++ cypress/e2e/Tests/Suites/GX-19713-practiceForm.cy.js
@@ -1,29 +1,25 @@
 import { faker } from '@faker-js/faker';
-import { pom } from '@pages/Pages2/POM';
+import { pom } from '@pages/Pages2/GX-19713-practiceForm.Page';
 import { removeLogs } from '@helper/RemoveLogs';
 
 describe('GX-19713 | Happy Path', () => {
-
 	before(() => {
 		cy.clearLocalStorage();
 		cy.clearCookies();
 	});
 
 	beforeEach(() => {
-		
 		cy.visit('https://demoqa.com/automation-practice-form');
 		cy.url().should('contain', '/automation-practice-form');
-			
 	});
 
 	it('TC01: Happy Path Test Tools', () => {
-
 		const randomName = faker.internet.userName();
 		const randomLastName = faker.internet.userName();
 		const randomEmail = faker.internet.email();
 		const randomNumber = faker.phone.number('##########');
 		const randomGender = faker.datatype.number({ min: 0, max: 2 });
-		const randomSubject = faker.random.alpha({ bannedChars: ['x','y','q','j','f','w','z'] });   
+		const randomSubject = faker.random.alpha({ bannedChars: ['x', 'y', 'q', 'j', 'f', 'w', 'z'] });
 		const randomHobbie = faker.datatype.number({ min: 3, max: 5 });
 		const randomAddress = faker.address.city();
 		const state = 'NCR';
@@ -38,24 +34,27 @@ describe('GX-19713 | Happy Path', () => {
 		pom.typeEmail(randomEmail);
 		pom.get.inputMail().should('have.value', randomEmail);
 
+		pom.clickDate();
+
 		pom.clickGenero(randomGender);
 
 		pom.typeNumber(randomNumber);
 		pom.get.inputNumber().should('have.value', randomNumber);
 
 		pom.typeSubject(randomSubject);
-	
+
 		pom.clickHobbie(randomHobbie);
 
 		pom.clickUpload();
-	
+
 		pom.typeAddress(randomAddress);
 		pom.get.inputAddress().should('have.value', randomAddress);
-		
+
 		pom.typeState(state);
 
 		pom.typeCity(city);
 
+		pom.clickSubmit();
 	});
 });
 removeLogs();
