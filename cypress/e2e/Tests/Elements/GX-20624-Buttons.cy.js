e9bc5b496a31b031024c4432c3441a7b66b20b63
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-15 20:21:44 -0400

    GX-20624 obtained double click element and did the assertion of TC1.

--- cypress/e2e/Tests/Elements/GX-20624-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-20624-Buttons.cy.js
@@ -1,18 +1,28 @@
 describe('GX-6424 | TC1: Validate that button “double click me', () => {
 	beforeEach('Precondition: Having access to the SUT ', () => {
-		cy.visit('https://demoqa.com/buttons');
+		cy.visit('/buttons');
 		cy.viewport(1920, 1980);
-    });
-    it('GX-6424 | TC1: Validate that button “double click me” displays message.', () =>
-    {
-        cy.get('[href="/commands/querying"]')
-            .click('#doubleClickBtn'); // double click??   
-        .should('have.attr', 'type') // ddd
-	})
-})
+	});
+	it('GX-6424 | TC1: Validate that button “double click me” displays message.', () => {
+		cy.get('#doubleClickBtn').dblclick();
+		cy.get('#doubleClickMessage').should('have.text', 'You have done a double click');
+	});
+
+	//it('GX-6424 | TC2: Validate that button “right click me” works correctly.', () => {
+	//cy.get('[href="/commands/querying"]').eq(2).click();
+	//cy.get('#query-btn') // Sintaxis Comando Get de Selector de IDs
+	//	.click(); // Esto es un método de Interacción (Acción)
+	//});
+	//it('GX-6424 | TC3: Validate that button “click me” works correctly.', () => {
+	//	cy.get('[href="/commands/querying"]').eq(2).click();
+	//	cy.get('#query-btn').click();
+	//	cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
+	//		.should('contain.text', 'Button'); // Assertion BDD para validar resultado esperado (de muchas formas)
+	//});
+});
 
 //Se importa la funcion
-import { removeLogs } from "@helper/RemoveLogs";
+import { removeLogs } from '@helper/RemoveLogs';
 
 //Se ejecuta la funcion para evitar los errores (puedes hacerlo al final de tu codigo)
-removeLogs()
+removeLogs();
