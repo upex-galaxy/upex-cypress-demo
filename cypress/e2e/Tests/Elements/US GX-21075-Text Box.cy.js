57893e8905dc096fce526d55ca2ee8e4f0e18711
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-19 12:24:43 -0400

    test cases created

---
+++ cypress/e2e/Tests/Elements/US GX-21075-Text Box.cy.js
@@ -0,0 +1,35 @@
+describe('US GX-21075 | TS: ✅ToolsQA | Elements | Text Box: Fill form and Submit', () => {
+	beforeEach('Having access to the SUT.', () => {
+		cy.visit('/text-box');
+	});
+
+	it('GX-21076 | TC1: Validate all fields are submitted with validate data and string is displayed.', () => {
+		cy.get('[href="/commands/querying"]').eq(2).click();
+	});
+
+	it('GX-21076 | TC2: Validate all fields are submitted with empty data and no string is displayed.', () => {
+		cy.get('[href="/commands/querying"]').eq(2).click();
+		cy.get('#query-btn') // Sintaxis Comando Get de Selector de IDs
+			.click(); // Esto es un método de Interacción (Acción)
+	});
+
+	it('GX-21076 | TC3: Validate field “email” is submitted with invalid data and red border is displayed.', () => {
+		cy.get('[href="/commands/querying"]').eq(2).click();
+		cy.get('#query-btn').click();
+		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
+			.should('contain.text', 'Button'); // Assertion BDD para validar resultado esperado (de muchas formas)
+	});
+
+	it('GX-21076 | TC4:  Validate field “email” is submitted with empty data and no log message is displayed.', () => {
+		cy.get('[href="/commands/querying"]').eq(2).click();
+		cy.get('#query-btn').click();
+		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
+			.should('contain.text', 'Button');
+	});
+});
+
+//Se importa la funcion
+import { removeLogs } from '@helper/RemoveLogs';
+
+//Se ejecuta la funcion para evitar los errores (puedes hacerlo al final de tu codigo)
+removeLogs();
