bf3689f580e91f4aa7cf7834dd224b3e67c1fc2a
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-19 17:38:37 -0400

    fixtures folder added and tc 2 in progress

--- cypress/e2e/Tests/Elements/US GX-21075-Text Box.cy.js
+++ cypress/e2e/Tests/Elements/US GX-21075-Text Box.cy.js
@@ -9,25 +9,22 @@ describe('US GX-21075 | TS: ✅ToolsQA | Elements | Text Box: Fill form and Subm
 		cy.get('#currentAddress').type('Caracas, Venezuela');
 		cy.get('#permanentAddress').type('The World');
 		cy.get('#submit').click();
-		cy.contains('Name:Upex');
-		cy.contains('Email:hola@gmail.com');
-		cy.contains('Current Address :Caracas, Venezuela');
-		cy.contains('Permananet Address :The World');
+		cy.contains('Name:Upex').should('be.visible');
+		cy.contains('Email:hola@gmail.com').should('be.visible');
+		cy.contains('Current Address :Caracas, Venezuela').should('be.visible');
+		cy.contains('Permananet Address :The World').should('be.visible');
 	});
 
-	//	it('GX-21076 | TC2: Validate all fields are submitted with empty data and no string is displayed.', () => {
-	//		cy.get('[href="/commands/querying"]').eq(2).click();
-	//		cy.get('#query-btn') // Sintaxis Comando Get de Selector de IDs
-	//			.click(); // Esto es un método de Interacción (Acción)
-	//	});
-	//
-	//	it('GX-21076 | TC3: Validate field “email” is submitted with invalid data and red border is displayed.', () => {
-	//		cy.get('[href="/commands/querying"]').eq(2).click();
-	//		cy.get('#query-btn').click();
-	//		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
-	//			.should('contain.text', 'Button'); // Assertion BDD para validar resultado esperado (de muchas formas)
-	//	});
-	//
+	it('GX-21076 | TC2: Validate all fields are submitted with empty data and no string is displayed.', () => {
+		cy.get('#submit').click();
+	});
+
+	//it('GX-21076 | TC3: Validate field “email” is submitted with invalid data and red border is displayed.', () => {
+	//	cy.get('[type="email"]').type('holagmail.com');
+	//	cy.get('#submit').click();
+	//	cy.contains('')
+	//});
+
 	//	it('GX-21076 | TC4:  Validate field “email” is submitted with empty data and no log message is displayed.', () => {
 	//		cy.get('[href="/commands/querying"]').eq(2).click();
 	//		cy.get('#query-btn').click();
