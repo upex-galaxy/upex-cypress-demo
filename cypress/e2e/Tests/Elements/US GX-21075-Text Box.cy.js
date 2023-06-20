b8ad4d85de5b245bd86797f602cd40600d89afed
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-19 21:37:38 -0400

    TC2 ready

--- cypress/e2e/Tests/Elements/US GX-21075-Text Box.cy.js
+++ cypress/e2e/Tests/Elements/US GX-21075-Text Box.cy.js
@@ -2,32 +2,41 @@ describe('US GX-21075 | TS: ✅ToolsQA | Elements | Text Box: Fill form and Subm
 	beforeEach('Having access to the SUT.', () => {
 		cy.visit('/text-box');
 	});
-});
 
-it('GX-21076 | TC1: Validate all fields are submitted with validate data and string is displayed.', () => {
-	cy.fixture('GX-21075/ToolsQA-TextBox.Page').then(the => {
-		cy.get(the.FullName.input).type(the.fullname.data.valid);
-		cy.get(the.email.input).type(the.email.data.valid);
-		cy.get(the.CurrentAddress.input).type(the.CurrentAddress.data.valid);
-		cy.get(the.PermanentAddress.input).type(the.PermanentAddress.data.valid);
-		cy.get(the.SubmitButton).click();
+	it('GX-21076 | TC1: Validate all fields are submitted with validate data and string is displayed.', () => {
+		cy.fixture('GX-21075/ToolsQA-TextBox.Page').then(the => {
+			cy.get(the.FullName.input).type(the.FullName.data.valid);
+			cy.get(the.email.input).type(the.email.data.valid);
+			cy.get(the.CurrentAddress.input).type(the.CurrentAddress.data.valid);
+			cy.get(the.PermanentAddress.input).type(the.PermanentAddress.data.valid);
+			cy.get(the.SubmitButton).click();
+		});
+	});
+
+	it('GX-21076 | TC2: Validate all fields are submitted with empty data and no string is displayed.', () => {
+		cy.fixture('GX-21075/ToolsQA-TextBox.Page').then(the => {
+			cy.get(the.SubmitButton).click();
+		});
+	});
+
+	it('GX-21076 | TC3: Validate field “email” is submitted with invalid data and red border is displayed.', () => {
+		cy.fixture('GX-21075/ToolsQA-TextBox.Page').then(the => {
+			cy.get(the.email.input).type(the.email.data.invalid);
+			cy.get(the.SubmitButton).click();
+			cy.contains('');
+		});
+
+		//	it('GX-21076 | TC4:  Validate field “email” is submitted with empty data and no log message is displayed.', () => {
+		//		cy.get('[href="/commands/querying"]').eq(2).click();
+		//		cy.get('#query-btn').click();
+		//		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
+		//			.should('contain.text', 'Button');
+		///	}
 	});
 });
 
-//it('GX-21076 | TC2: Validate all fields are submitted with empty data and no string is displayed.', () => {
-//	cy.get('#submit').click();
-//
-//});
+//Se importa la funcion
+import { removeLogs } from '@helper/RemoveLogs';
 
-//it('GX-21076 | TC3: Validate field “email” is submitted with invalid data and red border is displayed.', () => {
-//	cy.get('[type="email"]').type('holagmail.com');
-//	cy.get('#submit').click();
-//	cy.contains('')
-//});
-
-//	it('GX-21076 | TC4:  Validate field “email” is submitted with empty data and no log message is displayed.', () => {
-//		cy.get('[href="/commands/querying"]').eq(2).click();
-//		cy.get('#query-btn').click();
-//		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
-//			.should('contain.text', 'Button');
-///	}
+//Se ejecuta la funcion para evitar los errores (puedes hacerlo al final de tu codigo)
+removeLogs();
