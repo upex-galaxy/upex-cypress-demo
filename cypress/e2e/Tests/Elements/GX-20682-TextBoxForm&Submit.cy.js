5032d36b1c580a7627b99a578944ead9a6bda49b
Juliako <julikq22@gmail.com>
2023-06-21 18:55:22 -0300

    GX-20682-✅-tools-qa-elements-text-box-fill-form-and-submit {Se ha procedido a diseñar 3 casos de prueba y a agregar data en el archivo fixture}

--- cypress/e2e/Tests/Elements/GX-20682-TextBoxForm&Submit.cy.js
+++ cypress/e2e/Tests/Elements/GX-20682-TextBoxForm&Submit.cy.js
@@ -1,12 +1,37 @@
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
+
 describe('ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 	beforeEach('The user must navigate to the endpoint under test', () => {
 		cy.visit('https://demoqa.com/text-box');
 		cy.url().should('contain', 'text-box');
 	});
 
-	it('20683 | TC1: Validar que NO se muestra ningún mensaje cuando los campos ”Full Name”, “Current Address“ y “Permanent Address“ estén vacíos al enviar el formulario', () => {});
-	it('20683 | TC2: Validar que el mensaje es mostrado cuando los campos ”Full Name”, “Current Address“ y “Permanent Address“ estén completados al enviar el formulario', () => {});
-	it('20683 | TC3: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido', () => {});
+	it('20683 | TC1: Validar que NO se muestra ningún mensaje cuando los campos ”Full Name”, “Current Address“ y “Permanent Address“ estén vacíos al enviar el formulario', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.email).click().type(the.data.validEmailAddress);
+			cy.get(the.input.submitButton).click();
+			cy.get('.mb-1').should('have.text', 'Email:julian.battaglini@upexgalaxy.com');
+		});
+	});
+	it('20683 | TC2: Validar que el mensaje es mostrado cuando los campos ”Full Name”, “Current Address“ y “Permanent Address“ estén completados al enviar el formulario', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.fullName).click().type(the.data.user);
+			cy.get(the.input.currentAddress).click().type(the.data.firstAddress);
+			cy.get(the.input.permanentAddress).click().type(the.data.secondAddress);
+			cy.get(the.input.submitButton).click();
+			cy.get('.mb-1').should('contain.text', 'Name', 'Current Address', 'Permanent Address');
+		});
+	});
+	it('20683 | TC3: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.fullName).click().type(the.data.user);
+			cy.get(the.input.currentAddress).click().type(the.data.firstAddress);
+			cy.get(the.input.permanentAddress).click().type(the.data.secondAddress);
+			cy.get(the.input.submitButton).click();
+			cy.get('.mb-1').should('contain.text', 'Name', 'Current Address', 'Permanent Address');
+		});
+	});
 	it('20683 | TC4: Validar que el formulario pueda ser enviado si el campo  ”Email” está vacío', () => {});
 	it('20683 | TC5: Validar que el formulario pueda ser enviado cuando el campo  ”Email” es válido', () => {});
 });
