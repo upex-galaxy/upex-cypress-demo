fe8a310586cc21bb13b00adaa3e0124005f2692e
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-21 18:30:46 -0400

    tc1 progress

---
+++ cypress/e2e/Tests/Elements/GX-21318-login-saucedemo.cy.js
@@ -0,0 +1,7 @@
+describe('GX-21318-✅-swag-labs-account-iniciar-sesion-y-br-de-accesos', () => {
+	beforeEach('Having access to the SUT', () => {
+		cy.visit('https://www.saucedemo.com/');
+	});
+
+	it('GX-21319 | TC1: Validate user can log in with username “standard_user” successfully.');
+});
