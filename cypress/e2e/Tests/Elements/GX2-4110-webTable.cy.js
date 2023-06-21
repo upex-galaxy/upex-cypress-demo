125faf14ea7c0f5a0ed47cbe14e17c340973363a
Jor Abdo <jorabdo1991@gmail.com>
2023-06-20 19:02:05 -0500

    Creando TC, modificando CI suite

---
+++ cypress/e2e/Tests/Elements/GX2-4110-webTable.cy.js
@@ -0,0 +1,199 @@
+import { web } from '@pages/Elements/GX2-4110-webTables';
+import { faker } from '@faker-js/faker';
+import { removeLogs } from '@helper/RemoveLogs';
+
+removeLogs();
+describe('GX2-4110 | ToolsQA | Elements | Web Table', () => {
+	beforeEach('User must be situated in the website', () => {
+		cy.visit('/webtables');
+	});
+	it('4111 | TC1: Validate user Add a register with: First name, Last name, Email, Age, Salary and Department', () => {
+		web.clickAddBtn();
+		web.get.userForm().should('exist');
+		web.fillFirstName(faker.name.firstName()).invoke('val').should('be.a', 'string');
+		web.fillLastName(faker.name.lastName()).invoke('val').should('be.a', 'string');
+		web.fillEmail(faker.internet.email())
+			.invoke('val')
+			.then(val => {
+				expect(val).to.match(/^([a-zA-Z0-9_\-\\.]+)@([a-zA-Z0-9_\-\\.]+)\.([a-zA-Z]{2,5})$/);
+			});
+		web.fillAge(faker.datatype.number({ max: 100 }))
+			.invoke('val')
+			.then(val => {
+				expect(parseInt(val)).to.be.a('number');
+			});
+		web.fillSalary(faker.datatype.number({ max: 100000 }))
+			.invoke('val')
+			.then(val => {
+				expect(parseInt(val)).to.be.a('number');
+			});
+		web.fillDepartment(faker.name.jobArea()).invoke('val').should('be.a', 'string');
+		web.clickSubmitBtn();
+		web.get
+			.registerList()
+			.children()
+			.eq(3)
+			.within(span => {
+				expect(span).not.to.be.empty;
+			});
+	});
+	it('4111 | TC2: Validate the register do not store when user close the registration form', () => {
+		web.clickAddBtn();
+		web.get.userForm().should('exist');
+		web.fillFirstName(faker.name.firstName()).invoke('val').should('be.a', 'string');
+		web.fillLastName(faker.name.lastName()).invoke('val').should('be.a', 'string');
+		web.fillEmail(faker.internet.email())
+			.invoke('val')
+			.then(val => {
+				expect(val).to.match(/^([a-zA-Z0-9_\-\\.]+)@([a-zA-Z0-9_\-\\.]+)\.([a-zA-Z]{2,5})$/);
+			});
+		web.fillAge(faker.datatype.number({ max: 100 }))
+			.invoke('val')
+			.then(val => {
+				expect(parseInt(val)).to.be.a('number');
+			});
+		web.fillSalary(faker.datatype.number({ max: 100000 }))
+			.invoke('val')
+			.then(val => {
+				expect(parseInt(val)).to.be.a('number');
+			});
+		web.fillDepartment(faker.name.jobArea()).invoke('val').should('be.a', 'string');
+		web.clickCloseBtn();
+		web.getEmptyRow().should('be.empty');
+	});
+	it('4111 | TC3: Validate user can filter a register search by field.', () => {
+		web.get.searchBox().should('have.attr', 'placeholder', 'Type to search');
+		web.get.iconSearch().should('exist');
+		web.get.searchBox().type('al');
+		web.get
+			.registerList()
+			.find('div.rt-td')
+			.each($ele => {
+				if (/[ A-Za-z]*al/.test($ele.text())) {
+					cy.wrap($ele.text()).should('include', 'al', { matchCase: false });
+				}
+			});
+	});
+	it('4111 | TC4: Validate the register list is sorted alphabetically when First Name is clicked', () => {
+		web.clickFirstNameHeader()
+			.should('have.attr', 'class')
+			.and('match', /-sort-asc/);
+
+		web.getFirstNameList().then(() => {
+			expect(Cypress.env('arrayFirstName')).to.be.equal(Cypress.env('arrayFirstName').sort());
+		});
+	});
+	it('4111 | TC5: Validate the register list is sorted alphabetically when Last Name is clicked', () => {
+		web.clickLastNameHeader()
+			.should('have.attr', 'class')
+			.and('match', /-sort-asc/);
+
+		web.getLastNameList().then(() => {
+			expect(Cypress.env('arrayLastName')).to.be.equal(Cypress.env('arrayLastName').sort());
+		});
+	});
+	it('4111 | TC6: Validate the register list is sorted alphabetically when Email is clicked', () => {
+		web.clickEmailHeader()
+			.should('have.attr', 'class')
+			.and('match', /-sort-asc/);
+
+		web.getEmailList().then(() => {
+			expect(Cypress.env('arrayEmail')).to.be.equal(Cypress.env('arrayEmail').sort());
+		});
+	});
+	it('4111 | TC7: Validate the register list is sorted from minor to major amount when Salary is clicked', () => {
+		web.clickSalaryHeader()
+			.should('have.attr', 'class')
+			.and('match', /-sort-asc/);
+
+		web.getSalaryList().then(() => {
+			expect(Cypress.env('arraySalary')).to.be.equal(Cypress.env('arraySalary').sort((a, b) => b - a));
+		});
+	});
+	it.skip('4111 | TC8: Validate the register list is sorted alphabetically when Active is clicked', () => {
+		const sortedList = ['Alden', 'Cierra', 'Kierra'];
+		web.clickAction()
+			.should('have.attr', 'class')
+			.and('match', /-sort-asc/);
+
+		web.getFirstNameList().then(() => {
+			expect(Cypress.env('arrayFirstName')).to.be.equal(sortedList);
+		});
+	});
+	it('4111 | TC9: Validate user can edit a register.', () => {
+		web.clickEditBtn();
+		web.get.registrationForm().should('be.visible');
+		web.modifyFirstName();
+		web.modifyLastName();
+		web.modifyEmail();
+		web.clickSubmitBtn();
+		web.get.firstNameList().then(text => {
+			expect(text.text()).to.equal(Cypress.env('firstNameEdited'));
+		});
+		web.get.lastNameList().then(text => {
+			expect(text.text()).to.equal(Cypress.env('lastNameEdited'));
+		});
+		web.get.emailList().then(text => {
+			expect(text.text()).to.equal(Cypress.env('emailEdited'));
+		});
+	});
+	it('4111 | TC10: Validate user can Remove a register.', () => {
+		web.get
+			.firstNameList()
+			.then(text => {
+				Cypress.env('firstName', text.text());
+				web.clickDeleteBtn();
+			})
+			.then(() => {
+				cy.get(Cypress.env('firstName')).should('not.exist');
+			});
+	});
+	it('4111 | TC11: Validate user can perform a registers pagination by rows number.', () => {
+		const randomRow = Math.floor(Math.random() * 6);
+		web.get
+			.rowOptions()
+			.select(randomRow)
+			.invoke('val')
+			.then(val => {
+				Cypress.env('val', val);
+			})
+			.then(() => {
+				web.get.registerList().children().should('have.length', Cypress.env('val'));
+			});
+	});
+	it('4111 | TC12: Validate user can move between the paginations', () => {
+		// select 5 rows
+		web.get.rowOptions().select(0);
+
+		// get the number of the first page
+		web.getFirstPage();
+
+		// add 3 more register
+		web.addNewRegister();
+		web.addNewRegister();
+		web.addNewRegister();
+
+		// validate nextBtn
+		web.clickNextBtn();
+		web.get
+			.pageInput()
+			.invoke('val')
+			.then(val => {
+				expect(parseInt(val)).to.be.equal(parseInt(Cypress.env('row')) + 1);
+			});
+
+		// validate previousBtn
+		web.get.previousBtn().should('be.enabled');
+		web.clickPreviousBtn();
+		web.get
+			.pageInput()
+			.invoke('val')
+			.then(val => {
+				expect(parseInt(val)).to.be.equal(parseInt(Cypress.env('row')));
+			});
+
+		// validate page field
+		web.get.pageInput().type('2{enter}');
+		web.get.previousBtn().should('be.enabled');
+	});
+});
---
+++ cypress/support/pages/Elements/GX2-4110-webTables.js
@@ -0,0 +1,275 @@
+import { faker } from '@faker-js/faker';
+class Web {
+	get = {
+		addBtn: () => cy.get('#addNewRecordButton'),
+		firstName: () => cy.get('#firstName'),
+		lastName: () => cy.get('#lastName'),
+		email: () => cy.get('#userEmail'),
+		age: () => cy.get('#age'),
+		salary: () => cy.get('#salary'),
+		department: () => cy.get('#department'),
+		submitBtn: () => cy.get('button[id="submit"]'),
+		userForm: () => cy.get('#userForm'),
+		closeBtn: () => cy.get('.close'),
+		emptyFirstName: () => cy.get(':nth-child(4) > .rt-tr > :nth-child(1)'),
+		emptyLastName: () => cy.get(':nth-child(4) > .rt-tr > :nth-child(2)'),
+		emptyAge: () => cy.get(':nth-child(4) > .rt-tr > [style="flex: 40 0 auto; width: 40px; max-width: 40px;"]'),
+		emptyEmail: () => cy.get(':nth-child(4) > .rt-tr > :nth-child(4)'),
+		emptySalary: () => cy.get(':nth-child(4) > .rt-tr > :nth-child(5)'),
+		emptyDepartment: () => cy.get(':nth-child(4) > .rt-tr > :nth-child(6)'),
+		searchBox: () => cy.get('#searchBox'),
+		iconSearch: () => cy.get('.input-group-append >span'),
+		registerList: () => cy.get('.rt-tbody'),
+		header: () => cy.get('[role="columnheader"]'),
+		editBtn: () => cy.get('[title="Edit"]'),
+		deleteBtn: () => cy.get('[title="Delete"]'),
+		registrationForm: () => cy.get('.modal-content'),
+		firstNameList: () => cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(1)'),
+		lastNameList: () => cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)'),
+		emailList: () => cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(4)'),
+		rowOptions: () => cy.get('[aria-label="rows per page"]'),
+		nextBtn: () => cy.contains('button', 'Next'),
+		previousBtn: () => cy.contains('button', 'Previous'),
+		pageInput: () => cy.get('.-pageJump > input'),
+	};
+	clickAddBtn() {
+		this.get.addBtn().click();
+	}
+	fillFirstName(data) {
+		return this.get.firstName().type(data);
+	}
+	fillLastName(data) {
+		return this.get.lastName().type(data);
+	}
+	fillEmail(data) {
+		return this.get.email().type(data);
+	}
+	fillAge(data) {
+		return this.get.age().type(data);
+	}
+	fillSalary(data) {
+		return this.get.salary().type(data);
+	}
+	fillDepartment(data) {
+		return this.get.department().type(data);
+	}
+	clickSubmitBtn() {
+		this.get.submitBtn().click();
+	}
+	clickCloseBtn() {
+		this.get.closeBtn().click();
+	}
+	getEmptyRow() {
+		return (
+			this.get.emptyFirstName().invoke('val'),
+			this.get.emptyLastName().invoke('val'),
+			this.get.emptyAge().invoke('val'),
+			this.get.emptyEmail().invoke('val'),
+			this.get.emptySalary().invoke('val'),
+			this.get.emptyDepartment().invoke('val')
+		);
+	}
+	clickFirstNameHeader() {
+		return this.get.header().eq(0).click();
+	}
+	clickLastNameHeader() {
+		return this.get.header().eq(1).click();
+	}
+	clickEmailHeader() {
+		return this.get.header().eq(3).click();
+	}
+	clickSalaryHeader() {
+		return this.get.header().eq(4).click();
+	}
+	clickDepartmentHeader() {
+		return this.get.header().eq(5).click();
+	}
+	clickAction() {
+		return this.get.header().eq(6).click();
+	}
+	clickEditBtn() {
+		this.get.editBtn().eq(0).click();
+	}
+	clickDeleteBtn() {
+		this.get.deleteBtn().eq(0).click();
+	}
+	clickNextBtn() {
+		this.get.nextBtn().click();
+	}
+	clickPreviousBtn() {
+		this.get.previousBtn().click();
+	}
+	getFirstNameList() {
+		const arrayFirstName = [];
+		return cy
+			.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(1)')
+			.invoke('text')
+			.then(text => {
+				arrayFirstName.push(text);
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(2) > .rt-tr > :nth-child(1)')
+					.invoke('text')
+					.then(text => {
+						arrayFirstName.push(text);
+					});
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(3) > .rt-tr > :nth-child(1)')
+					.invoke('text')
+					.then(text => {
+						arrayFirstName.push(text);
+						Cypress.env('arrayFirstName', arrayFirstName);
+						cy.log(Cypress.env('arrayFirstName'));
+					});
+			});
+	}
+
+	getLastNameList() {
+		const arrayLastName = [];
+		return cy
+			.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)')
+			.invoke('text')
+			.then(text => {
+				arrayLastName.push(text);
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(2) > .rt-tr > :nth-child(2)')
+					.invoke('text')
+					.then(text => {
+						arrayLastName.push(text);
+					});
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(3) > .rt-tr > :nth-child(2)')
+					.invoke('text')
+					.then(text => {
+						arrayLastName.push(text);
+						Cypress.env('arrayLastName', arrayLastName);
+						cy.log(Cypress.env('arrayLastName'));
+					});
+			});
+	}
+	getEmailList() {
+		const arrayEmail = [];
+		return cy
+			.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(4)')
+			.invoke('text')
+			.then(text => {
+				arrayEmail.push(text);
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(2) > .rt-tr > :nth-child(4)')
+					.invoke('text')
+					.then(text => {
+						arrayEmail.push(text);
+					});
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(3) > .rt-tr > :nth-child(4)')
+					.invoke('text')
+					.then(text => {
+						arrayEmail.push(text);
+						Cypress.env('arrayEmail', arrayEmail);
+						cy.log(Cypress.env('arrayEmail'));
+					});
+			});
+	}
+	getSalaryList() {
+		const arraySalary = [];
+		return cy
+			.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(5)')
+			.invoke('text')
+			.then(text => {
+				arraySalary.push(parseInt(text));
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(2) > .rt-tr > :nth-child(5)')
+					.invoke('text')
+					.then(text => {
+						arraySalary.push(parseInt(text));
+					});
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(3) > .rt-tr > :nth-child(5)')
+					.invoke('text')
+					.then(text => {
+						arraySalary.push(parseInt(text));
+						Cypress.env('arraySalary', arraySalary);
+						cy.log(Cypress.env('arraySalary'));
+					});
+			});
+	}
+	getDepartmentList() {
+		const arrayDepartment = [];
+		return cy
+			.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(6)')
+			.invoke('text')
+			.then(text => {
+				arrayDepartment.push(text);
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(2) > .rt-tr > :nth-child(6)')
+					.invoke('text')
+					.then(text => {
+						arrayDepartment.push(text);
+					});
+			})
+			.then(() => {
+				cy.get('.rt-tbody > :nth-child(3) > .rt-tr > :nth-child(6)')
+					.invoke('text')
+					.then(text => {
+						arrayDepartment.push(text);
+						Cypress.env('arrayDepartment', arrayDepartment);
+						cy.log(Cypress.env('arrayDepartment'));
+					});
+			});
+	}
+	modifyFirstName() {
+		this.get
+			.firstName()
+			.clear()
+			.type(faker.name.firstName())
+			.then(text => {
+				Cypress.env('firstNameEdited', text.val());
+			});
+	}
+	modifyLastName() {
+		this.get
+			.lastName()
+			.clear()
+			.type(faker.name.lastName())
+			.then(text => {
+				Cypress.env('lastNameEdited', text.val());
+			});
+	}
+	modifyEmail() {
+		this.get
+			.email()
+			.clear()
+			.type(faker.internet.email())
+			.then(text => {
+				Cypress.env('emailEdited', text.val());
+			});
+	}
+	addNewRegister() {
+		this.get.addBtn().click();
+		this.get.firstName().type(faker.name.firstName());
+		this.get.lastName().type(faker.name.lastName());
+		this.get.email().type(faker.internet.email());
+		this.get.age().type(faker.datatype.number({ max: 100 }));
+		this.get.salary().type(faker.datatype.number({ max: 100000 }));
+		this.get.department().type(faker.name.jobArea());
+		this.get.submitBtn().click();
+	}
+	getFirstPage() {
+		this.get
+			.pageInput()
+			.invoke('val')
+			.then(val => {
+				Cypress.env('row', val);
+				cy.log(Cypress.env('row'));
+			});
+	}
+}
+export const web = new Web();
