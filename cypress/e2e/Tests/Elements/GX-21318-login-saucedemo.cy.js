36003d3766a65e21354ca626ff6fba5222b72054
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-22 11:50:03 -0400

    GX-21318 tc1 2 3 and 4 added

--- cypress/e2e/Tests/Elements/GX-21318-login-saucedemo.cy.js
+++ cypress/e2e/Tests/Elements/GX-21318-login-saucedemo.cy.js
@@ -3,5 +3,41 @@ describe('GX-21318-✅-swag-labs-account-iniciar-sesion-y-br-de-accesos', () => 
 		cy.visit('https://www.saucedemo.com/');
 	});
 
-	it('GX-21319 | TC1: Validate user can log in with username “standard_user” successfully.');
+	it('GX-21319 | TC1: Validate user can log in with username “standard_user” successfully.', () => {
+		cy.fixture('GX-21318/ToolsQA-LogIn-saucedemo').then(the => {
+			cy.get(the.userone.input).type(the.userone.data.valid);
+			cy.get(the.password.input).type(the.password.data.valid);
+			cy.get(the.loginbutton).click();
+			cy.get('#item_4_title_link').should('contain.text', 'Sauce Labs Backpack');
+		});
+	});
+	it('GX-21319 | TC2: Validate user can log in with username “problem_user” successfully.', () => {
+		cy.fixture('GX-21318/ToolsQA-LogIn-saucedemo').then(the => {
+			cy.get(the.usertwo.input).type(the.usertwo.data.valid);
+			cy.get(the.password.input).type(the.password.data.valid);
+			cy.get(the.loginbutton).click();
+			cy.get('#item_4_title_link').should('contain.text', 'Sauce Labs Backpack');
+		});
+	});
+	it('GX-21319 | TC3: Validate user can log in with username “performance_glitch_user” successfully.', () => {
+		cy.fixture('GX-21318/ToolsQA-LogIn-saucedemo').then(the => {
+			cy.get(the.userthree.input).type(the.userthree.data.valid);
+			cy.get(the.password.input).type(the.password.data.valid);
+			cy.get(the.loginbutton).click();
+			cy.get('#item_4_title_link').should('contain.text', 'Sauce Labs Backpack');
+		});
+	});
+	it('GX-21319 | TC4: Validate user can’t log in with username “locked_out_user”.', () => {
+		cy.fixture('GX-21318/ToolsQA-LogIn-saucedemo').then(the => {
+			cy.get(the.userblocked.input).type(the.userblocked.data.valid);
+			cy.get(the.password.input).type(the.password.data.valid);
+			cy.get(the.loginbutton).click();
+			cy.get('.error').eq('2').should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
+		});
+	});
 });
+//Se importa la funcion
+import { removeLogs } from '@helper/RemoveLogs';
+
+//Se ejecuta la funcion para evitar los errores (puedes hacerlo al final de tu codigo)
+removeLogs();
