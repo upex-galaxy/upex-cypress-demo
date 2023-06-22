51048de0e1bfc12145d5284634a320f73fd47d01
Juliako <julikq22@gmail.com>
2023-06-21 21:58:23 -0300

    GX-20682-✅-tools-qa-elements-text-box-fill-form-and-submit{Todos los tests han pasado con éxito, buscaré optimizar las test cases

--- cypress/e2e/Tests/Elements/GX-20682-TextBoxForm&Submit.cy.js
+++ cypress/e2e/Tests/Elements/GX-20682-TextBoxForm&Submit.cy.js
@@ -36,15 +36,54 @@ describe('ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 	});
 	it('20683 | TC4: Validar que el formulario NO pueda ser enviado si el campo ”Email” es inválido al no contener “@”', () => {
 		cy.fixture('demoQA/textBoxForm.Page').then(the => {
-			cy.get(the.input.email).click().type(the.data.invalid.validEmailAddress);
+			cy.get(the.input.email).click().type(the.data.invalid.invalidEmailAddress1);
 			cy.get(the.input.submitButton).click();
-			cy.get(the.email).should();
+			cy.get(the.error).should('have.css', 'border', '0.8px solid rgb(255, 0, 0)');
 		});
 	});
-	it('20683 | TC5: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido al no contener al menos un caracter alfanumérico antes del “@”', () => {});
-	it('20683 | TC6: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido al no contener al menos un caracter alfanumérico después del “@”', () => {});
-	it('20683 | TC7: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido al no contener “.” después de un caracter alfanumérico después del “@”', () => {});
-	it('20683 | TC8: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido al no contener al menos dos caracteres alfanuméricos después del “.”', () => {});
-	it('20683 | TC9: Validar que el formulario pueda ser enviado si el campo  ”Email” está vacío', () => {});
-	it('20683 | TC10: Validar que el formulario pueda ser enviado cuando el campo  ”Email” es válido', () => {});
+	it('20683 | TC5: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido al no contener al menos un caracter alfanumérico antes del “@”', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.email).click().type(the.data.invalid.invalidEmailAddress2);
+			cy.get(the.input.submitButton).click();
+			cy.get(the.error).should('have.css', 'border', '0.8px solid rgb(255, 0, 0)');
+		});
+	});
+	it('20683 | TC6: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido al no contener al menos un caracter alfanumérico después del “@”', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.email).click().type(the.data.invalid.invalidEmailAddress3);
+			cy.get(the.input.submitButton).click();
+			cy.get(the.error).should('have.css', 'border', '0.8px solid rgb(255, 0, 0)');
+		});
+	});
+	it('20683 | TC7: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido al no contener “.” después de un caracter alfanumérico después del “@”', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.email).click().type(the.data.invalid.invalidEmailAddress4);
+			cy.get(the.input.submitButton).click();
+			cy.get(the.error).should('have.css', 'border', '0.8px solid rgb(255, 0, 0)');
+		});
+	});
+	it('20683 | TC8: Validar que el formulario NO pueda ser enviado si el campo  ”Email” es inválido al no contener al menos dos caracteres alfanuméricos después del “.”', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.email).click().type(the.data.invalid.invalidEmailAddress5);
+			cy.get(the.input.submitButton).click();
+			cy.get(the.error).should('have.css', 'border', '0.8px solid rgb(255, 0, 0)');
+		});
+	});
+	it('20683 | TC9: Validar que el formulario pueda ser enviado si el campo  ”Email” está vacío', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.fullName).click().type(the.data.valid.user);
+			cy.get(the.input.currentAddress).click().type(the.data.valid.firstAddress);
+			cy.get(the.input.permanentAddress).click().type(the.data.valid.secondAddress);
+			cy.get(the.input.submitButton).click();
+			cy.get(the.input.fullName).should('be.empty');
+			cy.get('.mb-1').should('contain.text', 'Name', 'Current Address', 'Permanent Address');
+		});
+	});
+	it('20683 | TC10: Validar que el formulario pueda ser enviado cuando el campo  ”Email” es válido', () => {
+		cy.fixture('demoQA/textBoxForm.Page').then(the => {
+			cy.get(the.input.email).click().type(the.data.valid.validEmailAddress);
+			cy.get(the.input.submitButton).click();
+			cy.get('.mb-1').should('have.text', 'Email:julian.battaglini@upexgalaxy.com');
+		});
+	});
 });
