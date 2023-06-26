26bb41351198a3a6148848ba657e1785617917e0
ElenaValera <secretodelmar@gmail>
2023-06-26 19:25:15 +0200

    GX-21733 first commit

---
+++ cypress/e2e/Tests/Elements/GX-21733-buttons.cy.js
@@ -0,0 +1,5 @@
+describe('✅ToolsQA | Elements | Buttons', () => {
+	it('21734 | TC1: Validar funcionamiento del botón “double click me”', () => {
+		cy.visit('https://demoqa.com/buttons');
+	});
+});
