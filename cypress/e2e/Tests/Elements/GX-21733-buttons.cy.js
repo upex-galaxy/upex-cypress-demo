e3e79a7e68de39251ff0b8d5fa90912da2c07207
ElenaValera <secretodelmar@gmail>
2023-06-28 09:44:34 +0200

    Added first version of the tests

--- cypress/e2e/Tests/Elements/GX-21733-buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-21733-buttons.cy.js
@@ -1,5 +1,24 @@
 describe('✅ToolsQA | Elements | Buttons', () => {
-	it('21734 | TC1: Validar funcionamiento del botón “double click me”', () => {
+	beforeEach('precondición: visitar página Demo QA', () => {
 		cy.visit('https://demoqa.com/buttons');
+		cy.url().should('contain', 'buttons');
+	});
+
+	it.only('21734 | TC1: Validar funcionamiento del botón “double click me”', () => {
+		cy.get('#doubleClickBtn').dblclick();
+		cy.get('#doubleClickMessage').should('contain', 'You have done a double click');
+		//cy.get('[class="btn btn-primary"]').eq(0).dblclick();
+	});
+
+	it('21734 | TC2: Validar funcionamiento del botón "right click me"', () => {
+		cy.get('#rightClickBtn').rightClick();
+		cy.get('#rightClickMessage').should('contain', 'You have done a double click');
+	});
+
+	it('21734 | TC3: Validar funcionamiento del botón “click me”', () => {
+		cy.get('[class$="btn-primary"]').eq(2).click();
+		cy.get('dynamicClickMessage').should('contain', 'You have done a right click');
 	});
 });
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
