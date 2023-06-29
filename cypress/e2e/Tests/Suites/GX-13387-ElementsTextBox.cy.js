a0bccfefcacf86aa724044fabc2a436b2d94ffbb
Pablo <pvertone390@gmail.com>
2023-06-29 12:21:43 -0300

    GX-13387 Test(Elements): correciones de las validaciones 3 a 7

--- cypress/e2e/Tests/Suites/GX-13387-ElementsTextBox.cy.js
+++ cypress/e2e/Tests/Suites/GX-13387-ElementsTextBox.cy.js
@@ -21,48 +21,53 @@ describe('US GX-13387 | ToolsQA | Elements | Text Box: Fill form and Submit', ()
 		cy.get('.mb-1').should('not.exist');
 	});
 
-	it('13388 | TC03: Validar visualizar borde rojo en el campo email si no contiene @', () => {
+	it('13388 | TC03: Validar NO enviar formulario cuando campo "email" no contiene @', () => {
 		cy.get('[type="text"]').type('Pablo Vertone');
 		cy.get('[type="email"]').type('vertogmail.com');
 		cy.get('#currentAddress').type('Dirección 123');
 		cy.get('#permanentAddress').type('Permanent');
 		cy.get('#submit').click();
 		cy.get('.mr-sm-2.field-error.form-control').should('exist');
+		cy.get('.mb-1').should('not.exist');
 	});
 
-	it('13388 | TC04: Validar visualizar borde rojo en el campo email si no contiene caracteres alfanuméricos antes del @', () => {
+	it('13388 | TC04: Validar NO enviar formulario cuando campo "email" no contiene caracteres alfanuméricos antes del @', () => {
 		cy.get('[type="text"]').type('Pablo Vertone');
 		cy.get('[type="email"]').type('@gmail.com');
 		cy.get('#currentAddress').type('Dirección 123');
 		cy.get('#permanentAddress').type('Permanent');
 		cy.get('#submit').click();
 		cy.get('.mr-sm-2.field-error.form-control').should('exist');
+		cy.get('.mb-1').should('not.exist');
 	});
 
-	it('13388 | TC05: Validar visualizar borde rojo en el campo email si no contiene caracteres alfanuméricos después del @', () => {
+	it('13388 | TC05: Validar NO enviar formulario cuando campo "email" no contiene caracteres alfanuméricos después del @', () => {
 		cy.get('[type="text"]').type('Pablo Vertone');
 		cy.get('[type="email"]').type('verto@');
 		cy.get('#currentAddress').type('Dirección 123');
 		cy.get('#permanentAddress').type('Permanent');
 		cy.get('#submit').click();
 		cy.get('.mr-sm-2.field-error.form-control').should('exist');
+		cy.get('.mb-1').should('not.exist');
 	});
 
-	it('13388 | TC06: Validar visualizar borde rojo en el campo email si no contiene "." en el formato', () => {
+	it('13388 | TC06: Validar NO enviar formulario cuando campo "email" no contiene "." en el formato', () => {
 		cy.get('[type="text"]').type('Pablo Vertone');
 		cy.get('[type="email"]').type('verto@gmailcom');
 		cy.get('#currentAddress').type('Dirección 123');
 		cy.get('#permanentAddress').type('Permanent');
 		cy.get('#submit').click();
 		cy.get('.mr-sm-2.field-error.form-control').should('exist');
+		cy.get('.mb-1').should('not.exist');
 	});
 
-	it('13388 | TC07: Validar visualizar borde rojo en el campo email si no contiene al menos dos caracteres después del "."', () => {
+	it('13388 | TC07: Validar NO enviar formulario cuando campo "email" no contiene al menos dos caracteres después del "."', () => {
 		cy.get('[type="text"]').type('Pablo Vertone');
 		cy.get('[type="email"]').type('verto@gmail.c');
 		cy.get('#currentAddress').type('Dirección 123');
 		cy.get('#permanentAddress').type('Permanent');
 		cy.get('#submit').click();
 		cy.get('.mr-sm-2.field-error.form-control').should('exist');
+		cy.get('.mb-1').should('not.exist');
 	});
 });
