4bbd9543ee77b424729704ef190bbd66c7954590
JonatanDavidGauto <elchoni90@gmail.com>
2023-06-30 16:39:00 +0200

    GX-22346: Creación del TC 1 y una función en el POM

--- cypress/e2e/Tests/Widgets/GX-22346-Dropdown.cy.js
+++ cypress/e2e/Tests/Widgets/GX-22346-Dropdown.cy.js
@@ -1,2 +1,24 @@
 import { removeLogs } from '@helper/RemoveLogs';
 removeLogs();
+
+import { dropDownSelector } from '@pages/Widgets/GX-22346-Dropdown.Page';
+
+describe('✅ToolsQA | Widgets | Dropdown - Select Menu', () => {
+	beforeEach('user is located in the Sut', () => {
+		cy.visit('/select-menu');
+	});
+	it('22347 | TC01: Validar la selección de Una opción Aleatoria en Select Value', () => {
+		dropDownSelector
+			.SelectValueDropDownOptions()
+			.invoke('text')
+			.then(values => {
+				cy.log(values);
+				dropDownSelector.get.selectOption().should('have.text', values);
+			});
+	});
+	it('22347 | TC02: Validar que al escribir Una parte de la opción valida en el Dropdown Select Value se visualice la opción escrita', () => {
+		cy.fixture('data/GX-22346-Dropdown').then(the => {
+			dropDownSelector.SelectValueDropDownInput(the.SelectValue.data.valid);
+		});
+	});
+});
---
+++ cypress/support/pages/Widgets/GX-22346-Dropdown.Page.js
@@ -0,0 +1,25 @@
+class DropDownSelector {
+	get = {
+		SelectValueDropDown: () => cy.get('[id="withOptGroup"]'),
+		SelectOneDropDown: () => cy.get('#selectOne'),
+		OldSelectMenuDropDown: () => cy.get('#oldSelectMenu'),
+		MultiselectDropDown: () => cy.get('[class=" css-yk16xz-control"]').eq(2),
+		StandarMultiSelect: () => cy.get('#cars').eq(0),
+		dropdownMenu: () => cy.get('[class$=menu]'),
+		optionMenu: () => cy.get('[id*=react-select][id*=option]'),
+		selectOption: () => cy.get('[class$="singleValue"]'),
+	};
+
+	SelectValueDropDownInput(type) {
+		this.get.SelectValueDropDown().type(type);
+	}
+
+	SelectValueDropDownOptions() {
+		const option = Math.floor(Math.random() * 6);
+		dropDownSelector.get.SelectValueDropDown().click();
+		dropDownSelector.get.dropdownMenu().should('be.visible');
+		return this.get.optionMenu().eq(option).click();
+	}
+}
+
+export const dropDownSelector = new DropDownSelector();
