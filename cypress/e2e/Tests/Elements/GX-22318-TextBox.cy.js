b4b96ac0d5a1885ba4ace82d739044ff48848a44
ElenaValera <secretodelmar@gmail>
2023-07-01 18:22:10 +0200

    changes TC1-2

--- cypress/e2e/Tests/Elements/GX-22318-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-22318-TextBox.cy.js
@@ -1,61 +1,24 @@
 describe('✅ToolsQA Elements Text Box: Fill form and Submit', () => {
 	beforeEach('precondition: visit website Demo QA', () => {
-		cy.visit('https://demoqa.com/text-box'),
-		cy.url().should('contain', 'text box');
+		cy.visit('https://demoqa.com/text-box'), cy.url().should('contain', 'text-box');
 	});
 
-	it('TC1: Validate fill the Fill form successfully with valide data verifying the correct message', () => {
+	it.only('TC1: Validate fill the Fill form successfully with valide data verifying the correct message', () => {
+		//	cy.get('#userName').type('dog');
 		cy.fixture('data/Gx-22318-TextBox.json').then(the => {
-			cy.get(the.fullName.input).type(the.userName.data.valid);
-			cy.get(the.email.input).type(the.userEmail.data.valid);
-			cy.get(the.currentAddress.input).type(the.CurrentAddress.data.valid);
-			cy.get(the.permanentAddress.input).type(the.permanentAddress.data.valid);
+			cy.get(the.ids.fullName).type('Elena Valera');
+			cy.get(the.ids.email).type('secretodelmar@gmail.com');
+			cy.get(the.ids.currentAddress).type('Calle mar menor 5');
+			cy.get(the.ids.permanentAddress).type('Calle mar mayor 6');
+
 			cy.get(the.submitButton).click();
+			// AAA
+			// Arrange
+			// Act
+			// Assert
 		});
-		
-	
 	});
-		// step 2: fill e-mail
-
-		// ..
-
-		// action: click submit
-
-		// tests
-
-		
-	});
-
-	
-	it('TC2: Validate submit the Fill form completely empty and check the inexistent message', () => {
-
-	});
-
-	it('TC3: Validate Not to submit the Fill form when "email" does not contain an "@"', () => {
-
-	});
-
-	it('TC4: Validate Not to submit the Fill form when "email" does not contain minimum 1 alphanumeric character after "@"', () => {
-
-	});
-	it('TC5: Validate Not to submit the Fill form when "email" does not contain "." after 1 alphanumeric character after "@"', () => {
-		
-
-	});
-	it('TC6: Validate Not to submit the Fill form when "email" does not contain minimum 2 alphanumeric character after "." displaying a red border', () => {
-		
-
-	});
-
-	it('TC7: Validate Not to submit the Fill form when "email field" is empty and no log message is displayed', () => {
-		
-
-	});
-	
-	});
-
-
-
+});
 
 import { removeLogs } from '@helper/RemoveLogs';
 removeLogs();
