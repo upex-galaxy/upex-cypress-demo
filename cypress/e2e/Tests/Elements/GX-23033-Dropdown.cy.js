52ff049bf580b8ca5ef61ddd83589c3a960a0c6d
TheNotoriousMAGG <maran0301@gmail.com>
2023-07-03 16:29:25 -0400

    GX-23033 tc1 in progress

--- cypress/e2e/Tests/Elements/GX-23033-Dropdown.cy.js
+++ cypress/e2e/Tests/Elements/GX-23033-Dropdown.cy.js
@@ -1,10 +1,17 @@
-import { DropD } from '@pages/Elements/GX-23033-dropdown.Page';
+import { drop } from '@pages/Elements/GX-23033-dropdown.Page';
 
 describe('US GX-23033 | TS: ✅ToolsQA | Widgets | Dropdown - Select Menu', () => {
 	beforeEach('Precondition: Having access to SUT', () => {
-		cy.visit('/');
-		DropD.get.mainTitle().should('have.text', 'Select Menu');
+		cy.visit('/select-menu');
+		drop.get.mainTitle().should('have.text', 'Select Menu');
 	});
 
-	//	it('GX-21867| TC1: Validate user adds item from PLP to SC successfully.', () => {});
+	it('GX-23033 | TC1: Validate that dropdown “select value” works correctly.', () => {
+		drop.get.selectMenuOne();
+	});
 });
+//Se importa la funcion
+import { removeLogs } from '@helper/RemoveLogs';
+
+//Se ejecuta la funcion para evitar los errores (puedes hacerlo al final de tu codigo)
+removeLogs();
--- cypress/support/pages/Elements/GX-23033-dropdown.Page.js
+++ cypress/support/pages/Elements/GX-23033-dropdown.Page.js
@@ -1,6 +1,11 @@
-class dropdownPage {
+class DropdownPage {
 	get = {
 		mainTitle: () => cy.get('.playgound-header'),
+		selectMenuOne: () => cy.get('.col-sm-12').eq(1),
 	};
+
+	selectMenuOne() {
+		this.get.selectMenuOne.click();
+	}
 }
-export const DropD = dropdownPage();
+export const drop = new DropdownPage();
