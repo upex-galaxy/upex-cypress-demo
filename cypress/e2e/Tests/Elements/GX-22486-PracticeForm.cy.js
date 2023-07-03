974807cd39439b32afcd00c403a34c181fcc98fc
gualex89 <gualex89@gmail.com>
2023-07-03 10:59:18 -0400

    avance

--- cypress/e2e/Tests/Elements/GX-22486-PracticeForm.cy.js
+++ cypress/e2e/Tests/Elements/GX-22486-PracticeForm.cy.js
@@ -5,45 +5,91 @@ describe('22486 | TS: ✅ToolsQA | Forms | Practice Form', () =>
         cy.visit('https://demoqa.com/automation-practice-form');
         cy.url().should('contains', 'automation-practice-form')
     })
-    it('22487 | TC1: Validar rellenar el formulario con datos aleatorios (Happy path)', () =>
+    it('22487 | TC1: Validar rellenar el formulario con datos validos y aleatorios (Happy path)', () =>
     {
         const fName = faker.name.firstName();
         const lName = faker.name.lastName();
         const email = faker.internet.email();
-        const pNumber = faker.random.numeric(10);
-        const subject1 = faker.random.words();
-        const subject2 = faker.random.words();
-        const subjectsSeparadosPorComa = `${subject1}, ${subject2}`;
+        const phoneNumber = faker.random.numeric(10);
+        const address = faker.address.streetAddress();
 
-        
+        //First Name
         textForm.enterFirstName(fName);
         textForm.get.firstNameInput().should('have.value', fName);
+        //Last Name
         textForm.enterLastName(lName);
         textForm.get.lastNameInput().should('have.value', lName);
+        //Email
         textForm.enterEmail(email);
         textForm.get.emailInput().should('have.value', email);
-        
-        // textForm.get.genderInput().then(choice => {
-        //     const random = Cypress._.random(0, 2);
-        //     textForm.get.genderInput().eq(random).click();
-        //     textForm.get.genderInput().eq(random).should('be.checked');
-
-        // });
+        //select random gender
         textForm.selectRandomGender().then(indexGenderSelected => {
             textForm.get.radioGenderInput().eq(indexGenderSelected).should('be.checked');
             textForm.get.radioGenderInput().eq(indexGenderSelected).invoke('val').then((text) => {
-            cy.log(text);
+                cy.wrap(text).as('genderText');
+            })    
         })
-        textForm.enterMobile(pNumber);
-        textForm.get.mobileInput().should('have.value', pNumber);
-        textForm.enterSubjects(subjectsSeparadosPorComa);
-        
+        //Phone number
+        textForm.enterMobile(phoneNumber);
+        textForm.get.mobileInput().should('have.value', phoneNumber);
+        //date of birth
+        textForm.selectRandomDateOfBirth()
+        //Subjects
+        textForm.enterSubjects();
+        textForm.get.subjectsInput().should('have.text', 'Maths')
+        //Hobbie
+        textForm.selectRandomHobbie().then(indexHobbieSelected => {
+            textForm.get.radioHobbieInput().eq(indexHobbieSelected).should('be.checked');
+            textForm.get.labelHobbieInput().eq(indexHobbieSelected).invoke('text').then((text) => {
+                cy.wrap(text).as('hobbieText');
+            })
         })
+        //address    
+        textForm.enterAddress(address);
+        textForm.get.addressInput().should('have.value', address);
+        //state
+        textForm.get.stateSelecter().click();
+        textForm.selectRandomStateOption();
+        //city
+        textForm.get.citySelecter().click();
+        textForm.selectRandomCityOption();
+        //Picture
+        cy.get('#uploadPicture').selectFile('cypress/images/image.png')
+        //submitButton click
+        textForm.get.submitButton().click();
+
+        //Form Review
+        textForm.get.formTable().contains('td', fName, lName, email, phoneNumber, 'Maths', address);
+        cy.get('@genderText').then((genderText) => {
+            cy.get('.table.table-dark').contains('td', genderText);
+        })
+        textForm.get.dateOfBirthInput().invoke('val').then((text) => {
+            var fecha = new Date(text);
+            var dia = fecha.getDate();
+            var nombreMes = fecha.toLocaleString('en-us', { month: 'long' });
+            var año = fecha.getFullYear();
+            var fechaFormateada = dia + ' ' + nombreMes + ',' + año;
+            console.log(fechaFormateada);
+            cy.get('.table.table-dark').contains('td', fechaFormateada);
+        })
+        cy.get('@hobbieText').then((hobbieText) => {
+            cy.get('.table.table-dark').contains('td', hobbieText);
+        });
+        textForm.get.stateSelecter().invoke('text').then((text) => {
+            cy.get('.table.table-dark').contains('td', text);
+        })
+        textForm.get.citySelecter().invoke('text').then((text) => {
+            cy.get('.table.table-dark').contains('td', text);
+        })
+    })
+    it('22787 | TC2: Validar  que al tratar de enviar todos los campos vacíos no se muestre ningún mensaje.', () => {
+        //submitButton click
+        textForm.get.submitButton().click();
+        textForm.get.formTable().should('not.exist');
     })
 })
 
 import { faker } from "@faker-js/faker";
 import { textForm } from '@pages/Elements/GX-22486-PracticeForm.Page';
 import { removeLogs } from '@helper/RemoveLogs';
-
 removeLogs();
--- cypress/support/pages/Elements/GX-22486-PracticeForm.Page.js
+++ cypress/support/pages/Elements/GX-22486-PracticeForm.Page.js
@@ -7,12 +7,20 @@ class form {
         firstNameInput: () => cy.get('#firstName'),
         lastNameInput: () => cy.get('#lastName'),
         emailInput: () => cy.get('#userEmail'),
-        genderInput: () => cy.get('.custom-control-label'),
+        radioGenderInput: () => cy.get('#genterWrapper [class="custom-control-input"]'),
         mobileInput: () => cy.get('#userNumber'),
         dateOfBirthInput: () => cy.get('#dateOfBirthInput'),
+        dateOfBirthOptions: () => cy.get('[class*="react-datepicker__day--0"]'),
         subjectsInput: () => cy.get('.subjects-auto-complete__value-container'),
-        labelGenderInput: () => cy.get('#genterWrapper [class="custom-control-label"]'),
-        radioGenderInput: () => cy.get('#genterWrapper [class="custom-control-input"]'),
+        radioHobbieInput: () => cy.get('[id^="hobbies-checkbox"]'),
+        labelHobbieInput: () => cy.get('#hobbiesWrapper [class="custom-control-label"]'),
+        addressInput: () => cy.get('#currentAddress'),
+        stateSelecter: () => cy.get('#state'),
+        stateOptions: () => cy.get('[id^="react-select-3-option"]'),
+        citySelecter: () => cy.get('#city'),
+        cityOptions: () => cy.get('[id^="react-select-4-option"]'),
+        submitButton: () => cy.get('#submit'),
+        formTable: () => cy.get('.table.table-dark'),
 
     }
     enterFirstName(value) {
@@ -24,21 +32,64 @@ class form {
     enterEmail(value) {
         this.get.emailInput().type(value);
     }
-    enterMobile(value) {
-        this.get.mobileInput().type(value);
-    }
-    enterSubjects(value) {
-        this.get.subjectsInput().type(value); 
-    }
     selectRandomGender() {
         let randomInput
-        return this.get.labelGenderInput().then(largo => {
+        return this.get.radioGenderInput().then(largo => {
                 randomInput = Cypress._.random(0, largo.length - 1)
-                this.get.labelGenderInput().eq(randomInput).click();
+                this.get.radioGenderInput().eq(randomInput).click({force:true});
         }).then(() => {
             return randomInput
         })
     }
+    enterMobile(value) {
+        this.get.mobileInput().type(value);
+    }
+    
+    selectRandomDateOfBirth() {
+        this.get.dateOfBirthInput().click();
+        let randomInput
+        return this.get.dateOfBirthOptions().then(largo => {
+                randomInput = Cypress._.random(0, largo.length - 1)
+                this.get.dateOfBirthOptions().eq(randomInput).click();
+        }).then(() => {
+            return randomInput
+        })
+    }
+    enterSubjects(value) {
+        this.get.subjectsInput().type('Maths{enter}');
+
+    }
+    selectRandomHobbie() {
+        let randomInput
+        return this.get.radioHobbieInput().then(largo => {
+                randomInput = Cypress._.random(0, largo.length - 1)
+                this.get.radioHobbieInput().eq(randomInput).click({force:true});
+        }).then(() => {
+            return randomInput
+        })
+    }
+    enterAddress(value) {
+        this.get.addressInput().type(value);
+    }
+    selectRandomStateOption() {
+        let randomInput
+        return this.get.stateOptions().then(largo => {
+                randomInput = Cypress._.random(0, largo.length - 1)
+                this.get.stateOptions().eq(randomInput).click();
+        }).then(() => {
+            return randomInput
+        })
+    }
+    selectRandomCityOption() {
+        let randomInput
+        return this.get.cityOptions().then(largo => {
+                randomInput = Cypress._.random(0, largo.length - 1)
+                this.get.cityOptions().eq(randomInput).click();
+        }).then(() => {
+            return randomInput
+        })
+    }
+
 }
 
 export const textForm = new form();
