12acbdd8374939aec583beb9b742003cfef2ad2e
ElenaValera <secretodelmar@gmail>
2023-07-03 10:12:16 +0200

    changing again

--- cypress/e2e/Tests/Elements/GX-22318-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-22318-TextBox.cy.js
@@ -5,13 +5,20 @@ describe('✅ToolsQA Elements Text Box: Fill form and Submit', () => {
 
 	it.only('TC1: Validate fill the Fill form successfully with valide data verifying the correct message', () => {
 		//	cy.get('#userName').type('dog');
+		//arrange
 		cy.fixture('data/Gx-22318-TextBox.json').then(the => {
 			cy.get(the.ids.fullName).type('Elena Valera');
 			cy.get(the.ids.email).type('secretodelmar@gmail.com');
 			cy.get(the.ids.currentAddress).type('Calle mar menor 5');
 			cy.get(the.ids.permanentAddress).type('Calle mar mayor 6');
+			//act
+			cy.get(the.ids.submitButton).click();
+			//assert
+			cy.contains('Elena Valera').should('be visible');
+			cy.contains('secretodelmar@gmail.com').should('be visible');
+			cy.contains('Calle mar menor 5').should('be visible');
+			cy.contains('Calle mar mayor 6').should('be visible');
 
-			cy.get(the.submitButton).click();
 			// AAA
 			// Arrange
 			// Act
