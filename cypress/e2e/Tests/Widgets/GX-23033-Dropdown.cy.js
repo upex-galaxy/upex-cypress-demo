d4e58ae90f8c10bb015c08c922cb3d8026e7d88a
TheNotoriousMAGG <maran0301@gmail.com>
2023-07-04 14:16:03 -0400

    GX-23033 archives changed to folder Widgets

--- cypress/e2e/Tests/Widgets/GX-23033-Dropdown.cy.js
+++ cypress/e2e/Tests/Widgets/GX-23033-Dropdown.cy.js
@@ -1,4 +1,4 @@
-import { dropPage } from '@pages/Elements/GX-23033-dropdown.Page';
+import { dropPage } from '@pages/Widgets/GX-23033-dropdown.Page';
 
 describe('US GX-23033 | TS: ✅ToolsQA | Widgets | Dropdown - Select Menu', () => {
 	beforeEach('Precondition: Having access to SUT', () => {
