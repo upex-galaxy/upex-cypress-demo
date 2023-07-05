a1e29edf69a24e244f570f575fb9ba40914fe869
ElenaValera <secretodelmar@gmail>
2023-07-05 12:04:18 +0200

    last changes TC1, taking out last IDS

--- cypress/e2e/Tests/Elements/GX-22318-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-22318-TextBox.cy.js
@@ -1,34 +1,32 @@
 describe('✅ToolsQA Elements Text Box: Fill form and Submit', () => {
 	beforeEach('precondition: visit website Demo QA', () => {
-		cy.visit('https://demoqa.com/text-box'), cy.url().should('contain', 'text-box');
+		cy.visit('https://demoqa.com/text-box');
+		cy.url().should('contain', 'text-box');
 	});
 
 	it.only('TC1: Validate fill the Fill form successfully with valide data verifying the correct message', () => {
-		//	cy.get('#userName').type('dog');
 		//arrange
 		cy.fixture('data/Gx-22318-TextBox.json').then(the => {
-			cy.get(the.ids.fullName).type('Elena Valera');
-			cy.get(the.ids.email).type('secretodelmar@gmail.com');
-			cy.get(the.ids.currentAddress).type('Calle mar menor 5');
-			cy.get(the.ids.permanentAddress).type('Calle mar mayor 6');
+			cy.get(the.ids.fullName).type(the.validInput.userName);
+			cy.get(the.ids.email).type(the.validInput.userEmail);
+			cy.get(the.ids.currentAddress).type(the.validInput.currentAddress);
+			cy.get(the.ids.permanentAddress).type(the.validInput.permanentAddress);
+
 			//act
 			cy.get(the.ids.submitButton).click();
+
 			//assert
 
-			cy.get('#output').within(() => {
-				// this is for searching elements inside the element "output"
-				cy.get('#name').should('exist');
-				cy.get('#name').should('have.text', 'Name:Elena Valera');
+			cy.get('#output')
+				.should('exist')
+				.within(() => {
+					// this is for searching elements inside the element "output"
 
-				cy.get('#email').should('exist');
-				cy.get('#email').should('have.text', 'Email:secretodelmar@gmail.com');
-
-				cy.get('#currentAddress').should('exist');
-				cy.get('#currentAddress').should('have.text', 'Current Address :Calle mar menor 5 ');
-
-				cy.get('#permanentAddress').should('exist');
-				cy.get('#permanentAddress').should('have.text', 'Permananet Address :Calle mar mayor 6');
-			});
+					cy.get(the.outputIds.userName).should('have.text', the.validOutputs.userName);
+					cy.get(the.outputIds.userEmail).should('have.text', the.validOutput.userEmail);
+					cy.get(the.outputIds.currentAddress).should('have.text', the.validOutput.currentAddress);
+					cy.get(the.outputIds.permanentAddress).should('have.text', the.validInput.permanentAddress);
+				});
 
 			// AAA
 			// Arrange
