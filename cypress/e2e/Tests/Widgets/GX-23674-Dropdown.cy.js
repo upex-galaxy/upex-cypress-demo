551a6c22a1471cf74ea222c4ff8012cbf1d31f02
ElenaValera <secretodelmar@gmail>
2023-07-12 17:59:21 +0200

    "GX-23674-TC2-implementando modo random"

---
+++ cypress/e2e/Tests/Widgets/GX-23674-Dropdown.cy.js
@@ -0,0 +1,28 @@
+describe('US GX-23674 | TS: ✅ToolsQA | Widgets | Dropdown - Select Menu', () => {
+	beforeEach('Precondition: Visit website DemoQA', () => {
+		cy.visit('https://demoqa.com/select-menu');
+	});
+
+	it('23674 |TC1: Validate select option 1 in Select Value', () => {
+		cy.get('#withOptGroup').click();
+		cy.get('#react-select-2-option-0-0').click();
+		cy.get('[class$=singleValue]').should('have.text', 'Group 1, option 1');
+	});
+
+	it('23674 |TC2: Select title in Select One', () => {
+		const random = Math.floor(Math.random() * 6);
+
+		cy.get('#selectOne').click();
+
+		cy.get('[id^="react-select-3-option-0"]')
+			.eq(random)
+			.click()
+			.invoke('text')
+			.then(prueba => {
+				cy.get('.css-1pahdxg-control > .css-1hwfws3').should('have.text', prueba);
+			});
+	});
+});
+
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
