50ae505759dc22649fdc7ba892c24d7be79afaca
ElenaValera <secretodelmar@gmail>
2023-07-13 09:06:33 +0200

    "GX-23674 commit TC2"

--- cypress/e2e/Tests/Widgets/GX-23674-Dropdown.cy.js
+++ cypress/e2e/Tests/Widgets/GX-23674-Dropdown.cy.js
@@ -9,7 +9,7 @@ describe('US GX-23674 | TS: ✅ToolsQA | Widgets | Dropdown - Select Menu', () =
 		cy.get('[class$=singleValue]').should('have.text', 'Group 1, option 1');
 	});
 
-	it('23674 |TC2: Select title in Select One', () => {
+	it('23674 |TC2: Validate Select title in Select One', () => {
 		const random = Math.floor(Math.random() * 6);
 
 		cy.get('#selectOne').click();
@@ -21,6 +21,9 @@ describe('US GX-23674 | TS: ✅ToolsQA | Widgets | Dropdown - Select Menu', () =
 			.then(prueba => {
 				cy.get('.css-1pahdxg-control > .css-1hwfws3').should('have.text', prueba);
 			});
+	});
+	it('23674 |TC3: Validate select Old style Select Menu', () => {
+		cy.get('#oldSelectMenu').select('red');
 	});
 });
 
