5ebc8ca47ea9e0732891760c86eefc19ef0aea98
JonatanDavidGauto <elchoni90@gmail.com>
2023-07-13 23:48:10 +0200

    GX2-4771: Se creo el TC02

--- cypress/e2e/Tests/Elements/GX2-4771-CheckBox.cy.js
+++ cypress/e2e/Tests/Elements/GX2-4771-CheckBox.cy.js
@@ -14,4 +14,14 @@ describe('GX2-4771-✅-tools-qa-elements-check-box', () => {
 			Selected.get.FolderExpanded().should('have.lengthOf', the.Folder.MaxFolderOpen);
 		});
 	});
+
+	it('4772 | TC2: Validar el funcionamiento del Botón “Collapse All”.', () => {
+		cy.fixture('data/GX2-4771-CheckBox').then(the => {
+			//Precondition the whole folder is expanded
+			Selected.get.ButtonExpandAll().click();
+			// Use the collapse all button to close all folders.
+			Selected.get.ButtonCollapseAll().click();
+			Selected.get.FolderExpanded().should('have.length', the.Folder.MinFolderOpen);
+		});
+	});
 });
