c2be2f1a717fe6a058d78bb0c646024ce91bf60c
ElenaValera <secretodelmar@gmail>
2023-07-13 18:42:19 +0200

    "GX-23674 fixing TC3 fixture-invoke-expect

--- cypress/e2e/Tests/Widgets/GX-23674-Dropdown.cy.js
+++ cypress/e2e/Tests/Widgets/GX-23674-Dropdown.cy.js
@@ -9,7 +9,7 @@ describe('US GX-23674 | TS: ✅ToolsQA | Widgets | Dropdown - Select Menu', () =
 		cy.get('[class$=singleValue]').should('have.text', 'Group 1, option 1');
 	});
 
-	it('23674 |TC2: Validate Select title in Select One', () => {
+	it('23674 |TC2: Validate Select random title in Select One', () => {
 		const random = Math.floor(Math.random() * 6);
 
 		cy.get('#selectOne').click();
@@ -22,8 +22,32 @@ describe('US GX-23674 | TS: ✅ToolsQA | Widgets | Dropdown - Select Menu', () =
 				cy.get('.css-1pahdxg-control > .css-1hwfws3').should('have.text', prueba);
 			});
 	});
-	it('23674 |TC3: Validate select Old style Select Menu', () => {
-		cy.get('#oldSelectMenu').select('red');
+	it.only('23674 |TC3: Validate select a different random option in Old style Select Menu', () => {
+		// Act
+		// Selecciona un valor aleatorio del dropdown del Old Style
+		const numRandom = Cypress._.random(0, 10);
+		cy.fixture('data/GX-23674-Dropdown').then(the => {});
+		cy.visit('/select-menu');
+		cy.get('#oldSelectMenu').select(numRandom);
+		cy.log(the[numRandom]);
+		cy.get('#oldSelectMenu')
+			.invoke('text')
+			.then(textColor => {
+				cy.log(textColor);
+				expect(textColor).to.contain(the[numRandom]);
+			});
+		// get the text of the element in the 'random' position
+
+		// Assert
+		// El valor seleccionado aparece correctamente en el selector
+	});
+
+	it('23674 |TC4: Validate select all options in Multiselect Dropdown', () => {
+		cy.get('[class=" css-yk16xz-control"]').eq(2).click();
+		cy.get('#react-select-4-option-0').click();
+		cy.get('#react-select-4-option-1').click();
+		cy.get('#react-select-4-option-2').click();
+		cy.get('#react-select-4-option-3').click();
 	});
 });
 
