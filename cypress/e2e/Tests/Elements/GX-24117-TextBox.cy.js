9377bc3b913d8ded002178c8e54b2e1073bd4c26
Braian Ferreyra <braianezequielferreyra39936964@gmail.com>
2023-07-19 12:26:03 +0200

    GX-24117 commit segundo caso de prueba

--- cypress/e2e/Tests/Elements/GX-24117-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-24117-TextBox.cy.js
@@ -6,21 +6,37 @@ describe('24117 | TS: | Elements | Text Box', () => {
 		cy.visit('https://demoqa.com/text-box');
 	});
 
-	it.only('24117| TC1: Validar completar los campos con datos validos.', () => {
-		cy.fixture('data/GX-24117-TexBox.json').then((the)=> {
+	it('24117| TC1: Validar completar los campos con datos validos.', () => {
+		cy.fixture('data/GX-24117-TexBox.json').then((the) => {
 			cy.get(the.FullName.input).type(the.FullName.data.valid);
 			cy.get(the.FullName.input).should('have.value', the.FullName.data.valid);
 			cy.get(the.Email.input).type(the.Email.data.valid);
+			cy.get(the.Email.input).should('have.value', the.Email.data.valid);
 			cy.get(the.CurrentAddress.input).type(the.CurrentAddress.data.valid);
+			cy.get(the.CurrentAddress.input).should('have.value', the.CurrentAddress.data.valid);
 			cy.get(the.PermanentAddress.input).type(the.PermanentAddress.data.valid);
+			cy.get(the.PermanentAddress.input).should('have.value', the.PermanentAddress.data.valid);
 			cy.get('#submit').click();
 			cy.get('#name').should('contain', the.FullName.data.valid);
+			cy.get('#email').should('contain', the.Email.data.valid);
+			cy.get('[class="mb-1"]').should('contain', the.CurrentAddress.data.valid);
+			cy.get('[class="mb-1"]').should('contain', the.CurrentAddress.data.valid);
 			
 		});    
 		
 	});
 	it('24117| TC2: Validar al enviar el formulario cuando todos los campos son vacíos, no se muestre ningún mensaje.', () => {
-		cy.get();
+		cy.fixture('data/GX-24117-TexBox.json').then((the) => {
+			cy.get(the.FullName.input).should('be.empty');
+			cy.get(the.Email.input).should('be.empty');
+			cy.get(the.CurrentAddress.input).should('be.empty');
+			cy.get(the.PermanentAddress.input).should('be.empty');
+			cy.get('#submit').click();
+			cy.get('#name').should('not.exist');
+			cy.get('#email').should('not.exist');
+			cy.get('[class="mb-1"]').should('not.exist');
+			cy.get('[class="mb-1"]').should('not.exist');
+		});
 	});
 
 });
