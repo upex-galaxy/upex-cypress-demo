e85b7dbd55f8be6225502330286a09c68259405f
natipedraza22 <nati_oric77@hotmail.com>
2023-07-19 16:20:14 +0200

    GX-24109 Se corrige sintaxis de assertions

--- cypress/e2e/Tests/Elements/GX-24109-TextBoxFormAndSubmit.cy.js
+++ cypress/e2e/Tests/Elements/GX-24109-TextBoxFormAndSubmit.cy.js
@@ -3,7 +3,7 @@ describe(' GX-24109 | ✅ToolsQA | Elements | Text Box: Fill form and Submit', (
 		cy.visit('/text-box');
 	});
 
-	it('GX-24109 | TC1: Validar que al estar los campos llenos para “Nombre Completo”, “Dirección Actual” y “Dirección Permanente” , la cadena se muestre a continuación como un párrafo después de enviar.', () => {
+	it('GX-24109 | TC1: Validar el funcionamiento del formulario al completarlo con datos correctos.', () => {
 		cy.fixture('data/GX-24109-TextBoxForm').then(the => {
 			cy.get(the.FullName.input).type(the.FullName.data.valid); //campo nombre completo lleno
 			cy.get(the.FullName.input).should('have.value', the.FullName.data.valid);
@@ -12,13 +12,13 @@ describe(' GX-24109 | ✅ToolsQA | Elements | Text Box: Fill form and Submit', (
 			cy.get(the.PermanentAddress.input).type(the.PermanentAddress.data.valid); //campo permanente actual lleno
 			cy.get(the.PermanentAddress.input).should('have.value', the.PermanentAddress.data.valid);
 			cy.get(the.SubmitButton).click(); //click en boton
-			cy.get('#name').should('contain.text', 'Name:NataliaPedraza');
+			cy.get('#name').should('have.text', 'Name:NataliaPedraza');
 			cy.get('#currentAddress').should('have.value', the.CurrentAddress.data.valid);
 			cy.get('#permanentAddress').should('have.value', the.PermanentAddress.data.valid);
 		});
 	});
 
-	it('GX-24109 |TC2: Validar que al estar los campos vacíos para “Nombre Completo”, “Dirección Actual” y “Dirección Permanente” , no se muestre ningún mensaje de registro después del envío.', () => {
+	it('GX-24109 |TC2: Validar el no funcionamiento del formulario al quedar sus campos vacíos.', () => {
 		cy.fixture('data/GX-24109-TextBoxForm').then(the => {
 			cy.get(the.FullName.input).should('be.empty'); //campo nombre completo vacio
 			cy.get(the.CurrentAddress.input).should('be.empty'); //campo direccion actual  vacio
@@ -27,16 +27,16 @@ describe(' GX-24109 | ✅ToolsQA | Elements | Text Box: Fill form and Submit', (
 		});
 	});
 
-	it('GX-24109 | TC3: Validar que el campo "Correo electrónico" sea válido si contiene @,mínimo 1 caracter alfanumérico antes de “@”,1 carácter alfanumérico después de “@” y  2 caracteres alfanuméricos después de “.” ', () => {
+	it('GX-24109 | TC3: Validar el funcionamiento del campo del correo electrónico. ', () => {
 		cy.fixture('data/GX-24109-TextBoxForm').then(the => {
 			cy.get(the.Email.input).type(the.Email.data.valid); //campo email valido
 			cy.get(the.Email.input).should('have.value', the.Email.data.valid);
 			cy.get(the.SubmitButton).click(); //click en boton
-			cy.get('#email').should('contain.text', 'Email:natipedraza22@gmail.com');
+			cy.get('#email').should('have.text', 'Email:natipedraza22@gmail.com');
 		});
 	});
 
-	it('GX-24109 |  TC4: Validar que el campo "Correo electrónico" sea inválido sino contiene @.', () => {
+	it('GX-24109 |  TC4: Validar el no funcionamiento del input correo electrónico si el mismo no tiene @.', () => {
 		cy.fixture('data/GX-24109-TextBoxForm').then(the => {
 			cy.get(the.Email.input).type(the.Email.data.invalid1); //no contiene @.
 			cy.get(the.Email.input).should('have.value', the.Email.data.invalid1);
@@ -46,7 +46,7 @@ describe(' GX-24109 | ✅ToolsQA | Elements | Text Box: Fill form and Submit', (
 		});
 	});
 
-	it('GX-24109 |  TC5: Validar que el campo "Correo electrónico" sea inválido sino contiene (mínimo) 1 carácter alfanumérico antes de “@” .', () => {
+	it('GX-24109 |  TC5: Validar el no funcionamiento del input correo electrónico si el mismo no tiene (mínimo) 1 carácter alfanumérico antes de “@” .', () => {
 		cy.fixture('data/GX-24109-TextBoxForm').then(the => {
 			cy.get(the.Email.input).type(the.Email.data.invalid2); //no contiene (mínimo) 1 carácter alfanumérico antes de “@” .
 			cy.get(the.Email.input).should('have.value', the.Email.data.invalid2);
@@ -56,7 +56,7 @@ describe(' GX-24109 | ✅ToolsQA | Elements | Text Box: Fill form and Submit', (
 		});
 	});
 
-	it('GX-24109 |  TC6: Validar que el campo "Correo electrónico" sea inválido sino contiene "." después: 1 carácter alfanumérico después de “@”.', () => {
+	it('GX-24109 |  TC6: Validar el no funcionamiento del input correo electrónico si el mismo no tiene "." después: 1 carácter alfanumérico después de “@”.', () => {
 		cy.fixture('data/GX-24109-TextBoxForm').then(the => {
 			cy.get(the.Email.input).type(the.Email.data.invalid3); //no contiene "." después: 1 carácter alfanumérico después de “@”.
 			cy.get(the.Email.input).should('have.value', the.Email.data.invalid3);
@@ -66,7 +66,7 @@ describe(' GX-24109 | ✅ToolsQA | Elements | Text Box: Fill form and Submit', (
 		});
 	});
 
-	it('GX-24109 | TC7: Validar que el campo "Correo electrónico" sea inválido sino contiene (mínimo) 2 caracteres alfanuméricos después de “.”', () => {
+	it('GX-24109 | TC7: Validar el no funcionamiento del input correo electrónico si el mismo no tiene (mínimo) 2 caracteres alfanuméricos después de “.”.', () => {
 		cy.fixture('data/GX-24109-TextBoxForm').then(the => {
 			cy.get(the.Email.input).type(the.Email.data.invalid4); //no contiene (mínimo) 2 caracteres alfanuméricos después de “.”
 			cy.get(the.Email.input).should('have.value', the.Email.data.invalid4);
