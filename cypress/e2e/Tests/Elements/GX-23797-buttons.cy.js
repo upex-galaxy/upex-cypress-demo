ef64c3535170428d9c9c7c3e52810dfc9ba05bde
karina <karinamon1849@gmail.com>
2023-07-21 10:17:57 -0300

    primer commit

---
+++ cypress/e2e/Tests/Elements/GX-23797-buttons.cy.js
@@ -0,0 +1,5 @@
+describe('GX-23797 | ToolsQA | Elements | Text box:Fill form and Submit', () => {
+	beforeEach('Precondicion: El usuario debe estar ubicado en la pagina del PLP', () => {
+		cy.visit('https://demoqa.com/text-box');
+	});
+});
