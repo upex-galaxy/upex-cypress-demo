c13de4a157b9c949817e0af66cb90e7a01263a8d
Braian Ferreyra <braianezequielferreyra39936964@gmail.com>
2023-07-23 18:22:22 +0200

    GX-25499 commit del prmer TC.

---
+++ cypress/e2e/Tests/Elements/GX-25499-Dropdown.cy.js
@@ -0,0 +1,15 @@
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
+Cypress._.random(0, 5);
+ 
+
+describe('25499 | TS: | Widgets | Dropdown - Select Menu', () => {
+	beforeEach('visitar la página de Demo QA', () => {
+		cy.visit('https://demoqa.com/select-menu');
+	});
+	it('25499| TC1: Validar poder seleccionar una opcion del desplegable "Select Value."', () => {
+		cy.get('[class$=indicatorContainer]').eq(0).click();
+		cy.get('[id^="react-select-2-option"]').eq(Cypress._.random(0, 5)).click();
+	});
+	
+});
