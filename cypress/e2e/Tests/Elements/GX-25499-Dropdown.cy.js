3506596871f235606688b7bc98a6e7cf4fd6b372
Braian Ferreyra <braianezequielferreyra39936964@gmail.com>
2023-07-24 11:22:57 +0200

    GX-25499 commit del tercer y cuarto TC.

--- cypress/e2e/Tests/Elements/GX-25499-Dropdown.cy.js
+++ cypress/e2e/Tests/Elements/GX-25499-Dropdown.cy.js
@@ -11,10 +11,29 @@ describe('25499 | TS: | Widgets | Dropdown - Select Menu', () => {
 		cy.get('[class$=indicatorContainer]').eq(0).click();
 		cy.get('[id^="react-select-2-option"]').eq(Cypress._.random(0, 5)).click();
 	});
-	it('25499| TC2: Validar poder seleccionar una opcion del desplegable "Select One."', () => {
+	it('25499| TC2: Validar poder seleccionar un titulo del desplegable "Select One."', () => {
 		cy.get('[class$=indicatorContainer]').eq(1).click();
 		cy.get('[id^="react-select-3-option"]').eq(Cypress._.random(0, 5)).click();
 	});
+	it('25499| TC3: Validar poder seleccionar una opcion en el "Old Style Select Menu"', () => {
+		cy.get('#oldSelectMenu option').then(options => {
+			const randomIndex = Cypress._.random(0, options.length - 1);
+			const optionValue = options[randomIndex].value;
+			cy.get('#oldSelectMenu').select(optionValue);
+		});
+		
+	});
+	it('25499| TC4: Validar poder seleccionar hasta cuatro opciones en el desplegable "Multiselect drop down"', () => {
+		cy.get('[Class$="css-yk16xz-control"]').eq(2).click();
+		cy.get('[id^="react-select-4-option"]').eq(3).click();
+		cy.get('[id^="react-select-4-option"]').eq(2).click();
+		cy.get('[id^="react-select-4-option"]').eq(1).click();
+		cy.get('[id^="react-select-4-option"]').eq(0).click();
+		
+	});
+
+	
+	
 	
 	
 });
