69a60a4cd8295e4a72cdcc79485a4935703ad72d
Braian Ferreyra <braianezequielferreyra39936964@gmail.com>
2023-07-26 22:43:57 +0200

    GX2-5129 test sin POM

--- cypress/support/pages/Interactions/GX2-5129-Selectable.page.js
+++ cypress/support/pages/Interactions/GX2-5129-Selectable.page.js
@@ -1,14 +1,17 @@
 class Selectable {
-	get = {
+	get={
 		bttnList: () => cy.get('#demo-tab-list'),
 		bttnGrid: () => cy.get('#demo-tab-grid'),
-		listContainer: () => cy.get('#verticalListContainer li'),
+		listContainer:()=> cy.get('#verticalListContainer li'),
 	};
 	clickBttnGrid() {
 		this.get.bttnGrid().click();
 	}
 	clickBttnList() {
 		this.get.bttnList().click();
+	}
+	clickElementList(num) {
+		this.get.listContainer().eq(num).click();
 	}
 }
 export const selectable = new Selectable();
