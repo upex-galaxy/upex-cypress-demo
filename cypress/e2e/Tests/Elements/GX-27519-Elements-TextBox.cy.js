860e3a0a3f5ceb787ea5682eb07b706e6ecb5f95
Lucas Ramirez <rlucasmanuel@gmail.com>
2023-08-10 16:02:22 -0300

    creacion TC1

--- cypress/e2e/Tests/Elements/GX-27519-Elements-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-27519-Elements-TextBox.cy.js
@@ -1,31 +1,20 @@
-describe('TS#27520 : ✅ToolsQA | Elements | Text Box: Fill form and Submit', () =>
-{
-	beforeEach('Precondición: user should be stay in register page', () =>
-	{
-		cy.visit('https://demoqa.com/text-box') // Esto es un Comando de Acción directa
-	})
-	it('US # | TC#1: 27520 | Validate submit fill form with empty data', () =>
-	{
-		cy.get('[href="/commands/querying"]') // Sintaxis Comando Get de Selector de Atributos
-			.eq(2) // Esto es un método opcional
-			.click() // Esto es un método de Interacción
-	})
-	it('US # | TC#2: Aquí puedes escribir otro Caso de Prueba', () =>
-	{
-		cy.get('[href="/commands/querying"]')
-			.eq(2) 
-			.click() 
-		cy.get("#query-btn") // Sintaxis Comando Get de Selector de IDs
-			.click() // Esto es un método de Interacción (Acción)
-	})
-	it('US # | TC#3: Aquí puedes escribir otro Caso de Prueba', () =>
-	{
-		cy.get('[href="/commands/querying"]')
-			.eq(2) 
-			.click() 
-		cy.get("#query-btn")
-			.click()
-		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
-			.should('contain.text', 'Button') // Assertion BDD para validar resultado esperado (de muchas formas)
-	})
-})
+describe('TS#27520 : ✅ToolsQA | Elements | Text Box: Fill form and Submit', () => {
+	beforeEach('Precondition: user should be stay in register page', () => {
+		cy.visit('https://demoqa.com/text-box'); // Esto es un Comando de Acción directa
+		cy.url().should('contain', 'text-box');
+	});
+	it('US # | TC#1: 27520 | Validate submit fill form with empty data', () => {
+		cy.fixture('data/GX-27519-TextBox').then(the => {
+			//complete form
+			cy.get(the.fullName.input).type.apply(the.fullName.data.empty);
+			//click Submit
+			cy.get(the.buttonSubmit).click();
+			//assert
+			cy.get("div.border.col-md-12.col-sm-12").;
+		});
+	});
+});
+
+// cy.get(the.email.input).type.apply(the.email.data.empty);
+// cy.get(the.currentAddress.input).type.apply(the.currentAddress.data.empty);
+// cy.get(the.permanentAddress.input).type.apply(the.permanentAddress.data.empty);
