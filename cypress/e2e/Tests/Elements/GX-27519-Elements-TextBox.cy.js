786b17cbed878ae6c42307eb3cc3421e182e62d7
Lucas Ramirez <rlucasmanuel@gmail.com>
2023-08-11 15:48:51 -0300

    11/08 Realizacion TC1, 2 y 3

--- cypress/e2e/Tests/Elements/GX-27519-Elements-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-27519-Elements-TextBox.cy.js
@@ -5,16 +5,144 @@ describe('TS#27520 : ✅ToolsQA | Elements | Text Box: Fill form and Submit', ()
 	});
 	it('US # | TC#1: 27520 | Validate submit fill form with empty data', () => {
 		cy.fixture('data/GX-27519-TextBox').then(the => {
-			//complete form
-			cy.get(the.fullName.input).type.apply(the.fullName.data.empty);
+			//complete Form
+			cy.get(the.fullName.input).should('be.empty');
+			cy.get(the.email.input).should('be.empty');
+			cy.get(the.currentAddress.input).should('be.empty');
+			cy.get(the.permanentAddress.input).should('be.empty');
 			//click Submit
 			cy.get(the.buttonSubmit).click();
 			//assert
-			cy.get("div.border.col-md-12.col-sm-12").;
+			cy.get(the.fullName.output).should('not.exist');
+			cy.get(the.email.output).should('not.exist');
+			cy.get(the.currentAddress.output).should('not.exist');
+			cy.get(the.permanentAddress.output).should('not.exist');
 		});
 	});
+	it('US # | TC#2: 27520 | Validate submit fill form with valid data', () => {
+		cy.fixture('data/GX-27519-TextBox').then(the => {
+			//complete Form
+			cy.get(the.fullName.input).type(the.fullName.data.valid);
+			cy.get(the.fullName.input).should('have.value', the.fullName.data.valid);
+			cy.get(the.email.input).type(the.email.data.valid);
+			cy.get(the.email.input).should('have.value', the.email.data.valid);
+			cy.get(the.currentAddress.input).type(the.currentAddress.data.valid);
+			cy.get(the.currentAddress.input).should('have.value', the.currentAddress.data.valid);
+			cy.get(the.permanentAddress.input).type(the.permanentAddress.data.valid);
+			cy.get(the.permanentAddress.input).should('have.value', the.permanentAddress.data.valid);
+			//click Submit
+			cy.get(the.buttonSubmit).click();
+			//assert
+			cy.get(the.fullName.output).should('contain', the.fullName.data.valid);
+			cy.get(the.email.output).should('contain', the.email.data.valid);
+			cy.get(the.currentAddress.output).should('contain', the.currentAddress.data.valid);
+			cy.get(the.permanentAddress.output).should('contain', the.permanentAddress.data.valid);
+		});
+	});
+	it('US # | TC#3: 27520 | Validate not submit fill form when email not contain “@”', () => {
+		cy.fixture('data/GX-27519-TextBox').then(the => {
+			//complete Form
+			cy.get(the.fullName.input).type(the.fullName.data.validNumber);
+			cy.get(the.fullName.input).should('have.value', the.fullName.data.validNumber);
+			cy.get(the.email.input).type(the.email.data.invalid.not_At);
+			cy.get(the.email.input).should('have.value', the.email.data.invalid.not_At);
+			cy.get(the.currentAddress.input).type(the.currentAddress.data.validNumber);
+			cy.get(the.currentAddress.input).should('have.value', the.currentAddress.data.validNumber);
+			cy.get(the.permanentAddress.input).type(the.permanentAddress.data.validNumber);
+			cy.get(the.permanentAddress.input).should('have.value', the.permanentAddress.data.validNumber);
+			//click Submit
+			cy.get(the.buttonSubmit).click();
+			//assert
+			cy.get(the.email.input).should('have.css', 'border', '1px solid rgb(255, 0, 0)');
+			cy.get(the.fullName.output).should('not.exist');
+			cy.get(the.email.output).should('not.exist');
+			cy.get(the.currentAddress.output).should('not.exist');
+			cy.get(the.permanentAddress.output).should('not.exist');
+		});
+	});
+	// it('US # | TC#4: 27520 | ', () => {
+	// 	cy.fixture('data/GX-27519-TextBox').then(the => {
+	// 		//complete Form
+	// 		cy.get(the.fullName.input).type(the.fullName.data.valid);
+	// 		cy.get(the.fullName.input).should('be.empty');
+	// 		cy.get(the.email.input).type(the.email.data.valid);
+	// 		cy.get(the.email.input).should('be.empty');
+	// 		cy.get(the.currentAddress.input).type(the.currentAddress.data.valid);
+	// 		cy.get(the.currentAddress.input).should('be.empty');
+	// 		cy.get(the.permanentAddress.input).type(the.permanentAddress.data.valid);
+	// 		cy.get(the.permanentAddress.input).should('be.empty');
+	// 		//click Submit
+	// 		cy.get(the.buttonSubmit).click();
+	// 		//assert
+	// 		cy.get(the.fullName.output).should('contain', the.fullName.data.valid);
+	// 		cy.get(the.email.output).should('contain', the.email.data.valid);
+	// 		cy.get(the.currentAddress.output).should('contain', the.currentAddress.data.valid);
+	// 		cy.get(the.permanentAddress.output).should('contain', the.permanentAddress.data.valid);
+	// 	});
+	// });
+	// it('US # | TC#5: 27520 | ', () => {
+	// 	cy.fixture('data/GX-27519-TextBox').then(the => {
+	// 		//complete Form
+	// 		cy.get(the.fullName.input).type(the.fullName.data.valid);
+	// 		cy.get(the.fullName.input).should('be.empty');
+	// 		cy.get(the.email.input).type(the.email.data.valid);
+	// 		cy.get(the.email.input).should('be.empty');
+	// 		cy.get(the.currentAddress.input).type(the.currentAddress.data.valid);
+	// 		cy.get(the.currentAddress.input).should('be.empty');
+	// 		cy.get(the.permanentAddress.input).type(the.permanentAddress.data.valid);
+	// 		cy.get(the.permanentAddress.input).should('be.empty');
+	// 		//click Submit
+	// 		cy.get(the.buttonSubmit).click();
+	// 		//assert
+	// 		cy.get(the.fullName.output).should('contain', the.fullName.data.valid);
+	// 		cy.get(the.email.output).should('contain', the.email.data.valid);
+	// 		cy.get(the.currentAddress.output).should('contain', the.currentAddress.data.valid);
+	// 		cy.get(the.permanentAddress.output).should('contain', the.permanentAddress.data.valid);
+	// 	});
+	// });
+	// it('US # | TC#6: 27520 | ', () => {
+	// 	cy.fixture('data/GX-27519-TextBox').then(the => {
+	// 		//complete Form
+	// 		cy.get(the.fullName.input).type(the.fullName.data.valid);
+	// 		cy.get(the.fullName.input).should('be.empty');
+	// 		cy.get(the.email.input).type(the.email.data.valid);
+	// 		cy.get(the.email.input).should('be.empty');
+	// 		cy.get(the.currentAddress.input).type(the.currentAddress.data.valid);
+	// 		cy.get(the.currentAddress.input).should('be.empty');
+	// 		cy.get(the.permanentAddress.input).type(the.permanentAddress.data.valid);
+	// 		cy.get(the.permanentAddress.input).should('be.empty');
+	// 		//click Submit
+	// 		cy.get(the.buttonSubmit).click();
+	// 		//assert
+	// 		cy.get(the.fullName.output).should('contain', the.fullName.data.valid);
+	// 		cy.get(the.email.output).should('contain', the.email.data.valid);
+	// 		cy.get(the.currentAddress.output).should('contain', the.currentAddress.data.valid);
+	// 		cy.get(the.permanentAddress.output).should('contain', the.permanentAddress.data.valid);
+	// 	});
+	// });
+	// it('US # | TC#7: 27520 | ', () => {
+	// 	cy.fixture('data/GX-27519-TextBox').then(the => {
+	// 		//complete Form
+	// 		cy.get(the.fullName.input).type(the.fullName.data.valid);
+	// 		cy.get(the.fullName.input).should('be.empty');
+	// 		cy.get(the.email.input).type(the.email.data.valid);
+	// 		cy.get(the.email.input).should('be.empty');
+	// 		cy.get(the.currentAddress.input).type(the.currentAddress.data.valid);
+	// 		cy.get(the.currentAddress.input).should('be.empty');
+	// 		cy.get(the.permanentAddress.input).type(the.permanentAddress.data.valid);
+	// 		cy.get(the.permanentAddress.input).should('be.empty');
+	// 		//click Submit
+	// 		cy.get(the.buttonSubmit).click();
+	// 		//assert
+	// 		cy.get(the.fullName.output).should('contain', the.fullName.data.valid);
+	// 		cy.get(the.email.output).should('contain', the.email.data.valid);
+	// 		cy.get(the.currentAddress.output).should('contain', the.currentAddress.data.valid);
+	// 		cy.get(the.permanentAddress.output).should('contain', the.permanentAddress.data.valid);
+	// 	});
+	// });
 });
+//Se importa la funcion
+import { removeLogs } from '@helper/RemoveLogs';
 
-// cy.get(the.email.input).type.apply(the.email.data.empty);
-// cy.get(the.currentAddress.input).type.apply(the.currentAddress.data.empty);
-// cy.get(the.permanentAddress.input).type.apply(the.permanentAddress.data.empty);
+//Se ejecuta la funcion para evitar los errores (puedes hacerlo al final de tu codigo)
+removeLogs();
