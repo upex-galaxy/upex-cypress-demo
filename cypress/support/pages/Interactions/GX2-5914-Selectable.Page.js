ca77db3ccc4ad9be10f80a9e1d3626dbeeb0746b
Lucas Ramirez <rlucasmanuel@gmail.com>
2023-08-22 12:45:22 -0300

    GX2-5914 Test#03 possible grid defect

--- cypress/support/pages/Interactions/GX2-5914-Selectable.Page.js
+++ cypress/support/pages/Interactions/GX2-5914-Selectable.Page.js
@@ -4,6 +4,7 @@ class Selectable {
 		tabList: () => cy.get('[id="demo-tab-list"]'),
 		tabGrid: () => cy.get('[id="demo-tab-grid"]'),
 		listItem: () => cy.get('#verticalListContainer li'),
+		gridContainer: () => cy.get('#gridContainer'),
 		gridItem: () => cy.get('#gridContainer li'),
 	};
 	selectTabList() {
