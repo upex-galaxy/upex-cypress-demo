b03880055b17d37ca420a3ac4f8285ef2b5d9665
Jose Rosado <josea.rosadoa@gmail.com>
2023-08-22 11:37:45 -0400

    GX-29181: Agregando cambios

---
+++ cypress/e2e/Tests/Elements/GX-29181_Elements_Buttons.cy.js
@@ -0,0 +1,8 @@
+
+describe("TS GX-29182: ToolsQA | Elements | Buttons", () => {
+
+    beforeEach('Precondition: Usuario debe encontrarse en la web de ToolsQ, en el endpoint de buttons', () => {
+
+        cy.visit('https://demoqa.com/buttons')
+    });
+})
