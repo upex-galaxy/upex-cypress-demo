cb0032a3cadd4f6ec236a4a9287896ed348e490c
igflores <139146133+igflores@users.noreply.github.com>
2023-08-23 11:06:55 -0700

    avance

--- cypress/e2e/Tests/Elements/GX-29819-RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-29819-RadioButtons.cy.js
@@ -1,2 +1,24 @@
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
+
 //* Test Set:
-describe('🪶ToolsQA | Elements | Radio Buttons', () => {});
+describe('🪶ToolsQA | Elements | Radio Buttons', () => {
+	beforeEach('Estar ubicado en Radio Button Page', () => {
+		cy.visit('https://demoqa.com/radio-button');
+		cy.url().should('contain', 'radio-button');
+	});
+
+	it('GX-29820 | TC01: Validar visualizar el mensaje “You have selected Yes ”, al seleccionar el radio-button “Yes”', () => {
+		cy.get('#yesRadio').click({ force: true });
+		cy.get('.mt-3').should('contain', 'You have selected Yes');
+	});
+
+	it('GX-29820 | TC02: Validar visualizar el mensaje “You have selected Impressive”, al seleccionar el radio-button “Impressive”', () => {
+		cy.get('#impressiveRadio').click({ force: true });
+		cy.get('.mt-3').should('contain', 'You have selected Impressive');
+	});
+
+	it('GX-29820 | TC03: Validar NO seleccionar el radio-button “No“ con el cursor del mouse', () => {
+		cy.get('#noRadio').check({ force: true }).should('be.disabled');
+	});
+});
