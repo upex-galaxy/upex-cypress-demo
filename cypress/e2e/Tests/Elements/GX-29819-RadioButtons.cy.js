24f1957307a7d05678fb5a92a7963380e6aa69de
igflores <139146133+igflores@users.noreply.github.com>
2023-08-24 09:41:22 -0700

    test(GX-29819): prueba automatizada para radio-buttons  Se actualizaron los 3 TC:  GX-29820 | TC01: Validar visualizar el label "Yes" cuando se selecciona el radio-button "Yes"  GX-29820 | TC02: Validar visualizar el label “Impressive” cuando se selecciona el radio-button "Impressive"  GX-29820 | TC03: Validar que esté deshabilitado y no se pueda seleccionar el radio-button con label "No"
     On branch GX-29819/test/radio-buttons
     Your branch is up to date with 'origin/GX-29819/test/radio-buttons'.

--- cypress/e2e/Tests/Elements/GX-29819-RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-29819-RadioButtons.cy.js
@@ -1,24 +1,27 @@
 import { removeLogs } from '@helper/RemoveLogs';
-removeLogs();
 
 //* Test Set:
 describe('🪶ToolsQA | Elements | Radio Buttons', () => {
-	beforeEach('Estar ubicado en Radio Button Page', () => {
-		cy.visit('https://demoqa.com/radio-button');
-		cy.url().should('contain', 'radio-button');
+	beforeEach('Precondición: Estar ubicado en Radio Button Page', () => {
+		const endpoint = '/radio-button'; //variable para guardar el endpoint
+		cy.visit(`/${endpoint}`); //navegar
 	});
 
-	it('GX-29820 | TC01: Validar visualizar el mensaje “You have selected Yes ”, al seleccionar el radio-button “Yes”', () => {
+	//todo: Test Cases
+	it('GX-29820 | TC01: Validar visualizar el label "Yes" cuando se selecciona el radio-button "Yes"', () => {
 		cy.get('#yesRadio').click({ force: true });
-		cy.get('.mt-3').should('contain', 'You have selected Yes');
+		cy.contains('You have selected').children().should('have.text', 'Yes');
 	});
 
-	it('GX-29820 | TC02: Validar visualizar el mensaje “You have selected Impressive”, al seleccionar el radio-button “Impressive”', () => {
+	it('GX-29820 | TC02: Validar visualizar el label “Impressive” cuando se selecciona el radio-button "Impressive"', () => {
 		cy.get('#impressiveRadio').click({ force: true });
-		cy.get('.mt-3').should('contain', 'You have selected Impressive');
+		cy.contains('You have selected').children().should('have.text', 'Impressive');
 	});
 
-	it('GX-29820 | TC03: Validar NO seleccionar el radio-button “No“ con el cursor del mouse', () => {
-		cy.get('#noRadio').check({ force: true }).should('be.disabled');
+	it('GX-29820 | TC03: Validar que esté deshabilitado y no se pueda seleccionar el radio-button con label "No"', () => {
+		cy.get('#noRadio').should('be.disabled');
+		cy.contains('You have selected').should('not.exist');
 	});
 });
+
+removeLogs();
