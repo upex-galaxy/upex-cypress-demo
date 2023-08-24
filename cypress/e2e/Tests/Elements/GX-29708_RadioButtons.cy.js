8fec500426102113d335658cc598228caf979dee
Juan Francisco Volpe <juanvolpe87@gmail.com>
2023-08-24 15:23:04 -0300

    Se agregó POM para enteder mejor la estructura

--- cypress/e2e/Tests/Elements/GX-29708_RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-29708_RadioButtons.cy.js
@@ -1,28 +1,27 @@
 import { removeLogs } from '@helper/RemoveLogs';
-removeLogs();
+import { radioButton } from '../../../support/pages/Elements/GX-290708_RadioButtons.Page';
 
-describe('ToolsQA | Elements | Radio Buttons', () => {
+describe('GX-29708 ToolsQA | Elements | Radio Buttons', () => {
 	beforeEach('Precondición: El usuario debe estar situado en la página web.', () => {
-		cy.visit('https://demoqa.com/radio-button');
+		cy.visit('/radio-button');
 		cy.url().should('contain', 'radio-button');
 	});
 
 	it('29735 | TC1: Validar seleccionar el RB cuando la opción está habilitada (Yes).', () => {
-		cy.get('label[for=yesRadio]').should('have.text', 'Yes');
-		cy.get('div label').first().click();
+		radioButton.clickButtonYes();
 		cy.get('.text-success').should('have.text', 'Yes');
 	});
 
 	it('29735 | TC2: Validar seleccionar el RB cuando la opción está habilitada (Impressive).', () => {
-		cy.get('label[for=impressiveRadio]').should('have.text', 'Impressive');
-		cy.get('div label').eq(1).click();
+		radioButton.clickButtonImpressive();
 		cy.get('.text-success').should('have.text', 'Impressive');
 	});
 
 	it('29735 | TC3: Validar NO seleccionar el RB cuando la opción está deshabilitada (No).', () => {
-		cy.get('label[for=noRadio]').should('have.text', 'No');
 		cy.get('div label').last().should('have.class', 'disabled');
-		cy.get('div label').last().click();
+		radioButton.clickButtonNo();
 		cy.get('.text-success').should('not.exist');
 	});
 });
+
+removeLogs();
