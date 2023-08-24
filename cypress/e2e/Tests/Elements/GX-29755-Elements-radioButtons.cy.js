713e24ebf375cacac9ad10c2316f5ab2412a9875
ElenaValera <secretodelmar@gmail>
2023-08-24 17:37:50 +0200

    "GX-29755 TC2-working green"

--- cypress/e2e/Tests/Elements/GX-29755-Elements-radioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-29755-Elements-radioButtons.cy.js
@@ -9,17 +9,16 @@ describe('Elements | ToolsQA | RadioButtons', () => {
 		cy.visit('https://demoqa.com/radio-button');
 	});
 	//test case
-	it.only('TC1: Validate visualize log message "you have selected yes" when select "yes"', () => {
+	it('TC1: Validate visualize log message "you have selected yes" when select "yes"', () => {
 		cy.get('#yesRadio').click({ force: true });
 		cy.get('.mt-3').should('have.text', 'You have selected Yes');
 	});
-});
 
-//test case
-//it('TC2: VALIDAR visualizar log message (you have selected impressive) al seleccionar (impressive)', () => {
-//	cy.get('#yesRadio').click();
-//	cy.contains('you have selected').should('have.text', 'yes');
-//});
+	it('TC2: Validate visualize log message "you have selected impressive" when select "impressive"', () => {
+		cy.get('#impressiveRadio').click({ force: true });
+		cy.get('.mt-3').should('have.text', 'You have selected Impressive');
+	});
+});
 ///test case
 //it('TC3: VALIDAR que el botón "no" esté deshabilitado ...', () => {
 //	cy.get('#noRadio').should('be.disabled');
