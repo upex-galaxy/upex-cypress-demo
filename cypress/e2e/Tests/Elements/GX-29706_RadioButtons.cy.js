a4389af32be49006368540974444d984fe7a6bfc
saiotest <elyermad@gmail.com>
2023-08-24 11:26:30 -0300

    test(GX-29706): added test ids On branch GX-29706/test/radio-buttons Changes to be committed: 	modified:   .github/workflows/CI-Suite.yml 	modified:   cypress/e2e/Tests/Elements/GX-29706_RadioButtons.cy.js

--- cypress/e2e/Tests/Elements/GX-29706_RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-29706_RadioButtons.cy.js
@@ -8,15 +8,15 @@ describe('GX-29706: ToolsQA | Elements | Radio Buttons', () => {
 	});
 
 	//todo: Test Cases
-	it('TC1: Validar visualizar el label YES cuando se selecciona el radio button Yes', () => {
+	it('GX-29711 | TC1: Validar visualizar el label YES cuando se selecciona el radio button Yes', () => {
 		cy.get('#yesRadio').click({ force: true });
 		cy.contains('You have selected ').children().should('have.text', 'Yes');
 	});
-	it('TC2: Validar visualizar el label Impressive cuando se selecciona el radio button Impresive', () => {
+	it('GX-29711 | TC2: Validar visualizar el label Impressive cuando se selecciona el radio button Impresive', () => {
 		cy.get('#impressiveRadio').click({ force: true });
 		cy.contains('You have selected ').children().should('have.text', 'Impressive');
 	});
-	it('TC3: Validar que el radio button con label "NO" esté deshabilitado y no se pueda seleccionar', () => {
+	it('GX-29711 | TC3: Validar que el radio button con label "NO" esté deshabilitado y no se pueda seleccionar', () => {
 		cy.get('#noRadio').should('be.disabled');
 		cy.contains('You have selected ').should('not.exist');
 	});
