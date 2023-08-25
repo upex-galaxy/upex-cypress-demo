289793748ff52e7a7515c1aef7928c8b942913d2
Missael1494 <lmisa1494@gmail.com>
2023-08-24 19:33:19 -0600

    test(GX-29736): new automated tests for radioButtons
    3 Test Cases were added to the Story Covergae
    TC1: Validar visualizar el label YES cuando se selecciona el radio button Yes
    TC2: Validar visualizar el label Impressive cuando se selecciona el radio button Impressive
    TC3: Validar que el radio button con label "NO" este deshabilitado y no se pueda seleccionar
    
    On branch GX-29736/test/radio-buttons
    Changes to be committed:
    	modified:   cypress/e2e/Tests/Elements/GX-29736_RadioButtons.cy.js

--- cypress/e2e/Tests/Elements/GX-29736_RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-29736_RadioButtons.cy.js
@@ -1,10 +1,43 @@
+import { removeLogs } from '@helper/RemoveLogs';
+
 //test set:
-describe.skip('ToolsQA | Elements | Radio Buttons', () => {
+describe('GX-29736 ToolsQA | Elements | Radio Buttons', () => {
+	//todo: precondiciones
+	beforeEach('Precondición', () => {
+		const endpoint = '/radio-button';
+		cy.visit(`/${endpoint}`);
+	});
 
+	// afterEach('', () => {
 
-})
+	// })
 
+	it('TC1: Validar visualizar el label', () => {
+		//tags
+		cy.get('#yesRadio').click({ force: true });
+		cy.contains('You have selected').children().should('have.text', 'Yes');
+		//cy.contains('You have selected').children().should.equal('yes');
+		//
+	});
 
+	it('TC2: Validar visualizar el label Impressive cuando se selecciona el radio button Impressive', () => {
+		//tags
+		cy.get('#impressiveRadio').click({ force: true });
+		cy.contains('You have selected').children().should('have.text', 'impressive');
+		//cy.contains('You have selected').children().should.equal('yes');
 
+		//
+	});
 
+	it('TC3: Validar que el radio button con label "NO" este deshabilitado y no se pueda seleccionar', () => {
+		//tags
+		cy.get('#noRadio').should('be.disabled');
+		cy.contains('You have selected').should('not.exist');
+		//cy.contains('You have selected').children().should.equal('yes');
 
+		//
+	});
+});
+
+//ES module imports/exports
+removeLogs();
