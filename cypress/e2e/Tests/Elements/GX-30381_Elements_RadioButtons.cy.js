288b8a9b49759458c01a17b61af815390880ab5a
Paola Rosales <pao.rosa944@gmail.com>
2023-08-24 23:45:21 -0300

    test(GX-30381)Se agrego ids a los tcs On branch GX-30381-�-tools-qa-elements-radio-buttons Changes to be committed: 	modified:   .github/workflows/CI-Suite.yml 	modified:   cypress/e2e/Tests/Elements/GX-30381_Elements_RadioButtons.cy.js

--- cypress/e2e/Tests/Elements/GX-30381_Elements_RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-30381_Elements_RadioButtons.cy.js
@@ -7,16 +7,16 @@ describe('ToolsQA | Elements | Radio Buttons', () => {
 		const endpoint = '/radio-button';
 		cy.visit('/' + endpoint);
 	});
-	it('TC1:Validar visualizar el label YES cuando se seleciona el radio button YES  ', () => {
+	it('GX-30382|TC1:Validar visualizar el label YES cuando se seleciona el radio button YES  ', () => {
 		cy.get('#yesRadio').click({ force: true });
 		cy.contains('You have selected').children().should('have.text', 'Yes');
 	});
-	it('TC2:Validar visualizar el label impressive cuando se seleciona el radio button impresivve ', () => {
+	it('GX-30382|TC2:Validar visualizar el label impressive cuando se seleciona el radio button impresivve ', () => {
 		cy.get('#impressiveRadio').click({ force: true });
 		cy.contains('You have selected').children().should('have.text', 'Impressive');
 	});
 
-	it('Validar que el radio button con label no este deshabilitado y no se puede seleccionar ', () => {
+	it('GX-30382|TC3:Validar que el radio button con label no este deshabilitado y no se puede seleccionar ', () => {
 		cy.get('#noRadio').should('be.disabled');
 		cy.contains('You have selected').should('not.exist');
 	});
