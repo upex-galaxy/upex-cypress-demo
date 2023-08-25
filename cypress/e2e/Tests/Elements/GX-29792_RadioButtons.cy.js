79ef23766941f35c2e2eea9c5f49542bd93771a8
Choquete99 <ezechoqueyave@gmail.com>
2023-08-24 21:36:12 -0300

    test(GX-29792): new automated tests for radioBtns
    3 Test Cases more added to the Story Coverage.
    GX-29973 | TC1: Validar visualizar el label YES cuando se selecciona el radio button Yes
    GX-29973 | TC2: Validar visualizar el label YES cuando se selecciona el radio button Impressive
    GX-29973 | TC3: Validar que el radio button con label "NO" esté deshabilitado y no se pueda seleccionar
    
    On branch GX-29792/test/radio-buttons
    Changes to be committed:
    	modified:   cypress.config.js
    	new file:   cypress/e2e/Tests/Elements/GX-29792_RadioButtons.cy.js

---
+++ cypress/e2e/Tests/Elements/GX-29792_RadioButtons.cy.js
@@ -0,0 +1,27 @@
+import { removeLogs } from '@helper/RemoveLogs';
+//* Test Set:
+describe('GX-29792: ToolsQA| Elements | Radio Buttons', () => {
+	//todo: Precondiciones
+	beforeEach('Precondición', () => {
+		const endpoint = '/radio-button';
+		cy.visit(`/${endpoint}`);
+	});
+
+	//todo: Test Cases
+	it('GX-29973 | TC1: Validar visualizar el label YES cuando se selecciona el radio button Yes', () => {
+		cy.get('#yesRadio').click({ force: true });
+		cy.contains('You have selected ').children().should('have.text', 'Yes');
+	});
+	//todo: Test Cases
+	it('GX-29973 | TC2: Validar visualizar el label YES cuando se selecciona el radio button Impressive', () => {
+		cy.get('#impressiveRadio').click({ force: true });
+		cy.contains('You have selected ').children().should('have.text', 'Impressive');
+	}); //todo: Test Cases
+	it('GX-29973 | TC3: Validar que el radio button con label "NO" esté deshabilitado y no se pueda seleccionar', () => {
+		cy.get('#noRadio').click({ force: true });
+		cy.contains('You have selected ').should('not.exist');
+	});
+});
+
+//ES Module imports/exports
+removeLogs();
