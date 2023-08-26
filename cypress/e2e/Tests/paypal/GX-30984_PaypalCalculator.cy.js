41ca3c8cca0e75c10c1b5aff05016fea981b0d86
Paola Rosales <pao.rosa944@gmail.com>
2023-08-26 19:14:10 -0300

    GX-30986 modificaciones en los archivos de tcs y archivo de test automation

--- cypress/e2e/Tests/paypal/GX-30984_PaypalCalculator.cy.js
+++ cypress/e2e/Tests/paypal/GX-30984_PaypalCalculator.cy.js
@@ -1,15 +1,32 @@
 import { removeLogs } from '@helper/RemoveLogs';
-import { calculatorPage} from '@pages/paypal/GX-30984_CalculatorPage';
+import { calculatorPage } from '@pages/paypal/GX-30944_CalculatorPage';
+import data from '../../../fixtures/data/GX-30984_CalculatorPaypal.json';
 
-describe('', () => {
-	beforeEach('', () => {
+describe('GX-30944: Paypal | Comisiones | Calcular las comisiones para enviar y recibir', () => {
+	beforeEach('Precondition: para usar la Calculadora Paypal', () => {
 		cy.visit('https://vendercomprardolares.com/calculadora-comisiones-paypal.php');
-        
-        
-        calculatorPage.get.paypalComission().should("have.value","")
-    
-    });
 
-	it.skip();
+		// *descructuración:
+		//estoy diciendo que me cree una constante y que guarde la clase calculatorPage.get
+		const { paypalFee, paypalCommission, PaypalCommissionsTitle, PaypalCalculatorReceiveTitle, PaypalCalculatorSendTitle } = calculatorPage.get;
+
+		paypalCommission().should('have.value', data.Precondiciones.tasa); //validando tasa(comision)
+		paypalFee().should('have.value', data.Precondiciones.tarifa); //validando tarifa (rate )
+		PaypalCommissionsTitle().should('have.text', data.Precondiciones.Title.TitleComision); //validando que tenga text titulo de comisiones
+		PaypalCalculatorReceiveTitle().should('have.text', data.Precondiciones.TitleRecibir); //validando que tenga texto titulo para recibir
+		PaypalCalculatorSendTitle().should('have.text', data.Precondiciones.Title.TitleEnviar); //validando que tenga texto titulo para enviar
+	});
+
+	it('30985 | TC1: Validar cálculo de "Hay que Enviar" cuando el monto es válido', () => {
+		const { toGetInput, InputEnviarPaypal } = calculatorPage.get;
+		//estoy diciendo que me cree una constante que se llame expectedHayqueEnviar y en tengo la formula
+		const expectedHayQueEnviar = (data.CalculoRecibir.montoParaRecibir + data.Precondiciones.tarifa) / (1 - data.Precondiciones.tasa);
+		//esta tipeando el monto 100 en recibir
+		toGetInput().type(data.CalculoRecibir.montoParaRecibir);
+		//aca se debe validar que la resultado de la formula se igual en el campo "Hay que Enviar"
+		InputEnviarPaypal().should('have',expectedHayQueEnviar);
+		
+	});
 });
+
 removeLogs();
--- cypress/support/pages/paypal/GX-30984_CalculatorPage.js
+++ cypress/support/pages/paypal/GX-30984_CalculatorPage.js
@@ -1,12 +1,23 @@
-//*Arrange (Declaraciones de Variables y Atributos)=> Constructor== instanciar el driver / declarar variables
+import data  from '../../../fixtures/data/GX-30984_CalculatorPaypal.json';
 
+//* Arrange (Declaraciones de Variables y Atributos) => Constructor == instanciar el Driver / declarar variables.
 class CalculatorPage {
+	//*propiedades
+
 	get = {
-		// Obtener "locators"
-		paypalFee: () => cy.get('#fee'),
-		paypalComission: () => cy.get('#percentage'),
+		// obtener "Locators"
+		//input 0.30usd
+		paypalFee: () => this.cy.get(data.Precondiciones.Inputs.inputfee), //*propiedad y el metodo que arroja un elemento
+		paypalCommission: () => this.cy.get(data.Precondiciones.Inputs.inputPercentage),
+		PaypalCommissionsTitle: () => this.cy.get(data.Precondiciones.ElementTitle).eq(0),
+		PaypalCalculatorReceiveTitle: () => this.cy.get(data.Precondiciones.ElementTitle).eq(1),
+		PaypalCalculatorSendTitle: () => this.cy.get(data.Precondiciones.ElementTitle).eq(2),
+		toGetInput: () => this.cy.get(data.CalculoRecibir.Inputs.InputRecibir),
+		toSendInput: () => this.cy.get(data.CalculoEnviar.Inputs.inputEnviar),
+		InputEnviarPaypal: () => this.cy.get(data.CalculoRecibir.Input.InputEnviar),
 	};
 
-	//*Act/Methods (Definen las interacciones del Usuario y Algoritmo de la pagina)
+	//* Act/Methods (Definen las interacciones del Usuario y Algoritmos de la página)
 }
+
 export const calculatorPage = new CalculatorPage();
