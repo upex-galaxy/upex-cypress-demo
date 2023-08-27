75ed0a1e3d6a344d294d0bed6bca24c6e2ab1143
ElenaValera <secretodelmar@gmail>
2023-08-27 19:15:44 +0200

    GX-32032 changes POM"

--- cypress/support/pages/paypal/GX-31032-calculatorPage.js
+++ cypress/support/pages/paypal/GX-31032-calculatorPage.js
@@ -7,7 +7,9 @@ class CalculatorPage {
 		paypalCommission: () => cy.get('#percentage'),
 		paypalCommissionTitle: () => cy.get('h2').eq(0),
 		paypalCalculatorReceiveTitle: () => cy.get('h2').eq(1),
-		paypalCommissionTitle: () => cy.get('h2').eq(2),
+		paypalCalculatorSendTitle: () => cy.get('h2').eq(2),
+		toGetInput: () => cy.get('[name="toGet"]'),
+		toSendInput: () => cy.get('[name="amountSent"]'),
 	};
 	//2.ACT: Methods (definen las interacciones del usuario y algoritmos de la página)
 }
