c6d9df8bd30749aa6731c0c1ad941ffd127a263e
igflores <139146133+igflores@users.noreply.github.com>
2023-08-27 02:20:54 -0700

    GX-30993 Adición del TC02: Validar que la cantidad a Enviar solo admita valores valor numéricos  En el TC01:     - se adicionó la validación de la BR que indica el Borrado del campo luego de ingresar un valor, ya sea numérico o caracter especial diferente a "-" y "+"
     Se adicionó el TC02: Validar que la cantidad a Enviar solo admita valores valor numéricos
        - DUDA: quedó comentariada la parte final, donde se hace la validación de la BR, que indica el Borrado del campo luego de ingresar un valor, ya sea numérico o caracter especial diferente a "-" y "+", pues el SUT no cumple con lo establecido, no estoy segura de que deba subir los cambios si da error
    
     On branch GX-30991-�-paypal-comisiones-calcular-las-comisiones-para-enviar-y-recibir
     Your branch is up to date with 'origin/GX-30991-�-paypal-comisiones-calcular-las-comisiones-para-enviar-y-recibir'.
    
     Changes to be committed:
    	modified:   cypress/e2e/Tests/paypal/GX-30991-calculadoraComision.cy.js

--- cypress/e2e/Tests/paypal/GX-30991-calculadoraComision.cy.js
+++ cypress/e2e/Tests/paypal/GX-30991-calculadoraComision.cy.js
@@ -14,10 +14,20 @@ describe('GX-30991: Paypal | Comisiones | Calcular las comisiones para enviar y 
 		PaypalCalculatorSendTitle().should('have.text', 'Calculadora PayPal para Enviar');
 	});
 
-	it('30993 | TC01: Validar que la cantidad a recibir solo admita valores valor numéricos', () => {
+	it('30993 | TC01: Validar que la cantidad a Recibir solo admita valores valor numéricos', () => {
 		const { toGetInput, message } = calculatorPage.get;
 
 		toGetInput().type(1);
+		message().should('not.have.text', 'Indica cuánto vas a Recibir');
+		message().should('not.have.text', 'Solo puedes introducir Números');
+		toGetInput().clear();
+
+		toGetInput().type(-20);
+		message().should('not.have.text', 'Indica cuánto vas a Recibir');
+		message().should('not.have.text', 'Solo puedes introducir Números');
+		toGetInput().clear();
+
+		toGetInput().type(+81);
 		message().should('not.have.text', 'Indica cuánto vas a Recibir');
 		message().should('not.have.text', 'Solo puedes introducir Números');
 		toGetInput().clear();
@@ -28,8 +38,45 @@ describe('GX-30991: Paypal | Comisiones | Calcular las comisiones para enviar y 
 		toGetInput().clear();
 
 		toGetInput().type('Hola mundo');
+		toGetInput().should('not.have.text', null);
 		message().should('have.text', 'Indica cuánto vas a Recibir');
-		toGetInput().type('!#$%&*(((())+-+"');
+
+		toGetInput().type('!#$%&*(((())+-+');
+		toGetInput().should('not.have.text', null);
 		message().should('have.text', 'Solo puedes introducir Números');
+	});
+
+	it('30993 | TC02: Validar que la cantidad a Enviar solo admita valores valor numéricos', () => {
+		const { toSendInput, message1 } = calculatorPage.get;
+
+		toSendInput().type(1);
+		message1().should('not.have.text', 'Indica cuánto vas a Enviar');
+		message1().should('not.have.text', 'Solo puedes introducir Números');
+		toSendInput().clear();
+
+		toSendInput().type(-20);
+		message1().should('not.have.text', 'Indica cuánto vas a Enviar');
+		message1().should('not.have.text', 'Solo puedes introducir Números');
+		toSendInput().clear();
+
+		toSendInput().type(+81);
+		message1().should('not.have.text', 'Indica cuánto vas a Enviar');
+		message1().should('not.have.text', 'Solo puedes introducir Números');
+		toSendInput().clear();
+
+		toSendInput().type(15.8);
+		message1().should('not.have.text', 'Indica cuánto vas a Enviar');
+		message1().should('not.have.text', 'Solo puedes introducir Números');
+		toSendInput().clear();
+
+		/*
+		toSendInput().type('Hola mundo');
+		toSendInput().should('not.have.text', null);
+		message1().should('have.text', 'Indica cuánto vas a Enviar');
+
+		toSendInput().type('!#$%&*(((())+-+"');
+		toSendInput().should('not.have.text', null);
+		message1().should('have.text', 'Solo puedes introducir Números');
+		*/
 	});
 });
