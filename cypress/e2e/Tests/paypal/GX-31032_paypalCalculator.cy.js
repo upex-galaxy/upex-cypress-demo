363d2d583489d9d7698d9482b7c05d5b7405335a
ElenaValera <secretodelmar@gmail>
2023-08-28 10:04:22 +0200

    "GX-31032 Arreglando nombre de US en test"

---
+++ cypress/e2e/Tests/paypal/GX-31032_paypalCalculator.cy.js
@@ -0,0 +1,25 @@
+import { calculatorPage } from '@pages/paypal/GX-31032-calculatorPage';
+
+describe('31032 | Paypal | Comisiones | Calcular las comisiones para enviar y recibir', () => {
+	beforeEach('precondición: para usar la calculadora');
+
+	cy.visit('https://vendercomprardolares.com/calculadora-comisiones-paypal.php');
+
+	//destructuración con constantes
+	const (paypalFee, paypalCommission, paypalCommissionTitle, paypalCalculatorReceiveTitle, paypalCalculatorSendTitle) = calculatorPage.get;
+
+	paypalCommission().should('have.value', '5,4');
+	paypalFee().should('have.value', '0,30');
+	paypalCommissionTitle().should('have.text', 'Las Comisiones Paypal');
+	paypalCalculatorReceiveTitle().should('have.text', 'Calculadora Paypal para recibir');
+	paypalCalculatorSendTitle().should('have.text', 'Calculadora Paypal para Enviar');
+
+	//cy.get('selector').should(have.text, '5,4')
+	//calculatorPage.get.paypalCommission().should('have.value', '5,4');
+	//calculatorPage.get.paypalFee().should('have.value', '0,30');
+
+	//assertions
+	it('31033|TC1...', () => {
+		calculatorPage.get.toGetInput().type('10');
+	});
+});
