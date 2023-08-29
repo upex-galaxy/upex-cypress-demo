ecb97c27f0e9fa06a3fe395e63b4cec95938e724
angelica mendoza <angelicamendozaa672@gmail.com>
2023-08-29 13:04:54 -0300

    test 2y3 radio button

--- cypress/e2e/Tests/Elements/GX-30846-RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-30846-RadioButtons.cy.js
@@ -13,6 +13,18 @@ describe('GX30846: ToolsQA | Elements | Radio Buttons', () => {
 		cy.get('[class=text-success]').should('have.text', 'Yes');
 		cy.contains('You have selected ').children().should('have.text', 'Yes');
 	});
+
+	it('TC2:  Validar visualizar el label Impressive cuando selecciona el radio button impresive', () => {
+		//* CSS Selectors in Cypress
+		cy.get('#impressiveRadio').click({ force: true }).should('be.checked');
+		cy.contains('You have selected ').children().should('have.text', 'Impressive');
+	});
+
+	it('TC3:  Validar visualizar el label "NO" este deshabilitado y no se pueda seleccionar', () => {
+		//* CSS Selectors in Cypress
+		cy.get('#noRadio').should('be.disabled');
+		cy.get('you have selected').should('not.exist');
+	});
 });
 
 //ES Module import/exports
