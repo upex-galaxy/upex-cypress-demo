0e4b97c20d997d9486677f49b80bf2dfcee00748
angelica mendoza <angelicamendozaa672@gmail.com>
2023-08-28 21:55:52 -0300

    test 1 radio button

--- cypress/e2e/Tests/Elements/GX-30846-RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-30846-RadioButtons.cy.js
@@ -1,2 +1,19 @@
+import { removeLogs } from '@helper/RemoveLogs';
 //* Test Set:
-describe.skip('ToolsQA | Elements | Radio Buttons', () => {});
+describe('GX30846: ToolsQA | Elements | Radio Buttons', () => {
+	//todo: Precondiciones
+	beforeEach('Precondición', () => {
+		const endpoint = '/radio-button';
+		cy.visit(`/${endpoint}`);
+	});
+
+	it('TC1:  Validar visualizar el label YES cuando selecciona el radio button Yes', () => {
+		//* CSS Selectors in Cypress
+		cy.get('#yesRadio').click({ force: true }).should('be.checked');
+		cy.get('[class=text-success]').should('have.text', 'Yes');
+		cy.contains('You have selected ').children().should('have.text', 'Yes');
+	});
+});
+
+//ES Module import/exports
+removeLogs();
