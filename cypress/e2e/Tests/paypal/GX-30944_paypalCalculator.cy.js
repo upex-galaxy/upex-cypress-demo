5563f25bd821d88220c437cf5372b5713dd834a0
saiotest <elyermad@gmail.com>
2023-08-29 12:22:41 -0300

    blackhole-clase#6: GX-30944

--- cypress/e2e/Tests/paypal/GX-30944_paypalCalculator.cy.js
+++ cypress/e2e/Tests/paypal/GX-30944_paypalCalculator.cy.js
@@ -4,7 +4,7 @@ describe('GX-30944: Paypal | Comisiones | Calcular las comisiones para enviar y 
 	beforeEach('Precondition: para usar la Calculadora Paypal', () => {
 		cy.visit('https://vendercomprardolares.com/calculadora-comisiones-paypal.php');
 
-		// *descructuración:
+		// * Descructuración:
 		const { paypalFee, paypalCommission, PaypalCommissionsTitle, PaypalCalculatorReceiveTitle, PaypalCalculatorSendTitle } = calculatorPage.get;
 
 		paypalCommission().should('have.value', '5,4');
@@ -14,8 +14,81 @@ describe('GX-30944: Paypal | Comisiones | Calcular las comisiones para enviar y 
 		PaypalCalculatorSendTitle().should('have.text', 'Calculadora PayPal para Enviar');
 	});
 
-	it('TC1', () => {
-		calculatorPage.get.toGetInput().type('10');
-		expect(true).equal(true);
+	it('TC1: Verificar poder Calcular la comisión cuando se introduce 1 caracter', () => {
+		const { inputParaRecibir, inputParaEnviar, inputHayQueEnviar, inputCommissionParaRecibir, inputSeReciben, inputCommissionParaEnviar } =
+			calculatorPage.get;
+		const commission = 0.054;
+		const fee = 0.3;
+
+		const givenValueToGet = 5;
+		inputParaRecibir().type(givenValueToGet.toString());
+
+		calculatorPage.getInputValue(inputHayQueEnviar()).then(calculatedValueToSend => {
+			cy.log(calculatedValueToSend);
+			const expectedCalculatedValue = calculatorPage.calculateFormulaToGet(givenValueToGet, commission, fee);
+			expect(calculatedValueToSend).equal(expectedCalculatedValue);
+
+			calculatorPage.getInputValue(inputCommissionParaRecibir()).then(calculatedCommission => {
+				cy.log(calculatedCommission);
+				const expectedCom = calculatorPage.getOutputCommision(calculatedValueToSend, givenValueToGet);
+				expect(calculatedCommission).equal(expectedCom);
+			});
+		});
+
+		const givenValueToSend = 9;
+		inputParaEnviar().type(givenValueToSend.toString());
+
+		calculatorPage.getInputValue(inputSeReciben()).then(calculatedValueToGet => {
+			cy.log(calculatedValueToGet);
+			const expectedCalculatedValue = calculatorPage.calculateFormulaToSend(givenValueToSend, commission, fee);
+			expect(calculatedValueToGet).equal(expectedCalculatedValue);
+
+			calculatorPage.getInputValue(inputCommissionParaEnviar()).then(calculatedCommission => {
+				cy.log(calculatedCommission);
+				const expectedCom = calculatorPage.getOutputCommision(givenValueToSend, calculatedValueToGet);
+				expect(calculatedCommission).equal(expectedCom);
+			});
+		});
+	});
+
+	it.only('TC2: Verificar poder Calcular la comisión cuando se introduce 306 caracteres', () => {
+		const { inputParaRecibir, inputParaEnviar, inputHayQueEnviar, inputCommissionParaRecibir, inputSeReciben, inputCommissionParaEnviar } =
+			calculatorPage.get;
+		const commission = 0.054;
+		const fee = 0.3;
+
+		const stringValueToGet =
+			'345645645666666666666666666666666663456345634563456435645364563456453645634563456345634563546345634566346766767465746874787685678567856785678567856785678657856786578567856786786578678678547567463456345634563563456345634565643563456456346435634566546634566736736767653746574567847567847878675485678755877777';
+		inputParaRecibir().type(stringValueToGet);
+
+		calculatorPage.getInputValue(inputHayQueEnviar()).then(calculatedValueToSend => {
+			cy.log(calculatedValueToSend);
+			const givenValueToGet = parseFloat(stringValueToGet);
+			const expectedCalculatedValue = calculatorPage.calculateFormulaToGet(givenValueToGet, commission, fee);
+			expect(calculatedValueToSend).equal(expectedCalculatedValue);
+
+			calculatorPage.getInputValue(inputCommissionParaRecibir()).then(calculatedCommission => {
+				cy.log(calculatedCommission);
+				const expectedCom = calculatorPage.getOutputCommision(calculatedValueToSend, givenValueToGet);
+				expect(calculatedCommission).equal(expectedCom);
+			});
+		});
+
+		const stringValueToSend =
+			'345645645666666666666666666666666663456345634563456435645364563456453645634563456345634563546345634566346766767465746874787685678567856785678567856785678657856786578567856786786578678678547567463456345634563563456345634565643563456456346435634566546634566736736767653746574567847567847878675485678755877777';
+		inputParaEnviar().type(stringValueToSend);
+
+		calculatorPage.getInputValue(inputSeReciben()).then(calculatedValueToGet => {
+			cy.log(calculatedValueToGet);
+			const givenValueToSend = parseFloat(stringValueToSend);
+			const expectedCalculatedValue = calculatorPage.calculateFormulaToSend(givenValueToSend, commission, fee);
+			expect(calculatedValueToGet).equal(expectedCalculatedValue);
+
+			calculatorPage.getInputValue(inputCommissionParaEnviar()).then(calculatedCommission => {
+				cy.log(calculatedCommission);
+				const expectedCom = calculatorPage.getOutputCommision(givenValueToSend, calculatedValueToGet);
+				expect(calculatedCommission).equal(expectedCom);
+			});
+		});
 	});
 });
--- cypress/support/pages/paypal/GX-30944_CalculatorPage.js
+++ cypress/support/pages/paypal/GX-30944_CalculatorPage.js
@@ -2,17 +2,61 @@
 
 class CalculatorPage {
 	get = {
-		// obtener "Locators"
+		// obtener Locators
 		paypalFee: () => cy.get('#fee'),
 		paypalCommission: () => cy.get('#percentage'),
 		PaypalCommissionsTitle: () => cy.get('h2').eq(0),
 		PaypalCalculatorReceiveTitle: () => cy.get('h2').eq(1),
 		PaypalCalculatorSendTitle: () => cy.get('h2').eq(2),
-		toGetInput: () => cy.get('[name="toGet"]'),
-		toSendInput: () => cy.get('[name="amountSent"]'),
+		inputParaRecibir: () => cy.get('input[name=toGet]'),
+		inputParaEnviar: () => cy.get('input[name=amountSent]'),
+		inputHayQueEnviar: () => cy.get('input[name=toCharge]'),
+		inputSeReciben: () => cy.get('input[name=amountAfterFees]'),
+		inputCommissionParaRecibir: () => cy.get('input[name=fees1]'),
+		inputCommissionParaEnviar: () => cy.get('input[name=fees]'),
 	};
 
 	//* Act/Methods (Definen las interacciones del Usuario y Algoritmos de la página)
+	calculateFormulaToGet(inputValue, commission, fee) {
+		const value = (inputValue + fee) / (1 - commission);
+		return parseFloat(value.toFixed(2));
+	}
+	calculateFormulaToSend(inputValue, commission, fee) {
+		const com = inputValue * commission;
+		const value = inputValue - (com + fee);
+		return parseFloat(value.toFixed(2));
+	}
+
+	getOutputCommision(ToSend, ToGet) {
+		const value = ToSend - ToGet;
+		return parseFloat(value.toFixed(2));
+	}
+
+	getInputValue(InputLocator) {
+		return InputLocator.invoke('val').then(val => {
+			const valRefined = val.replaceAll('.', '');
+			const stringValue = valRefined.replaceAll(',', '.');
+			const numberValue = parseFloat(stringValue);
+			return numberValue;
+		});
+	}
+
+	getRandomString(givenLength, onlyNumbers, decimalNumbers, specialChars) {
+		const numChars = '0123456789';
+		const textChars = 'abcdefghijklmnñopqrstuvwxyz';
+		const specialChars = '=?¡*¨][_:#"$$&%/';
+		const upperTextChars = textChars.toUpperCase();
+
+		let randomString = '';
+
+		if (onlyNumbers === true) {
+			for (i = 0; i < givenLength; i++) {
+				const randomIndex = Math.floor(Math.random() * numChars.length);
+				randomString += numChars[randomIndex];
+			}
+			return randomString;
+		}
+	}
 }
 
 export const calculatorPage = new CalculatorPage();
