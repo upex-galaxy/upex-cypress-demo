a513bb7fb52e69a3effda61e41cb03a9c20ca18e
Lucas Ramirez <rlucasmanuel@gmail.com>
2023-08-31 16:23:37 -0300

    GX30259-Dropdown-TC2 and TC3 modifique in progress whith random choice, TC4 in progress match with array

--- cypress/e2e/Tests/Widgets/GX-30259-Dropdown.cy.js
+++ cypress/e2e/Tests/Widgets/GX-30259-Dropdown.cy.js
@@ -17,16 +17,44 @@ describe('GX-30259 | TS: ✅ToolsQA | Widgets | Dropdown - Select Menu', () => {
 			dropDown.get.anotherOptionSelectValue().should('have.text', data.selectValue.outOfGroup.value2);
 		});
 	});
-	it('30260 | TC02: Validate choice random option in dropdown "Select Value"', () => {
+	it.only('30260 | TC02: Validate choice random option in dropdown "Select Value"', () => {
 		cy.fixture('/data/GX-30259-Dropdown.json').then(data => {
 			dropDown.clickSelectValue(); //click to active and show options
-			dropDown.get.allOptionsSelectValue().eq(Cypress._.random(0, 5)).click();
-			dropDown.get.valueSelectedSelectValue().eq(0).should('contain.text', 'option');
+
+			const lengthOptionSelectValue = dropDown.get.allOptionsSelectValue().its('length');
+			const n = lengthOptionSelectValue - 1;
+
+			function RandomItem(min, max) {
+				min = Math.ceil(0);
+				max = Math.floor(n);
+				return Math.floor(Math.random() * (max - min + 1) + min);
+			}
+
+			dropDown.get.allOptionsSelectValue().eq(RandomItem()).click();
+			dropDown.get.valueSelectedSelectValue().should('contain.text', 'option');
 		});
 	});
 	it.only('30260 | TC03: Validate choice a random option in dropdown "Select One"', () => {
 		cy.fixture('/data/GX-30259-Dropdown.json').then(data => {
 			dropDown.clickSelectOne();
+
+			const lengthOptionSelectOne = parseFloat(dropDown.get.allOptionsSelectOne().its('length'));
+			const n = parseFloat(lengthOptionSelectOne) - 1;
+
+			function RandomItem(min, max) {
+				min = Math.ceil(0);
+				max = Math.floor(n);
+				return Math.floor(Math.random() * (max - min + 1) + min);
+			}
+
+			dropDown.get.allOptionsSelectOne().eq(RandomItem()).click();
+			dropDown.get.valueSelectedSelectOne().invoke('text').should('eq', data.selectOne.option[n]);
+		});
+	});
+	it.only('30260 | TC04: Validate show massage "No options" when the type text not match in dropdown "Select One"', () => {
+		cy.fixture('/data/GX-30259-Dropdown.json').then(data => {
+			dropDown.typeSelectOne('hola');
+			dropDown.get.messageNoMatchSelectOne().should('have.text', data.selectOne.messageNoMatch);
 		});
 	});
 });
--- cypress/support/pages/Widgets/GX-30259-Dropdown.Page.js
+++ cypress/support/pages/Widgets/GX-30259-Dropdown.Page.js
@@ -1,5 +1,6 @@
 class dropDownPage {
 	get = {
+		//DropdownSelectValue
 		containerSelectValue: () => cy.get('#withOptGroup'),
 		titleGroup1SelectValue: () => cy.get('#react-select-2-group-0-heading'),
 		option1Group1SelectValue: () => cy.get('#react-select-2-option-0-0'),
@@ -10,8 +11,12 @@ class dropDownPage {
 		rootOptionSelectValue: () => cy.get('#react-select-2-option-2'),
 		anotherOptionSelectValue: () => cy.get('#react-select-2-option-3'),
 		allOptionsSelectValue: () => cy.get('[id^="react-select-2-option"]'),
-		valueSelectedSelectValue: () => cy.get('[class^=" css-1uccc91-singleValue"]'),
+		valueSelectedSelectValue: () => cy.get('[class^=" css-1uccc91-singleValue"]').eq(0),
+		//DropdownSelectOne
 		containerSelectOne: () => cy.get('#selectOne'),
+		allOptionsSelectOne: () => cy.get('[id^="react-select-3-option"]'),
+		valueSelectedSelectOne: () => cy.get('[class^=" css-1uccc91-singleValue"]').eq(0), //in case SelectValue show and option, use "eq(1)"
+		messageNoMatchSelectOne: () => cy.get('[class^=" css-1gl4k7"]'),
 	};
 
 	clickSelectValue() {
@@ -19,6 +24,9 @@ class dropDownPage {
 	}
 	clickSelectOne() {
 		this.get.containerSelectOne().click();
+	}
+	typeSelectOne(text) {
+		this.get.containerSelectOne().type(text);
 	}
 }
 export const dropDown = new dropDownPage();
