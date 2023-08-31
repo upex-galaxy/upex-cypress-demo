a5cf0da737697f3ed365361675b7a589feb2b0de
Paola Rosales <pao.rosa944@gmail.com>
2023-08-31 00:19:07 -0300

    GX-30984 se modifico los archivos de fixtures,pom y TS 	modified:   cypress/e2e/Tests/paypal/GX-30984_PaypalCalculator.cy.js 	modified:   cypress/fixtures/data/GX-30984_CalculatorPaypal.json 	modified:   cypress/support/pages/paypal/GX-30984_CalculatorPage.js

--- cypress/e2e/Tests/paypal/GX-30984_PaypalCalculator.cy.js
+++ cypress/e2e/Tests/paypal/GX-30984_PaypalCalculator.cy.js
@@ -14,24 +14,14 @@ describe('US GX-30984 | TS: Paypal | Comisiones | Calcular las comisiones para e
 	});
 
 	it.only('30985 | TC1: Validar cálculo de "Hay que Enviar" cuando el monto es válido', () => {
-		const { toGetInput, toSendInput, InputComision, InputEnviar } = calculatorPage.get;
-		//estoy diciendo que me cree una constante que se llame expectedHayqueEnviar y en tengo la formula
-		let valueToSend = (100 + 0.3) / (1 - 0.054).toFixed(12);
-		let reducirValue = valueToSend.toFixed(2);
-		let ObtValue = reducirValue.replace('.', ',');
-		console.log(ObtValue);
-		//esta tipeando el monto 100 en recibir
-		toGetInput().type(data.CalculoRecibir.montoParaRecibir);
-		//aca se debe validar que el resultado de la formula se igual al resultado de páypal en el campo "Hay que Enviar"
+//const { toGetInput, toSendInput, InputComision, InputEnviar } = calculatorPage.get;
 
-		toSendInput()
-			.invoke('val')
-			.then(toSendInput => {
-				expect(toSendInput).to.equal(ObtValue);
-			});
-		toGetInput().should('have.value', data.CalculoRecibir.montoParaRecibir);
-		InputEnviar().should('have.value', ObtValue);
-		InputComision().should('have.value', '6,03');
+
+		
 	});
-	it.skip('30985 | TC2:Validar cálculo de Se Reciben cuando el monto es válido', () => {});
+	it.skip('30985 | TC2:Validar cálculo de Se Reciben cuando el monto es válido', () => {
+		
+
+		
+	});
 });
--- cypress/support/pages/paypal/GX-30984_CalculatorPage.js
+++ cypress/support/pages/paypal/GX-30984_CalculatorPage.js
@@ -1,24 +1,63 @@
-import data from '../../../fixtures/data/GX-30984_CalculatorPaypal.json';
-
 //* Arrange (Declaraciones de Variables y Atributos) => Constructor == instanciar el Driver / declarar variables.
 class CalculatorPage {
 	//*propiedades
 
 	get = {
 		// obtener "Locators"
-		//input 0.30usd
-		Rate: () => cy.get(data.Precondiciones.Inputs.inputRate), //*propiedad y el metodo que arroja un elemento
-		Commision: () => cy.get(data.Precondiciones.Inputs.inputComision),
-		CommissionsTitle: () => cy.get(data.Precondiciones.ElementTitle).eq(0),
-		CalculatorRecibirTitle: () => cy.get(data.Precondiciones.ElementTitle).eq(1),
-		CalculatorSendTitle: () => cy.get(data.Precondiciones.ElementTitle).eq(2),
-		toGetInput: () => cy.get(data.CalculoRecibir.Inputs.InputRecibir),
-		toSendInput: () => cy.get('[name="toCharge"]'),
-		InputEnviar: () => cy.get(data.CalculoRecibir.Inputs.InputEnviar),
-		InputComision:()=> cy.get(data.CalculoRecibir.Inputs.InputComision)
+		// input 0.30usd
+		Rate: () => cy.get('#fee'), //*propiedad y el metodo que arroja un elemento
+		Commision: () => cy.get('#percentage'),
+		//Inputs para recibir
+		toGetInput: () => cy.get('input[name="toGet"]'),
+		toSendInput: () => cy.get('[name="toCharge"]'), //input para "Hay que Enviar"
+		InputComision: () => cy.get('[name="fees1"]'), //La Comisión es de:
+
+		// Input para enviar
+		InputEnviar: () => cy.get('input[name="amountSent"]'), //input para enviar
+		InputComission: () => cy.get('[name="fees"]').equal(1),
+		InputRecibe: () => cy.get('name=["amountAfterFees"]'),
 	};
 
 	//* Act/Methods (Definen las interacciones del Usuario y Algoritmos de la página)
+
+	//30985 | TC1: Validar cálculo de "Hay que Enviar" cuando el monto es válido'
+	//metodo para tipear el monto agregando un argumento
+	typeInputComision(num) {
+		this.get.InputComision().type(num);
+	}
+	//metodo para invocar el valor agregando un argumento de input
+	GetValueComision(ValueComision) {
+		return ValueComision.invoke('val');
+	}
+	//funcion con promesa donde se debe obtener un valor primero y luego debe ejecutarse otra accion para eliminar los separadores
+	RealValue(ValueComision) {
+		return this.GetValueComision(ValueComision).then(val => {
+			//con el then tomara el valor obtenido
+			let Value = val.replaceAll('.', ''); //Reemplaza todos los puntos con una cadena vacía para eliminar los separadores de miles.
+			let ValueMod = Value.replace(',', '.'); //Luego, reemplaza todas las comas por puntos, para garantizar un formato decimal válido.
+			let ValueReal = parseFloat(ValueMod); // Convierte la cadena resultante en un número de punto flotante (decimal).
+			return ValueReal; //devuelve el valor numérico obtenido después de todas las transformaciones.
+		});
+	}
+	//metodo para calcular la comision
+	CalculoFormula(Monto, Rate, Comision) {
+		let formula = (Monto + Comision) / (1 - Rate); //elcalculo se esta guardando en una variable
+		cy.log(formula);
+		return parseFloat(formula).toFixed(2); //Luego de calcular la fórmula, se utiliza parseFloat() para asegurar de que el resultado sea interpretado como un número en punto flotante (decimal). Luego se utiliza .toFixed(2) para redondear el número a dos decimales.
+	}
 }
+
+// 	// .invoke('val')
+// 	// .then(toSendInput => {
+// 	// 	expect(toSendInput).to.equal(ObtValue);
+// 	// });
+
+// //esta tipeando el monto 100 en recibir
+// toGetInput().type(data.CalculoRecibir.montoParaRecibir);
+// //aca se debe validar que el resultado de la formula se igual al resultado de páypal en el campo "Hay que Enviar"
+
+// toGetInput().should('have.value', data.CalculoRecibir.montoParaRecibir);
+// InputEnviar().should('have.value', ObtValue);
+// InputComision().should('have.value', '6,03');
 
 export const calculatorPage = new CalculatorPage();
