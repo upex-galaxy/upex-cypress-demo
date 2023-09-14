da3bc302ae8abce1ae4461223d9dceb62b2d8632
Braian Ferreyra <braianezequielferreyra39936964@gmail.com>
2023-09-14 16:30:07 +0200

    GX2-6642 tenth commit

--- cypress/e2e/Tests/Interactions/GX2-6642-Dragabble.cy.js
+++ cypress/e2e/Tests/Interactions/GX2-6642-Dragabble.cy.js
@@ -9,31 +9,50 @@ describe('GX2-6642 ✅ToolsQA | Interactions | Dragabble', () => {
 	});
 	it('GX2-6643 | TC1: Validar poder arrastrar en cualquier posición el tablero “Drag me“ de la pestaña “Simple“.', () => {
 		Dragabble.dragToRandomPosition();
+		cy.get('#dragBox').should('have.css', 'left');
+		cy.get('#dragBox').should('have.css', 'top');
+        
 	});
 	it('GX2-6643 | TC2: Validar poder arrastrar el tablero “Only X“ para izquierda y derecha respetando su eje de la pestaña “Axis Restricted”.', () => {
 		Dragabble.clickAxisRestricted();
 		Dragabble.moveX();
+		cy.get('#restrictedX').should('have.css', 'left');
+		cy.get('#restrictedX').should('have.css', 'top');
 	});
 	it('GX2-6643 | TC3: Validar poder arrastrar el tablero “Only Y“ para arriba y abajo respetando su eje de la pestaña “Axis Restricted”.', () => {
 		Dragabble.clickAxisRestricted();
 		Dragabble.moveY();
+		cy.get('#restrictedY').should('have.css', 'left');
+		cy.get('#restrictedY').should('have.css', 'top');
 	});
 	it('GX2-6643 | TC4: Validar el cuadro que contiene el texto “I`m contained within the box“ NO se pueda arrastrar fuera del área de acción delimitada correspondiente de la pestaña “Container Restricted”.', () => {
 		Dragabble.clickContainerRestricted();
 		Dragabble.moveInTheBox();
+		cy.get('.draggable.ui-widget-content.ui-draggable.ui-draggable-handle').should('have.css', 'left');
+		cy.get('.draggable.ui-widget-content.ui-draggable.ui-draggable-handle').should('have.css', 'top');
 	});
 	it('GX2-6643 | TC5: Validar el cuadro que contiene el texto “I`m contained within my parent“ NO se pueda arrastrar fuera del área de acción delimitada correspondiente de la pestaña “Container Restricted”.', () => {
 		Dragabble.clickContainerRestricted();
 		Dragabble.moveInTheParents();
+		cy.get('.draggable.ui-widget-content.m-3 span').should('have.css', 'left');
+		cy.get('.draggable.ui-widget-content.m-3 span').should('have.css', 'top');
 	});
-	it('GX2-6643 | TC6: Validar al arrastrar el cuadro “I will always stick to the center“ de la pestaña “Cursor Style” que el cursor siempre se ubique en el centro del mismo.', () => {
+	it.only('GX2-6643 | TC6: Validar al arrastrar el cuadro “I will always stick to the center“ de la pestaña “Cursor Style” que el cursor siempre se ubique en el centro del mismo.', () => {
 		Dragabble.clickCursorStyle();
 		Dragabble.moveBoxCenter();
-		Dragabble.validationMoveCenter();
+		cy.get('#cursorCenter').should('have.css', 'left');
+		cy.get('#cursorCenter').should('have.css', 'top');
 	});
 	it('GX2-6643 | TC7: Validar al arrastrar el cuadro “My cursor is at top left“ de la pestaña “Cursor Style” que el cursor siempre se ubique en la parte superior izquierda del mismo.', () => {
 		Dragabble.clickCursorStyle();
 		Dragabble.moveBoxTopLeft();
-		Dragabble.validationMoveTopLeft();
+		cy.get('#cursorTopLeft').should('have.css', 'left');
+		cy.get('#cursorTopLeft').should('have.css', 'top');
+	});
+	it('GX2-6643 | TC8: Validar al arrastrar el cuadro “My cursor is at bottom“ de la pestaña “Cursor Style” que el cursor siempre se ubique en la parte inferior del mismo.', () => {
+		Dragabble.clickCursorStyle();
+		Dragabble.moveBoxBottom();
+		cy.get('#cursorBottom').should('have.css', 'left');
+		cy.get('#cursorBottom').should('have.css', 'top');
 	});
 });
--- cypress/support/pages/Interactions/GX2-6642-Dragabble.page.js
+++ cypress/support/pages/Interactions/GX2-6642-Dragabble.page.js
@@ -6,7 +6,7 @@ class dragabble {
 		onlyY: () => cy.get('#restrictedY'),
 		tabContainerRestricted: () => cy.get('#draggableExample-tab-containerRestriction'),
 		theBox: () => cy.get('.draggable.ui-widget-content.ui-draggable.ui-draggable-handle'),
-		theParents: () => cy.get('.draggable.ui-widget-content.m-3'),
+		theParents: () => cy.get('.draggable.ui-widget-content.m-3 span'),
 		tabCursorStyle: () => cy.get('#draggableExample-tab-cursorStyle'),
 		cursorCenter: () => cy.get('#cursorCenter'),
 		cursorTopLeft: () => cy.get('#cursorTopLeft'),
@@ -15,16 +15,8 @@ class dragabble {
 	//Movimiento random
 	dragToRandomPosition() {
 		cy.get('#dragBox').move({ deltaX: 200, deltaY: 125 });
-		
 	}
-	validationRandomPosition() {
-		return this.get
-			.dragMe()
-			.invoke('attr', 'style')
-			.then(style => {
-				return style;
-			});
-	}
+	
 	//Movimiento de coordenadas X, Y.
 	clickAxisRestricted() {
 		this.get.tabAxisRestricted().click();
@@ -32,25 +24,11 @@ class dragabble {
 	moveX() {
 		cy.get('#restrictedX').move({ deltaX: 500, deltaY: 200 }, { force: true });
 	}
-	validationMoveX() {
-		return this.get
-			.onlyX()
-			.invoke('attr', 'style')
-			.then(style => {
-				return style;
-			});
-	}
+	
 	moveY() {
 		cy.get('#restrictedY').move({ deltaX: 300, deltaY: 500 }, { force: true });
 	}
-	validationMoveY() {
-		return this.get
-			.onlyY()
-			.invoke('attr', 'style')
-			.then(style => {
-				return style;
-			});
-	}
+	
 	//movimiento dentro de estructura
 	clickContainerRestricted() {
 		this.get.tabContainerRestricted().click();
@@ -58,25 +36,11 @@ class dragabble {
 	moveInTheBox() {
 		cy.get('.draggable.ui-widget-content.ui-draggable.ui-draggable-handle').move({ deltaX: 300, deltaY: 500 }, { force: true });
 	}
-	validationMoveInTheBox() {
-		return this.get
-			.theBox()
-			.invoke('attr', 'style')
-			.then(style => {
-				return style;
-			});
-	}
+	
 	moveInTheParents() {
-		cy.get('.draggable.ui-widget-content.m-3').move({ deltaX: 20, deltaY: 90 });
+		cy.get('.draggable.ui-widget-content.m-3 span').move({ deltaX: 20, deltaY: 90 });
 	}
-	validationMoveInTheParents() {
-		return this.get
-			.theParents()
-			.invoke('attr', 'style')
-			.then(style => {
-				return style;
-			});
-	}
+	
 	//movimiento de cursor//
 	clickCursorStyle() {
 		this.get.tabCursorStyle().click();
@@ -84,36 +48,11 @@ class dragabble {
 	moveBoxCenter(ejeX, ejeY) {
 		this.get.cursorCenter().move({ deltaX: ejeX, deltaY: ejeY });
 	}
-	validationMoveCenter() {
-		return this.get
-			.cursorCenter()
-			.invoke('attr', 'style')
-			.then(style => {
-				return style;
-			});
-	}
 	moveBoxTopLeft(ejeX, ejeY) {
 		this.get.cursorTopLeft().move({ deltaX: ejeX, deltaY: ejeY });
 	}
-	validationMoveTopLeft() {
-		return this.get
-			.cursorTopLeft()
-			.invoke('attr', 'style')
-			.then(style => {
-				return style;
-			});
-	}
 	moveBoxBottom(ejeX, ejeY) {
 		this.get.cursorBotton().move({ deltaX: ejeX, deltaY: ejeY });
-	}
-	validationMoveBoxBottom() {
-		return this.get
-			.cursorBotton()
-			.invoke('attr', 'style')
-			.then(style => {
-				return style;
-			});
-	}
+	}	
 }
-
 export const Dragabble = new dragabble();
