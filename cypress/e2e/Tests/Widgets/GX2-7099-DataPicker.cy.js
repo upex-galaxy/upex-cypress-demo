fb9d3227689143eed0de6c76decdd1f8d5ef8aed
PabloCz <Pablotest@gmail.com>
2023-09-21 16:28:15 -0300

    GX2-7099 Configuraciones básicas para empezar

---
+++ cypress/e2e/Tests/Widgets/GX2-7099-DataPicker.cy.js
@@ -0,0 +1,9 @@
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
+describe('🪶ToolsQA | Widgets | Date Picker', () => {
+	beforeEach('PCR: Situarse en la Website Demoqa en el endpoint data-picker', () => {
+		cy.visit('/date-picker');
+	});
+
+	it('testKei', () => {});
+});
