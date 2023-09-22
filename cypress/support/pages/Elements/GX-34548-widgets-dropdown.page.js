41cb60c60f25853322ee33931ae28dafff695381
Yesi <109559305+yeyu2083@users.noreply.github.com>
2023-09-22 12:00:27 -0300

    34548 Cambios en TC4, procedo al reporte

--- cypress/support/pages/Elements/GX-34548-widgets-dropdown.page.js
+++ cypress/support/pages/Elements/GX-34548-widgets-dropdown.page.js
@@ -6,10 +6,8 @@ class dropdown {
 		selectTitle: () => cy.get('.css-1wa3eu0-placeholder').eq(0),
 		oldStyle: () => cy.get("div[id^='selectMenuContainer'] .col-md-6").eq(4),
 		oldSelectMenu: () => cy.get('select#oldSelectMenu>option'),
-		multiselectDropdown: () =>  cy.get('.col-md-6.col-sm-12>p>b').eq(0),
+		multiselectDropdown: () => cy.get('.col-md-6.col-sm-12>p>b').eq(0),
 		optionMenu: () => cy.get('[id*=react-select][id*=option]'),
-		multiselect: () => cy.get('[class="css-1rhbuit-multiValue"]'),
-		closedMultiSelect: () => cy.get('[class=" css-1wy0on6"]').eq(2),
 		standardMultiselect: () => cy.get('.col-md-6.col-sm-12>p>b').eq(1),
 		itemMultiselectStandard: () => cy.get('select[id^=cars] option'),
 	};
@@ -33,36 +31,28 @@ class dropdown {
 		this.get.oldStyle().click();
 	}
 	clickmultidropdown() {
-		this.get.multiselectDropdown().click()
+		this.get.multiselectDropdown().click();
 	}
-	itemMultiSelect() {
-		const elementRandom = Math.floor(Math.random() * 3);
-		this.get.multiselect().click();
-		this.get.optionMenu().eq(elementRandom).click();
-		this.get.closedMultiSelect().click()
-		}
-	
+
 	clickStandardMultiselec() {
-		this.get.standardMultiselect().click()
+		this.get.standardMultiselect().click();
 	}
-	
+
 	itemOldStyle() {
-		
-		const color = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Black', 'White', 'Voilet', 'Indigo', 'Magenta', 'Aqua']
+		const color = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Black', 'White', 'Voilet', 'Indigo', 'Magenta', 'Aqua'];
 		const selectElement = this.get.oldSelectMenu();
-		const randomColor =color[Math.floor(Math.random() * color.length)] ;
-		selectElement.invoke('val', randomColor)
-	
-		Cypress.env('selectColor' ,randomColor)
+		const randomColor = color[Math.floor(Math.random() * color.length)];
+		selectElement.invoke('val', randomColor);
+
+		Cypress.env('selectColor', randomColor);
 	}
 	itemMultiStandard() {
-		
-		const cars = ['Volvo', 'Saab', 'Opel', 'Audi']
+		const cars = ['Volvo', 'Saab', 'Opel', 'Audi'];
 		const selectElement = this.get.itemMultiselectStandard();
-		const randomCars =cars[Math.floor(Math.random() * cars.length)] ;
-		selectElement.invoke('val', randomCars)
-	
-		Cypress.env('selectCars' ,randomCars)
+		const randomCars = cars[Math.floor(Math.random() * cars.length)];
+		selectElement.invoke('val', randomCars);
+
+		Cypress.env('selectCars', randomCars);
 	}
 }
 
