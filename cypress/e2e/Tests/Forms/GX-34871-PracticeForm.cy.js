e7e51f1dbc64ebc5bac5e32121e56cb4e0545231
igflores <139146133+igflores@users.noreply.github.com>
2023-09-22 06:36:49 -0700

    GX-34871 | Working on the TC01: type firstName and lastName is done (using "faker")
    Working on the TC01: type firstName and lastName is done (using "faker")
    
     Changes to be committed:
    	modified:   cypress/e2e/Tests/Forms/GX-34871-PracticeForm.cy.js
    	modified:   cypress/support/pages/Forms/GX-34871-PracticeFormPage.js

--- cypress/e2e/Tests/Forms/GX-34871-PracticeForm.cy.js
+++ cypress/e2e/Tests/Forms/GX-34871-PracticeForm.cy.js
@@ -1,11 +1,19 @@
 import { removeLogs } from '@helper/RemoveLogs';
 import { pForm } from '@pages/Forms/GX-34871-PracticeFormPage';
+import { faker } from '@faker-js/faker';
 
 describe('GX-34872 | ✅ToolsQA | Forms | Practice Form', () => {
-	beforeEach('Precondition: to use the Practice Form', () => {});
+	beforeEach('Precondition: to use the Practice Form', () => {
+		cy.visit('https://demoqa.com/automation-practice-form');
+		cy.url().should('contain', 'practice-form');
+	});
 
-	it('GX-34871 | TC01: ', () => {
-		expect(1).to.eql(1);
+	it('GX-34871 | TC01: Validate that a popup displays all the valid data', () => {
+		const generatedName = pForm.fillFirstName();
+		pForm.get.firstNameInput().should('have.value', generatedName);
+
+		const generatedLastName = pForm.fillLastName();
+		pForm.get.lastNameInput().should('have.value', generatedLastName);
 	});
 });
 
--- cypress/support/pages/Forms/GX-34871-PracticeFormPage.js
+++ cypress/support/pages/Forms/GX-34871-PracticeFormPage.js
@@ -1,17 +1,35 @@
-class PracticeForm {
+import { faker } from '@faker-js/faker';
+class PracticeFormPage {
 	get = {
-		firstName: () => cy.get('#firstName'),
-		lastName: () => cy.get('#lastName'),
-		email: () => cy.get('#userEmail'),
-		maleGender: () => cy.get('#gender-radio-1'),
-		femaleGender: () => cy.get('#gender-radio-2'),
-		otherGender: () => cy.get('#gender-radio-3'),
-		mobile: () => cy.get('#userNumber'),
-		//dateBirth: () cy.get(''),
-		subjects: () => cy.get('[class="value-container subjects-auto-complete"]'),
-		sportsHobbies: () => cy.get('#hobbies-checkbox-1'),
-		readingHobbies: () => cy.get('#hobbies-checkbox-2'),
-		musicHobbies: () => cy.get('#hobbies-checkbox-3'),
+		// * ---- inputs -----
+		firstNameInput: () => cy.get('#firstName'),
+		lastNameInput: () => cy.get('#lastName'),
+		emailInput: () => cy.get('#userEmail'),
+		maleGenderInput: () => cy.get('#gender-radio-1'),
+		femaleGenderInput: () => cy.get('#gender-radio-2'),
+		otherGenderInput: () => cy.get('#gender-radio-3'),
+		mobileInput: () => cy.get('#userNumber'),
+		//dateBirthInput: () cy.get(''),
+		subjectsInput: () => cy.get('[class="value-container subjects-auto-complete"]'),
+		sportsHobbiesInput: () => cy.get('#hobbies-checkbox-1'),
+		readingHobbiesInput: () => cy.get('#hobbies-checkbox-2'),
+		musicHobbiesInput: () => cy.get('#hobbies-checkbox-3'),
+		uploadPictureInput: () => cy.get('#uploadPicture'),
+
+		// * ---- Log Messages -----
 	};
+
+	fillFirstName() {
+		const randomName = faker.name.firstName();
+		this.get.firstNameInput().type(randomName);
+		return randomName;
+	}
+
+	fillLastName() {
+		const randomLastName = faker.name.lastName();
+		this.get.lastNameInput().type(randomLastName);
+		return randomLastName;
+	}
 }
-export const pForm = new PracticeForm();
+
+export const pForm = new PracticeFormPage();
