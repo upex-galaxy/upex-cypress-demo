623c4503872e7779611032eb11676c136e1ec4e4
rafo18 <rafaelbarreto10@hotmail.com>
2023-09-26 20:21:30 -0400

    GX-35657: Se realizo el primer TC y se mejoro el POM

--- cypress/e2e/Tests/Forms/GX-35657-Form.cy.js
+++ cypress/e2e/Tests/Forms/GX-35657-Form.cy.js
@@ -3,13 +3,50 @@ import { faker } from '@faker-js/faker';
 
 import { removeLogs } from '@helper/RemoveLogs';
 
+const name = faker.name.firstName();
+const lastName = faker.name.lastName();
+const email = faker.internet.email();
+const mobileNumber = faker.datatype.number('##########');
+const currentAddress = faker.address.streetAddress();
+
 describe(' GX-35657 | ToolsQA | Forms | Practice Form', () => {
 	beforeEach(() => {
 		cy.visit('/automation-practice-form');
 	});
 
 	it('35658 | TC1: Validar enviar formulario con datos validos.', () => {
-		form.fillForm({});
+		form.fillForm({
+			firstName: name,
+			lastName: lastName,
+			email: email,
+			gender: true,
+			mobileNumber: mobileNumber,
+			subject: true,
+			hobbies: true,
+			picture: 'cypress/fixtures/images/upexlogo.png',
+			currentAddress: currentAddress,
+			state: true,
+			city: true,
+		}).then(index => {
+			const [gender, subject, hobbies, state, city, birthday] = index;
+			form.get.studentInfo('Student Name').should('have.text', `${name} ${lastName}`);
+
+			form.get.studentInfo('Student Email').should('have.text', email);
+			form.get.studentInfo('Gender').should('have.text', gender);
+			form.get.studentInfo('Mobile').should('have.text', mobileNumber);
+			form.get.studentInfo('Date of Birth').should('contain.text', birthday);
+			form.get.studentInfo('Subjects').should('have.text', subject);
+			form.get.studentInfo('Hobbies').should('have.text', hobbies);
+			form.get.studentInfo('Address').should('have.text', currentAddress);
+			form.get.studentInfo('State and City').should('have.text', `${state} ${city}`);
+			form.get.studentInfo('Picture').should('have.text', 'upexlogo.png');
+
+			cy.log('genero', gender);
+			cy.log('subject', subject);
+			cy.log('hobbies', hobbies);
+			cy.log('state', state);
+			cy.log('city', city);
+		});
 	});
 });
 
--- cypress/support/pages/Forms/GX-35657-Form.page.js
+++ cypress/support/pages/Forms/GX-35657-Form.page.js
@@ -8,7 +8,7 @@ class Form {
 		//email
 		email: () => cy.get('[id=userEmail]'),
 		//gender
-		gender: () => cy.get('[class=custom-control-input][id*=gender-radio]'),
+		gender: () => cy.get('[class=custom-control-label]'),
 		//mobileNumber
 		mobileNumber: () => cy.get('[id=userNumber]'),
 		//date of birth
@@ -21,7 +21,7 @@ class Form {
 		selectSubject: () => cy.get('[id*=react-select]'),
 		//Picture
 		picture: () => cy.get('[id=uploadPicture]'),
-		hobbies: () => cy.get('[class=custom-control-input][id*=hobbies-checkbox]'),
+		hobbies: () => cy.get('[class=custom-control-label]'),
 		currentAddress: () => cy.get('[id=currentAddress]'),
 		//state
 		state: () => cy.get('[id=state]'),
@@ -29,8 +29,9 @@ class Form {
 		//city
 		city: () => cy.get('[id=city]'),
 		selectCity: () => cy.get('[id*=react-select]'),
-
 		btn: () => cy.get('[id=submit]'),
+		//Validations
+		studentInfo: value => cy.get('table[class*=table]').contains('td', value).siblings(),
 	};
 
 	fillForm({
@@ -39,7 +40,6 @@ class Form {
 		email = '',
 		gender = false,
 		mobileNumber = '',
-		// dateOfBirth = '',
 		subject = false,
 		hobbies = false,
 		picture = '',
@@ -47,100 +47,158 @@ class Form {
 		state = false,
 		city = false,
 	}) {
-		this.get.firstName().then(() => (firstName == '' ? this.get.firstName().clear() : this.get.firstName().clear().type(firstName)));
-		this.get.lastName().then(() => (lastName == '' ? this.get.lastName().clear() : this.get.lastName().clear().type(lastName)));
-		this.get.email().then(() => (email == '' ? this.get.email().clear() : this.get.email().clear().type(email)));
-		this.get.gender().then(index => {
-			let random;
-			random = Cypress._.random(0, index.length - 1);
-			cy.log(index.length);
-			gender == false ? this.get.gender() : this.get.gender().eq(random).click({ force: true });
-		});
-		this.get
-			.mobileNumber()
-			.then(() => (mobileNumber == '' ? this.get.mobileNumber().clear() : this.get.mobileNumber().clear().type(mobileNumber)));
-		//realizar if para los dias del mes
-		this.get.dateOfBirth().then(() => {
-			let month;
-			let day;
-			let year;
+		let Gender;
+		let Subject;
+		let Hobbies;
+		let State;
+		let City;
+		let Birthday;
 
-			this.get
-				.dateOfBirth()
-				.click()
-				.then(() => {
-					month = faker.date.month();
-					day = faker.datatype.number({ min: 1, max: 28 }).toString();
-					year = faker.datatype.number({ min: 1940, max: 2023 }).toString();
-					cy.log(month);
-					cy.log(day);
-					cy.log(year);
-					this.get.month().select(month);
-					this.get.year().select(year);
-					this.get.day().contains(day).click();
-				});
-		});
-
-		this.get.subject().then(() => {
-			let random;
-			if (subject == false) {
-				this.get.subject();
-			} else {
-				this.get.subject().type('a');
-				this.get.selectSubject().then(index => {
+		return cy
+			.get('*')
+			.then(() => {
+				this.get.firstName().then(() => (firstName == '' ? this.get.firstName().clear() : this.get.firstName().clear().type(firstName)));
+				this.get.lastName().then(() => (lastName == '' ? this.get.lastName().clear() : this.get.lastName().clear().type(lastName)));
+				this.get.email().then(() => (email == '' ? this.get.email().clear() : this.get.email().clear().type(email)));
+				this.get.gender().then(index => {
+					let random;
+					random = Cypress._.random(0, index.length - 4);
 					cy.log(index.length);
-					random = Cypress._.random(2, index.length - 2);
-					this.get.selectSubject().eq(random).click();
+					gender == false
+						? this.get.gender()
+						: this.get
+								.gender()
+								.eq(random)
+								.click()
+								.then($gender => {
+									Gender = $gender.text();
+									cy.log('genro1', Gender);
+								});
 				});
-			}
-		});
+				this.get
+					.mobileNumber()
+					.then(() => (mobileNumber == '' ? this.get.mobileNumber().clear() : this.get.mobileNumber().clear().type(mobileNumber)));
+				//realizar if para los dias del mes
+				this.get.dateOfBirth().then(() => {
+					let month;
+					let day;
+					let year;
 
-		this.get.hobbies().then(index => {
-			let random;
-			random = Cypress._.random(0, index.length - 1);
-			cy.log(index.length);
-			hobbies == false ? this.get.hobbies() : this.get.hobbies().eq(random).click({ force: true });
-		});
-
-		this.get
-			.picture()
-			.click()
-			.then(() => (picture == '' ? this.get.picture() : this.get.picture().selectFile(picture)));
-
-		this.get
-			.currentAddress()
-			.then(() => (currentAddress == '' ? this.get.currentAddress().clear() : this.get.currentAddress().clear().type(currentAddress)));
-
-		this.get.state().then(() => {
-			let random;
-			if (state == false) {
-				this.get.state();
-			} else {
-				this.get.state().click();
-				this.get.selectState().then(index => {
-					random = Cypress._.random(2, index.length - 1);
-					this.get.selectState().eq(2).click({ force: true });
+					this.get
+						.dateOfBirth()
+						.click()
+						.then(() => {
+							month = faker.date.month();
+							day = faker.datatype.number({ min: 1, max: 28 }).toString();
+							year = faker.datatype.number({ min: 1940, max: 2023 }).toString();
+							cy.log(month);
+							cy.log(day);
+							cy.log(year);
+							this.get.month().select(month);
+							this.get.year().select(year);
+							this.get.day().contains(day).click();
+							Birthday = `${day} ${month},${year}`;
+						});
 				});
-			}
-		});
 
-		// this.get.city().click();
+				this.get.subject().then(() => {
+					let random;
+					if (subject == false) {
+						this.get.subject();
+					} else {
+						this.get.subject().type('a');
+						this.get.selectSubject().then(index => {
+							random = Cypress._.random(0, index.length - 3);
+							this.get
+								.selectSubject()
+								.eq(random)
+								.click()
+								.then($subject => {
+									Subject = $subject.text();
+									cy.log(Subject);
+								});
+						});
+					}
+				});
 
-		this.get.city().then(() => {
-			let random;
-			if (city == false) {
-				this.get.city();
-			} else {
-				this.get.city().click();
-				this.get.selectCity().then(index => {
+				this.get.hobbies().then(index => {
+					let random;
+					random = Cypress._.random(3, index.length - 1);
 					cy.log(index.length);
-					random = Cypress._.random(2, index.length - 1);
-					this.get.selectCity().eq(2).click({ force: true });
+					hobbies == false
+						? this.get.hobbies()
+						: this.get
+								.hobbies()
+								.eq(random)
+								.click()
+								.then($hobbies => {
+									Hobbies = $hobbies.text();
+								});
 				});
-			}
-		});
 
-		this.get.btn().click();
+				this.get
+					.picture()
+					.click()
+					.then(() =>
+						picture == ''
+							? this.get.picture()
+							: this.get
+									.picture()
+									.selectFile(picture)
+									.then($picture => {
+										console.log('imagen', $picture);
+									})
+					);
+
+				this.get
+					.currentAddress()
+					.then(() => (currentAddress == '' ? this.get.currentAddress().clear() : this.get.currentAddress().clear().type(currentAddress)));
+
+				this.get.state().then(() => {
+					let random;
+					if (state == false) {
+						this.get.state();
+					} else {
+						this.get.state().click();
+						this.get.selectState().then(index => {
+							random = Cypress._.random(1, index.length - 4);
+							this.get
+								.selectState()
+								.eq(random)
+								.click({ force: true })
+								.then($state => {
+									State = $state.text();
+								});
+						});
+					}
+				});
+
+				// this.get.city().click();
+
+				this.get.city().then(() => {
+					let random;
+					if (state == false) {
+						this.get.state();
+					} else {
+						this.get.city().click();
+						this.get.selectCity().then(index => {
+							random = Cypress._.random(2, index.length - 2);
+							this.get
+								.selectCity()
+								.eq(random)
+								.click({ force: true })
+								.then($city => {
+									City = $city.text();
+								});
+						});
+					}
+				});
+
+				this.get.btn().click();
+			})
+			.then(() => {
+				return [Gender, Subject, Hobbies, State, City, Birthday];
+			});
 	}
 }
 
