a6b1833d4215a204e0ee6296c5c7736e8e91e0a2
igflores <139146133+igflores@users.noreply.github.com>
2023-09-28 10:41:25 -0700

    GX-34871 Adding: get static data of subjects
     Changes to be committed:
    	modified:   cypress/e2e/Tests/Forms/GX-34871-PracticeForm.cy.js
    	modified:   cypress/support/pages/Forms/GX-34871-PracticeFormPage.js

--- cypress/e2e/Tests/Forms/GX-34871-PracticeForm.cy.js
+++ cypress/e2e/Tests/Forms/GX-34871-PracticeForm.cy.js
@@ -8,13 +8,24 @@ describe('GX-34872 | ✅ToolsQA | Forms | Practice Form', () => {
 		cy.url().should('contain', 'practice-form');
 	});
 
-	it('GX-34871 | TC01: Validate that a popup displays all the valid data', () => {
-		const formData = pForm.fillForm();
-
+	it.only('GX-34871 | TC01: Validate that a popup displays all the valid data', () => {
+		const formData = pForm.fillAndGetInputData();
 		pForm.get.firstNameInput().should('have.value', formData.firstName);
 		pForm.get.lastNameInput().should('have.value', formData.lastName);
 		pForm.get.emailInput().should('have.value', formData.email);
 		pForm.get.mobileInput().should('have.value', formData.mobile);
+		pForm.get.currentAddressInput().should('have.value', formData.address);
+
+		pForm.get.subjectsInput().type('a');
+		//obtener largo, si es igual a 0, hacer función para que haga random de nuevo: borrar la tra y poner otra
+		const num = Cypress._.random(0, 3);
+		cy.get('[id*="react-select-2-option"]').eq(num).click();
+
+		//pForm.get.maleGenderInput().should('be.visible');
+		//pForm.get.femaleGenderInput().should('be.visible');
+		//pForm.get.otherGenderInput().should('be.visible');
+		//console.log('ESTAN AHI LOS 3 RADIOS');
+
 		expect(1).to.eq(1);
 	});
 });
--- cypress/support/pages/Forms/GX-34871-PracticeFormPage.js
+++ cypress/support/pages/Forms/GX-34871-PracticeFormPage.js
@@ -2,47 +2,70 @@ import { faker } from '@faker-js/faker';
 
 class PracticeFormPage {
 	get = {
-		// * ---- inputs -----
 		firstNameInput: () => cy.get('#firstName'),
 		lastNameInput: () => cy.get('#lastName'),
 		emailInput: () => cy.get('#userEmail'),
-		maleGenderInput: () => cy.get('#gender-radio-1'),
+
+		maleGenderInput: () => cy.get('#gender-radio-1').parent(),
 		femaleGenderInput: () => cy.get('#gender-radio-2'),
 		otherGenderInput: () => cy.get('#gender-radio-3'),
+
 		mobileInput: () => cy.get('#userNumber'),
 		//dateBirthInput: () cy.get(''),
-		subjectsInput: () => cy.get('[class="value-container subjects-auto-complete"]'),
+		subjectsInput: () => cy.get('#subjectsInput'),
 		sportsHobbiesInput: () => cy.get('#hobbies-checkbox-1'),
 		readingHobbiesInput: () => cy.get('#hobbies-checkbox-2'),
 		musicHobbiesInput: () => cy.get('#hobbies-checkbox-3'),
 		uploadPictureInput: () => cy.get('#uploadPicture'),
 		currentAddressInput: () => cy.get('#currentAddress'),
+		stateInput: () => cy.get('[id="state"]'),
 	};
 
+	selectRandomGender() {
+		const randomGender = Math.floor(Math.random() * 3);
+		switch (randomGender) {
+			case 0:
+				this.get.maleGenderInput().click({ force: true });
+				break;
+			case 1:
+				this.get.femaleGenderInput().click({ force: true });
+				break;
+			case 2:
+				this.get.otherGenderInput().click({ force: true });
+				break;
+		}
+	}
+
 	fillField(fieldName, value) {
-		const field = this.get[fieldName]();
+		const field = this.get[fieldNyarname]();
 		field.type(value);
 		return value;
 	}
-
-	fillForm() {
+	fillAndGetInputData() {
 		const randomFirstName = faker.name.firstName();
 		const randomLastName = faker.name.lastName();
 		const randomEmail = faker.internet.email();
 		const randomMobile = faker.datatype.number({ min: 1000000000, max: 9999999999 });
+		const randomAddress = faker.address.streetAddress();
 
 		this.fillField('firstNameInput', randomFirstName);
 		this.fillField('lastNameInput', randomLastName);
 		this.fillField('emailInput', randomEmail);
 		this.fillField('mobileInput', randomMobile.toString());
+		this.fillField('currentAddressInput', randomAddress);
+
+		//this.selectRandomGender();
 
 		return {
 			firstName: randomFirstName,
 			lastName: randomLastName,
 			email: randomEmail,
 			mobile: randomMobile.toString(),
+			address: randomAddress,
 		};
 	}
+
+	//*método del Subject, selección, longitud, random, etc
 }
 
 export const pForm = new PracticeFormPage();
