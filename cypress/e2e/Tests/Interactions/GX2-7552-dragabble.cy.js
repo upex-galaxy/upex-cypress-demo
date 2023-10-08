49ff4497a340c7602a97f2c3597935f84463cc32
Yesi <109559305+yeyu2083@users.noreply.github.com>
2023-10-08 14:22:41 -0300

    GX2-7552 realizo el TC2, TC3, TC4

--- cypress/e2e/Tests/Interactions/GX2-7552-dragabble.cy.js
+++ cypress/e2e/Tests/Interactions/GX2-7552-dragabble.cy.js
@@ -1,6 +1,7 @@
 import { removeLogs } from '@helper/RemoveLogs';
 removeLogs();
 import { dragMove } from '@pages/Interactions/GX2-7552-dragabble.page';
+import '@4tw/cypress-drag-drop';
 
 describe('GX2-7552-✅-tools-qa-interactions-dragabble', () => {
 	beforeEach('Usuario debe estar situado en https://demoqa.com/dragabble', () => {
@@ -19,5 +20,38 @@ describe('GX2-7552-✅-tools-qa-interactions-dragabble', () => {
 				clientY: $inicial.top + 80,
 			});
 		});
+	});
+	it('7553 | TC2: Validar  poder movilizar OnlyX en la posicion X y OnlyY en la posicion Y', () => {
+		dragMove.clickAxis();
+		dragMove.get.axisRestricted().should('have.text', 'Axis Restricted');
+		dragMove.clickOnly();
+		dragMove.get.OnlyX().move({ deltaX: 250 });
+		dragMove.get.OnlyX().should('have.css', 'top', '0px').and('be.visible');
+		dragMove.clickOnlyY();
+		dragMove.get.OnlyY().move({ deltaX: 150, deltaY: 150 });
+		dragMove.get.OnlyY().should('have.css', 'left', '0px').and('be.visible');
+	});
+	it('7553 | TC3: Validar  poder movilizar  el cuadro y el texto dentro de su caja contenedora', () => {
+		dragMove.clickContenedor();
+		dragMove.get.restricted().should('have.text', 'Container Restricted');
+		dragMove.clickContenedorBox();
+		dragMove.get.contenedorBox().move({ deltaX: 350, deltaY: 100 });
+		dragMove.get.contenedorBox().should('have.css', 'left', '350px').and('be.visible');
+		dragMove.clickContenedorParent();
+		dragMove.get.contenedorParent().move({ deltaX: 86, deltaY: 150 });
+		dragMove.get.contenedorParent().should('have.css', 'top', '86px').and('be.visible');
+	});
+	it('7553 | TC4: Validar poder movilizar el cursor top, center y bottom dentro de su caja contenedora', () => {
+		dragMove.clickCursoStyle();
+		dragMove.get.cursoStyle().should('have.text', 'Cursor Style');
+		dragMove.clickCursorCenter();
+		dragMove.get.cursorCenter().move({ deltaX: 250, deltaY: 100 });
+		dragMove.get.cursorCenter().should('have.css', 'top', '90px').and('be.visible');
+		dragMove.clickCursorTopLeft();
+		dragMove.get.cursorTopLeft().move({ deltaX: 350, deltaY: 0 });
+		dragMove.get.cursorTopLeft().should('have.css', 'top', '51px').and('be.visible');
+		dragMove.clickCursorBottom();
+		dragMove.get.cursorBottom().move({ deltaX: 100, deltaY: 200 });
+		dragMove.get.cursorBottom().should('have.css', 'top', '146px').and('be.visible');
 	});
 });
--- cypress/support/pages/Interactions/GX2-7552-dragabble.page.js
+++ cypress/support/pages/Interactions/GX2-7552-dragabble.page.js
@@ -5,20 +5,50 @@ class dgb {
 		axisRestricted: () => cy.get('#draggableExample-tab-axisRestriction'),
 		OnlyX: () => cy.get('#restrictedX'),
 		OnlyY: () => cy.get('#restrictedY'),
-		containeRestricted: () => cy.get('#draggableExample-tab-containerRestriction'),
-		contenedorBox: () => cy.get('.draggable ui-widget-content ui-draggable ui-draggable-handle'),
+		restricted: () => cy.get('#draggableExample-tab-containerRestriction'),
+		contenedorBox: () => cy.get('.draggable.ui-widget-content.ui-draggable.ui-draggable-handle'),
 		contenedorParent: () => cy.get('span.ui-widget-header.ui-draggable.ui-draggable-handle'),
 		cursoStyle: () => cy.get('#draggableExample-tab-cursorStyle'),
 		cursorCenter: () => cy.get('#cursorCenter'),
-		cursorTopLeft: () => cy.get('#cursortopLeft'),
+		cursorTopLeft: () => cy.get('#cursorTopLeft'),
 		cursorBottom: () => cy.get('#cursorBottom'),
 	};
 	clickSimple() {
 		this.get.simple().click();
-    }
-    clickDragbox() {
-        this.get.dragBox().click()
-    }
+	}
+	clickDragbox() {
+		this.get.dragBox().click();
+	}
+	clickAxis() {
+		this.get.axisRestricted().click();
+	}
+	clickOnly() {
+		this.get.OnlyX().click();
+	}
+	clickOnlyY() {
+		this.get.OnlyY().click();
+	}
+	clickContenedor() {
+		this.get.restricted().click();
+	}
+	clickContenedorBox() {
+		this.get.contenedorBox().click();
+	}
+	clickContenedorParent() {
+		this.get.contenedorParent().click();
+	}
+	clickCursoStyle() {
+		this.get.cursoStyle().click();
+	}
+	clickCursorCenter() {
+		this.get.cursorCenter().click();
+	}
+	clickCursorTopLeft() {
+		this.get.cursorTopLeft().click();
+	}
+	clickCursorBottom() {
+		this.get.cursorBottom().click();
+	}
 }
 
 export const dragMove = new dgb();
