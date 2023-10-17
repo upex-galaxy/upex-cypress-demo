dee5fea89583c81cf52a2862ccd7d823df7681df
Daryelin Rossell <daryelin.rossell@gmail.com>
2023-10-17 18:30:03 -0400

    GX-39854 selector de elemento

--- cypress/e2e/Tests/Elements/GX-39854-radioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-39854-radioButtons.cy.js
@@ -1,25 +1,24 @@
 describe('US GX-39854 | ToolsQA | Elements | Radio Buttons', () => {
-    // Precondiciones
-    beforeEach('The user must be in Tool QA-Raddio-Buttons', () => {
-    cy.visit('https://demoqa.com/radio-button')
-    cy.url().should('contain', 'radio-button');
-    });
-    // TC1
-    it('39855|  TC1: Validate that “Yes” can be selected in Radio Buttons.',() => {
-        cy.get('#yesRadio').click({ force : true }) // Click on button
-        cy.get('p.mt-3').should('have.text', 'You have selected Yes')
-        //cy.get('.text-success').should('have.text', 'Yes')
-    } );
+	// Precondiciones
+	beforeEach('The user must be in Tool QA-Raddio-Buttons', () => {
+		cy.visit('https://demoqa.com/radio-button');
+		cy.url().should('contain', 'radio-button');
+	});
+	// TC1
+	it('39855|  TC1: Validate that “Yes” can be selected in Radio Buttons.', () => {
+		cy.get('[for="yesRadio"]').click(); // Click on button
+		cy.get('p.mt-3').should('have.text', 'You have selected Yes');
+		//cy.get('.text-success').should('have.text', 'Yes')
+	});
 
-    //TC2
-    it('39855|  TC2: Validate that “Impressive” can be selected in Radio Buttons',() => {
-        cy.get('#impressiveRadio').click({ force : true }) // Click on button
-        cy.get('p.mt-3').should('have.text', 'You have selected Impressive')
-        //cy.get('.text-success').should('have.text', 'Impressive')
-    } );
-    //TC3
-    it('39855|TC3: Validate that “NO” cannot be selected in Radio Buttons',() => {
-        cy.get('#noRadio').should('be.disabled')
-    } );
-    
+	//TC2
+	it('39855|  TC2: Validate that “Impressive” can be selected in Radio Buttons', () => {
+		cy.get('[for="impressiveRadio"]').click(); // Click on button
+		cy.get('p.mt-3').should('have.text', 'You have selected Impressive');
+		//cy.get('.text-success').should('have.text', 'Impressive')
+	});
+	//TC3
+	it('39855|TC3: Validate that “NO” cannot be selected in Radio Buttons', () => {
+		cy.get('#noRadio').should('be.disabled');
+	});
 });
