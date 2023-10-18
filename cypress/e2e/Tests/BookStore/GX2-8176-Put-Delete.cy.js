28275c834582e78ccf19b23dd4c64a0c36e97f7a
PabloCz <Pablotest@gmail.com>
2023-10-18 10:26:21 -0300

    GX2-8176 Config inicial

---
+++ cypress/e2e/Tests/BookStore/GX2-8176-Put-Delete.cy.js
@@ -0,0 +1,5 @@
+describe('BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-DELETE)', () => {
+	beforeEach('PCR: Situarse en la Web , Loguearse correctamente , add 1 o más productos', () => {
+		cy.visit('/login');
+	});
+});
