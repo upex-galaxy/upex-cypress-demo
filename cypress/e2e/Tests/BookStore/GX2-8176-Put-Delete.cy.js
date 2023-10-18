63b73a2f9ac4297c7ee2ce7a4b3b980667eccc9a
PabloCz <Pablotest@gmail.com>
2023-10-18 18:12:30 -0300

    GX2-8176 add bookstorePage , json y Precondicion lista

--- cypress/e2e/Tests/BookStore/GX2-8176-Put-Delete.cy.js
+++ cypress/e2e/Tests/BookStore/GX2-8176-Put-Delete.cy.js
@@ -1,5 +1,23 @@
-describe('BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-DELETE)', () => {
-	beforeEach('PCR: Situarse en la Web , Loguearse correctamente , add 1 o más productos', () => {
-		cy.visit('/login');
+//TC Cancelado debido a problemas del sitoWeb
+//8177 | TC1: Validar remover producto utilizando el icono "TrashCan"
+//8177 | TC2: Validar remover todos los productos utilizando el boton "Delete All Books".
+import data from '../../../fixtures/data/GX2-8176-Bookstore-put-delete.json';
+import { BookStorePage } from '@pages/BookStore/GX2-8176-Bookstore-put-delete';
+const initialSetup = () => {
+	BookStorePage.Precondition().then(([response1, response2, response3]) => {
+		expect(response1.status).to.eq(200);
+		expect(response2.status).to.eq(200);
+		expect(response3.status).to.eq(201);
+		expect(response2.body).to.eq(true);
+		expect(response3.body.books[0].isbn).to.equal(Cypress.env('book1'));
+		expect(response3.body.books[1].isbn).to.equal(Cypress.env('book2'));
 	});
+};
+
+describe('BookStore | Grid | Actualizar y Eliminar Libros de la Tienda (PUT-DELETE)', () => {
+	beforeEach('precondition', () => {
+		initialSetup();
+	});
+
+	it('TC3: (DELETE) Validar remover un producto del profile', () => {});
 });
