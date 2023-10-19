73aaf4045e8d24d995c6319a2689bddde3bdd696
Daryelin Rossell <daryelin.rossell@gmail.com>
2023-10-19 16:49:15 -0400

    GX-39854 eliminando el archivo de la documentacion de la carpeta test plan

--- cypress/e2e/Tests/Elements/GX-39854-radioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX-39854-radioButtons.cy.js
@@ -5,21 +5,20 @@ describe('US GX-39854 | ToolsQA | Elements | Radio Buttons', () => {
 		cy.url().should('contain', 'radio-button');
 	});
 	// TC1
-	it('39855|  TC1: Validate that “Yes” can be selected in Radio Buttons.', () => {
+	it('  TC1: Validate that “Yes” can be selected in Radio Buttons.', () => {
 		cy.get('[for="yesRadio"]').click(); // Click on button
 		cy.get('p.mt-3').should('have.text', 'You have selected Yes');
 		//cy.get('.text-success').should('have.text', 'Yes')
 	});
 
 	//TC2
-	it('39855|  TC2: Validate that “Impressive” can be selected in Radio Buttons', () => {
+	it(' TC2: Validate that “Impressive” can be selected in Radio Buttons', () => {
 		cy.get('[for="impressiveRadio"]').click(); // Click on button
 		cy.get('p.mt-3').should('have.text', 'You have selected Impressive');
 		//cy.get('.text-success').should('have.text', 'Impressive')
 	});
 	//TC3
-	it('39855|TC3: Validate that “NO” cannot be selected in Radio Buttons', () => {
+	it('TC3: Validate that “NO” cannot be selected in Radio Buttons', () => {
 		cy.get('#noRadio').should('be.disabled');
-		
 	});
 });
