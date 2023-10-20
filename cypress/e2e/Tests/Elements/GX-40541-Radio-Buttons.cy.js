2678ff1441f40b63af65e71e2789bb37efd0cfb2
Franciscomedina240792 <franciscomedina240792@gmail.com>
2023-10-19 23:27:06 -0600

    GX-40541

--- cypress/e2e/Tests/Elements/GX-40541-Radio-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX-40541-Radio-Buttons.cy.js
@@ -2,19 +2,19 @@ describe('ToolsQA | Elements | Radio Buttons', () => {
     beforeEach(() => {
         cy.visit('https://demoqa.com/radio-button')
     })
-    it('40554 | TC1:   validar Selección de Radio Button "Yes"', () => {
+    it('40541 | TC1:   validar Selección de Radio Button "Yes"', () => {
         //click
         cy.get('[class="custom-control-label"]').eq(0).click()
         const Yesclickmsn = "Yes"
         cy.get('[class=text-success]').should('have.text', Yesclickmsn)
     })
-     it('40554 | TC2:   validar Selección de Radio Button "Impressive"', () => {
+     it('40541 | TC2:   validar Selección de Radio Button "Impressive"', () => {
         //click
         cy.get('[class="custom-control-label"]').eq(1).click()
         const Impressiveclickmsn = "Impressive"
         cy.get('[class=text-success]').should('have.text', Impressiveclickmsn)
      })
-    it('40554 | TC3:   validar Selección de Radio Button "No"', () => {
+    it('40541 | TC3:   validar Selección de Radio Button "No"', () => {
         //click
         cy.get('[class="custom-control-label disabled"]').eq(0).click()
         const Noclickmsn = "No"
