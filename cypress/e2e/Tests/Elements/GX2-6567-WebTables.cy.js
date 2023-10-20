c76af9a1de0a1ac79935a980740c73169218f76c
apscecilia <apscecilia@gmail.com>
2023-10-20 18:37:57 -0300

    GX2-6567 Se modifica CI suite, MD, se completan los 8 TC

--- cypress/e2e/Tests/Elements/GX2-6567-WebTables.cy.js
+++ cypress/e2e/Tests/Elements/GX2-6567-WebTables.cy.js
@@ -1,22 +1,128 @@
-import { Faker } from '@faker-js/faker';
 import { webTables } from '@pages/Elements/GX2-6567-WebTables.Page';
-import { removeLogs } from '@helper/RemoveLogs.js';
-removeLogs();
+import { faker } from '@faker-js/faker';
+// import { removeLogs } from '@helper/RemoveLogs.js';
+// removeLogs();
 
 describe('GX2-6567 ✅ToolsQA | Elements | Web Table', () => {
 	beforeEach('Precondition: The user should be located in demoqa.com/webtables', () => {
 		cy.visit('/webtables');
 		cy.url().should('contain', 'webtables');
 	});
-	it.only('6568 | TC1: Validate add new record', () => {
-		webTables.elements.addButtonClick().click();
-		webTables.addNewRecord();
-		webTables.elements.summitClick().click();
+
+	it('6568 | TC1: Validate add new record', () => {
+		webTables.elements.getFillRowCount().then($rows => {
+			const rowCountBefore = $rows.length;
+			webTables.elements.addRecordButton().click();
+			webTables.addNewRecord().then(randomData => {
+				webTables.elements.submitButton().click();
+				webTables.elements.getFillRowCount().should('have.length', rowCountBefore + 1);
+				webTables.elements
+					.getTable()
+					.find('[class= rt-tr-group]')
+					.eq(rowCountBefore)
+					.within(() => {
+						webTables.elements.getGridCell().eq(0).should('contain', randomData.firstName);
+						webTables.elements.getGridCell().eq(1).should('contain', randomData.lastName);
+						webTables.elements.getGridCell().eq(2).should('contain', randomData.age);
+						webTables.elements.getGridCell().eq(3).should('contain', randomData.email);
+						webTables.elements.getGridCell().eq(4).should('contain', randomData.salary);
+						webTables.elements.getGridCell().eq(5).should('contain', randomData.department);
+					});
+			});
+		});
 	});
-	it('6568 | TC2: Validate cancel adding record', () => {});
-	it('6568 | TC3: Validate records filter', () => {});
-	it('6568 | TC4: Validate sort  record by field', () => {});
-	it('6568 | TC5: Validate delete record', () => {});
-	it('6568 | TC6: Validate page layout', () => {});
-	it('6568 | TC7: Validate show specified number of rows by page.', () => {});
+
+	it('6568 | TC2: Validate cancel adding record', () => {
+		webTables.elements.getFillRowCount().then($rows => {
+			const rowCountBefore = $rows.length;
+			webTables.elements.addRecordButton().click();
+			webTables.elements.addRecordForm().should('be.visible');
+			webTables.addNewRecord();
+			webTables.elements.closeButton().click();
+			webTables.elements.addRecordForm().should('not.exist');
+			webTables.elements.addRecordButton().click();
+			webTables.elements.addRecordForm().should('be.visible');
+			webTables.elements.closeButton().click();
+			webTables.elements.addRecordForm().should('not.exist');
+			webTables.elements.getFillRowCount().should('have.length', rowCountBefore);
+		});
+	});
+
+	it('6568 | TC3: Validate records filter', () => {
+		webTables.filterRecord().then(randomData => {
+			const searchTerm = randomData;
+			webTables.elements.getTable().should('contain', searchTerm);
+		});
+	});
+
+	it('6568 | TC4: Validate record record by field', () => {
+		const columnHeaders = ['First Name', 'Last Name', 'Age', 'Email', 'Salary'];
+		const randomHeader = faker.helpers.arrayElement(columnHeaders);
+
+		webTables.sortedRecord(randomHeader).then(data => {
+			const { dataInColumn, sortedData } = data;
+			expect(dataInColumn).not.to.equal(sortedData);
+		});
+	});
+
+	it.only('6568 | TC5: Validate record edit', () => {
+		//let initialData;
+
+		webTables.selectRecord().then(randomGet => {
+			webTables.getCurrentData(randomGet);
+			//cy.log('initialData:', initialData);
+			cy.wrap(randomGet).then(index => {
+				webTables.elements.getEditButton().eq(index).click();
+			});
+		});
+		const editedData = webTables.recordEdit();
+		webTables.elements
+			.submitButton()
+			.click()
+			.then(cellTextArray => {
+				cy.wrap(cellTextArray).then(initial => {
+					expect(editedData.newFirstName).to.not.equal(initial[0]);
+				});
+			});
+	});
+
+	it('6568 | TC6: Validate record delete ', () => {
+		webTables.elements.getFillRowCount().then($rows => {
+			const rowCountBefore = $rows.length;
+			webTables.selectRecord().then(randomGet => {
+				cy.wrap(randomGet).then(index => {
+					webTables.elements.getDeleteButton().eq(index).click();
+					webTables.elements.getFillRowCount().should('have.length', rowCountBefore - 1);
+				});
+			});
+		});
+	});
+
+	it('6568 | TC7: Validate moving between the pagination bY the PREVIOUS and the NEXT button', () => {
+		const pageNumber = 1;
+		for (let i = 0; i < 10; i++) {
+			webTables.elements.addRecordButton().click();
+			webTables.addNewRecord();
+			webTables.elements.submitButton().click();
+		}
+		webTables.elements.btnPrevious().should('be.disabled');
+		webTables.elements.btnPrevious().should('have.css', 'color', 'rgba(0, 0, 0, 0.6)');
+		webTables.elements.btnNext().should('be.enabled');
+		webTables.elements.btnNext().trigger('mouseover');
+		webTables.elements.btnNext().should('have.css', 'cursor', 'pointer');
+		webTables.elements.nextButton().click();
+		webTables.elements.btnPrevious().should('be.enabled');
+		webTables.elements.getPageNumber().should('have.value', pageNumber + 1);
+		webTables.elements.previousButton().click();
+		webTables.elements.btnPrevious().should('be.disabled');
+		webTables.elements.getPageNumber().should('have.value', pageNumber);
+	});
+
+	it('6568 | TC8: Validate show specified number of rows by page', () => {
+		webTables.selectRowsSize().then(randomOption => {
+			webTables.elements.getTable().then(() => {
+				webTables.elements.getRowsSize().should('have.length.gte', parseInt(randomOption));
+			});
+		});
+	});
 });
--- cypress/support/pages/Elements/GX2-6567-WebTables.Page.js
+++ cypress/support/pages/Elements/GX2-6567-WebTables.Page.js
@@ -1,31 +1,157 @@
 import { faker } from '@faker-js/faker';
 class Webtables {
 	elements = {
-		addButtonClick: () => cy.get('#addNewRecordButton'),
+		addRecordButton: () => cy.get('#addNewRecordButton'),
 		inputFirstName: () => cy.get('#firstName'),
 		inputLastName: () => cy.get('#lastName'),
 		inputEmail: () => cy.get('#userEmail'),
 		inputAge: () => cy.get('#age'),
 		inputSalary: () => cy.get('#salary'),
 		inputDepartment: () => cy.get('#department'),
-		summitClick: () => cy.get('#submit'),
+		submitButton: () => cy.get('#submit'),
+		closeButton: () => cy.get('.close'),
+		searchBox: () => cy.get('#searchBox'),
+		getFillRowCount: () => cy.get('[class*= mr-2]'),
+		getEditButton: () => cy.get('[id*= edit-record]'),
+		getDeleteButton: () => cy.get('[id*= delete-record]'),
+		previousButton: () => cy.get('.-previous .-btn'),
+		nextButton: () => cy.get('.-next .-btn'),
+		rowsSize: () => cy.get('[class*= -pageSizeOptions] select'),
+		getGridCell: () => cy.get('[class= rt-td]'),
+		getTable: () => cy.get('[class= rt-tbody]'),
+		addRecordForm: () => cy.get('[class=modal-content]'),
+		getHeader: () => cy.get('.rt-th'),
+		getColumn: () => cy.get('.rt-tr .rt-td:nth-child'),
+		getPageNumber: () => cy.get('input[type="number"]'),
+		btnPrevious: () => cy.get('div.-previous button.-btn'),
+		btnNext: () => cy.get('div.-next button.-btn'),
+		getRowsSize: () => cy.get('.rt-tr'),
 	};
-	addNewRecord() {
+
+	randomRecord() {
 		const randomName = faker.name.firstName();
 		const randomLastName = faker.name.lastName();
 		const randomEmail = faker.internet.email();
 		const randomAge = faker.datatype.number({ min: 18, max: 100 });
 		const randomSalary = faker.datatype.number({ min: 1000, max: 50000 });
 		const randomDepartment = faker.word.noun();
+		return {
+			firstName: randomName,
+			lastName: randomLastName,
+			email: randomEmail,
+			age: randomAge,
+			salary: randomSalary,
+			department: randomDepartment,
+		};
+	}
 
-		this.elements.inputFirstName().type(randomName);
-		this.elements.inputLastName().type(randomLastName);
-		this.elements.inputEmail().type(randomEmail);
-		this.elements.inputAge().type(randomAge);
-		this.elements.inputSalary().type(randomSalary);
-		this.elements.inputDepartment().type(randomDepartment);
+	addNewRecord() {
+		const randomData = this.randomRecord();
+		this.elements.inputFirstName().type(randomData.firstName);
+		this.elements.inputLastName().type(randomData.lastName);
+		this.elements.inputAge().type(randomData.age);
+		this.elements.inputEmail().type(randomData.email);
+		this.elements.inputSalary().type(randomData.salary);
+		this.elements.inputDepartment().type(randomData.department);
+		return cy.wrap(randomData);
+	}
 
-		return randomName, randomLastName, randomEmail, randomAge, randomSalary, randomDepartment;
+	filterRecord() {
+		const arrayData = ['Cierra', 'vega', 39, 'cierra@example.com', 100, 'al', 'kierra', 29, 'leg', 4, 'surance'];
+		const randomData = faker.helpers.arrayElement(arrayData);
+		this.elements.searchBox().type(randomData);
+		return cy.wrap(randomData);
+	}
+
+	sortedRecord(columnHeader) {
+		// Obtener el índice de la columna seleccionada
+		return cy.contains('.rt-th', columnHeader).then(header => {
+			const columnIndex = header.index();
+			cy.log(columnIndex);
+			// Obtener todos los datos de la columna
+			const dataInColumn = [];
+			return cy
+				.get(`.rt-tr .rt-td:nth-child(${columnIndex + 1})`)
+				.each(cell => {
+					dataInColumn.push(cell.text());
+				})
+				.then(() => {
+					const filteredData = dataInColumn.filter(item => item !== ''); // Filtrar celdas vacías
+					const sortedData = [...filteredData].sort(); // Ordenar alfabéticamente (ascendente)
+					return { dataInColumn: dataInColumn, sortedData: sortedData };
+				});
+		});
+	}
+
+	selectRecord() {
+		let randomGet;
+		return this.elements
+			.getFillRowCount()
+			.then(items => {
+				const totalRecord = items.length;
+				const randomEdit = Math.floor(Math.random() * totalRecord);
+				randomGet = randomEdit;
+			})
+			.then(() => {
+				return randomGet;
+			});
+	}
+
+	recordEdit() {
+		const randomData = this.randomRecord();
+		this.elements.inputFirstName().clear().type(randomData.firstName);
+		this.elements.inputEmail().clear().type(randomData.email);
+		this.elements.inputSalary().type(randomData.salary);
+		this.elements.inputDepartment().clear().type(randomData.department);
+		return {
+			newFirstName: randomData.firstName,
+			newEmail: randomData.email,
+			newSalary: randomData.salary,
+			newDepartment: randomData.department,
+		};
+	}
+	getCurrentData(randomGet) {
+		return this.elements
+			.getTable()
+			.find('[class= rt-tr-group]')
+			.eq(randomGet)
+			.within(() => {
+				const cellTextArray = [];
+
+				for (let i = 0; i < 6; i++) {
+					webTables.elements
+						.getGridCell()
+						.eq(i)
+						.invoke('text')
+						.then(text => {
+							cellTextArray.push(text); // Almacena el valor de la celda en el arreglo
+							cy.log(text);
+						});
+				}
+				//return cellTextArray;
+				return cy.wrap(cellTextArray);
+			});
+	}
+
+	selectRecordDelete() {
+		let randomGet;
+		return this.elements
+			.getDeleteButton()
+			.then(items => {
+				const totalRecord = items.length;
+				const randomDelete = Math.floor(Math.random() * totalRecord);
+				randomGet = randomDelete;
+			})
+			.then(() => {
+				return randomGet;
+			});
+	}
+
+	selectRowsSize() {
+		const rowsOption = ['5 rows', '10 rows', '20 rows', '25 rows', '50 rows', '100 rows'];
+		const randomOption = faker.helpers.arrayElement(rowsOption);
+		this.elements.rowsSize().select(randomOption);
+		return cy.wrap(randomOption);
 	}
 }
 export const webTables = new Webtables();
