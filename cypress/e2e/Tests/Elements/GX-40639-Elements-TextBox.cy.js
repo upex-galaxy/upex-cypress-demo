c114aef6d1cad1ea2616878fef4088e6b72bd22a
Franciscomedina240792 <franciscomedina240792@gmail.com>
2023-10-23 22:17:14 -0600

    GX-40639: se modificó el archivo .json para agilizar el trabajo en el archivo .js, se agregó el TC 4, de intentar validar el formulario sin un Domain

--- cypress/e2e/Tests/Elements/GX-40639-Elements-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-40639-Elements-TextBox.cy.js
@@ -17,8 +17,8 @@ describe('ToolsQA | Elements | Text Box', () => {
         cy.fixture("data/GX-40639-TextBox").then(data => {
             cy.get(data.FullNameInput).type(data.FullNameValid),
                 cy.get(data.FullNameInput).should('have.value', data.FullNameValid),//verificación campo 'Full name'
-                cy.get(data.email.input).type(data.email.data.valid),
-                cy.get(data.email.input).should('have.value', data.email.data.valid),//Verificación campo 'email'
+                cy.get(data.emailInput).type(data.emailValid),
+                cy.get(data.emailInput).should('have.value', data.emailValid),//Verificación campo 'email'
                 cy.get(data.CurrentAddress.input).type(data.CurrentAddress.data.valid),
                 cy.get(data.CurrentAddress.input).should('have.value', data.CurrentAddress.data.valid),//verificación campo 'Current Address'
                 cy.get(data.PermanentAddress.input).type(data.PermanentAddress.data.valid),
@@ -29,12 +29,37 @@ describe('ToolsQA | Elements | Text Box', () => {
         cy.fixture("data/GX-40639-TextBox").then(data => {
             cy.get(data.FullNameInput).type(data.FullNameValid),
                 cy.get(data.FullNameInput).should('have.value', data.FullNameValid),//Verificación campo 'Full Name'
-                cy.get(data.email.input).type(data.email.data.invalid.invalidMail),
-                cy.get(data.email.input).should('have.value', data.email.data.invalid.invalidMail),//Verificación campo 'email' Vacío
+                cy.get(data.emailInput).type(data.emailInvalid.invalidMail),
+                cy.get(data.emailInput).should('have.value', data.emailInvalid.invalidMail),//Verificación campo 'email' Vacío
                 cy.get(data.CurrentAddress.input).type(data.CurrentAddress.data.valid),
                 cy.get(data.CurrentAddress.input).should('have.value', data.CurrentAddress.data.valid),//Verificación campo 'Current Address'
                 cy.get(data.PermanentAddress.input).type(data.PermanentAddress.data.valid),
                 cy.get(data.PermanentAddress.input).should('have.value', data.PermanentAddress.data.valid)//Verificación campo 'PErmanent Address'
         })
-    })
+    });
+    it('40639 | TC3: Validar completar el formulario con Email sin @', () => {
+        cy.fixture("data/GX-40639-TextBox").then(data => {
+            cy.get(data.FullNameInput).type(data.FullNameValid),
+                cy.get(data.FullNameInput).should('have.value', data.FullNameValid),//Verificación campo 'Full Name'
+                cy.get(data.emailInput).type(data.emailInvalid.emailWithOutAt),
+                cy.get(data.emailInput).should('have.value', data.emailInvalid.emailWithOutAt),//Verificación campo 'email' sin @
+                cy.get(data.CurrentAddress.input).type(data.CurrentAddress.data.valid),
+                cy.get(data.CurrentAddress.input).should(data.CurrentAddress.data.valid),//Verificación campo 'Current Address'
+                cy.get(data.PermanentAddress.input).type(data.PermanentAddress.data.valid),
+                cy.get(data.PermanentAddress.input).should(data.PermanentAddress.data.valid)//Verificación campo 'Permanent Address'
+        })
+    });
+    it('40639 | TC4: Validar Completar el formulario com Email sin Domain', () => {
+        cy.fixture("data/GX-40639-TextBox").then(data => {
+            cy.get(data.FullNameInput).type(data.FullNameValid),
+                cy.get(data.FullNameInput).should('have.value', data.FullNameValid),//Validar campo 'Full Name'
+                cy.get(data.emailInput).type(data.emailInvalid.emailWithOutDomain),
+                cy.get(data.emailInput).should('have.value', data.emailInvalid.emailWithOutDomain),//Validar Campo 'email'
+                cy.get(data.CurrentAddress.input).type(data.CurrentAddress.data.valid),
+                cy.get(data.CurrentAddress.input).should('have.value', data.CurrentAddress.data.valid),//Validar Campo 'Current Address'
+                cy.get(data.PermanentAddress.input).type(data.PermanentAddress.data.valid),
+                cy.get(data.PermanentAddress.input).should('have.value', data.PermanentAddress.data.valid)
+        })
+    });
+    
 })
