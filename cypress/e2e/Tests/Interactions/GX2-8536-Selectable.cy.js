cb3182571f2a79affdc96c75ffe0a713bd2d738e
Erika <tolosa.erika503@gmail.com>
2023-10-24 15:49:01 +0100

    GX-8536 Primeras actualizaciones

---
+++ cypress/e2e/Tests/Interactions/GX2-8536-Selectable.cy.js
@@ -0,0 +1,4 @@
+//GX2 - 8537 - TC1 Validar poder seleccionar los elementos de la pestaña “List“
+//GX2-8537- TC2 Validar poder des-seleccionar los elementos de la pestaña “List“
+//GX2-8537- TC3 Validar poder seleccionar los elementos de la pestaña “Grid“
+//GX2-8537- TC4 Validar poder des-seleccionar los elementos de la pestaña “Grid“
