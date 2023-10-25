4b70e6556853a66a1c8400ac5e146fd4808dbeb4
LuisManuel7 <luis_ns_124@hotmail.com>
2023-10-25 16:54:13 -0400

    correccion de casos de prueba de broken link

--- cypress/e2e/Tests/Elements/GX-40938-broken-links-images.cy.js
+++ cypress/e2e/Tests/Elements/GX-40938-broken-links-images.cy.js
@@ -3,13 +3,13 @@ describe('GX-40938 | TS: ✅ToolsQA | Elements | Broken Links Images', () => {
 		cy.visit('/broken');
 		cy.wait(2000);
 	});
-	it.skip('40945| TC1: Validar que el logotipo de Tool QA se muestre correctamente', () => {
+	it('40945| TC1: Validar que el logotipo de Tool QA se muestre correctamente', () => {
 		cy.get('img[src="/images/Toolsqa.jpg"]').eq(1).should('be.visible');
 	});
-	it.skip('40945| TC2: Validar que aparezcas la imagen rota en el titulo “Broken image”', () => {
+	it('40945| TC2: Validar que aparezcas la imagen rota en el titulo “Broken image”', () => {
 		cy.get('img[src="/images/Toolsqa_1.jpg"]').should('be.visible');
 	});
-	it.skip('40945| TC3: Validar que al hacer click en el link “Valid link” funcione correctamente', () => {
+	it('40945| TC3: Validar que al hacer click en el link “Valid link” funcione correctamente', () => {
 		cy.get('[href="http://demoqa.com"]').click();
 		cy.url().should('contain', 'https://demoqa.com/');
 	});
