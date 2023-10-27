61505defd22e6110f1836af9982c29420db17d87
LuisManuel7 <luis_ns_124@hotmail.com>
2023-10-27 13:24:56 -0400

    GX-41418 TexBox Se realizaron los 6 casos de prueba junto con la fixtures exitoxamente

--- cypress/e2e/Tests/Elements/GX-41418-TexBox.cy.js
+++ cypress/e2e/Tests/Elements/GX-41418-TexBox.cy.js
@@ -4,20 +4,74 @@ describe('GX-41418 | TS: 🪶ToolsQA | Elements | Text Box: Fill form and Submit
 	});
 	it('41426| TC1: Validar  ingresar al formulario con datos validos', () => {
 		cy.fixture('data/Elements/GX-41418-TextBox').then(the => {
-			cy.get('#userName').type(the.fullName.data.valido);
-			cy.get('#userName').should('have.value', the.fullName.data.valido);
+			cy.get('#userName').type(the.fullName.data.valido).should('have.value', the.fullName.data.valido);
+
+			cy.get('#userEmail').type(the.email.dataEmail.valido);
+			cy.get('#userEmail').should('have.value', the.email.dataEmail.valido);
+
+			cy.get('#currentAddress').type(the.currentAddress.dataCurrentAddress.valido);
+			cy.get('#currentAddress').should('have.value', the.currentAddress.dataCurrentAddress.valido);
+
+			cy.get('#permanentAddress').type(the.permanentAddress.dataPermanentAddress.valido);
+			cy.get('#permanentAddress').should('have.value', the.permanentAddress.dataPermanentAddress.valido);
+
+			cy.get('#submit').click();
 		});
 	});
-	it.skip('US # | TC#2: Aquí puedes escribir otro Caso de Prueba', () => {
-		cy.get('[href="/commands/querying"]').eq(2).click();
-		cy.get('#query-btn') // Sintaxis Comando Get de Selector de IDs
-			.click(); // Esto es un método de Interacción (Acción)
+	it.skip('41426| TC2: Validar no poder ingresar el formulario cuando el “email” no contenga @', () => {
+		cy.fixture('data/Elements/GX-41418-TextBox').then(the => {
+			cy.get('#userName').should('be.empty');
+
+			cy.get('#userEmail').type(the.email.dataEmail.invalido);
+			cy.get('#currentAddress').should('be.empty');
+			cy.get('#permanentAddress').should('be.empty');
+			cy.get('#submit').click();
+			cy.get('input.mr-sm-2.field-error.form-control').should('be.enabled');
+		});
 	});
-	it.skip('US # | TC#3: Aquí puedes escribir otro Caso de Prueba', () => {
-		cy.get('[href="/commands/querying"]').eq(2).click();
-		cy.get('#query-btn').click();
-		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
-			.should('contain.text', 'Button'); // Assertion BDD para validar resultado esperado (de muchas formas)
+	it('41426| TC3 Validar no poder ingresar el formulario cuando el “email” no contenga 1 carácter alfanuméricos antes del “@”', () => {
+		cy.fixture('data/Elements/GX-41418-TextBox').then(the => {
+			cy.get('#userName').should('be.empty');
+
+			cy.get('#userEmail').type(the.email.dataEmail.invalido1);
+			cy.get('#currentAddress').should('be.empty');
+			cy.get('#permanentAddress').should('be.empty');
+			cy.get('#submit').click();
+			cy.get('input.mr-sm-2.field-error.form-control').should('be.enabled');
+		});
+	});
+	it.skip('41426| TC4 Validar no poder ingresar el formulario cuando el  “email” no contenga 1 carácter alfanuméricos  después del @', () => {
+		cy.fixture('data/Elements/GX-41418-TextBox').then(the => {
+			cy.get('#userName').should('be.empty');
+
+			cy.get('#userEmail').type(the.email.dataEmail.invalido2);
+			cy.get('#currentAddress').should('be.empty');
+			cy.get('#permanentAddress').should('be.empty');
+			cy.get('#submit').click();
+			cy.get('input.mr-sm-2.field-error.form-control').should('be.enabled');
+		});
+	});
+	it.skip('41426 TC5 Validar no poder ingresar el formulario cuando el “email” no contenga 1 carácter alfanuméricos  antes del “.”', () => {
+		cy.fixture('data/Elements/GX-41418-TextBox').then(the => {
+			cy.get('#userName').should('be.empty');
+
+			cy.get('#userEmail').type(the.email.dataEmail.invalido3);
+			cy.get('#currentAddress').should('be.empty');
+			cy.get('#permanentAddress').should('be.empty');
+			cy.get('#submit').click();
+			cy.get('input.mr-sm-2.field-error.form-control').should('be.enabled');
+		});
+	});
+	it.skip('41426| TC6 Validar no poder ingresar el formulario cuando el “email” no contenga 1 carácter alfanuméricos  después del “.”', () => {
+		cy.fixture('data/Elements/GX-41418-TextBox').then(the => {
+			cy.get('#userName').should('be.empty');
+
+			cy.get('#userEmail').type(the.email.dataEmail.invalido4);
+			cy.get('#currentAddress').should('be.empty');
+			cy.get('#permanentAddress').should('be.empty');
+			cy.get('#submit').click();
+			cy.get('input.mr-sm-2.field-error.form-control').should('be.enabled');
+		});
 	});
 });
 import { removeLogs } from '@helper/RemoveLogs';
