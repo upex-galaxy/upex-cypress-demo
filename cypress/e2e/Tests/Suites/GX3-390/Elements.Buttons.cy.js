e1a0cd40caac6ce41d4591286b523765fd7aec74
Franco-Carrera <carrerafranco825@gmail.com>
2023-11-14 11:50:40 -0300

    GX3-390 test(elements): Los TC han pasado y la feature esta funcionando como se espera

---
+++ cypress/e2e/Tests/Suites/GX3-390/Elements.Buttons.cy.js
@@ -0,0 +1,17 @@
+describe('⚡️ToolsQA | Elements | Buttons', () => {
+	beforeEach('Usuario debe estar situado en el endpoint buttons', () => {
+		cy.visit('https://demoqa.com/buttons');
+		cy.url().should('contain', 'buttons');
+	});
+	it('392 | TC1: Validar hacer doble click en el botón de doble click', () => {
+		cy.get("[type='button']").eq(1).dblclick();
+		cy.wait(2000);
+	});
+	it('392 | TC2: Validar hacer click derecho en el botón de click derecho', () => {
+		cy.get('#rightClickBtn').rightclick();
+		cy.wait(2000);
+	});
+	it('392 | TC3: Validar hacer click en el botón de click', () => {
+		cy.get('.mt-4 > button').eq(1).click();
+	});
+});
