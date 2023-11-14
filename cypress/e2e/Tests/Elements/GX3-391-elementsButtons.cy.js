2a9548c33123a08a8fc62b832394b25fe35c09d1
Julia <juliafekiac@gmail.com>
2023-11-15 00:55:17 +0100

    TC2 ha pasado la prueba correctamente

--- cypress/e2e/Tests/Elements/GX3-391-elementsButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-391-elementsButtons.cy.js
@@ -1,5 +1,8 @@
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
+
 describe('TS-397 : ToolsQA | Elements | Buttons', () => {
-	beforeEach('Precondition: User must be situated on demoQA web page - end point button', () => {
+	beforeEach('Precondition: User must be situated on demoQA web page - end point buttons', () => {
 		cy.visit('https://demoqa.com/buttons');
 		cy.url().should('contain', 'buttons');
 	});
@@ -8,6 +11,14 @@ describe('TS-397 : ToolsQA | Elements | Buttons', () => {
 		cy.get('#doubleClickMessage').should('exist');
 		cy.get('#doubleClickMessage').should('contain.text', 'You have done a double click');
 	});
-	//	it('TC2: Verify ‘RightClick’ button can be clicked', () => {});
-	//	it('TC3: Verify ‘Click’ button can be clicked', () => {});
+	it('TC2: Verify ‘RightClick’ button can be clicked', () => {
+		cy.get('#rightClickBtn').rightclick();
+		cy.get('#rightClickMessage').should('exist');
+		cy.get('#rightClickMessage').should('contain.text', 'You have done a right click');
+	});
+	// it('TC3: Verify ‘Click’ button can be clicked', () => {
+	// 	cy.get("[type='button']").eq(3).click();
+	// 	cy.get('#dynamicClickMessage').should('exist');
+	// 	cy.get('#dynamicClickMessage').should('contain.text', 'You have done a dynamic click');
+	// });
 });
