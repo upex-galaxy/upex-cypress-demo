786538e403f1495672460188defb0e4e9c3e3ff0
Julia <juliafekiac@gmail.com>
2023-11-16 00:05:01 +0100

    GX3-391 : modificacion del archivo cypress

--- cypress/e2e/Tests/Elements/GX3 -391 - tools - qa - elements - buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3 -391 - tools - qa - elements - buttons.cy.js
@@ -11,11 +11,11 @@ describe('TS-397 : ToolsQA | Elements | Buttons', () => {
 		cy.get('#doubleClickMessage').should('exist');
 		cy.get('#doubleClickMessage').should('contain.text', 'You have done a double click');
 	});
-	// it('TC2: Verify ‘RightClick’ button can be clicked', () => {
-	// 	cy.get('#rightClickBtn').rightclick();
-	// 	cy.get('#rightClickMessage').should('exist');
-	// 	cy.get('#rightClickMessage').should('contain.text', 'You have done a right click');
-	// });
+	it('TC2: Verify ‘RightClick’ button can be clicked', () => {
+		cy.get('#rightClickBtn').rightclick();
+		cy.get('#rightClickMessage').should('exist');
+		cy.get('#rightClickMessage').should('contain.text', 'You have done a right click');
+	});
 	// it('TC3: Verify ‘Click’ button can be clicked', () => {
 	// 	cy.get("[type='button']").eq(3).click();
 	// 	cy.get('#dynamicClickMessage').should('exist');
