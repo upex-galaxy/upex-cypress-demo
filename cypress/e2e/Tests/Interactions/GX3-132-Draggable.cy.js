944ee518ecc5c28e840fc8316acf69357bc747be
Daryelin Rossell <daryelin.rossell@gmail.com>
2023-11-16 16:29:24 -0400

    test(GX3-132): agregando varios TC, falta analizar como obtener el cursor

--- cypress/e2e/Tests/Interactions/GX3-132-Draggable.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-132-Draggable.cy.js
@@ -16,9 +16,44 @@ describe('US GX3-132 | ToolsQA | Interactions | Dragabble', () => {
 		draggable.get.simpleTabVisible().should('be.visible');
 	});
 
-	it('132 | TC2: Validar que "Drag me" se puede arrastrar como se desee en cualquier dirección de la pestaña “Simple”', () => {
-		//draggable.get.buttonDragBox().click();
+	it('132 | TC2:  Validar que "Drag me" se puede arrastrar de forma aleatoria en cualquier dirección de la pestaña “Simple”', () => {
+		draggable.get.simpleTab().click();
 		draggable.moveRandom(randomX, randomY);
-		
+		draggable.get.buttonDragBox().should('have.css', 'left', `${randomX}px`);
+		draggable.get.buttonDragBox().should('have.css', 'top', `${randomY}px`);
+	});
+	it('132 | TC3:  Validar que "Only X"  y "Only Y" se pueden mover aleatoriamente en el eje correspondiente según su nombre X o Y de la pestaña “Axis Restricted”', () => {
+		draggable.get.axisRestrictedTab().click();
+		draggable.moveRandomX(randomX, 0);
+		draggable.get.buttonOnlyX().should('have.css', 'left', `${randomX}px`);
+		draggable.get.buttonOnlyX().should('have.css', 'top', `${0}px`);
+		draggable.moveRandomY(0, randomY);
+		draggable.get.buttonOnlyY().should('have.css', 'top', `${randomY}px`);
+		draggable.get.buttonOnlyY().should('have.css', 'left', `${0}px`);
+	});
+	it('132 | TC4: Validar que “I`m contained within the box” y “Im contained within my parent” se pueden mover aleatoriamente solo dentro del área de acción delimitada de la pestaña “Container Restricted”', () => {
+		const ramdomBoxX = faker.datatype.number({ min: 0, max: 280 });
+		const ramdomBoxY = faker.datatype.number({ min: 0, max: 106 });
+		const ramdomParentX = faker.datatype.number({ min: 0, max: 13 });
+		const ramdomParentY = faker.datatype.number({ min: 0, max: 86 });
+
+		draggable.get.containerRestrictedTab().click();
+		draggable.moveRandomDelimitedBox(ramdomBoxX, ramdomBoxY);
+		draggable.get.textContainedBox().should('have.css', 'left', `${ramdomBoxX}px`);
+		draggable.get.textContainedBox().should('have.css', 'top', `${ramdomBoxY}px`);
+
+		draggable.moveRandomDelimitedParent(ramdomParentX, ramdomParentY);
+		draggable.get.textContainedParent().should('have.css', 'left', `${ramdomParentX}px`);
+		draggable.get.textContainedParent().should('have.css', 'top', `${ramdomParentY}px`);
+	});
+	it.only('132 | TC5: Validar que el cursor permanece en el centro del cuadro "I will always stick to the center” de la pestaña Cursor Style”', () => {
+		draggable.get.cursorStyleTab().click();
+		const ramdomCenterX = faker.datatype.number({ min: 450, max: 1400 });
+		const ramdomCenterY = faker.datatype.number({ min: 450, max: 1000 });
+		cy.log(ramdomCenterX, ramdomCenterY);
+		draggable.get
+			.cursorCenter()
+			.trigger('mousedown', { which: 1 })
+			.trigger('mousemove', { which: 1, pageX: ramdomCenterX, pageY: ramdomCenterY });
 	});
 });
--- cypress/support/pages/Interactions/GX3-132-Draggable.page.js
+++ cypress/support/pages/Interactions/GX3-132-Draggable.page.js
@@ -1,15 +1,35 @@
 class Dragabble {
 	get = {
 		simpleTab: () => cy.get('#draggableExample-tab-simple'),
-		axisRestrictedTab: () => cy.get('#draggableExample-tab-simple'),
+		axisRestrictedTab: () => cy.get('#draggableExample-tab-axisRestriction'),
 		containerRestrictedTab: () => cy.get('#draggableExample-tab-containerRestriction'),
 		cursorStyleTab: () => cy.get('#draggableExample-tab-cursorStyle'),
 		simpleTabVisible: () => cy.get('.nav-item.active'),
 		buttonDragBox: () => cy.get('#dragBox'),
+		buttonOnlyX: () => cy.get('#restrictedX'),
+		buttonOnlyY: () => cy.get('#restrictedY'),
+		textContainedBox: () => cy.get('div.draggable.ui-widget-content.ui-draggable.ui-draggable-handle'),
+		textContainedParent: () => cy.get('span.ui-widget-header.ui-draggable.ui-draggable-handle'),
+		cursorCenter: () => cy.get('#cursorCenter'),
+		cursorTopLeft: () => cy.get('#cursorTopLeft'),
+		cursorBottom: () => cy.get('#cursorBottom'),
 	};
 
-	moveRandom(X, Y) {
-		this.get.buttonDragBox().move({ X, Y, force: true });
+	moveRandom(deltaX, deltaY) {
+		this.get.buttonDragBox().move({ deltaX, deltaY, force: true });
+	}
+	moveRandomX(deltaX, deltaY) {
+		this.get.buttonOnlyX().move({ deltaX, deltaY, force: true });
+	}
+	moveRandomY(deltaX, deltaY) {
+		this.get.buttonOnlyY().move({ deltaX, deltaY, force: true });
+	}
+
+	moveRandomDelimitedBox(deltaX, deltaY) {
+		this.get.textContainedBox().move({ deltaX, deltaY, force: true });
+	}
+	moveRandomDelimitedParent(deltaX, deltaY) {
+		this.get.textContainedParent().move({ deltaX, deltaY, force: true });
 	}
 }
 export const draggable = new Dragabble();
