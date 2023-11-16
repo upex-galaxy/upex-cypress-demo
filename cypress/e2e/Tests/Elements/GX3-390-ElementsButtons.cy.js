0ef68dd4535d481281c66f4160d3256ac93c579d
Franco-Carrera <carrerafranco825@gmail.com>
2023-11-16 20:15:11 -0300

    GX-390 Test(ElementsButtons): Se han realizado assertions para validar que se contengan determinados atributos y textos en los elementos respectivos, las pruebas están pasadas

--- cypress/e2e/Tests/Elements/GX3-390-ElementsButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-390-ElementsButtons.cy.js
@@ -4,14 +4,25 @@ describe('⚡️ToolsQA | Elements | Buttons', () => {
 		cy.url().should('contain', 'buttons');
 	});
 	it('392 | TC1: Validar hacer doble click en el botón de doble click', () => {
-		cy.get("[type='button']").eq(1).dblclick();
+		cy.get("[type='button']")
+			.eq(1)
+			.dblclick()
+			.should('be.visible')
+			.and('not.equal', 'triple')
+			.and('contain', 'Double')
+			.and('have.id', 'doubleClickBtn');
 		cy.wait(2000);
 	});
 	it('392 | TC2: Validar hacer click derecho en el botón de click derecho', () => {
-		cy.get('#rightClickBtn').rightclick();
+		cy.get('#rightClickBtn')
+			.rightclick()
+			.should('not.equal', 'right')
+			.and('not.be.empty')
+			.and('contain', 'Righ')
+			.and('have.attr', 'type', 'button');
 		cy.wait(2000);
 	});
 	it('392 | TC3: Validar hacer click en el botón de click', () => {
-		cy.get('.mt-4 > button').eq(1).click();
+		cy.get('.mt-4 > button').eq(1).click().should('exist').and('have.attr', 'type', 'button').and('contain.text', 'Me');
 	});
 });
