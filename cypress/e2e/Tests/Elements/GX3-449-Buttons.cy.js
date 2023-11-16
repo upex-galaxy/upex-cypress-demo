5aaa95d676d062396e9d3d70fbdea07758afe209
eleana2610 <eleanavcg@gmail.com>
2023-11-16 12:54:43 -0300

    GX3-449 actualizar nombre de Tests

--- cypress/e2e/Tests/Elements/GX3-449-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-449-Buttons.cy.js
@@ -2,15 +2,15 @@ describe('Buttons', () => {
 	beforeEach('', () => {
 		cy.visit('https://demoqa.com/buttons');
 	});
-	it('double click button', () => {
+	it('451 | TC1: Validate functionality of Double Click Me button', () => {
 		cy.get('#doubleClickBtn').dblclick();
 		cy.get('#doubleClickMessage').should('exist').contains('You have done a double click');
 	});
-	it('Right click on button', () => {
+	it('451 | TC2 : Validate functionality of Right Click Me button', () => {
 		cy.get('.row').find('#rightClickBtn').rightclick();
 		cy.get('#rightClickMessage').should('contain', 'You have done a right click');
 	});
-	it('single click on button', () => {
+	it('451 | TC3: Validate functionality of Click me button', () => {
 		cy.get('button[class="btn btn-primary"]').eq(2).contains('Click Me').click();
 		cy.get('#dynamicClickMessage').contains('You have done a dynamic click');
 	});
