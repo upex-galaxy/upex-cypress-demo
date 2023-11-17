39ca2e84bf8ce73cadd502d52633ce189572d516
Erika <tolosa.erika503@gmail.com>
2023-11-17 12:20:01 +0000

    GX3-440 Se actualizan cambios en mi Test

--- cypress/e2e/Tests/Widgets/GX3-440-SelecMenuTest.cy.js
+++ cypress/e2e/Tests/Widgets/GX3-440-SelecMenuTest.cy.js
@@ -1,58 +1,58 @@
-import selectMenu from '@pages/Widgets/GX3-440-SelecMenu.page.js';
+import SelectMenu from 'cypress/support/pages/Widgets/GX3-440-SelecMenu.page.js';
 
-describe('GX3-440 | ToolsQA | Widgets | selectMenu - Select Menu', () => {
+describe('GX3-440 | ToolsQA | Widgets | SelectMenu - Select Menu', () => {
 	beforeEach('Precondition: Visit website DemoQA', () => {
 		cy.visit('https://demoqa.com/select-menu');
 	});
 
 	it('440 | TC1: Validate dropdown "Select Value" by select each one.', () => {
 		cy.fixture('/data/Widgets/GX3-440-SelecMenu.json').then(data => {
-			selectMenu.clickSelectValue();
-			selectMenu.get.optionA().should('have.text', data.selectValue.group1.OptionA);
-			selectMenu.get.optionB().should('have.text', data.selectValue.group1.OptionB);
-			selectMenu.get.optionC().should('have.text', data.selectValue.group2.OptionC);
-			selectMenu.get.optionD().should('have.text', data.selectValue.group2.OptionD);
-			selectMenu.get.optionF().should('have.text', data.selectValue.outOfGroup.OptionF);
-			selectMenu.get.optionE().should('have.text', data.selectValue.outOfGroup.optionE);
+			SelectMenu.clickSelectValue();
+			SelectMenu.get.optionA().should('have.text', data.selectValue.group1.OptionA);
+			SelectMenu.get.optionB().should('have.text', data.selectValue.group1.OptionB);
+			SelectMenu.get.optionC().should('have.text', data.selectValue.group2.OptionC);
+			SelectMenu.get.optionD().should('have.text', data.selectValue.group2.OptionD);
+			SelectMenu.get.optionF().should('have.text', data.selectValue.outOfGroup.OptionF);
+			SelectMenu.get.optionE().should('have.text', data.selectValue.outOfGroup.optionE);
 		});
 	});
 
 	it('440 | TC2: Validate dropdown "Select Value" by selecting a random one.', () => {
 		// Hacer clic en el menú desplegable "Select Value"
-		selectMenu.clickSelectValue();
+		SelectMenu.clickSelectValue();
 		// Obtener todas las opciones del menú
-		selectMenu.get.allOptionsSelectValue().then(options => {
+		SelectMenu.get.allOptionsSelectValue().then(options => {
 			// Obtener la longitud de las opciones
 			const max = options.length - 1;
 			// Seleccionar una opción aleatoria
 			const randomIndex = Cypress._.random(0, max);
 			options.eq(randomIndex).click();
 			// Verificar que la opción seleccionada contenga el texto 'option'
-			selectMenu.get.valueSelectedSelectValue().should('contain.text', 'option');
+			SelectMenu.get.valueSelectedSelectValue().should('contain.text', 'option');
 		});
 	});
 
-	it('440 | TC3: Validate show message "No options" when the type text not match in selectMenu "Select Value"', () => {
+	it('440 | TC3: Validate show message "No options" when the type text not match in SelectMenu "Select Value"', () => {
 		cy.fixture('ruta/a/tu/fixture.json').then(data => {
 			// Iniciar la acción de escribir texto en el campo de selección
-			selectMenu.typeSelectValue('Hola');
+			SelectMenu.typeSelectValue('Hola');
 			// Verificar que el mensaje mostrado sea el esperado desde el fixture
-			selectMenu.get.inputInvalidSelectValue().should('have.text', data.selectValue.messageSelectValue);
+			SelectMenu.get.inputInvalidSelectValue().should('have.text', data.selectValue.messageSelectValue);
 		});
 	});
 
 	it('440 | TC4: Validate dropdown "Select One" by selecting each option.', () => {
 		// Acceder al menú desplegable "Select One"
-		selectMenu.containerSelectOne().click();
+		SelectMenu.containerSelectOne().click();
 		// Obtener todas las opciones del menú "Select One" desde el fixture
 		cy.fixture('/data/Widgets/GX3-440-SelecMenu.json').then(data => {
 			const options = data.selectOne.options;
 			// Iterar sobre cada opción
 			options.forEach(optionText => {
 				// Seleccionar la opción por su texto
-				selectMenu.containerSelectOne().select(optionText);
+				SelectMenu.containerSelectOne().select(optionText);
 				// Verificar que la opción seleccionada tenga el texto correcto
-				selectMenu.valueSelectedSelectOne().should('have.text', optionText);
+				SelectMenu.valueSelectedSelectOne().should('have.text', optionText);
 			});
 		});
 	});
@@ -61,14 +61,14 @@ describe('GX3-440 | ToolsQA | Widgets | selectMenu - Select Menu', () => {
 /*
 440 | TC1: Validate dropdown "Select Value" by select each one.
 440 | TC2: Validate dropdown "Select Value" by selecting a random one.
-440 | TC3: Validate show massage "No options" when the type text not match in selectMenu "Select Value"
+440 | TC3: Validate show massage "No options" when the type text not match in SelectMenu "Select Value"
 440 | TC4: Validate dropdown "Select One" by selecting a random one
-440 | TC5: Validate show massage "No options" when the type text not match in selectMenu "Select One"
+440 | TC5: Validate show massage "No options" when the type text not match in SelectMenu "Select One"
 440 | TC6: Validate dropdown "Old Style Select Menu" by selecting a random one.
 440 | TC7: Validate dropdown "Multiselect drop down" by selecting a random one.
 440 | TC8: Validate dropdown "Multiselect drop down" by selecting a random two.
 440 | TC9: Validate dropdown "Multiselect drop down" by selecting a random three.
 440 | TC10: Validate dropdown "Multiselect drop down" by selecting all four.
-440 | TC11: Validate show massage "No options" when the type text not match in selectMenu "Multiselect drop down"
+440 | TC11: Validate show massage "No options" when the type text not match in SelectMenu "Multiselect drop down"
 440 | TC12: Validate dropdown "Standard multi select" by selecting a random one.
 */
