a4b7987b79e2bc5318ca08d538d93e6208e6c2bf
Julia <juliafekiac@gmail.com>
2023-11-19 03:05:41 +0100

    GX3-470 : Modificados los selectores evitando click({force=true}) en TC1, TC2

--- cypress/e2e/Tests/Elements/GX3-470-RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-470-RadioButtons.cy.js
@@ -8,36 +8,28 @@ describe('GX3-470 | Tool QA | Elements | Radio Buttons', () => {
 		cy.visit('https://demoqa.com/radio-button');
 		cy.url().should('contain', 'radio-button');
 		for (let i = 0; i < 3; i++) {
-			cy.get('[type="radio"]').eq(i).should('exist'); //.and('be.visible');
+			cy.get('[type="radio"]').eq(i).should('exist');
 			cy.get('[type="radio"]').eq(i).should('not.be.checked');
-			cy.get('.custom-control-label').eq(i).should('contain.text', buttonName[i]);
+			cy.get('.custom-control-label').eq(i).should('contain.text', buttonName[i]).and('be.visible');
 		}
-		//  Yes Button
-		// cy.get('[type="radio"]').eq(0).should('exist'); //.and('be.visible');
-		// cy.get('[type="radio"]').eq(0).should('not.be.checked');
-		// cy.get('.custom-control-label').eq(0).should('contain.text', 'Yes');
-		// //cy.get('[type="radio"]').eq(0).parentElement.textContent.should('equal', 'Yes');
-		// //cy.get('[type="radio"]').eq(0).parentElement.should('contain', 'Yes');
-		// //cy.get('.custom-control-label .textContent').eq(0).should('contain', 'Yes');
-		// //  Impressive Button
-		// cy.get('[type="radio"]').eq(1).should('exist'); //.and('be.visible');
-		// cy.get('[type="radio"]').eq(1).should('not.be.checked');
-		// cy.get('.custom-control-label').eq(1).should('contain.text', 'Impressive');
-		// //  No Button
-		// cy.get('[type="radio"]').eq(2).should('exist'); //.and('be.visible');
-		// cy.get('[type="radio"]').eq(2).should('not.be.checked');
-		// cy.get('.custom-control-label').eq(2).should('contain.text', 'No');
 	});
 	it('GX3-470 | TC1: Validar poder marcar el boton ‘YES’', () => {
-		cy.get('[type="radio"]').eq(0).click({ force: true });
+		cy.get('[for="yesRadio"]').should('be.visible');
+		cy.get('[for="yesRadio"]').click();
 		cy.get('[type="radio"]').eq(0).should('be.checked');
 		cy.get('.mt-3').should('contain.text', 'You have selected');
 		cy.get('.text-success').should('contain.text', 'Yes');
 	});
 	it('GX3-470 | TC2: Validar poder marcar el boton ‘Impressive’', () => {
-		cy.get('[type="radio"]').eq(1).click({ force: true });
+		cy.get('[for="impressiveRadio"]').should('be.visible');
+		cy.get('[for="impressiveRadio"]').click();
 		cy.get('[type="radio"]').eq(1).should('be.checked');
 		cy.get('.mt-3').should('contain.text', 'You have selected');
 		cy.get('.text-success').should('contain.text', 'Impressive');
 	});
+	// it.only('GX3-470 | TC3: Validar no poder marcar el boton ‘No’', () => {
+	// 	cy.get('[type="radio"]').eq(2).should('be.disabled');
+	// 	cy.get('.mt-3').should('not.exist');
+	// 	cy.get('.text-success').should('not.exist');
+	// });
 });
