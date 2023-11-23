34b150177deacca90db7b71ba431a0f7370a082c
eleana2610 <eleanavcg@gmail.com>
2023-11-23 09:04:44 -0300

    GX3 496 update test cases

--- cypress/e2e/Tests/Elements/GX3-496-fillFormAnSubmit.cy.js
+++ cypress/e2e/Tests/Elements/GX3-496-fillFormAnSubmit.cy.js
@@ -1,31 +1,17 @@
+import data from '@data/Elements/GX-496-fillFormAndSubmit.json';
 describe('Text Box: Fill Form and Submit', () => {
 	beforeEach(() => {
 		cy.visit('https://demoqa.com/text-box');
 	});
-	it('497 | TC1: Validar funcionalidad de campo full Name', () => {
-		cy.fixture('data/Elements/GX-496-fillFormAndSubmit').then(the => {
-			cy.get(the.fullName.input).type(the.fullName.data.validName).should('have.value', the.fullName.data.validName);
-		});
-	});
-	it('497 | TC2: Validar Funcion campo Correo Electronico', () => {
-		cy.fixture('data/Elements/GX-496-fillFormAndSubmit').then(the => {
-			cy.get(the.userEmail.input).type(the.userEmail.data.validEmail).should('have.value', the.userEmail.data.validEmail);
-		});
-	});
-	it('497 | TC3: Validar formato valido de correo incorrecto', () => {
-		cy.fixture('data/Elements/GX-496-fillFormAndSubmit').then(the => {
-			cy.get(the.userEmail.input).type(the.userEmail.data.validEmail).should('not.have.value', the.userEmail.data.invalidEmail);
-		});
-	});
-	it('497 | TC4: Validar Funcionalidad de boton Submit', () => {
-		cy.fixture('data/Elements/GX-496-fillFormAndSubmit').then(the => {
-			cy.get(the.submitButton.input).should('be.visible').click();
-		});
-	});
-	it('497 | TC6: Validar despliegue de mensaje de error', () => {
-		cy.fixture('data/Elements/GX-496-fillFormAndSubmit').then(the => {
-			cy.get(the.outputMessage.input).should('exist');
-			cy.get(the.outputMessage.customerConfirmedInput).should('contain', 'Juana de Arco');
-		});
+	it.only('497 | TC1: Validar formulario', () => {
+		cy.get('#userName').type(data.fullName.validName);
+		cy.get('#userName').should('have.value', data.fullName.validName);
+		cy.get('#userEmail').type(data.userEmail.data.validEmail);
+		cy.get('#userEmail').should('have.value', data.userEmail.data.validEmail);
+		cy.get('#currentAddress-wrapper').type(data.currentAddress.dataCurrentAddress.validAddress);
+		cy.get('#permanentAddress').type(data.permanentAddress.permanentAddressData);
+		cy.get('#submit').click();
+		cy.get('#output');
+		cy.get('#output').should('contain', data.fullName.validName, data.userEmail.validEmail, data.currentAddress.validAddress);
 	});
 });
