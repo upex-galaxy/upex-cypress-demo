23d05e4870b50a3cbe2b73f35256e2a2f2f16dd7
Erika <tolosa.erika503@gmail.com>
2023-11-30 14:35:25 +0000

    GX3-706 Se avanza en la creacion del codigo del TC1

--- cypress/e2e/Tests/Forms/GX3-706-PracticeForm.cy.js
+++ cypress/e2e/Tests/Forms/GX3-706-PracticeForm.cy.js
@@ -1,1 +1,37 @@
-import { Formulario } from '@pages/Forms/GX-706-PracticeForm.Page';
+import { removeLogs } from '@helper/RemoveLogs';
+import { faker } from '@faker-js/faker';
+import { formPage } from '@pages/Forms/GX-706-PracticeForm.Page';
+
+describe('GX3-706 ToolsQA | Forms | Practice Form', () => {
+	beforeEach('PCR: Usuario debe situarse en el web de Demo QA', () => {
+		cy.visit('https://demoqa.com/automation-practice-form');
+		cy.url().should('contain', 'automation-practice-form');
+	});
+
+	it('707 | TC1: Validar poder crear el registro de estudiante con valores válidos', () => {
+		const randomFirstName = faker.name.firstName();
+		const randomLastName = faker.name.lastName();
+		const randomMobile = faker.phone.number();
+		const randomSubjects = faker.database.subjects();
+		const randomAdress = faker.address.address();
+
+		formPage.typeFirstName(randomFirstName);
+		formPage.get.inputFirstName().should('have.value', randomFirstName);
+
+		formPage.typeLastName(randomLastName);
+		formPage.get.inputLastName().should('have.value', randomLastName);
+
+		formPage.typeMobile(randomMobile);
+		formPage.get.inputMobile().should('have.value', randomMobile);
+
+		formPage.typeSubjects(randomSubjects);
+		formPage.get.inputSubjects().should('have.value', randomSubjects);
+
+		formPage.typeAdress(randomAdress);
+		formPage.get.inputAddress().should('have.value', randomAdress);
+
+		formPage.typeEmail();
+		formPage.get.inputMail().should('have.value', randomAdress);
+	});
+	removeLogs();
+});
--- cypress/support/pages/Forms/GX-706-PracticeForm.Page.js
+++ cypress/support/pages/Forms/GX-706-PracticeForm.Page.js
@@ -1,25 +1,120 @@
-import { faker } from '@faker-js/faker';
-const randomFirsName = faker.person.firstName();
-const randomLastName = faker.person.lastname();
-const randomMobile = faker.number.mobile();
-const randomSubjets = faker.text.subjets();
-const randomAdress = faker.address.address();
-
+import { data } from '@data/GX3-706.PracticeForm.json';
 class Formulario {
 	get = {
-		inputFirstName: () => cy.get('[id="firstName"]'),
-		inputLastName: () => cy.get('[id="lastName"]'),
-		inputMail: () => cy.get('[id="userEmail"]'),
+		inputFirstName: () => cy.get('#firstName'),
+		inputLastName: () => cy.get('#lastName'),
+		inputMail: () => cy.get('#userEmail'),
 		selectGenero: () => cy.get('div[class*="custom-control-inline"]'),
-		inputMobile: () => cy.get('[id="userNumber"]'),
-		selectDate: () => cy.get('[id="dateOfBirthInput"]'),
-		inputSubject: () => cy.get('[id="subjectsContainer"]'),
+		inputMobile: () => cy.get('#userNumber'),
+		selectDate: () => cy.get('#dateOfBirthInput'),
+		selectMonth: () => cy.get('.react-datepicker__month-select'),
+		selectYear: () => cy.get('.react-datepicker__year-select'),
+		selectDay: () => cy.get('.react-datepicker__day'),
+		inputSubjects: () => cy.get('#subjectsContainer'),
 		selectHobbie: () => cy.get('div[class*="custom-control-inline"]'),
-		selectFileButton: () => cy.get('[id="uploadPicture"]'),
-		inputAddress: () => cy.get('[id="currentAddress"]'),
-		selectState: () => cy.get('[class=" css-1hwfws3"]').eq(0),
-		selectCity: () => cy.get('[class=" css-1hwfws3"]').eq(1),
-		submitButton: () => cy.get('[id="submit"]'),
+		selectFileButton: () => cy.get('#uploadPicture'),
+		inputAddress: () => cy.get('#currentAddress'),
+		selectState: () => cy.get('#state'),
+		selectOneState: () => cy.get('#react-select-3-input'),
+		selectCity: () => cy.get('#city'),
+		selectOneCity: () => cy.get('#react-select-4-input'),
+		submitButton: () => cy.get('#submit'),
 	};
+
+	typeFirstName(randomFirstName) {
+		randomFirstName && this.get.inputFirstName().type(randomFirstName);
+	}
+	typeLastName(randomLastName) {
+		randomLastName && this.get.inputLastName().type(randomLastName);
+	}
+	typeMobile(randomMobile) {
+		randomMobile && this.get.inputMobile().type(randomMobile);
+	}
+	typeSubjects(randomSubjects) {
+		randomSubjects && this.get.inputSubjects().type(randomSubjects);
+	}
+	typeAdress(randomAddress) {
+		randomAddress && this.get.inputAddress().type(randomAddress);
+	}
+	selectOneGenero() {
+		this.get.selectGenero().click();
+	}
+	typeEmail() {
+		this.get.inputMail().type(data.email.validMail);
+	}
+	selectBirthDay() {
+		this.get.selectDate().click();
+		this.selectRandomMonth();
+		this.selectRandomYear();
+		this.selectRandomDay();
+	}
+	selectRandomMonth() {
+		const randomMonthIndex = Cypress._.random(0, 11);
+		this.get.month().select(randomMonthIndex.toString());
+		this.get
+			.month()
+			.invoke('text')
+			.then(selectedMonth => {
+				cy.log(selectedMonth);
+			});
+	}
+	selectRandomYear() {
+		this.get.yearOptions().then(yearOptions => {
+			if (yearOptions.length > 0) {
+				const randomYearIndex = Cypress._.random(0, yearOptions.length - 1);
+				cy.wrap(yearOptions[randomYearIndex])
+					.invoke('text')
+					.then(selectedYear => {
+						this.get.year().select(selectedYear);
+					});
+			}
+		});
+	}
+	selectRandomDay() {
+		this.get.daysOfMonthAndYearSelected().then(selectedDay => {
+			const dayLength = selectedDay.length;
+			cy.log('The length of the available days is: ' + selectedDay);
+			cy.log(dayLength);
+			const randomDayIndex = Cypress._.random(0, dayLength - 1);
+			cy.log('The selected day is: ' + randomDayIndex);
+
+			cy.wrap(selectedDay.eq(randomDayIndex))
+				.invoke('text')
+				.then(selectedDayText => {
+					cy.wrap(selectedDay.eq(randomDayIndex)).click();
+					cy.log('The selected day is: ' + selectedDayText);
+				});
+		});
+	}
+	selectOneHobbie() {
+		this.get.selectHobbie().click();
+	}
+	selectPicture1() {
+		this.get.selectFileButton().get(data.picture.file1);
+	}
+	selectPicture2() {
+		this.get.selectFileButton().get(data.picture.file2);
+	}
+	selectRandomState() {
+		this.get.selectState().click();
+		return cy.get(this.get.selectOneState()).then(options => {
+			const randomIndex = Cypress._.random(0, options.length - 1);
+			const selectedState = options.eq(randomIndex).invoke('text');
+			options.eq(randomIndex).click();
+			return selectedState;
+		});
+	}
+	selectRandomCity() {
+		this.get.selectCity().click();
+		return cy.get(this.get.selectOneCity()).then(options => {
+			const randomIndex = Cypress._.random(0, options.length - 1);
+			const selectedCity = options.eq(randomIndex).invoke('text');
+			options.eq(randomIndex).click();
+			return selectedCity;
+		});
+	}
+	selectSubmit() {
+		this.get.submitButton().click();
+	}
 }
-export const form = new Formulario();
+export const formPage = new Formulario();
