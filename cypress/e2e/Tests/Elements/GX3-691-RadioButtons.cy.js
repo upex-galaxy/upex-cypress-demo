1a0bd82cd8a3d8f6f2c83ec83e57336d52fcb54a
JanetziJackiewicz <janetzi.jackiewicz@gmail.com>
2023-12-04 19:43:00 -0300

    GX3-691(test):Se cambio el nombre de los TC

--- cypress/e2e/Tests/Elements/GX3-691-RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-691-RadioButtons.cy.js
@@ -5,17 +5,17 @@ describe('GX3-691 | TS: ToolsQA | Elements | Radio Buttons', () => {
 	});
 
 	//ACTIONS y ASSENT
-	it('Validar que al presionar YES se muestre el mensaje: "You have selected  YES', () => {
+	it('692 | TC1: Validar que al presionar YES se muestre el mensaje: "You have selected  YES', () => {
 		cy.get('[for="yesRadio"]').click();
 		cy.get('.text-success').should('have.text', 'Yes');
 	});
 
-	it('Validar que al presionar IMPRESSIVE se muestre el mensaje: "You have selected IMPRESSIVE', () => {
+	it('692 | TC2: Validar que al presionar IMPRESSIVE se muestre el mensaje: "You have selected IMPRESSIVE', () => {
 		cy.get('[for="impressiveRadio"]').click();
 		cy.get('.text-success').should('have.text', 'Impressive');
 	});
 
-	it('Validar que al presionar NO se el elemento este deshabilitado', () => {
+	it('692 | TC3: Validar que al presionar NO se el elemento este deshabilitado', () => {
 		cy.get('[for="noRadio"]').click();
 		cy.get('#noRadio').should('be.disabled');
 	});
