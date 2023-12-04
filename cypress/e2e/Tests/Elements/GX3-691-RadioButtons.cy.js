ef23dada738809cc3df8c60170a1ba88c4b99257
JanetziJackiewicz <janetzi.jackiewicz@gmail.com>
2023-12-04 18:37:49 -0300

    GX3-691(test):Mi primer commit

---
+++ cypress/e2e/Tests/Elements/GX3-691-RadioButtons.cy.js
@@ -0,0 +1,22 @@
+describe('GX3-691 | TS: ToolsQA | Elements | Radio Buttons', () => {
+	// ARRANGE
+	beforeEach('Visitar la pagina demoQA', () => {
+		cy.visit('/radio-button');
+	});
+
+	//ACTIONS y ASSENT
+	it('Validar que al presionar YES se muestre el mensaje: "You have selected  YES', () => {
+		cy.get('[for="yesRadio"]').click();
+		cy.get('.text-success').should('have.test', 'Yes');
+	});
+
+	it('Validar que al presionar IMPRESSIVE se muestre el mensaje: "You have selected IMPRESSIVE', () => {
+		cy.get('[for="impressiveRadio"]').click();
+		cy.get('.text-success').should('have.text', 'Impressive');
+	});
+
+	it('Validar que al presionar NO se el elemento este deshabilitado', () => {
+		cy.get('[for="noRadio"]').click();
+		cy.get('#noRadio').should('be.disabled');
+	});
+});
