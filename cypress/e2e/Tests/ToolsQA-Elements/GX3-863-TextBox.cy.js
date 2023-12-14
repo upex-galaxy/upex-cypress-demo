c1877b9632cab86e30ae04ab284ec77d19e7f974
Andre <castellanosyaisbeli@gmail.com>
2023-12-14 14:14:00 -0300

    GX3-863 Primer TC done

--- cypress/e2e/Tests/ToolsQA-Elements/GX3-863-TextBox.cy.js
+++ cypress/e2e/Tests/ToolsQA-Elements/GX3-863-TextBox.cy.js
@@ -3,4 +3,17 @@ describe('GX3-863 | ToolsQA | Elements | Text Box: Fill form and Submit', () => 
 		cy.visit('https://demoqa.com/text-box');
 		cy.url().should('contain', 'text-box');
 	});
+	it('GX3-867 | TC1: Validar llenar y enviar formulario con datos correctos', () => {
+		cy.fixture('data/GX3-863-TextBox').then(the => {
+			cy.get(the.FullName.input).type(the.FullName.data.filled);
+			cy.get(the.CurrentAddress.input).type(the.CurrentAddress.data.filled);
+			cy.get(the.PermanentAddress.input).type(the.PermanentAddress.data.filled);
+			cy.get(the.Email.input).type(the.Email.data.valid);
+			cy.get(the.SubmitButton).click();
+			cy.get('#name').should('contain.text', the.FullName.data.filled);
+			cy.get('#email').should('contain', the.Email.data.valid);
+			cy.get('#currentAddress.mb-1').should('contain', the.CurrentAddress.data.filled);
+			cy.get('#permanentAddress.mb-1').should('contain', the.PermanentAddress.data.filled);
+		});
+	});
 });
