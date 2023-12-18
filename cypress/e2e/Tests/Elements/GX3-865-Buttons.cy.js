8ba821a12f52f8ec36267efd72a0762a15a88dd6
Laysecaplz <124854171+Laysecaplz@users.noreply.github.com>
2023-12-17 23:12:43 -0300

    finalizado

---
+++ cypress/e2e/Tests/Elements/GX3-865-Buttons.cy.js
@@ -0,0 +1,20 @@
+describe('GX3-865 | demoQA | Buttons');
+
+it('simple', () => {
+	cy.visit('https://demoqa.com/buttons');
+});
+
+it('866 | TC1: Validar boton doble click', () => {
+	cy.get('doubleClickBtn').should('be.visible').dblclick();
+	cy.get('doubleClickMessage').should('have.text', 'You have done a double click');
+});
+
+it('866 | TC2: Validar boton click derecho', () => {
+	cy.get('rightClickBtn').should('be.visible').rightclick();
+	cy.get('rightClickMessage').should('have.text', 'You have done a right click');
+});
+
+it('866 | TC3: Validar boton click izquierdo', () => {
+	cy.get('86yAK').should('be.visible').click();
+	cy.get('rightClickMessage').should('have.text', 'You have done a dynamic click');
+});
