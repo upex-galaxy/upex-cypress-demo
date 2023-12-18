4f5ab37b4d0808b6f1375d9ae29d62b7e5689609
Andre <castellanosyaisbeli@gmail.com>
2023-12-18 19:41:29 -0300

    GX3-912 archivo arreglado

--- cypress/e2e/Tests/Interactions/test.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Search Gaza war in google and verify if you get the properly result', () => {
-	beforeEach('PRC: User must be at Google front page', () => {
-		cy.visit('https://www.google.com/');
-	});
-	it('Tc1 validate search to be successful', () => {
-		cy.get();
-	});
-});
