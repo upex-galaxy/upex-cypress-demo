5b17fc39f6ac37463b1b463077d20705f33cf40d
ZwitterKaneda <smentek@gmail.com>
2023-12-19 14:42:39 -0300

    GX3-1234 finalicé con el segundo TC, queda pendiente el resto

--- cypress/e2e/Tests/Widgets/GX3-1234-probandoGIT.cy.js
+++ cypress/e2e/Tests/Widgets/GX3-1234-probandoGIT.cy.js
@@ -2,4 +2,7 @@ describe('test', () => {
 	it('test probando git', () => {
 		cy.log('Hola, espero que alguien me este prestando atención');
 	});
+	it('test probando git con diferentes push', () => {
+		cy.log('Parece que el jefe vino a controlar como estan las cosas');
+	});
 });
