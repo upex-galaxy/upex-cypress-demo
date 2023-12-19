33a51b88ea3b67a93e4486e9d5f0b1550aad17e2
LuisManuel7 <luis_ns_124@hotmail.com>
2023-12-19 12:47:41 -0400

    GX3-888 ci: Se crearon los arcviso de prueba, arhivo POM, y arhivo md

---
+++ cypress/e2e/Tests/Elements/GX3-888-Dinamic.cy.js
@@ -0,0 +1,24 @@
+import { Dinamico } from '@pages/Elements/GX3-888-Dinamic';
+describe('US GX3-888 | TS: ToolsQA | Elements | Dynamic Properties', () => {
+	beforeEach('Estar en la pagina de demoqa', () => {
+		cy.visit('https://demoqa.com/dynamic-properties');
+		Dinamico.buttomDisabled().should('be.disabled');
+	});
+	it.skip('25131|TC1:Validar poder obtener elemento “This text has random Id“', () => {
+		Dinamico.Randomtext().should('contain', 'This text has random Id');
+	});
+	it('888|TC2:Validar que el elemento este habilitado después de 5 segundos', () => {
+		Dinamico.buttomEnabled().should('not.be.disabled');
+	});
+	it.skip('22812 | TC03: Validar cambiar estado de elemento "Color Change" al pasar cinco segundos', () => {
+		cy.get('button#colorChange', { timeout: 4999 }).should('not.have.css', 'color', 'rgb(220, 53, 69)');
+		cy.get('button#colorChange', { timeout: 5000 }).should('have.css', 'color', 'rgb(220, 53, 69)');
+	});
+
+	it.skip('22812 | TC04: Validar cambiar estado de elemento "Visible" al pasar cinco segundos', () => {
+		cy.get('button#visibleAfter', { timeout: 4999 }).should('not.exist');
+		cy.get('button#visibleAfter', { timeout: 5000 }).should('be.visible');
+	});
+});
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
