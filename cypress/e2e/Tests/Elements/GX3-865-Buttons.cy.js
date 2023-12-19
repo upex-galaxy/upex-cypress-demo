92ed47c6383bf5d749338850341579ffbe907bbf
Laysecaplz <124854171+Laysecaplz@users.noreply.github.com>
2023-12-19 00:14:39 -0300

    GX3-865 id objeto

--- cypress/e2e/Tests/Elements/GX3-865-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-865-Buttons.cy.js
@@ -1,21 +1,22 @@
-
-describe('GX3-865 | demoQA | Buttons', () => {
-	it('buttons', () => {
+describe('865 | demoQA | Buttons', () => {
+	beforeEach('visit page buttons', () => {
 		cy.visit('https://demoqa.com/buttons');
+		cy.url().should('contain', 'buttons');
 	});
 
 	it('866 | TC1: Validar boton doble click', () => {
-		cy.get('doubleClickBtn').should('be.visible').dblclick();
-		cy.get('doubleClickMessage').should('have.text', 'You have done a double click');
+		cy.get('#doubleClickBtn').should('be.visible').dblclick();
+		cy.get('#doubleClickMessage').should('have.text', 'You have done a double click');
 	});
 
 	it('866 | TC2: Validar boton click derecho', () => {
-		cy.get('rightClickBtn').should('be.visible').rightclick();
-		cy.get('rightClickMessage').should('have.text', 'You have done a right click');
+		cy.get('#rightClickBtn').should('be.visible').rightclick();
+		cy.get('#rightClickMessage').should('have.text', 'You have done a right click');
+		
 	});
 
 	it('866 | TC3: Validar boton click izquierdo', () => {
-		cy.get('86yAK').should('be.visible').click();
-		cy.get('rightClickMessage').should('have.text', 'You have done a dynamic click');
+		cy.get('#86yAK').should('be.visible').click();
+		cy.get('#rightClickMessage').should('have.text', 'You have done a dynamic click');
 	});
 });
