7a5df902dd200389e189dc94053e81a90e07bb04
JanetziJackiewicz <janetzi.jackiewicz@gmail.com>
2023-12-20 14:47:28 -0300

    GX3-798 Cambios en el assertion

--- cypress/e2e/Tests/Elements/GX3-798-TextBox-Fill-form-and-Submit.cy.js
+++ cypress/e2e/Tests/Elements/GX3-798-TextBox-Fill-form-and-Submit.cy.js
@@ -1,9 +1,8 @@
-const { contains } = require('cypress/types/jquery');
-
 describe('GX3-798 | TS: ToolsQA | Elements | Text Box', () => {
 	// ARRANGE
-	beforeEach('Visitar la pagina demoQA', () => {
-		cy.visit('/text-box');
+	beforeEach('', () => {
+		cy.visit('https://demoqa.com/text-box');
+		cy.url().should('contain', 'text-box');
 	});
 
 	//casos
@@ -16,21 +15,42 @@ describe('GX3-798 | TS: ToolsQA | Elements | Text Box', () => {
 		//steps
 		cy.get('input#userName').type(j_UserName);
 		cy.get('input#userEmail').type(j_UserEmails);
-		cy.get('textarea#currentAddress').type('j_CurrentAddress');
-		cy.get('textarea#permanentAddress').type('j_PermanentAddress');
+		cy.get('textarea#currentAddress').type(j_CurrentAddress);
+		cy.get('textarea#permanentAddress').type(j_PermanentAddress);
 		cy.get('#submit').click();
 
-		//Assertions
-		cy.get('#userName').should('contains.text', j_UserName);
-		cy.get('#userEmail').should('contains.text', j_UserEmails);
-		cy.get('#userName').should('contains.text', j_UserName);
-		cy.get('#userName').should('contains.text', j_UserName);
+		//Assertion
+		cy.get('p#name').should('contains.text', j_UserName);
+		cy.get('p#email').should('contains.text', j_UserEmails);
+		cy.get('p#currentAddress').should('contains.text', j_CurrentAddress);
+		cy.get('p#permanentAddress').should('contains.text', j_PermanentAddress);
 	});
 
-	//it('799|TC2: Validar que se realiza el envio exitoso cuando  solo el campo emails tiene un valor valido el resto de los campos es vacio.', () => {});
-	//it('799|TC3: Validar que el envio es fallido cuando el campo email no contiene @.', () => {});
-	//it('799|TC4: Validar que el envio es fallido cuando el campo email No contiene (mínimo) 1 carácter alfanumérico antes de @', () => {});
-	//it('799|TC5: Validar que el envio es fallido cuando el campo email No contiene (mínimo) 1 carácter alfanumérico después de @', () => {});
-	//it('799|TC6: Validar que el envio es fallido cuando el campo email No contiene "." después: 1 carácter alfanumérico después de @', () => {});
-	//it('799|TC7: Validar que el envio es fallido cuando el campo email No contiene (mínimo) 2 caracteres alfanuméricos después de .', () => {});
+	it('799|TC2: Validar que se realiza el envio exitoso cuando  solo el campo emails tiene un valor valido el resto de los campos es vacio', () => {
+		cy.get('input#userName').should('be.empty');
+		cy.get('input#userEmail').type(j_UserEmails);
+		cy.get('textarea#currentAddress').should('be.empty');
+		cy.get('textarea#permanentAddress').should('be.empty');
+		cy.get('#submit').click();
+
+		//Assertion
+		cy.get('p#email').should('contains.text', j_UserEmails);
+	});
+
+	it('799|TC3: Validar que el envio es fallido cuando el campo email no contiene @.', () => {
+		//const
+		const j_UserEmails1 = 'janetzigmail.com';
+		//steps
+		cy.get('input#userName').type(j_UserName);
+		cy.get('input#userEmail').type(j_UserEmails1);
+		cy.get('textarea#currentAddress').type(j_CurrentAddress);
+		cy.get('textarea#permanentAddress').type(j_PermanentAddress);
+		cy.get('#submit').click();
+
+		//Assertion
+		cy.get('p#name').should('contains.text', j_UserName);
+		cy.get('p#email').should('contains.text', j_UserEmails);
+		cy.get('p#currentAddress').should('contains.text', j_CurrentAddress);
+		cy.get('p#permanentAddress').should('contains.text', j_PermanentAddress);
+	});
 });
