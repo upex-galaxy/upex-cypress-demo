30f97016298e6353e001e9ae43d53394981bf147
LuisManuel7 <luis_ns_124@hotmail.com>
2023-12-24 12:40:15 -0400

    GX3-1005-DrowdownMenu:Finalizacion del los archivos .cy, page.js y .md

--- cypress/e2e/Tests/Widgets/GX3-1005-DropdownMenu.cy.js
+++ cypress/e2e/Tests/Widgets/GX3-1005-DropdownMenu.cy.js
@@ -1,33 +1,26 @@
+import { DropDown } from '@pages/Widgets/GX3-1005-DropdownMenu.page';
 describe('US GX3-1005 | ToolsQA | Widgets | Dropdown - Select Menu', () => {
 	beforeEach('el usuario se encuentra en el endpoint /select-menu', () => {
 		cy.visit('https://demoqa.com/select-menu');
 	});
 
-	it('25131|TC1:Validar que el usuario selecciona un elemento del menu "Select Value"', () => {
-		cy.get('#withOptGroup').click();
-		cy.get('.css-11unzgr').should('exist');
-		cy.get('#react-select-2-option-0-0').click();
-		cy.get('#withOptGroup').should('contain', 'Group 1, option 1');
+	it('1008|TC1:Validar que el usuario selecciona un elemento del menu "Select Value"', () => {
+		DropDown.ClickSelectorValue().should('exist');
+		DropDown.ClickSelectorValue1().should('contain', 'Group 1, option 1');
 	});
-
-	it.only('25131|TC1:Validar que el usuario selecciona un elemento del menu "Select One"', () => {
-		cy.get('#selectOne').click();
-		cy.get('.css-1s9izoc').should('exist');
-		cy.get('#react-select-3-option-0-0').click();
-		cy.get('#selectOne').should('contain', 'Dr.');
+	it('1008|TC2:Validar que el usuario selecciona un elemento del menu "Select One"', () => {
+		DropDown.ValidarSelectorList().should('exist');
+		DropDown.SelectOneOption().should('contain', 'Dr.');
 	});
-	it('25131|TC1:Validar que el usuario selecciona un elemento del menu "Old Style Select Menu"', () => {
-		cy.get('#oldSelectMenu').select('Green').should('have.value', '2');
+	it('1008|TC3:Validar que el usuario selecciona un elemento del menu "Old Style Select Menu"', () => {
+		DropDown.SelectOldMenu().should('have.value', '2');
 	});
-	it('25131|TC1:Validar que el usuario selecciona uno o más elementos del "Multiselect drop down"', () => {
-		cy.get('.css-tlfecz-indicatorContainer').eq(2).click();
-		cy.get('.css-26l3qy-menu').should('be.visible');
-		cy.get('#react-select-4-option-1').click();
-		cy.get('#react-select-4-option-2').click();
-		cy.get('.css-1hwfws3').eq(2).should('contain', 'Blue').and('contain', 'Black');
+	it('1008|TC4:Validar que el usuario selecciona uno o más elementos del "Multiselect drop down"', () => {
+		DropDown.IndicadorMultiMenu().should('be.visible');
+		DropDown.SelectMultiMenu().should('contain', 'Blue').and('contain', 'Black');
 	});
-	it('25131|TC1:Validar que el usuario selecciona varios elementos del menú "Standard multi select"', () => {
-		cy.get('#cars').select(['Volvo', 'Saab', 'Opel']).should('have.css', 'background-color', 'rgb(255, 255, 255)');
+	it('1008|TC5:Validar que el usuario selecciona varios elementos del menú "Standard multi select"', () => {
+		DropDown.SelectMenuStandard().should('have.css', 'background-color', 'rgb(255, 255, 255)');
 	});
 });
 
--- cypress/support/pages/Widgets/GX3-1005-DropdownMenu.page.js
+++ cypress/support/pages/Widgets/GX3-1005-DropdownMenu.page.js
@@ -15,7 +15,35 @@ class Dropdown {
 		selectorStandard: () => cy.get('#cars'),
 	};
 	ClickSelectorValue() {
-		this.get.selectorValue.click();
-		this.get.selectorValueList();
+		this.get.selectorValue().click();
+		return this.get.selectorValueList();
+	}
+	ClickSelectorValue1() {
+		this.get.selectorValueOption1().click();
+		return this.get.selectorValue();
+	}
+	ValidarSelectorList() {
+		this.get.selectorOne().click();
+		return this.get.selectorOneList();
+	}
+	SelectOneOption() {
+		this.get.selectorOneOption0().click();
+		return this.get.selectorOne();
+	}
+	SelectOldMenu() {
+		return this.get.selectorOldMenu().select('Green');
+	}
+	IndicadorMultiMenu() {
+		this.get.indicadorMultiMenu().eq(2).click();
+		return this.get.selectorMultiMenuList();
+	}
+	SelectMultiMenu() {
+		this.get.selectorMultiMenu1().click();
+		this.get.selectorMultimenu2().click();
+		return this.get.selectorMultiMenu().eq(2);
+	}
+	SelectMenuStandard() {
+		return this.get.selectorStandard().select(['Volvo', 'Saab', 'Opel']);
 	}
 }
+export const DropDown = new Dropdown();
