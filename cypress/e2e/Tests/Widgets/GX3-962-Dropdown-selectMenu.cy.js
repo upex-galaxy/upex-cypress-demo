32176d620ae3b9cc061b0368f011d9e8e50d6a52
Ariel Jusid <arieljusid@gmail.com>
2023-12-25 21:14:10 -0300

    CI & Tests Update

--- cypress/e2e/Tests/Widgets/GX3-962-Dropdown-selectMenu.cy.js
+++ cypress/e2e/Tests/Widgets/GX3-962-Dropdown-selectMenu.cy.js
@@ -1,5 +1,18 @@
-//Se importa la funcion
 import { removeLogs } from "@helper/RemoveLogs";
 
-//Se ejecuta la funcion para evitar los errores (puedes hacerlo al final de tu codigo)
-removeLogs()
+describe('🧪GX3-962 | TS: ⚡️ToolsQA | Widgets | Dropdown - Select Menu', () => {
+	beforeEach('PRC: El usuario debe ubicarse en la página de Select Menu de DemoQA', () => {
+		cy.visit('https://demoqa.com/select-menu');
+		cy.url().should('contain', 'select');
+	});
+
+    it('962 | TC1: Validar que elemento aleatorio seleccionado en “Select Value” se muestre en input y se cierre menú', () => {
+        
+    });
+
+
+
+});
+
+
+removeLogs();
