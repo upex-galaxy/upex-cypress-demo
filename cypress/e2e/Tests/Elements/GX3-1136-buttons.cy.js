1f8fc7d05f0086f20a7544f2316f38bb1590b564
ZwitterKaneda <smentek@gmail.com>
2024-01-04 14:59:23 -0300

    GX3-1234 terminado el segundo caso de prueba, quedan pendientes 1 TC

--- cypress/e2e/Tests/Elements/GX3-1136-buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1136-buttons.cy.js
@@ -2,4 +2,7 @@ describe('ToolsQA | Elements | Buttons', () => {
 	it('1137 | TC01: Validar funcionamiento del botón "Double Click Me"', () => {
 		expect(1).to.equal(1);
 	});
+	it('1137 | TC02: Validar funcionamiento del botón "Rigth Click Me"', () => {
+		expect(1).to.equal(1);
+	});
 });
