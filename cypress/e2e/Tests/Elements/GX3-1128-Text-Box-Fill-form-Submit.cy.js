46a181a7dfdb9814384578dd528de14134003ff8
macabreras <miguelcabr.ec@gmail.com>
2024-01-11 19:32:05 -0500

    GX3-1128 Desarrollo del 1er TC validando Assert de input: Full Name, Currente Address y Permanent Address

--- cypress/e2e/Tests/Elements/GX3-1128-Text-Box-Fill-form-Submit.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1128-Text-Box-Fill-form-Submit.cy.js
@@ -5,5 +5,15 @@ describe('⚡️ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 		cy.url().should('contain', 'text-box');
 	});
 	//Diseño de Test Cases:
-	//it('GX3-1129 | TC1: Validar mostrar cadena de texto (mensaje) después de enviar los campos válidos “Full Name”, “Current Address” and “Permanent Address”.', () => {});
+	it('GX3-1129 | TC1: Validar mostrar cadena de texto (mensaje) después de enviar los campos válidos “Full Name”, “Current Address” and “Permanent Address”.', () => {
+		cy.fixture('data/GX3-1128-Text-Box-Fill.json').then(the => {
+			cy.get(the.FullName.input).type(the.FullName.data.filled);
+			cy.get(the.CurrentAddress.input).type(the.CurrentAddress.data.filled);
+			cy.get(the.PermanentAddress.input).type(the.PermanentAddress.data.filled);
+			cy.get(the.SubmitButton).click();
+			cy.get('#name').should('contain.text', the.FullName.data.filled);
+			cy.get('#currentAddress.mb-1').should('contain', the.CurrentAddress.data.filled);
+			cy.get('#permanentAddress.mb-1').should('contain', the.PermanentAddress.data.filled);
+		});
+	});
 });
