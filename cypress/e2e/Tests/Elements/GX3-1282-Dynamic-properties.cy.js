c839913c7824e73392275f23befe5f02eb719ec7
LucasSebastianCasco <lucassebaacasco@gmail.com>
2024-01-15 19:30:25 -0300

    GX3-1283 se modifica nuevamente el nombre en los TC para levantar TC con CI

--- cypress/e2e/Tests/Elements/GX3-1282-Dynamic-properties.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1282-Dynamic-properties.cy.js
@@ -7,20 +7,20 @@ describe('US GX3-1282| ToolsQA | Elements | Dynamic Properties', () => {
 		cy.url().should('contain', 'dynamic-properties');
 	});
 
-	it('GX3-1283|TC001 Validate being able to get item with Dymanic ID', () => {
+	it('1283|TC001 Validate being able to get item with Dymanic ID', () => {
 		cy.get('p').should('contain', 'This text has random Id');
 	});
 
-	it('GX3-1283|TC002 Validate that the item changes state to enabled after 5 seconds', () => {
+	it('1283|TC002 Validate that the item changes state to enabled after 5 seconds', () => {
 		cy.get('#enableAfter').should('be.disabled');
 		cy.get('#enableAfter', { timeout: 5000 }).should('be.enabled');
 	});
 
-	it('GX3-1283|TC003 Validate that the color changes after 5 seconds', () => {
+	it('1283|TC003 Validate that the color changes after 5 seconds', () => {
 		cy.get('#colorChange', { timeout: 5000 }).should('have.css', 'color', 'rgb(220, 53, 69)');
 	});
 
-	it('GX3-1283|TC004 Validate that the button is visible after 5 seconds', () => {
+	it('1283|TC004 Validate that the button is visible after 5 seconds', () => {
 		cy.get('#visibleAfter').should('not.exist');
 		cy.get('#visibleAfter', { timeout: 5000 }).should('be.visible');
 	});
