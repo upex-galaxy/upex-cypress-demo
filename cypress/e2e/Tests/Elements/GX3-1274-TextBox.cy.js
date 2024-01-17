8048c1cb500fec5e23e55e55682513ae345a30b7
dmicu1 <danielmicu26@gmail.com>
2024-01-17 22:06:17 +0100

    Esto tarda mucho en comitear

--- cypress/e2e/Tests/Elements/GX3-1274-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1274-TextBox.cy.js
@@ -1,8 +1,12 @@
+import json from '@data/GX3-1274-TextBox.json';
+
 describe('GX3-1274-tools-qa-elements-text-box-fill-form-and-submit', () => {
-	beforeEach('PRC-Visitar la pagina Demoqa', () => {
+	beforeEach('PRC-Visitar la pagina Demo', () => {
 		cy.visit('https://demoqa.com/text-box');
 	});
-	it('|TC1|  Validar formulario llenando correctamente todos los campos', () => {
+	it('|TC1|  Validar formulario llenando correctamente todos los campos', function () {
+		cy.fixture('data/GX3-1274-TextBox').as('data');
+
 		cy.get('[href="/commands/querying"]') // Sintaxis Comando Get de Selector de Atributos
 			.eq(2) // Esto es un método opcional
 			.click(); // Esto es un método de Interacción
