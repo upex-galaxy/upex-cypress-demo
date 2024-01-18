ed3ff93a89a0454e83e57aa209abb114baa2be4e
dmicu1 <danielmicu26@gmail.com>
2024-01-18 15:44:12 +0100

    TC2 realizado y guardar un archivo de Utilidades

--- cypress/e2e/Tests/Elements/GX3-1274-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1274-TextBox.cy.js
@@ -1,10 +1,11 @@
-import json from '@data/GX3-1274-TextBox.json';
+import * as json from '@data/GX3-1274-TextBox.json';
+import { getRealValue } from '@helper/GX-1274-Utilidades';
 
 describe('GX3-1274-tools-qa-elements-text-box-fill-form-and-submit', () => {
 	beforeEach('PRC-Visitar la pagina Demo', () => {
 		cy.visit('https://demoqa.com/text-box');
 	});
-	it('GX3-1274|TC1|  Validar formulario llenando correctamente todos los campos', function () {
+	it.skip('GX3-1274|TC1|  Validar formulario llenando correctamente todos los campos', function () {
 		//Formulario
 		cy.fixture('data/GX3-1274-TextBox').then(data => {
 			cy.get('#userName-wrapper input').type(data.validCredentials.FullName);
@@ -13,16 +14,53 @@ describe('GX3-1274-tools-qa-elements-text-box-fill-form-and-submit', () => {
 		cy.get('#currentAddress-wrapper textarea').type(json.validCredentials.CurrentAddress);
 		cy.get('#permanentAddress-wrapper textarea').type(json.validCredentials.PermanentAddress);
 		cy.get('#submit').click();
+		//Configurar constante para agarrar texto agregado Assertions
+		cy.get('#output #name')
+			.invoke('text')
+			.then(name => {
+				cy.log(name);
+			});
+		cy.get('#output #name').invoke('text').as('name');
+		cy.get('#output #email').invoke('text').as('email');
+		cy.get('#output #currentAddress').invoke('text').as('currentAddress');
+		cy.get('#output #permanentAddress').invoke('text').as('permanentAddress');
+		//Assertions
+		cy.then(() => {
+			expect(getRealValue(this.name)).equal(json.validCredentials.FullName);
+			expect(getRealValue(this.email)).equal(json.validCredentials.Email);
+			expect(getRealValue(this.currentAddress)).equal(json.validCredentials.CurrentAddress);
+			expect(getRealValue(this.permanentAddress)).equal(json.validCredentials.PermanentAddress);
+		});
 	});
-	it.skip('GX3-1274|TC2| Validar formulario con Email invalido', () => {
-		cy.get('[href="/commands/querying"]').eq(2).click();
-		cy.get('#query-btn') // Sintaxis Comando Get de Selector de IDs
-			.click(); // Esto es un método de Interacción (Acción)
+	it('GX3-1274|TC2| Validar NO poder rellenar formulario con Email invalido', () => {
+		const invalidEmails = [
+			json.invalidCredentialsForEmail.Email0,
+			json.invalidCredentialsForEmail.Email1,
+			json.invalidCredentialsForEmail.Email2,
+			json.invalidCredentialsForEmail.Email3,
+			json.invalidCredentialsForEmail.Email4,
+		];
+
+		for (const email of invalidEmails) {
+			cy.get('#userEmail-wrapper input').type(email);
+			cy.get('#submit').click();
+			// Esperar a que el mensaje de error esté presente
+			cy.get('.field-error').should('be.visible');
+			// Asegurarse de que el campo tenga un borde rojo
+			cy.get('.mr-sm-2.field-error.form-control').should('have.css', 'border-color', 'rgb(255, 0, 0)');
+			//Borrar campo para seguir con la siguiente data
+			cy.get('#userEmail-wrapper input').clear();
+		}
 	});
+
 	it.skip('GX3-1274|TC3| Validar formulario con campos vacíos', () => {
-		cy.get('[href="/commands/querying"]').eq(2).click();
-		cy.get('#query-btn').click();
-		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
-			.should('contain.text', 'Button'); // Assertion BDD para validar resultado esperado (de muchas formas)
+		cy.get('#userEmail-wrapper input').type(json.invalidCredentialsForEmail.Email1);
+		cy.get('#submit').click();
+		cy.get('#userEmail-wrapper input').type(json.invalidCredentialsForEmail.Email1);
+		cy.get('#submit').click();
+		cy.get('#userEmail-wrapper input').type(json.invalidCredentialsForEmail.Email2);
+		cy.get('#submit').click();
+		cy.get('#userEmail-wrapper input').type(json.invalidCredentialsForEmail.Email3);
+		cy.get('#submit').click();
 	});
 });
