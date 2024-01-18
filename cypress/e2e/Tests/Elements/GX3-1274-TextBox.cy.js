5cfcec28017ebb223964f801dcae4d01360a8dd6
dmicu1 <danielmicu26@gmail.com>
2024-01-18 01:59:43 +0100

    Primer TC1-#HappyPath

--- cypress/e2e/Tests/Elements/GX3-1274-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1274-TextBox.cy.js
@@ -4,19 +4,22 @@ describe('GX3-1274-tools-qa-elements-text-box-fill-form-and-submit', () => {
 	beforeEach('PRC-Visitar la pagina Demo', () => {
 		cy.visit('https://demoqa.com/text-box');
 	});
-	it('|TC1|  Validar formulario llenando correctamente todos los campos', function () {
-		cy.fixture('data/GX3-1274-TextBox').as('data');
-
-		cy.get('[href="/commands/querying"]') // Sintaxis Comando Get de Selector de Atributos
-			.eq(2) // Esto es un método opcional
-			.click(); // Esto es un método de Interacción
+	it('GX3-1274|TC1|  Validar formulario llenando correctamente todos los campos', function () {
+		//Formulario
+		cy.fixture('data/GX3-1274-TextBox').then(data => {
+			cy.get('#userName-wrapper input').type(data.validCredentials.FullName);
+		});
+		cy.get('#userEmail-wrapper input').type(json.validCredentials.Email);
+		cy.get('#currentAddress-wrapper textarea').type(json.validCredentials.CurrentAddress);
+		cy.get('#permanentAddress-wrapper textarea').type(json.validCredentials.PermanentAddress);
+		cy.get('#submit').click();
 	});
-	it('|TC2| Validar formulario con Email invalido', () => {
+	it.skip('GX3-1274|TC2| Validar formulario con Email invalido', () => {
 		cy.get('[href="/commands/querying"]').eq(2).click();
 		cy.get('#query-btn') // Sintaxis Comando Get de Selector de IDs
 			.click(); // Esto es un método de Interacción (Acción)
 	});
-	it('|TC3| Validar formulario con campos vacíos', () => {
+	it.skip('GX3-1274|TC3| Validar formulario con campos vacíos', () => {
 		cy.get('[href="/commands/querying"]').eq(2).click();
 		cy.get('#query-btn').click();
 		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
