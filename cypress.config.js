eccacc540a1082f5a8895e9b71f7925ea3d79a59
saiotest <elyermad@gmail.com>
2024-01-19 18:13:32 -0300

    refine(repo): update repo structure On branch QA Your branch is up to date with 'origin/QA'.
    Changes to be committed:
    	deleted:    .cypress-cucumber-preprocessorrc.json
    	modified:   .eslintrc.json
    	modified:   .github/workflows/regression.yml
    	renamed:    .github/sanityLilia.yml -> .github/workflows/sanityLilia.yml
    	renamed:    .github/workflows/sanity.yml -> .github/workflows/sanityTemplate.yml
    	deleted:    cucumber-formatter/cucumber-html-report.js
    	modified:   cypress.config.js
    	new file:   cypress.reporter.json
    	renamed:    cypress/e2e/Tests/How-to/API-testing/exampleAPI.example.cy.js -> cypress/e2e/How-to/API-testing/exampleAPI.example.cy.js
    	renamed:    cypress/e2e/Tests/How-to/UI-testing/exampleUI.example.cy.js -> cypress/e2e/How-to/UI-testing/exampleUI.example.cy.js
    	renamed:    cypress/e2e/Tests/How-to/UI-testing/pageObjectModel.example.cy.js -> cypress/e2e/How-to/UI-testing/pageObjectModel.example.cy.js
    	renamed:    cypress/e2e/Tests/marathon-challenge/elements.example.cy.js -> cypress/e2e/How-to/marathon-challenge/elements.example.cy.js
    	renamed:    cypress/e2e/Tests/marathon-challenge/widgets.example.cy.js -> cypress/e2e/How-to/marathon-challenge/widgets.example.cy.js
    	deleted:    cypress/e2e/cucumber-test/Gherkin/GherkinTutorial.feature
    	deleted:    cypress/e2e/cucumber-test/stepDefinitions/GherkinTutorial.js
    	modified:   jsconfig.json
    	deleted:    jsregression.json
    	modified:   package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,14 @@
 import { defineConfig } from 'cypress';
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
-import pkg from '@badeball/cypress-cucumber-preprocessor';
-const { addCucumberPreprocessorPlugin } = pkg;
-import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild.js';
 import { downloadFile } from 'cypress-downloadfile/lib/addPlugin.js';
 import allureWriter from '@shelex/cypress-allure-plugin/writer';
+import 'dotenv/config';
 
 async function setupNodeEvents(on, config) {
 	// This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-	await addCucumberPreprocessorPlugin(on, config);
-
 	on('task', { downloadFile });
-
-	on(
-		'file:preprocessor',
-		createBundler({
-			plugins: [createEsbuildPlugin(config)],
-		})
-	);
+	on('file:preprocessor', createBundler());
 	allureWriter(on, config);
-
 	// Make sure to return the config object as it might have been modified by the plugin.
 	return config;
 }
@@ -27,10 +16,18 @@ async function setupNodeEvents(on, config) {
 export default defineConfig({
 	// @Ely: CYPRESS DASHBOARD PARA VER NUESTRAS EJECUCIONES EN LA WEB:
 	projectId: '',
-	downloadsFolder: 'cypress/downloads',
-	// 1280×720 is considered to be the most suitable screen resolution for the desktop website version:
 	viewportWidth: 1920,
 	viewportHeight: 1080,
+	downloadsFolder: 'cypress/downloads',
+	videosFolder: 'cypress/videos',
+	screenshotsFolder: 'cypress/screenshots',
+	screenshotOnRunFailure: true,
+	scrollBehavior: 'center',
+	// 1280×720 is considered to be the most suitable screen resolution for the desktop website version:
+	// Number of times to retry a failed test. If a number is set, tests will retry in both runMode and openMode:
+	retries: process.env.CI ? 2 : 0,
+	// Whether Cypress will record a video of the test run when running on headless:
+	video: Boolean(process.env.CI),
 	// Whether Cypress will watch and restart tests on test file changes:
 	watchForFileChanges: false,
 	// En Caso de hacer testing en SUT con seguridad web:
@@ -38,12 +35,8 @@ export default defineConfig({
 	// multi-reporters: one report.xml + mochawesome.json per file.
 	reporter: 'cypress-multi-reporters',
 	reporterOptions: {
-		configFile: 'jsconfig.json',
+		configFile: 'cypress.reporter.json',
 	},
-	// Number of times to retry a failed test. If a number is set, tests will retry in both runMode and openMode:
-	retries: process.env.CI ? 2 : 0,
-	// Whether Cypress will record a video of the test run when running on headless:
-	video: false,
 	// E2E Testing runner
 	e2e: {
 		// Glob pattern to determine what test files to load:
