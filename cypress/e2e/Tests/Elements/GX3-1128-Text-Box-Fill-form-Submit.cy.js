8e0a3c6fa8d6eb42c38ed52615fd3122fe8e246e
macabreras <miguelcabr.ec@gmail.com>
2024-01-19 14:01:10 -0500

    GX3-1128 Desarrollo de TC negativos del 6 al 10 y modificación de archivo json para validar el border red en campo email

--- cypress/e2e/Tests/Elements/GX3-1128-Text-Box-Fill-form-Submit.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1128-Text-Box-Fill-form-Submit.cy.js
@@ -4,7 +4,8 @@ describe('⚡️ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 		cy.visit('https://demoqa.com/text-box');
 		cy.url().should('contain', 'text-box');
 	});
-	//Diseño de Test Cases:
+	//Test Cases:
+	//Form HappyPath
 	it('GX3-1129 | TC1: Validar mostrar cadena de texto (mensaje) después de enviar los campos válidos “Full Name”, “Current Address” and “Permanent Address”.', () => {
 		cy.fixture('data/GX3-1128-Text-Box-Fill.json').then(the => {
 			cy.get(the.FullName.input).type(the.FullName.data.filled);
@@ -37,7 +38,7 @@ describe('⚡️ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 			cy.get(the.SubmitButton).click();
 		});
 	});
-	it.only('GX3-1129 | TC5: Validar mostrar cadena de texto (mensaje) después de enviar todos los campos válidos del form “Full Name”, “Email”, “Current Address” and “Permanent Address”.', () => {
+	it('GX3-1129 | TC5: Validar mostrar cadena de texto (mensaje) después de enviar todos los campos válidos del form “Full Name”, “Email”, “Current Address” and “Permanent Address”.', () => {
 		cy.fixture('data/GX3-1128-Text-Box-Fill.json').then(the => {
 			cy.get(the.FullName.input).type(the.FullName.data.filled);
 			cy.get(the.Email.input).type(the.Email.data.valid);
@@ -48,6 +49,43 @@ describe('⚡️ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 			cy.get('#currentAddress.mb-1').should('contain', the.CurrentAddress.data.filled);
 			cy.get('#email').should('contain', the.Email.data.valid);
 			cy.get('#permanentAddress.mb-1').should('contain', the.PermanentAddress.data.filled);
+		});
+	});
+	//Test Case negative:
+	//Textbox displays a red border
+	it('GX3-1129 | TC6: Validar textbox displays a red border después de enviar sin carácter arroba “@“ en campo Email.', () => {
+		cy.fixture('data/GX3-1128-Text-Box-Fill.json').then(the => {
+			cy.get(the.Email.input).type(the.Email.data.invalid.emailOne);
+			cy.get(the.SubmitButton).click();
+			cy.get('.mr-sm-2.field-error.form-control').should('have.class', 'field-error');
+		});
+	});
+	it('GX3-1129 | TC7: Validar textbox displays a red border después de enviar 1 carácter alfanumérico antes de “@“ en campo Email.', () => {
+		cy.fixture('data/GX3-1128-Text-Box-Fill.json').then(the => {
+			cy.get(the.Email.input).type(the.Email.data.invalid.emailTwo);
+			cy.get(the.SubmitButton).click();
+			cy.get('.mr-sm-2.field-error.form-control').should('have.class', 'field-error');
+		});
+	});
+	it('GX3-1129 | TC8: Validar textbox displays a red border después de enviar 1 carácter alfanumérico después de “@“ en campo Email.', () => {
+		cy.fixture('data/GX3-1128-Text-Box-Fill.json').then(the => {
+			cy.get(the.Email.input).type(the.Email.data.invalid.emailThree);
+			cy.get(the.SubmitButton).click();
+			cy.get('.mr-sm-2.field-error.form-control').should('have.class', 'field-error');
+		});
+	});
+	it('GX3-1129 | TC9: Validar textbox displays a red border después de enviar sin punto (.) despúes de un carácter alfanumérico después de “@“ en campo Email.', () => {
+		cy.fixture('data/GX3-1128-Text-Box-Fill.json').then(the => {
+			cy.get(the.Email.input).type(the.Email.data.invalid.emailFour);
+			cy.get(the.SubmitButton).click();
+			cy.get('.mr-sm-2.field-error.form-control').should('have.class', 'field-error');
+		});
+	});
+	it('GX3-1129 | TC10: Validar textbox displays a red border después de enviar sin dos caracteres alfanumericos después del (.) en campo Email.', () => {
+		cy.fixture('data/GX3-1128-Text-Box-Fill.json').then(the => {
+			cy.get(the.Email.input).type(the.Email.data.invalid.emailFive);
+			cy.get(the.SubmitButton).click();
+			cy.get('.mr-sm-2.field-error.form-control').should('have.class', 'field-error');
 		});
 	});
 });
