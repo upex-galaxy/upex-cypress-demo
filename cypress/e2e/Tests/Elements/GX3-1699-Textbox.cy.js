edd292a8514cacdf274f20de7a625a507d78713d
Marianela Serodino Runco <serodinonela@gmail.com>
2024-01-24 10:00:05 +0000

    Created fixture file with static data

--- cypress/e2e/Tests/Elements/GX3-1699-Textbox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1699-Textbox.cy.js
@@ -1,6 +1,6 @@
 describe('GX3-1699 | ToolsQA | Elements | Text Box: Fill form and Submit', () => { 
-    beforeEach('Visit ToolsQa web, TextBox section', () => {
+	beforeEach('Visit ToolsQa web, TextBox section', () => {
 		cy.visit('https://demoqa.com/text-box');
 		cy.url().should('contain', 'text-box');
 	});
- })
+});
