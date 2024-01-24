ddc9f0b93f439edebb286eed32a7ec4707ab7c28
Marianela Serodino Runco <serodinonela@gmail.com>
2024-01-24 11:37:54 +0000

    created TC2 and refactored the code

--- cypress/e2e/Tests/Elements/GX3-1699-Textbox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-1699-Textbox.cy.js
@@ -1,6 +1,10 @@
 import the from '@data/GX3-1699-TextBox.json';
 
 describe('GX3-1699 | ToolsQA | Elements | Text Box: Fill form and Submit', () => { 
+	const inputsSelectors = [the.input.fullName, the.input.email, the.input.currentAddress,the.input.permanentAddress];
+	const outputSelectors = [the.output.name, the.output.email, the.output.currentAddress, the.output.permanentAddress];
+	const dataToFillOutForm = [the.data.fullName, the.data.validEmail, the.data.currentAddress,the.data.permanentAddress];
+
 	beforeEach('Visit ToolsQa web, TextBox section', () => {
 		cy.visit('https://demoqa.com/text-box');
 		cy.url().should('contain', 'text-box');
@@ -8,18 +12,31 @@ describe('GX3-1699 | ToolsQA | Elements | Text Box: Fill form and Submit', () =>
 
 	it('1700 | TC1: Check that the user can fill the form with valid data.', () => {
 		//Fill out the form
-		cy.get(the.input.fullName).type(the.data.fullName);
-		cy.get(the.input.email).type(the.data.validEmail);
-		cy.get(the.input.currentAddress).type(the.data.currentAddress);
-		cy.get(the.input.permanentAddress).type(the.data.permanentAddress);
+		inputsSelectors.forEach((input, index) => {
+			cy.get(input).type(dataToFillOutForm[index]);
+		});
 
 		// Submit data
 		cy.get(the.submitButton).click();
 
 		//Assertions
-		cy.get(the.output.name).should('include.text', the.data.fullName);
-		cy.get(the.output.email).should('include.text', the.data.validEmail);
-		cy.get(the.output.currentAddress).should('include.text', the.data.currentAddress);
-		cy.get(the.output.permanentAddress).should('include.text', the.data.permanentAddress);
+		outputSelectors.forEach((output, index) => {
+			cy.get(output).should('include.text', dataToFillOutForm[index]);
+		});
+	});
+
+	it('1700 | TC2: Check that no message is displayed when leaving fields empty.', () => {
+		//Check that each field is empty
+		inputsSelectors.forEach(field => {
+			cy.get(field).clear();
+		});
+
+		// Submit empty form
+		cy.get(the.submitButton).click();
+
+		//Assertions
+		outputSelectors.forEach(outputSelector => {
+			cy.get(outputSelector).should('not.exist');
+		});
 	});
 });
