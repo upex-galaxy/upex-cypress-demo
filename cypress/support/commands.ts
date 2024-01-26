9f17d70c0f8fdb4216ab18c521fb79165b9af730
saiotest <elyermad@gmail.com>
2024-01-26 03:56:15 -0300

    refactor: support typescript for cy and added page() command for POM On branch test/GX3-1870/checkout-book-destination Changes to be committed: 	modified:   .eslintrc.json 	modified:   .github/workflows/sanityTemplate.yml 	modified:   .vscode/settings.json 	new file:   coverage/S35/GX3-1870.md 	renamed:    cypress.config.js -> cypress.config.ts 	new file:   cypress.d.ts 	new file:   cypress/e2e/Tests/Checkout/GX3-1870_spaceCheckout.cy.ts 	deleted:    cypress/support/commands.js 	new file:   cypress/support/commands.ts 	renamed:    cypress/support/e2e.js -> cypress/support/e2e.ts 	deleted:    cypress/support/helper/RemoveLogs.js 	new file:   cypress/support/helper/testUtilities.ts 	modified:   cypress/support/helper/testUtils.js 	new file:   cypress/support/pages/SpaceCheckoutPage.ts 	new file:   cypress/support/pages/SpaceLogin.Page.ts 	new file:   cypress/support/pages/SpaceProduct.Page.ts 	modified:   package.json 	modified:   readme.md 	renamed:    jsconfig.json -> tsconfig.json 	modified:   yarn.lock

---
+++ cypress/support/commands.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+// ***********************************************
+// https://on.cypress.io/custom-commands
+// ***********************************************
+import { SpaceCheckoutPage } from '@pages/SpaceCheckoutPage';
+import { SpaceLoginPage } from '@pages/SpaceLogin.Page';
+import { SpaceDestinationPage } from '@pages/SpaceProduct.Page';
+import 'cypress-file-upload';
+
+beforeEach(() => {
+	Cypress.on('uncaught:exception', () =>  false ); // returning false here prevents Cypress from failing the test
+	cy.intercept({ resourceType: /^(xhr|fetch)$/ }, { statusCode: 200, body: { data: 'fake data' } });
+	cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+});
+
+//* Este es el commando principal para usar los Page Object Models sin necesidad de importarlos en cada archivo de test
+Cypress.Commands.add('page', () => {
+	const page = {
+		spaceLoginPage: new SpaceLoginPage(),
+		spaceProductPage: new SpaceDestinationPage(),
+		spaceCheckoutPage: new SpaceCheckoutPage(),
+	}
+	return cy.wrap(page)
+ })
+
+Cypress.Commands.add('react', (dataReactToolbox: string, options?: { hasText: string }) => {
+	const selector = `[data-react-toolbox=${dataReactToolbox}]`
+	if (options?.hasText) {
+		cy.contains(selector, options.hasText)
+	} else {
+		cy.get(selector)
+	}
+ })
