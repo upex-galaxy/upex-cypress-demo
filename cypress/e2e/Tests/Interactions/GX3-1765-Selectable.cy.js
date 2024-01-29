d36b423d284b860a71d2f320401553b04af816df
Marianela Serodino Runco <serodinonela@gmail.com>
2024-01-29 13:58:54 +0000

    Changed file naming from GX3-1766 to GX3-1765

--- cypress/e2e/Tests/Interactions/GX3-1765-Selectable.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-1765-Selectable.cy.js
@@ -1,8 +1,7 @@
-import { selectablePage } from '@pages/Interactions/GX3-1766-Selectable.page'; 
-describe('ToolsQA | Interactions | Selectable', () => { 
-	const listItems = ['Cras justo odio','Dapibus ac facilisis in', 'Morbi leo risus', 'Porta ac consectetur ac'];
+import { selectablePage } from '@pages/Interactions/GX3-1765-Selectable.page';
+describe('ToolsQA | Interactions | Selectable', () => {
+	const listItems = ['Cras justo odio', 'Dapibus ac facilisis in', 'Morbi leo risus', 'Porta ac consectetur ac'];
 	const gridItems = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-	
 
 	beforeEach('Go to Demo QA Web, Selectable section', () => {
 		cy.visit('https://demoqa.com/selectable');
@@ -10,21 +9,13 @@ describe('ToolsQA | Interactions | Selectable', () => {
 	});
 
 	it('1766 | TC1:Check that the "List" and "Grid" tabs are visible by default', () => {
-		selectablePage.get
-			.listTab()
-			.should('be.visible')
-			.and('have.attr', 'aria-selected', 'true');
-		selectablePage.get
-			.gridTab()
-			.should('be.visible')
-			.and('have.attr', 'aria-selected', 'false');
+		selectablePage.get.listTab().should('be.visible').and('have.attr', 'aria-selected', 'true');
+		selectablePage.get.gridTab().should('be.visible').and('have.attr', 'aria-selected', 'false');
 	});
 
 	it('1766 | TC2: “Check that the "List" tab is opened by default and its content is visible”.', () => {
 		selectablePage.get.listContainer().each((li, index) => {
-			cy.wrap(li)
-				.should('be.visible')
-				.and('have.text', listItems[index]);
+			cy.wrap(li).should('be.visible').and('have.text', listItems[index]);
 		});
 	});
 
@@ -33,28 +24,18 @@ describe('ToolsQA | Interactions | Selectable', () => {
 		const randomIndex = Math.floor(Math.random() * listItems.length);
 		const randomItem = listItems[randomIndex];
 		selectablePage.selectOneItem(randomItem, 'listContainer');
-		selectablePage.get
-			.listContainer()
-			.contains(randomItem)
-			.should('have.css', 'background-color', 'rgb(0, 123, 255)');
-		
+		selectablePage.get.listContainer().contains(randomItem).should('have.css', 'background-color', 'rgb(0, 123, 255)');
+
 		selectablePage.selectOneItem(randomItem, 'listContainer');
-		selectablePage.get
-			.listContainer()
-			.contains(randomItem)
-			.should('have.css', 'background-color', 'rgb(255, 255, 255)');
+		selectablePage.get.listContainer().contains(randomItem).should('have.css', 'background-color', 'rgb(255, 255, 255)');
 	});
 
 	it('1766 | TC4: “Check that multiple items on "List" tab can be selected and deselected”.', () => {
 		selectablePage.selectMultipleItems('listContainer');
-		selectablePage.get
-			.listContainer()
-			.should('have.css', 'background-color', 'rgb(0, 123, 255)');
+		selectablePage.get.listContainer().should('have.css', 'background-color', 'rgb(0, 123, 255)');
 
 		selectablePage.selectMultipleItems('listContainer');
-		selectablePage.get
-			.listContainer()
-			.should('have.css', 'background-color', 'rgb(255, 255, 255)');
+		selectablePage.get.listContainer().should('have.css', 'background-color', 'rgb(255, 255, 255)');
 	});
 
 	it('1766 | TC5: “Check that one item on the "Grid" tab can be selected and deselected”. ', () => {
@@ -63,29 +44,19 @@ describe('ToolsQA | Interactions | Selectable', () => {
 		const randomIndex = Math.floor(Math.random() * gridItems.length);
 		const randomItem = gridItems[randomIndex];
 		selectablePage.selectOneItem(randomItem, 'gridContainer');
-		selectablePage.get
-			.gridContainer()
-			.contains(randomItem)
-			.should('have.css', 'background-color', 'rgb(0, 123, 255)');
+		selectablePage.get.gridContainer().contains(randomItem).should('have.css', 'background-color', 'rgb(0, 123, 255)');
 
 		selectablePage.selectOneItem(randomItem, 'gridContainer');
-		selectablePage.get
-			.gridContainer()
-			.contains(randomItem)
-			.should('have.css', 'background-color', 'rgb(255, 255, 255)');
+		selectablePage.get.gridContainer().contains(randomItem).should('have.css', 'background-color', 'rgb(255, 255, 255)');
 	});
 
 	it('1766 | TC6: “Check that multiple items on "Grid" tab can be selected and deselected”.', () => {
 		selectablePage.clickGridTab();
-		
-		selectablePage.selectMultipleItems('gridContainer');
-		selectablePage.get
-			.gridContainer()
-			.should('have.css', 'background-color', 'rgb(0, 123, 255)');
 
 		selectablePage.selectMultipleItems('gridContainer');
-		selectablePage.get
-			.gridContainer()
-			.should('have.css', 'background-color', 'rgb(255, 255, 255)');
+		selectablePage.get.gridContainer().should('have.css', 'background-color', 'rgb(0, 123, 255)');
+
+		selectablePage.selectMultipleItems('gridContainer');
+		selectablePage.get.gridContainer().should('have.css', 'background-color', 'rgb(255, 255, 255)');
 	});
 });
