267a533f8019e9291666db6d58462ea703979a8e
Braian Ferreyra <braianezequielferreyra39936964@gmail.com>
2024-02-06 23:49:54 +0100

    GX3-2176 third commit, primer TC creado

---
+++ cypress/e2e/Tests/GX3-2176_DropdownSelectMenu.cy.ts
@@ -0,0 +1,11 @@
+
+describe('GX3-2176_⚡️ToolsQA | Widgets | Dropdown - Select Menu ',() => {
+	beforeEach('Prc: Usuario debe ingresar a la pagina para hacer pruebas',() => {
+	    cy.visit('/select-menu');
+	});
+	it('2176| TC1: Validar poder seleccionar un elemento de la lista desplegable de "Select Value" ', () => {;
+		cy.get('#withOptGroup').click();
+		cy.get('#react-select-2-option-0-0').click();
+		cy.contains('Group 1, option 1');
+	});
+});
