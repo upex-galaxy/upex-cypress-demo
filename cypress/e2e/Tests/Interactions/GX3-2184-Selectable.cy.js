b321015bcf3f725fea30da1fc24a5167a0e3e2f3
AlexanderWindecker <waa_alex@hotmail.com>
2024-02-14 18:10:25 -0300

    GX3-2184 Se completan los test. Todos los test pasaron correctamente.

--- cypress/e2e/Tests/Interactions/GX3-2184-Selectable.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-2184-Selectable.cy.js
@@ -1,25 +1,56 @@
-import { selectable } from "../../../support/pages/GX3-2184-Selectable.Page";
+import { selectablePage } from "../../../support/pages/GX3-2184-Selectable.Page";
 
 describe("ToolsQA | Interactions | Selectable", () =>{
 
+const baseUrl = "https://demoqa.com/selectable";
+
 	beforeEach(() =>{
-		cy.visit("https://demoqa.com/selectable")
-		cy.url().should("contain", "selectable")
+		cy.visit(baseUrl)
+		cy.url().should("include", "selectable")
 
 
 	})
-	it("Validar poder hacer click en un elemento del grid", ()=>{
-		selectable.clickGrid(),
-		selectable.get.buttongrid().should("have.class", "active")
-		selectable.selectGridthree()
-		selectable.selectGridFour()
-		selectable.selectGridOne()
-		selectable.selectGridFive()
-		selectable.selectGridSeven()
-		selectable.selectGridTwo()
-		selectable.selectGridNine()
-		selectable.selectGridSix()
-		selectable.selectGridHeight()
-	})	
+	it("2185 | TC1: Validar seleccionar los elementos de List", ()=>{
+		selectablePage.clickList()
+		selectablePage.get.buttonlist().should("have.class", "active")
+		const numberOfElements = 1;
+		for (let i = 0; i < numberOfElements; i++){
+			selectablePage.selectAllList(i);
+			selectablePage.get.listOne().should("have.class", "active");
+			cy.wait(1000);
+		}
+	});
+	it("2185 | TC2: Validar deseleccionar los elementos de List", ()=>{
+	selectablePage.clickList()
+		
+		const numberOfElements = 2;
+		for (let i = 0; i < numberOfElements; i++){
+			selectablePage.selectAllList(i);
+			cy.wait(1000);
+
+		}
+	});
+	it("2185 | TC3: Validar seleccionar los elementos de Grid.", ()=>{
+		selectablePage.clickGrid()
+		selectablePage.get.buttongrid().should("have.class", "active")
+		const numberOfElements = 1;
+		for (let i = 0; i < numberOfElements; i++) {
+      	selectablePage.selectAllGrid(i);
+		selectablePage.get.gridOne().should("have.class", "active");
+        cy.wait(1000);
+    }		
+	});		
+	it("2185 | TC4: Validar deseleccionar los elementos de Grid.", ()=>{
+		selectablePage.clickGrid()
+		const numberOfElements = 2;
+		for (let i = 0; i < numberOfElements; i++) {
+      	selectablePage.selectAllGrid(i);
+		cy.wait(1000);
+      
+    }
+		
+	});	
+	
+	
 
 })
--- cypress/support/pages/GX3-2184-Selectable.Page.js
+++ cypress/support/pages/GX3-2184-Selectable.Page.js
@@ -2,20 +2,11 @@ class Selectable {
 	get = { 
 		endpoint: () => cy.visit("https://demoqa.com/selectable/"),
 		buttonlist: () => cy.get("#demo-tab-list"),
-		listOne: () => cy.get("#verticalListContainer"),
-		listTwo: () => cy.get("#verticalListContainer"),
-		listThree: () => cy.get("#verticalListContainer"),
-		listFour: () => cy.get("#verticalListContainer"),
+		listOne: () => cy.get("#verticalListContainer li"),
+		
 		buttongrid: () => cy.get("#demo-tab-grid"),
 		gridOne: () => cy.get("#gridContainer li"),
-		gridTwo: () => cy.get("#gridContainer li"),
-		gridThree: () => cy.get("#gridContainer li"),
-		gridFour: () => cy.get("#gridContainer li"),
-		gridFive: () => cy.get("#gridContainer li"),
-		gridSix: () => cy.get("#gridContainer li"),
-		gridSeven: () => cy.get("#gridContainer li"),
-		gridHeight: () => cy.get("#gridContainer li"),
-		gridNine: () => cy.get("#gridContainer li"),
+	
 		
 
 		
@@ -23,39 +14,18 @@ class Selectable {
 
 	
 	clickList(){
-		this.get.buttonlist().click()
+		this.get.buttonlist().click();
+	}
+	selectAllList(){
+		this.get.listOne().click({ multiple: true })
+	}
+	clickGrid(){
+		this.get.buttongrid().click();
+	}
+	selectAllGrid(){
+		this.get.gridOne().click({ multiple: true });
 	}
 	
-	clickGrid(){
-		this.get.buttongrid().click()
-	}
-	selectGridOne(){
-		this.get.gridOne().eq(0).click()
-	}
-	selectGridTwo(){
-		this.get.gridTwo().eq(1).click()
-	}
-	selectGridthree(){
-		this.get.gridThree().eq(2).click()
-	}
-	selectGridFour(){
-		this.get.gridFour().eq(3).click()
-	}
-	selectGridFive(){
-		this.get.gridFive().eq(4).click()
-	}
-	selectGridSix(){
-		this.get.gridSix().eq(5).click()
-	}
-	selectGridSeven(){
-		this.get.gridSeven().eq(6).click()
-	}
-	selectGridHeight(){
-		this.get.gridHeight().eq(7).click()
-	}
-	selectGridNine(){
-		this.get.gridNine().eq(8).click()
-	}
 }
 
-export const selectable = new Selectable;
+export const selectablePage = new Selectable;
