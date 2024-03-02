c96a7f1e58c78a48d2df537806c6bb2db94bd8ca
LucasSebastianCasco <lucassebaacasco@gmail.com>
2024-03-02 09:41:08 -0300

    GX3-2469 ajuste en el test y generacion de archivo yml

--- cypress/e2e/Tests/Interactions/GX3-2469-Droppablee.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-2469-Droppablee.cy.js
@@ -6,13 +6,13 @@ describe('GX3-2469 | ToolsQA | Interactions | Droppable ',()=>{
 		cy.url().should('contain','droppable')
 	})
 
-	it('2469 | TC01 Validate that "Droppable Box" changes name and color when paired with Dragable Box',()=>{
+	it('2476 | TC01 Validate that "Droppable Box" changes name and color when paired with Dragable Box',()=>{
 		droppablePage.get.dragMeBox().trigger('mousedown', { which: 1 });
 		droppablePage.get.dropHereBoxSimple().trigger('mousemove').trigger('mouseup', { force: true });
 		droppablePage.get.dropHereBoxSimple().should('have.text','Dropped!').and('have.css', 'background-color','rgb(70, 130, 180)');	
 	})
 
-	it('2469| TC02 validate move "Not Acceptable" and "Aceptable" boxes to "Drope here" box in tap "Accept"' ,()=>{
+	it('2476| TC02 validate move "Not Acceptable" and "Aceptable" boxes to "Drope here" box in tap "Accept"' ,()=>{
 		
 		droppablePage.clickOnAcceptTab();
 		droppablePage.get.notAcceptBox().trigger('mousedown', {which:1},{force: true});
@@ -25,14 +25,14 @@ describe('GX3-2469 | ToolsQA | Interactions | Droppable ',()=>{
 
 	})
 
-	it('2469| TC03 move "Drag me" to "Outer droppable (not greedy)" in tab "Prevent Propogation"',()=>{
+	it('2476| TC03 move "Drag me" to "Outer droppable (not greedy)" in tab "Prevent Propogation"',()=>{
 		droppablePage.clickOnProptab()
 		droppablePage.get.dragMe().trigger('mousedown', {which:1},{force: true})
 		droppablePage.get.textOuterDroppable().trigger('mousemove',{ force: true }).trigger('mouseup', {force: true});
 		droppablePage.get.outerNotGreedy().should('have.text','Dropped!Inner droppable (not greedy)').and('have.css', 'background-color','rgb(70, 130, 180)');
 	})
 
-	it('2469| TC04 move "Drag me" to "Inner droppable (not greedy)" in tab "Prevent Propogation"',()=>{
+	it('2476| TC04 move "Drag me" to "Inner droppable (not greedy)" in tab "Prevent Propogation"',()=>{
 		droppablePage.clickOnProptab()
 		droppablePage.get.dragMe().trigger('mousedown', {which:1},{force: true})
 		droppablePage.get.innerNotGreedy().trigger('mousemove',{ force: true }).trigger('mouseup', {force: true});
@@ -40,21 +40,21 @@ describe('GX3-2469 | ToolsQA | Interactions | Droppable ',()=>{
 		droppablePage.get.outerNotGreedy().should('have.text','Dropped!Dropped!').and('have.css', 'background-color','rgb(70, 130, 180)');
 	})
 
-	it('2469| TC05 move "Drag me" to "Outer droppable (greedy)" in tab "Prevent Propogation"',()=>{
+	it('2476| TC05 move "Drag me" to "Outer droppable (greedy)" in tab "Prevent Propogation"',()=>{
 		droppablePage.clickOnProptab()
 		droppablePage.get.dragMe().trigger('mousedown', {which:1},{force: true})
 		droppablePage.get.textOuterDroppableGreedy().trigger('mousemove',{ force: true }).trigger('mouseup', {force: true});
 		droppablePage.get.outerGreedy().should('have.text','Dropped!Inner droppable (greedy)').and('have.css', 'background-color','rgb(70, 130, 180)')
 	})
 
-	it('2469| TC06 move "Drag me" to "Inner droppable (greedy)" in tab "Prevent Propogation"',()=>{
+	it('2476| TC06 move "Drag me" to "Inner droppable (greedy)" in tab "Prevent Propogation"',()=>{
 		droppablePage.clickOnProptab()
 		droppablePage.get.dragMe().trigger('mousedown', {which:1},{force: true})
 		droppablePage.get.innerGreedy().trigger('mousemove',{ force: true }).trigger('mouseup', {force: true});
 		droppablePage.get.innerGreedy().should('have.text','Dropped!').and('have.css', 'background-color','rgb(70, 130, 180)');
 	})
 
-	it('2469| TC07 move "will revert" box to "Drop Here" box on Revert tab',()=>{
+	it('2476| TC07 move "will revert" box to "Drop Here" box on Revert tab',()=>{
 		droppablePage.clickOnDragTab()
 		let initialPosition;
 		cy.get('#revertable').then(($element) => {	
@@ -73,7 +73,7 @@ describe('GX3-2469 | ToolsQA | Interactions | Droppable ',()=>{
   		});	
 	})
 	
-	it('2469| TC08 move "Not revert" box to "Drop Here" box on Revert tab',()=>{
+	it('2476| TC08 move "Not revert" box to "Drop Here" box on Revert tab',()=>{
 		droppablePage.clickOnDragTab()
 		droppablePage.get.norRevertBox().trigger('mousedown', {which:1},{force: true})
 		droppablePage.get.dropHereBox().trigger('mousemove',{ force: true }).trigger('mouseup', {force: true});
