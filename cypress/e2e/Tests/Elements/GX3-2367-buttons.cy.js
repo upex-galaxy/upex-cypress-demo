071a3a876e907de3404ada0f26fc0d077a74a434
melina-decastro <melina-decastro@hotmail.com>
2024-03-02 16:30:25 -0300

    GX3-2367 mi primeraUS de automation trabaje en los tres radio buttons, ya pasaron los 3 tc correctamente

--- cypress/e2e/Tests/Elements/GX3-2367-buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-2367-buttons.cy.js
@@ -3,45 +3,32 @@ describe ('GX3-2367 | TS: ⚡️ToolsQA | Elements | Radio Buttons', ()=>
 	beforeEach('Precondición: Usuario debe estar situado en la página demoqa',()=>
 	{
 	cy.visit('https://demoqa.com/radio-button');
-
-	})
+	cy.url().should('include', 'radio-button');
+	});
+	
 	it('US2367 | TC1: Validar  poder presionar el botón YES', () =>
 	{
-		cy.get('[href="/commands/querying"]')
-		.eq(2)
+		cy.get("[for='yesRadio']")
 		.click()
-
+		cy.contains('Yes')
 	})
-	it('US2367')
+
+
+	it('US2367 | TC2: Validar  poder presionar el botón Impressive', () =>
+	{
+		cy.get("[for='impressiveRadio']")
+		.click()
+		cy.contains('Impressive')
+	})
+
+it('US2367 | TC3: Validar no poder presionar el botón No', () =>
+	{
+		cy.get("[for='noRadio']")
+		cy.contains('No')
+	})
 	
 	})
 
 
 
-	/*it('US # | TC#1: Aquí puedes escribir un Caso de Prueba', () =>
-	{
-		cy.get('[href="/commands/querying"]') // Sintaxis Comando Get de Selector de Atributos
-			.eq(2) // Esto es un método opcional
-			.click() // Esto es un método de Interacción
-	})
-	it('US # | TC#2: Aquí puedes escribir otro Caso de Prueba', () =>
-	{
-		cy.get('[href="/commands/querying"]')
-			.eq(2) 
-			.click() 
-		cy.get("#query-btn") // Sintaxis Comando Get de Selector de IDs
-			.click() // Esto es un método de Interacción (Acción)
-	})
-	it('US # | TC#3: Aquí puedes escribir otro Caso de Prueba', () =>
-	{
-		cy.get('[href="/commands/querying"]')
-			.eq(2) 
-			.click() 
-		cy.get("#query-btn")
-			.click()
-		cy.get('.query-btn') // Sintaxis Comando Get de Selector de Clases
-			.should('contain.text', 'Button') // Assertion BDD para validar resultado esperado (de muchas formas)
-	})
-})*/
-
-
+	
