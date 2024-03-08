234eaefa1fb87f9545a157bb68def21372e7c68f
melina-decastro <melina-decastro@hotmail.com>
2024-03-07 22:44:22 -0300

    Gx3 2367 ⚡️ tools qa elements radio buttons

--- cypress/e2e/Tests/Elements/GX3-2367-buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-2367-buttons.cy.js
@@ -1,35 +1,24 @@
-describe ('GX3-2367 | TS: ⚡️ToolsQA | Elements | Radio Buttons', ()=>
-{	
-	beforeEach('Precondición: Usuario debe estar situado en la página demoqa',()=>
-	{
-	cy.visit('https://demoqa.com/radio-button');
-	cy.url().should('include', 'radio-button');
-	});
-	
-	it('US2367 | TC1: Validar  poder presionar el botón YES', () =>
-	{
-		cy.get("[for='yesRadio']")
-		.click()
-		cy.get('.text-sucess').should('have.text', radioYes);
+describe('GX3-2367 | TS: ⚡️ToolsQA | Elements | Radio Buttons', () => {
+	beforeEach('Precondición: Usuario debe estar situado en la página demoqa', () => {
+		cy.visit('https://demoqa.com/radio-button');
+		cy.url().should('include', 'radio-button');
 	});
 
-
-	it('US2367 | TC2: Validar  poder presionar el botón Impressive', () =>
-	{
-		cy.get("[for='impressiveRadio']")
-		.click()
-		cy.get('.text-sucess').should('have.text', radioImpressive);
+	it('US2367 | TC1: Validar  poder presionar el botón YES', () => {
+		cy.get('[for=\'yesRadio\']')
+			.click();
+		cy.get('.text-sucess').should('have.text', 'Yes');
 	});
 
-it('US2367 | TC3: Validar no poder presionar el botón No', () =>
-	{
-		cy.get("[for='noRadio']")
-		getRadio(radioNo).should('be.disabled');
-	});
-	
+	it('US2367 | TC2: Validar  poder presionar el botón Impressive', () => {
+		cy.get('[for=\'impressiveRadio\']')
+			.click();
+		cy.get('.text-success').should('have.text', 'Impressive');
 	});
 
+	it('US2367 | TC3: Validar no poder presionar el botón No', () => {
+		cy.get('[for=\'noRadio\']');
+		getRadio('No').should('be.disabled');
+	});
 
-
-
-	
+});
