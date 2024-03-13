599b8fead04dbd93dcd7f653eb790f357b338fbb
Liliana Prospel <liliana.prospel@gmail.com>
2024-03-13 14:44:35 -0500

    GX3-2690 | Cambio de nomemclatura de los TC

--- cypress/e2e/Tests/Elements/GX3-2690-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-2690-Buttons.cy.js
@@ -1,22 +1,22 @@
-describe ('GX3-2690 | TS: ToolsQA | Elements | Buttons', () => {
+describe ('GX3-2691 | TS: ToolsQA | Elements | Buttons', () => {
 	beforeEach (() => {
 		cy.visit('https://demoqa.com/buttons'); //Ingresa a la página que se va a testear
 		cy.url().should('contains', 'buttons'); //Comprueba que la URL contenga la cadena buttons
 	});
-	it('GX3-2690 | TC1: Validar que existan los botones "Double Click Me - Right Click Me - Click Me"', () => {
+	it('GX3-2691 | TC1: Validar que existan los botones "Double Click Me - Right Click Me - Click Me"', () => {
 		cy.get('button[type="button"][id="doubleClickBtn"]').should('have.length', 1);
 		cy.get('button[type="button"][id="rightClickBtn"]').should('have.length', 1);
 		cy.get('button').contains('Click Me').should('have.length', 1);
 	});
-	it('GX3-2690 | TC2: Validar mostrar mensaje "You have done a double click" cuando se hace doble click en el botón "Double Click"', () => {
+	it('GX3-2691 | TC2: Validar mostrar mensaje "You have done a double click" cuando se hace doble click en el botón "Double Click"', () => {
 		cy.get('button[type="button"][id="doubleClickBtn"]').dblclick();	
 		cy.get('#doubleClickMessage').should('have.text', 'You have done a double click');
 	});
-	it('GX3-2690 | TC3: Validar mostrar mensaje "You have done a right click" cuando se hace click derecho en el botón "Right Click Me"', () => {
+	it('GX3-2691 | TC3: Validar mostrar mensaje "You have done a right click" cuando se hace click derecho en el botón "Right Click Me"', () => {
 		cy.get('button[type="button"][id="rightClickBtn"]').rightclick();	
 		cy.get('#rightClickMessage').should('have.text', 'You have done a right click');
 	});
-	it('GX3-2690 | TC4: Validar mostrar mensaje "You have done a dynamic click" cuando se hace un click en el botón "Click Me"', () => {
+	it('GX3-2691 | TC4: Validar mostrar mensaje "You have done a dynamic click" cuando se hace un click en el botón "Click Me"', () => {
 		cy.get('button').eq(3).click();
 		cy.get('#dynamicClickMessage').should('have.text', 'You have done a dynamic click');
 	});
