54fa89cae89c60b57edeefd97a29983b12b4a38c
Liliana Prospel <liliana.prospel@gmail.com>
2024-03-14 23:57:38 -0500

    GX3-2737 | Se agrega TC3 y TC4 a la US

--- cypress/e2e/Tests/Elements/GX3-2737-checkBox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-2737-checkBox.cy.js
@@ -4,17 +4,27 @@ describe('GX3-2738 | TS: ToolsQA | Elements | CheckBox', () => {
 		cy.url().should('contains', 'checkbox');
 	});
 
-	it.only('GX3-2738 | TC1: Validar expandir todas las casillas de verificación ',() => {
+	it('GX3-2738 | TC1: Validar expandir todas las casillas de verificación desde el botón "Expand all"',() => {
 		cy.get('svg.rct-icon-expand-all').as('expandAllIcon');		
 		cy.get('@expandAllIcon').click();
 		cy.get('ol').should('have.length', 7);
 	});
 
-	it('GX3-2738 | TC2: Validar contraer todas las casillas de verificación ',() => {
+	it('GX3-2738 | TC2: Validar contraer todas las casillas de verificación desde el botón "Collapse all" ',() => {
 		cy.get('svg.rct-icon-expand-all').as('expandAllIcon');
 		cy.get('@expandAllIcon').click();
 		cy.get('svg.rct-icon-collapse-all').as('collapseAllIcon');	
 		cy.get('@collapseAllIcon').click();
 		cy.get('ol').should('have.length', 1);
+	});
+
+	it.only('GX3-2738 | TC3: Validar poder desplegar cada toggle (Home, Desktop, Documents, WorkSpace, Offie, Downloads)',() => {
+		cy.get('.rct-collapse-btn').as('btnCollapse');		
+		cy.get('@btnCollapse').eq(0).click();
+		cy.get('@btnCollapse').eq(1).click();
+		cy.get('@btnCollapse').eq(2).click();
+		cy.get('@btnCollapse').eq(3).click();		
+		cy.get('@btnCollapse').eq(4).click();		
+		cy.get('@btnCollapse').eq(5).click();
 	});
 })
