da94716f55fbcdaa276ddaf60da9cc590ab4db18
Marianela Serodino Runco <serodinonela@gmail.com>
2024-03-19 14:46:01 +0000

    GX3-2823 Crated Test case 1 and 2. Added methods to POM to get Today´s date and time

--- cypress/e2e/Tests/Widgets/GX3-2823-DatePicker.cy.js
+++ cypress/e2e/Tests/Widgets/GX3-2823-DatePicker.cy.js
@@ -4,7 +4,10 @@ describe('ToolsQA | Widgets | Date Picker', () => {
 	beforeEach(() => {
 		cy.visit('/date-picker');
 	});
-	it('2823 | TC1: Testin', () => {
-		cy.log('Hello');
+	it('2823 | TC1: Check that the Select Date input shows today´s date', () => {
+		DatePickerPage.get.datePickerInput().invoke('val').should('equal', DatePickerPage.getTodayDateFormatted());
+	});
+	it('2823 | TC2: Check that the Date And Time input shows today´s date and time', () => {
+	 	DatePickerPage.get.dateAndTimePickerInput().invoke('val').should('equal', DatePickerPage.getFormattedDateTimeIntl());
 	});
 });
--- cypress/support/pages/GX3-2823-DatePicker.Page.js
+++ cypress/support/pages/GX3-2823-DatePicker.Page.js
@@ -1,7 +1,35 @@
 class DatePicker {
-	get = {};
-	getTodayDate() {
+	get = {
+		datePickerInput: () => cy.get('#datePickerMonthYearInput'),
+		dateAndTimePickerInput: () => cy.get('#dateAndTimePickerInput'),
+	};
+	getTodayDateFormatted() {
+		const today = new Date();
+		const day = String(today.getDate()).padStart(2, '0');
+		const month = String(today.getMonth()+ 1 ).padStart(2, '0');
+		const year = today.getFullYear();
+		const formattedDate = `${month}/${day}/${year}`;
 
+		return formattedDate;
+	}
+	getFormattedDateTimeIntl() {
+		const now = new Date();
+
+		const options = {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			hour12: true // Use 12-hour time format with AM/PM
+		};
+		// Generate the formatted string
+		let formattedDateTime = new Intl.DateTimeFormat('en-US', options).format(now);
+
+		// Remove the 'at' if it exists
+		formattedDateTime = formattedDateTime.replace(' at ', ' ');
+
+		return formattedDateTime;
 	}
 }
 export const DatePickerPage = new DatePicker();
