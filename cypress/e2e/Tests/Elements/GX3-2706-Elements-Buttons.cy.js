06bfb8d6fce84f084c43cc71f19d4c13419b41f3
melina-decastro <melina-decastro@hotmail.com>
2024-03-20 12:46:16 -0300

    GX3-2706 Se probaron los botones double click, right click y click

--- cypress/e2e/Tests/Elements/GX3-2706-Elements-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-2706-Elements-Buttons.cy.js
@@ -12,9 +12,9 @@ describe('2707: GX3-2706 | ToolsQA | Elements | Buttons', () =>
 	});
 	it('US 2706 | TC2: Validar hacer click en Right Click Me', () =>
 	{
-	cy.get('[type="button"]').eq(2).trigger('contextmenu');
+		cy.get('[type="button"]').eq(2).trigger('contextmenu');
 		cy.get('#rightClickMessage').should('have.text', 'You have done a right click');
-
+	});
 	it('US 2706 | TC3: Validar hacer click en Click Me', () =>
 	{
 		cy.get('[type="button"]').eq(3).click();
