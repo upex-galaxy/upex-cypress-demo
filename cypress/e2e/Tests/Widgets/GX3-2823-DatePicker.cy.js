cfab2027f1a2ad7d1ce8ec85d4d7e50c19e2259e
Marianela Serodino Runco <serodinonela@gmail.com>
2024-03-21 10:19:11 +0000

    GX3-2823 Created TC4

--- cypress/e2e/Tests/Widgets/GX3-2823-DatePicker.cy.js
+++ cypress/e2e/Tests/Widgets/GX3-2823-DatePicker.cy.js
@@ -3,11 +3,34 @@ import { DatePickerPage } from '../../../support/pages/GX3-2823-DatePicker.Page'
 describe('ToolsQA | Widgets | Date Picker', () => {
 	beforeEach(() => {
 		cy.visit('/date-picker');
+		cy.url().should('include', 'date-picker');;
 	});
 	it('2823 | TC1: Check that the Select Date input shows today´s date', () => {
-		DatePickerPage.get.datePickerInput().invoke('val').should('equal', DatePickerPage.getTodayDateFormatted());
+		DatePickerPage.get.datePickerInput().invoke('val').should('equal', DatePickerPage.getFormattedDateIntl());
 	});
 	it('2823 | TC2: Check that the Date And Time input shows today´s date and time', () => {
 	 	DatePickerPage.get.dateAndTimePickerInput().invoke('val').should('equal', DatePickerPage.getFormattedDateTimeIntl());
+	});
+	it('2823 | TC3: Check that a random date can be selected', () => {
+		DatePickerPage.get.datePickerInput().click();
+		DatePickerPage.selectRandomDate();
+		cy.get('@formattedDates').then(({ formattedDate1, formattedDate2 }) => {
+			DatePickerPage.get.datePickerInput().invoke('val').should('equal', formattedDate1);
+			DatePickerPage.get.datePickerInput().click();
+			DatePickerPage.get.dateHeader().invoke('text').should('equal', formattedDate2);
+		});
+	});
+	it('2823 | TC4: Check that the left arrow shows the previous month', () => {
+		DatePickerPage.get.datePickerInput().click();
+		DatePickerPage.selectRandomDate();
+		DatePickerPage.get.datePickerInput().click();
+		cy.get('@selectedMonth').then(({ randomMonthIndex, monthNamesArray }) => {
+			DatePickerPage.get.previousMonth().click();
+
+			const expectedPreviousMonthIndex = randomMonthIndex === 0 ? monthNamesArray.length - 1 : randomMonthIndex - 1;
+			const expectedPreviousMonthName = monthNamesArray[expectedPreviousMonthIndex];
+
+			DatePickerPage.get.dateHeader().invoke('text').should('include', expectedPreviousMonthName);
+		});
 	});
 });
--- cypress/support/pages/GX3-2823-DatePicker.Page.js
+++ cypress/support/pages/GX3-2823-DatePicker.Page.js
@@ -2,15 +2,24 @@ class DatePicker {
 	get = {
 		datePickerInput: () => cy.get('#datePickerMonthYearInput'),
 		dateAndTimePickerInput: () => cy.get('#dateAndTimePickerInput'),
+		monthSelector: () => cy.get('.react-datepicker__month-select'),
+		yearSelector: () => cy.get('.react-datepicker__year-select'),
+		daySelector: () => cy.get('.react-datepicker__week > div:not(.react-datepicker__day--outside-month)'),
+		dateHeader : () => cy.get('.react-datepicker__current-month'),
+		previousMonth : () => cy.get('.react-datepicker__navigation--previous'),
+		nextMonth : () => cy.get('.react-datepicker__navigation--next')
 	};
-	getTodayDateFormatted() {
-		const today = new Date();
-		const day = String(today.getDate()).padStart(2, '0');
-		const month = String(today.getMonth()+ 1 ).padStart(2, '0');
-		const year = today.getFullYear();
-		const formattedDate = `${month}/${day}/${year}`;
+	getFormattedDateIntl() {
+		const now = new Date();
 
-		return formattedDate;
+		const options = {
+			year: 'numeric',
+			month: '2-digit', // Ensures the month is always represented with two digits
+			day: '2-digit', // Ensures the day is always represented with two digits
+		};
+
+		// The 'en-US' locale typically uses slashes as separators for dates
+		return new Intl.DateTimeFormat('en-US', options).format(now);
 	}
 	getFormattedDateTimeIntl() {
 		const now = new Date();
@@ -31,5 +40,51 @@ class DatePicker {
 
 		return formattedDateTime;
 	}
+	selectRandomMonth() {
+		this.get.monthSelector().then($months => {
+			const monthNamesArray = $months.find('option').map((index, option) => Cypress.$(option).text()).get();
+			const randomMonthIndex = Math.floor(Math.random() * monthNamesArray.length);
+			cy.wrap($months).select(randomMonthIndex).then(() => {
+				const monthName = $months.find('option:selected').text();
+				const monthValue = randomMonthIndex + 1;
+				 cy.wrap({ randomMonthIndex, monthName, monthValue, monthNamesArray }).as('selectedMonth');
+			});
+		});
+	}
+	selectRandomYear() {
+		this.get.yearSelector().then($years => {
+			const yearCount = $years.find('option').length;
+			const randomYear = Math.floor(Math.random() * yearCount);
+			cy.wrap($years).select(randomYear).then(() => {
+				const year = $years.find('option:selected').val();
+				cy.wrap(year).as('selectedYear');
+			});
+		});
+	}
+	selectRandomDay() {
+		this.get.daySelector().then($days => {
+			const dayCount = $days.length;
+			const randomDay = Math.floor(Math.random() * dayCount);
+			const selectedDay = $days.eq(randomDay).click().text();
+			cy.wrap(selectedDay).as('selectedDay');
+		});
+	}
+	selectRandomDate() {
+		this.selectRandomMonth();
+		this.selectRandomYear();
+		cy.get('@selectedMonth').then(({ monthValue }) => {
+			this.selectRandomDay();
+			cy.get('@selectedDay').then((selectedDay) => {
+				cy.get('@selectedYear').then((selectedYear) => {
+					const formattedDate1 = `${monthValue.toString().padStart(2, '0')}/${selectedDay.padStart(2, '0')}/${selectedYear}`;
+					cy.get('@selectedMonth').then(({ monthName }) => {
+						const formattedDate2 = `${monthName} ${selectedYear}`;
+						cy.wrap({ formattedDate1, formattedDate2 }).as('formattedDates');
+					});
+				});
+			});
+		});
+	}
+
 }
 export const DatePickerPage = new DatePicker();
