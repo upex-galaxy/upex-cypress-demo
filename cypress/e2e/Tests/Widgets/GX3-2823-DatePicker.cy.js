faddc75564f8e487eb51eb67bfe08f77d234b2bc
Marianela Serodino Runco <serodinonela@gmail.com>
2024-03-25 15:28:27 +0000

    GX3-2823 Renamed Test Suite

--- cypress/e2e/Tests/Widgets/GX3-2823-DatePicker.cy.js
+++ cypress/e2e/Tests/Widgets/GX3-2823-DatePicker.cy.js
@@ -1,17 +1,17 @@
 import { DatePickerPage } from '../../../support/pages/GX3-2823-DatePicker.Page';
 
-describe('ToolsQA | Widgets | Date Picker', () => {
+describe('GX3-2823 | ToolsQA | Widgets | Date Picker', () => {
 	beforeEach(() => {
 		cy.visit('/date-picker');
 		cy.url().should('include', 'date-picker');;
 	});
-	it('2823 | TC1: Check that the Select Date input shows today´s date', () => {
+	it('2824 | TC1: Check that the Select Date input shows today´s date', () => {
 		DatePickerPage.getSelectDate.datePickerInput().invoke('val').should('equal', DatePickerPage.getFormattedDateIntl());
 	});
-	it('2823 | TC2: Check that the Date And Time input shows today´s date and time', () => {
+	it('2824 | TC2: Check that the Date And Time input shows today´s date and time', () => {
 	 	DatePickerPage.getDateAndTime.dateAndTimePickerInput().invoke('val').should('equal', DatePickerPage.getFormattedDateTimeIntl());
 	});
-	it('2823 | TC3: Check that a random date can be selected', () => {
+	it('2824 | TC3: Check that a random date can be selected', () => {
 		DatePickerPage.getSelectDate.datePickerInput().click();
 		DatePickerPage.selectRandomDate();
 		cy.get('@formattedDates').then(({ dateFormat, monthAndYearFormat }) => {
@@ -24,7 +24,7 @@ describe('ToolsQA | Widgets | Date Picker', () => {
 			});
 		});
 	});
-	it('2823 | TC4: Check that the left arrow shows the previous month', () => {
+	it('2824 | TC4: Check that the left arrow shows the previous month', () => {
 		DatePickerPage.getSelectDate.datePickerInput().click();
 		DatePickerPage.selectRandomDate();
 		DatePickerPage.getSelectDate.datePickerInput().click();
@@ -32,7 +32,7 @@ describe('ToolsQA | Widgets | Date Picker', () => {
 			DatePickerPage.getSelectDate.dateHeader().invoke('text').should('include', expectedPreviousMonthName);
 		});
 	});
-	it('2823 | TC5: Check that the right arrow shows the next month', () => {
+	it('2824 | TC5: Check that the right arrow shows the next month', () => {
 		DatePickerPage.getSelectDate.datePickerInput().click();
 		DatePickerPage.selectRandomDate();
 		DatePickerPage.getSelectDate.datePickerInput().click();
@@ -40,13 +40,13 @@ describe('ToolsQA | Widgets | Date Picker', () => {
 			DatePickerPage.getSelectDate.dateHeader().invoke('text').should('include', expectedNextMonthName);
 		});;
 	});
-	it('2823 | TC6: Check that the user can type in a date', () => {
+	it('2824 | TC6: Check that the user can type in a date', () => {
 		DatePickerPage.getSelectDate.datePickerInput().clear().should('be.visible').and('be.empty');
 		let randomDate = DatePickerPage.generateRandomDate();
 		DatePickerPage.getSelectDate.datePickerInput().type(randomDate).type('{enter}');
 		DatePickerPage.getSelectDate.datePickerInput().invoke('val').should('equal',randomDate);
 	});
-	it('2823 | TC7: Check that a random date and time can be selected', () => {
+	it('2824 | TC7: Check that a random date and time can be selected', () => {
 		DatePickerPage.getDateAndTime.dateAndTimePickerInput().click();
 		DatePickerPage.selectRandomDateAndTime();
 		cy.get('@formattedDateAndTime').then((formattedDateAndTime) => {
@@ -63,14 +63,14 @@ describe('ToolsQA | Widgets | Date Picker', () => {
 			DatePickerPage.getDateAndTime.selectedYear().should('contain', '✓');
 		});
 	});
-	it('2823 | TC8: Check that the right arrow shows the previous month', () => {
+	it('2824 | TC8: Check that the right arrow shows the previous month', () => {
 		DatePickerPage.getDateAndTime.dateAndTimePickerInput().click();
 		DatePickerPage.selectRandomMonthDropdown();
 		DatePickerPage.verifyMonthNavigation('next', '@selectedMonthDropdown').then( ({ expectedNextMonthName }) => {
 			DatePickerPage.getSelectDate.dateHeader().invoke('text').should('include', expectedNextMonthName);
 		});
 	});
-	it('2823 | TC9: Check that the left arrow shows the previous month', () => {
+	it('2824 | TC9: Check that the left arrow shows the previous month', () => {
 		DatePickerPage.getDateAndTime.dateAndTimePickerInput().click();
 		DatePickerPage.selectRandomMonthDropdown();
 		DatePickerPage.verifyMonthNavigation('previous', '@selectedMonthDropdown').then( ({ expectedPreviousMonthName }) => {
