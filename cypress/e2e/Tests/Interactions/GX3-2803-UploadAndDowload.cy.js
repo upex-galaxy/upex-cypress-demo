b1df1b7dca94a5a16c8442bbc80c68314ef9ad39
AlexanderWindecker <waa_alex@hotmail.com>
2024-03-25 11:24:00 -0300

    GX3-2803 Finalizamos el test con los TC creados.

---
+++ cypress/e2e/Tests/Interactions/GX3-2803-UploadAndDowload.cy.js
@@ -0,0 +1,22 @@
+import { selectPage } from '../../../support/pages/GX3-2803-UploadAndDowload.Page';
+
+describe('', () => {
+
+	beforeEach('', () => {
+		cy.visit('/upload-download');
+		cy.url().should('contain','upload-download');
+	});
+
+	it('TC1 | Validar descarga de archivo', () => {
+		selectPage.get.selectDowload().should('have.text', 'Download');
+		selectPage.BtnSelectDowload();
+		cy.readFile('cypress/downloads/sampleFile.jpeg', 'binary').should((buffer) => {
+			expect(buffer.length).to.be.gt(1000);
+		});
+	});
+	it('TC2 | Validar subida de imagen', () => {
+		selectPage.BtnSelectFile();
+		selectPage.get.confirmFilePath().should('be.visible');
+	});
+
+});
---
+++ cypress/support/pages/GX3-2803-UploadAndDowload.Page.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+class Select {
+	get = {
+		selectDowload: () => cy.get('#downloadButton'),
+		uploadFile: () => cy.get('#uploadFile'),
+		confirmFilePath: () => cy.get('#uploadedFilePath')
+
+	};
+
+	BtnSelectDowload() {
+		this.get.selectDowload().click();
+	}
+
+	BtnSelectFile() {
+		this.get.uploadFile().selectFile('cypress/fixtures/images/sampleFile.jpeg');
+	};
+}
+
+export const selectPage = new Select;
