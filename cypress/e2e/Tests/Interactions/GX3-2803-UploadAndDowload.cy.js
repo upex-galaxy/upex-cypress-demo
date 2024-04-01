b541b72b3ff335f3b1b84e0fb98e96db1e544be3
AlexanderWindecker <waa_alex@hotmail.com>
2024-04-01 14:27:30 -0300

    Corrigiendo errores

--- cypress/e2e/Tests/Interactions/GX3-2803-UploadAndDowload.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-2803-UploadAndDowload.cy.js
@@ -7,14 +7,14 @@ describe('', () => {
 		cy.url().should('contain','upload-download');
 	});
 
-	it('TC1 | Validar descarga de archivo', () => {
+	it('TC1 | Validar que se descargue de archivo', () => {
 		selectPage.get.selectDowload().should('have.text', 'Download');
 		selectPage.BtnSelectDowload();
 		cy.readFile('cypress/downloads/sampleFile.jpeg', 'binary').should((buffer) => {
 			expect(buffer.length).to.be.gt(1000);
 		});
 	});
-	it('TC2 | Validar subida de imagen', () => {
+	it('TC2 | Validar que se suba una imagen', () => {
 		selectPage.BtnSelectFile();
 		selectPage.get.confirmFilePath().should('be.visible');
 	});
