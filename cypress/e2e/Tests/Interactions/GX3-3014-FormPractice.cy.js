45112a26e09e39af6ef293051ca63c30839ee206
AlexanderWindecker <waa_alex@hotmail.com>
2024-04-11 21:44:20 -0300

    GX3-3014 Limpiamos un poco el codigo. Y empezamos con las assersion

--- cypress/e2e/Tests/Interactions/GX3-3014-FormPractice.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-3014-FormPractice.cy.js
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { formPage } from '../../../support/pages/GX3-3014-FormsPractice.Page';
+import { faker } from '@faker-js/faker';
 
 describe('GX3-3014 | ToolsQA | Forms | Practice Form' , () => {
 
@@ -8,8 +10,37 @@ describe('GX3-3014 | ToolsQA | Forms | Practice Form' , () => {
 	});
 
 	it('3015 | TC1 | Validar poder completar el formulario para el estudiante con valores validos', () => {
-		formPage.enterData();
-		// formPage.get.document().should('contain', 'Thanks for submitting the form');
+		const randomName = faker.person.firstName('female' | 'male');
+		const randomLastName = faker.person.lastName();
+		const randomEmail = faker.internet.email();
+		const randomGender = Cypress._.random(0, 2);
+		const randomNumber = faker.string.numeric(10);
+		const randomAddress = faker.location.streetAddress();
+		const randomHobbies = Cypress._.random(0, 2);
+		const randomState = Cypress._.random(0, 3);
+		const randomCity = Cypress._.random();
+		const randomSubject = faker.string.alpha({ count: 1, casing: 'upper', exclude: ['Q', 'Ñ', 'K', 'J', 'F', 'X', 'W', 'Z'] });
+
+		formPage.typeInputName(randomName);
+		formPage.typeInputLast(randomLastName);
+		formPage.typeInputEmail(randomEmail);
+		formPage.selectInputGender(randomGender);
+		formPage.typeInputNumber(randomNumber);
+		formPage.selectInputDate();
+		formPage.typeAddAddress(randomAddress);
+		formPage.typeInputHobbies(randomHobbies);
+		formPage.selectUpPicture();
+		formPage.randomState(randomState);
+		formPage.randomCity(randomCity);
+		formPage.selectInputSubject(randomSubject);
+
+		formPage.btnSubmit();
+
+		formPage.get.document().should('contain.text', 'Thanks for submitting the form');
+		formPage.get.resultFirtsName().should('contain.text', randomName);
+		formPage.get.resultLastName().should('contain.text', randomLastName);
+		formPage.get.resultEmail().should('contain.text', randomEmail);
+
 	});
 
 });
--- cypress/support/pages/GX3-3014-FormsPractice.Page.js
+++ cypress/support/pages/GX3-3014-FormsPractice.Page.js
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable no-trailing-spaces */
-import { faker } from '@faker-js/faker';
-
 class Form {
 	get = {
+
 		inputName: () => cy.get('#firstName'),
 		inputLast: () => cy.get('#lastName'),
 		inputEmail: () => cy.get('#userEmail'),
@@ -13,63 +13,77 @@ class Form {
 		selectSubject: () => cy.get('[id*="react-select-2-option"]'),
 		inputHobbies: () => cy.get('[for*="hobbies-checkbox"]'),
 		upPicture: () => cy.get('#uploadPicture'),
-		addText: () => cy.get('#currentAddress'),
+		addAddress: () => cy.get('#currentAddress'),
 		state: () => cy.get('#state'),
-		selectState: () => cy.get('[id*="react-select-3-option"]'),	
+		selectState: () => cy.get('[id*="react-select-3-option"]'),
 		city: () => cy.get('#city'),
 		selectCity: () => cy.get('[id*="react-select-4-option"]'),
 		submit: () => cy.get('#submit'),
-		document: () => cy.get('.modal-content'),
+		document: () => cy.get('.modal-title'),
+		
+	    //Result
+		resultFirtsName: () => cy.get('tr').eq(1),
+		resultLastName: () => cy.get('tr').eq(1),
+		resultEmail: () => cy.get('tr').eq(2),
+		resultGender: () => cy.get('tr').eq(3),
 	};
 
-	enterData() {
-		const randomName = faker.person.firstName('female' | 'male');
-		const randomLastName = faker.person.lastName();
-		const newEmail = faker.internet.email();
-		const newNumber = faker.string.numeric(10);
-		const rGender = Cypress._.random(0, 2);
-		const rHobbies = Cypress._.random(0, 2);
-
+	typeInputName(randomName) {
 		this.get.inputName().type(randomName);
+	}
+	typeInputLast(randomLastName) {
 		this.get.inputLast().type(randomLastName);
-		this.get.inputEmail().type(newEmail);
-		this.get.inputGender().eq(rGender).click();
-		this.get.inputNumber().type(newNumber);
-		this.get.inputDate().invoke('val', '12 Feb 1995');
-		this.selectInputSubject();
-		this.get.inputHobbies().eq(rHobbies).click();
+	}
+	typeInputEmail(randomEmail) {
+		this.get.inputEmail().type(randomEmail);
+	}
+	selectInputGender(randomGender) {
+		this.get.inputGender().eq(randomGender).click();
+	}
+	typeInputNumber(randomNumber) {
+		this.get.inputNumber().type(randomNumber);
+	}
+	typeAddAddress(randomAddress) {
+		this.get.addAddress().type(randomAddress);
+	}
+
+	typeInputHobbies(randomHobbies) {
+		this.get.inputHobbies().eq(randomHobbies).click();
+
+	}
+	selectUpPicture() {
 		this.get.upPicture().selectFile('cypress/fixtures/images/upexgalaxy.gif');
-		this.get.addText().type('I live in my house');
-		this.rState();
-		this.rCity();
+
+	}
+
+	selectInputSubject(randomSubject) {		
+		this.get.inputSubject().type(randomSubject);
+		this.get.selectSubject().then(i => {
+			const r = Cypress._.random(0, i.length - 1);
+			cy.wrap(i).eq(r).click();
+		});
+		
+	}
+
+	selectInputDate() {
+		this.get.inputDate().invoke('val', '12 Feb 1995');
+
+	}
+
+	randomState(randomState) {
+		this.get.state().click();
+		this.get.selectState().eq(randomState).click();
+	}
+
+	randomCity(randomCity) {
+		this.get.city().click();
+		this.get.selectCity().eq(randomCity).click();
+	}
+
+	btnSubmit() {
 		this.get.submit().click();
 
 	}
-
-	selectInputSubject() {
-
-		const randomSubject = faker.string.alpha({ count: 1, casing: 'upper', exclude: ['Q','Ñ','K','J','F','X','W','Z'] });
-		if (typeof randomSubject === 'string') {
-			this.get.inputSubject().type(randomSubject);
-			this.get.selectSubject().then(i => {
-				const r = Cypress._.random(0, i.length - 1);
-				cy.wrap(i).eq(r).click();
-			});
-		}
-	}
-	
-	rState() {
-		const rState = Cypress._.random(0,3);
-		this.get.state().click();
-		this.get.selectState().eq(rState).click();
-	}
-
-	rCity() {
-		const rCity = Cypress._.random();
-		this.get.city().click();
-		this.get.selectCity().eq(rCity).click();
-	}
-
 }
 
-export const formPage = new Form;
+export const formPage = new Form();
