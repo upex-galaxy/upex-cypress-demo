bb183fceafe393041fb7cd808c16804ed1372eaf
Erika <tolosa.erika503@gmail.com>
2024-04-16 15:17:16 +0200

    GX3-2815 Se actualiza para borrar rama

--- cypress/e2e/Tests/Widgets/GX3-2815-DatePicker.cy.js
+++
@@ -1,8 +0,0 @@
-describe ('Toolsqa-widgets-date-picker', () => {
-	beforeEach (() => {
-		cy.visit ('https://demoqa.com/date-picker');
-	});
-});
-it ('GX3-2816 | TC1: Validar poder seleccionar fecha actual en formato correcto'), (
-	
-)
