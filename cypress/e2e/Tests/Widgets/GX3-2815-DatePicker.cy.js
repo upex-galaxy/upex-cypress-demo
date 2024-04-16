536f4ba4217a2db747bcdbd5706d24c080e7f7ac
Erika <tolosa.erika503@gmail.com>
2024-04-16 15:12:24 +0200

    GX3-2815 Se cancela rama

---
+++ cypress/e2e/Tests/Widgets/GX3-2815-DatePicker.cy.js
@@ -0,0 +1,8 @@
+describe ('Toolsqa-widgets-date-picker', () => {
+	beforeEach (() => {
+		cy.visit ('https://demoqa.com/date-picker');
+	});
+});
+it ('GX3-2816 | TC1: Validar poder seleccionar fecha actual en formato correcto'), (
+	
+)
