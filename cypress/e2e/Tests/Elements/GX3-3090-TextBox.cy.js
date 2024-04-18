21ae42fa55227f77fb692338dc3c615df01ee9cb
Dana3444 <dianahigueras@hotmail.com>
2024-04-18 15:58:29 +0200

    GX3-3090 Asigno mas fixtures. TC01

--- cypress/e2e/Tests/Elements/GX3-3090-TextBox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3090-TextBox.cy.js
@@ -1,17 +1,49 @@
-describe('Account TextBox'), () =>
+describe('Account TextBox', () =>
 {
 	beforeEach('Visitar la página de registro',() =>
 	{
 		cy.visit('https://demoqa.com/text-box');
-		cy.contatins('Test Box').should('be.visible');
+		cy.contains('Test Box').should('be.visible');
 		cy.url().should('contain','text-box');
-		cy.fixture('data/Elements/GX3-3090-TextBox').then((the)=>
-		cy.contains(the.{
-
-		};
-
 	}
 	);
-};
-cy.contains('Log in');
-cy.fixture('');
+});
+it('TC01: Registrarse exitosamente',() =>
+{
+	cy.fixture('data/Elements/GX3-3090-TextBox').then((the) =>
+	{
+		cy.get(the.input.fullName)
+			.type(the.data.fullNamevalid);
+		cy.get(the.input.email)
+			.type(the.data.emailvalid);
+		cy.get(the.input.currentAddress)
+			.type(the.data.caddressvalid);
+		cy.get(the.input.permanentAddress)
+			.type(the.data.peraddressvalid);
+		cy.get(the.SubmitButton).click();
+	});
+});
+it('TC02.No registrarse con email inválido. sin caracter delante @',() =>
+{
+	cy.fixture('data/Elements/GX3-3090-TextBox'),() =>
+	{
+		cy.get(the.input.fullName)
+			.type(the.data.fullNamevalid);
+		cy.get(the.input.email)
+			.type(the.data.emailvalid);
+		cy.get(the.input.currentAddress)
+			.type(the.data.caddressvalid);
+		cy.get(the.input.permanentAddress)
+			.type(the.data.peraddressvalid);
+		cy.get(the.SubmitButton).click();
+	};
+}
+);
+it('TC03.No registrarse con email inválido. sin caracter detrás @');
+it('TC04.No registrarse con email inválido. sin @');
+it('TC05.No registrarse con email inválido. sin punto detras @');
+it('TC06.No registrarse con email inválido. sin caracter detrás del punto');
+it('TC07.Registrarse con email vacío');
+it('TC08.Registrarse con Full Name vacío');
+it('TC09.Registrarse con Current Address vacío');
+it('TC10.Registrarse con Permanent Address vacío');
