d5b66362b94184af8ffda2b2fc727caf20b0de83
Dana3444 <dianahigueras@hotmail.com>
2024-04-25 19:00:54 +0200

    GX3-3192 Se realiza caso de prueba 1 y 2. Modifico Page y data de la fixture

--- cypress/e2e/Tests/Interactions/GX3-3192-selectable.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-3192-selectable.cy.js
@@ -1,52 +1,49 @@
-//import {selectablePage} from '../../../support/pages/GX3-3192-Selectable.Page';
-//const  {selectablePage} '../../../support/pages/GX3-3192-Selectable.Page.js';
+import { selectablePage } from '../../../support/pages/GX3-3192-Selectable.Page';
+import data from '.././../../fixtures/data/Interactions/GX3-3192-selectable.json';
+//const {selectable} = Cypress.env('endpoint');
 
 describe('⚡️ToolsQA | Interactions | Selectable', () => {
 
 	beforeEach('Precondición:', () => {
 		cy.visit('/selectable');
 		cy.url().should('contain', 'selectable');
+		//cy.url().should('contain',selectable);
 	});
 
-	it('TC01. Validar que la pestaña  <List> esté seleccionada por defecto y sus valores sean correctos. ',() => {
-		//cy.get('#demo-tab-list').should('have.css','aria-selected','true');
-		cy.get('#demo-tab-list').should('have.text','List');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(0);
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(0).should('have.text','Cras justo odio');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(1).should('have.text','Dapibus ac facilisis in');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(2).should('have.text','Morbi leo risus');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(3).should('have.text','Porta ac consectetur ac');
+	it('TC01. Validar que la pestaña  <List> esté seleccionada por defecto y sus listas NO están seleccionados. ',() => {
+		selectablePage.get.tabList;
+		//selectablePage.get.tabList.should('have.text','List');
 
-		cy.get('#verticalListContainer li');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(3).should('have.css','background-color','#f8f9fa?');
+		cy.get(data.elements.ListTab).should('have.class','active');
+		cy.get(data.elements.VerticalListDiv).should('not.have.class','active');
+
+		cy.get(data.elements.VerticalListDiv).should('contain','Cras justo odio');
+		cy.get(data.elements.VerticalListDiv).should('contain','Dapibus ac facilisis in');
+		cy.get(data.elements.VerticalListDiv).should('contain','Morbi leo risus');
+		cy.get(data.elements.VerticalListDiv).should('contain','Porta ac consectetur ac');
+
 	});
 
-	it('TC02. TC02: Validar que un o varios elementos de la pestaña  <List>  puedan ser seleccionados correctamente.',() => {
-		//const RandomList = Math.floor(Math.random() * 3);
-		//cy.get('#demo-tab-list').should('have.css','aria-selected','true');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(0).click();
-		//cy.get('.mt-2.list-group-item.list-group-item-action').eq(0).should('have.css','background-color','#f8f9fa?');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(1).should('have.text','Dapibus ac facilisis in');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(2).should('have.text','Morbi leo risus');
-		cy.get('.mt-2.list-group-item.list-group-item-action').eq(3).should('have.css','Background-color','#f8f9fa?');
+	it.only('TC02: Validar que un o varios elementos de la pestaña  <List>  puedan ser seleccionados y desseleccionados correctamente.',() => {
+		const RandomList = Math.floor(Math.random() * 3);
 
-		cy.get('.mt-2.list-group-item.active.list-group-item-action').eq(0).click();
+		selectablePage.clickOnListedItems();
+		selectablePage.clickOnGridTab();
+		//cy.get('.mt-2.list-group-item.list-group-item-action').eq(RandomList).click();
+
 	});
 
-	it.only('TC03. Validar poder seleccionar la pestaña  <Grid>  y sus valores sean correctos.',() => {
+	it('TC03. Validar poder seleccionar la pestaña  <Grid>  y sus valores sean correctos.',() => {
 		cy.get('#demo-tab-grid').click();
 		//cy.get('#gridContainer');
 		cy.get('#gridContainer').should('have.text','OneTwoThreeFourFiveSixSevenEightNine');
-
-		cy.get('.list-group-item.list-group-item-action').eq(1).should('have.css','Background-color','#f8f9fa?');
-		cy.get('.list-group-item.list-group-item-action').eq(1).should('have.css','color','#495057');
 
 		cy.get('.list-group-item.list-group-item-action').eq(1);
 		// cy.get('.list-group-item.list-group-item-action').eq(0).click();
 		// cy.get('.list-group-item.active.list-group-item-action').eq(0).should('exist');
 	});
 
-	it.only('TC04. Validar poder seleccionar la pestaña  <Grid>  y sus valores sean correctos.',() => {
+	it('TC04. Validar poder seleccionar la pestaña  <Grid>  y sus valores sean correctos.',() => {
 		cy.get('#demo-tab-grid').click();
 		//cy.get('#gridContainer');
 		cy.get('#gridContainer').should('have.text','OneTwoThreeFourFiveSixSevenEightNine');
--- cypress/support/pages/GX3-3192-Selectable.Page.js
+++ cypress/support/pages/GX3-3192-Selectable.Page.js
@@ -5,7 +5,17 @@ class SelectablePage {
 		verticalListContainer: () => cy.get('#verticalListContainer li'),
 		ListedItemSelected: () => cy.get('.mt-2.list-group-item.active.list-group-item-action')
 	};
-	clickOnLIstTab(value) {
+
+	clickOnListTab() {
+		this.get.tabList().click();
+	}
+	clickOnListedItems(value) {
+		this.get.verticalListContainer().eq(value).click();
+	}
+	clickOnGridTab() {
+		this.get.tabGrid().click();
+	}
+	clickOnGridItems(value) {
 		this.get.verticalListContainer().eq(value).click();
 	}
 }
