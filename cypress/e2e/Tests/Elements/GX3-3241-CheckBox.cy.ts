0e3866d8ece460be7739eed962508dc10525ced2
Marianela Serodino Runco <serodinonela@gmail.com>
2024-04-26 12:54:28 +0100

    GX3-3241 Added collapseAll method and modified TC1 to check functionality of expand and collapse all

--- cypress/e2e/Tests/Elements/GX3-3241-CheckBox.cy.ts
+++ cypress/e2e/Tests/Elements/GX3-3241-CheckBox.cy.ts
@@ -5,7 +5,7 @@ describe('GX3-3241 | ToolsQA | Elements | Checkbox', () => {
 		cy.visit('https://demoqa.com/checkbox');
 		cy.url().should('contain', 'checkbox');
 	});
-	it('GX3-3242 | TC1 | Verify that clicking the "Expand All" button expands all folders', () => {
+	it('GX3-3242 | TC1 | Verify that the user can expand all folders and then collapse all except "Home"', () => {
 		checkbox.clickExpandAll();
 		checkbox.fetchAndStoreFolderNames();
 		cy.get<string[]>('@folderNames').then((folderNames: string[]) => {
@@ -13,5 +13,13 @@ describe('GX3-3241 | ToolsQA | Elements | Checkbox', () => {
 				cy.contains('.rct-title', name).should('be.visible');
 			});
 		});
+		checkbox.clickCollapseAll();
+		checkbox.fetchAndStoreFolderNames();
+		cy.get<string[]>('@folderNames').then((folderNames: string[]) => {
+			expect(folderNames.length).to.eq(1);
+			expect(folderNames[0]).to.eq('Home');
+			cy.contains('.rct-title', 'Home').should('be.visible');
+		});
 	});
+
 });
--- cypress/support/pages/GX3-3241-Checkbox.page.ts
+++ cypress/support/pages/GX3-3241-Checkbox.page.ts
@@ -1,13 +1,18 @@
 class Checkbox {
 	get: {
         expandAll: () => Cypress.Chainable<JQuery<HTMLElement>>;
+		collapseAll: () => Cypress.Chainable<JQuery<HTMLElement>>;
         folders: () => Cypress.Chainable<JQuery<HTMLElement>>;
     } = {
 			expandAll: () => cy.get('[aria-label="Expand all"]'),
+			collapseAll: () => cy.get('[aria-label="Collapse all"]'),
 			folders: () => cy.get('.rct-title')
 		};
-	 clickExpandAll(): void {
+	clickExpandAll(): void {
 		this.get.expandAll().click();
+	}
+	clickCollapseAll(): void {
+		this.get.collapseAll().click();
 	}
 	fetchAndStoreFolderNames(): void {
 		this.get.folders().then(folders => {
