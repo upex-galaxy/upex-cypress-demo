75ffe5968fc52fb8cbab78bd1dbefa4ce3d9b492
Dana3444 <dianahigueras@hotmail.com>
2024-05-03 20:22:43 +0200

    GX3-3092 Refino TC02

--- cypress/e2e/Tests/Elements/GX3-3290-UploadDownload.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3290-UploadDownload.cy.js
@@ -1,48 +1,29 @@
 import { uploadDownload } from'../../../support/pages/GX3-3290-UploadDownload.Page';
 import data from'../../../fixtures/data/Elements/GX3-3290-UploadDownload.json';
 
-describe('GX3-3290 | ToolsQA | Elements | Upload and Download',() => {
+describe('ToolsQA | Elements | Upload and Download', () => {
 	beforeEach('Precondition',() => {
-		cy.visit('/upload-download');
-		cy.url().should('contain','upload and download');
-
-	});
-
-	it.only('TC01: Validate download files successfully',() =>
-	{
-		uploadDownload.get.downloadButton().should('have.text','download');
-		uploadDownload.clickDownload();
-
-		// cy.verifyDownload('small-file-10MB.zip');
-		//cy.verifyDownload('sampleFile.jpeg');
-		cy.verifyDownload('sampleFile.jpeg');
-	});
-	it.only('TC02: Validate upload files successfully',() =>
-	{
-		uploadDownload.get.downloadButton().should('have.text','download');
-		uploadDownload.clickDownload();
-
-		// cy.verifyDownload('small-file-10MB.zip');
-		//cy.verifyDownload('sampleFile.jpeg');
-		cy.verifyDownload('sampleFile.jpeg');
-	});
-
-	beforeEach('', () => {
 		cy.visit('/upload-download');
 		cy.url().should('contain','upload-download');
 	});
 
-	it('TC1 | Validar que se descargue de archivo', () => {
+	it('TC01: Validate download files successfully',() =>
+	{
 		uploadDownload.get.downloadButton().should('have.text','Download');
 		uploadDownload.clickDownload();
+
 		cy.readFile('cypress/downloads/sampleFile.jpeg', 'binary').should((buffer) => {
 			expect(buffer.length).to.be.gt(1000);
 		});
+
 	});
-	it('TC2 | Validar que se suba una imagen', () => {
-		uploadDownload.get.uploadButton().should('have.text','Seleccionar archivo');
-		uploadDownload.
-			uploadDownload.get.uploadPathElement().should('contain','upex');
+	it.only('TC02: Validate upload files successfully',() =>
+	{
+		//uploadDownload.get.uploadButton().should('have.text','Seleccionar archivo');
+		uploadDownload.clickUpload();
+		//añadir archivo
+		uploadDownload.PathUpload();
+		uploadDownload.get.uploadPathElement().should('contain','upex');
 	});
 
 });
