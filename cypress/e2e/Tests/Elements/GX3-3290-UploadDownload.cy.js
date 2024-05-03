672ecb6da080e28c9506e261297ba3d59aefe09e
Dana3444 <dianahigueras@hotmail.com>
2024-05-03 17:22:15 +0200

    GX3-3290 TC01

--- cypress/e2e/Tests/Elements/GX3-3290-UploadDownload.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3290-UploadDownload.cy.js
@@ -1,17 +1,21 @@
-import {uploadDownload} from'../../../support/pages/GX3-3290-UploadDownload.Page';
-import data from'../../../fixtures/data/Elements/GX3-3290-UploadDownload.json;
+import { uploadDownload } from'../../../support/pages/GX3-3290-UploadDownload.Page';
+import data from'../../../fixtures/data/Elements/GX3-3290-UploadDownload.json';
 
-describe('GX3-3290 | ToolsQA | Elements | Upload and Download',()=>{
-	beforeEach('Precondition',() => 
+describe('GX3-3290 | ToolsQA | Elements | Upload and Download',() => {
+	beforeEach('Precondition',() =>
 	{
 		cy.visit('/upload-download');
 		cy.url().should('contain','upload and download');
-		
+
 	});
-	
-	it('TC01: Validate upload files successfully',()=>
+
+	it.only('TC01: Validate download files successfully',() =>
 	{
-uploadDownload.get.downloadButton();
-uploadDownload.clickDownload();
+		uploadDownload.get.downloadButton().should('have.text','download');
+		uploadDownload.clickDownload();
+
+		// cy.verifyDownload('small-file-10MB.zip');
+		//cy.verifyDownload('sampleFile.jpeg');
+		cy.verifyDownload('sampleFile.jpeg');
 	});
 });
