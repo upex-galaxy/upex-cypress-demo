52d0cc6ebc17d27436a14b072d2dd4ea9eed8bc3
Dana3444 <dianahigueras@hotmail.com>
2024-05-10 14:25:36 +0200

    GX3-3365 Modifico el Test

--- cypress/e2e/Tests/Widgets/GX3-3365-DropdowSelect.cy.js
+++ cypress/e2e/Tests/Widgets/GX3-3365-DropdowSelect.cy.js
@@ -1,4 +1,4 @@
-import { dropdown } from '../../../support/pages/GX3-431-DropdowSelect.Page';
+import { dropdown } from '../../../support/pages/GX3-3365-DropdowSelect.Page';
 import data from'../../../fixtures/data/Widgets/GX3-431-DropdowSelect.json';
 
 describe('GX3-431 ToolsQA | Widgets | Dropdown - Select Menu',() => {
