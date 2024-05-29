a96bb4b5d3d63c096dfdb82e944651012e9c9838
kenlyr <kenlyrojas96@gmail.com>
2024-05-28 21:14:39 -0500

    GX3-3431 Creación de Precondition

---
+++ cypress/e2e/Tests/Elements/GX3-3431-RadioButtons.cy.js
@@ -0,0 +1,19 @@
+describe ('GX3-3432 | TS: ToolsQA | Elements | Radio Buttons', () =>
+{
+	beforeEach('Usuario debe estar situado en la página de Radio Button', () =>
+	{
+		cy.visit('https://demoqa.com/radio-button')
+	})
+it('GX3-3432 | TC1: Validar poder seleccionar radio button Yes', () => 
+{
+	cy.get
+})
+it('GX3-3432',()=>
+{
+
+})
+it('',()=>
+{
+	
+})
+})
