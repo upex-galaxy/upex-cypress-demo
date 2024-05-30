5fa8f1a44c93d3718168a5956fa2b2e5d29ba78d
kenlyr <kenlyrojas96@gmail.com>
2024-05-30 13:43:02 -0500

    Diseño de casos de pruebas finalizado

--- cypress/e2e/Tests/Elements/GX3-3431-RadioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3431-RadioButtons.cy.js
@@ -2,18 +2,25 @@ describe ('GX3-3432 | TS: ToolsQA | Elements | Radio Buttons', () =>
 {
 	beforeEach('Usuario debe estar situado en la página de Radio Button', () =>
 	{
-		cy.visit('https://demoqa.com/radio-button')
-	})
-it('GX3-3432 | TC1: Validar poder seleccionar radio button Yes', () => 
-{
-	cy.get
-})
-it('GX3-3432',()=>
-{
-
-})
-it('',()=>
-{
-	
-})
-})
+		cy.visit('https://demoqa.com/radio-button');
+	});
+	it('GX3-3432 | TC1: Validar poder seleccionar radio button Yes', () =>
+	{
+		cy.get('input#yesRadio').click({ force: true }).should('be.checked');//El click debió ser forzado debido a la obstrucción de otro elemnento
+		cy.get('label[for=\'yesRadio\']').should('contain', 'Yes');
+	});
+	it('GX3-3432 | TC2: Validar poder seleccionar radio button Impressive',() =>
+	{
+		cy.get('input#impressiveRadio').click({ force: true }).should('be.checked');//El click debió ser forzado debido a la obstrucción de otro elemnento
+		cy.get('label[for=\'impressiveRadio\']').should('contain', 'Impressive');
+	});
+	it('GX3-3432 |  TC3: Validar que esté deshabilitado radio button No', () =>
+	{
+		cy.get('input#noRadio').should('be.disabled');
+		cy.get('label[for=\'noRadio\']').should('contain', 'No');
+	});
+});
+//Se importa la funcion
+import { removeLogs } from '@helper/RemoveLogs';
+//Se ejecuta la funcion para evitar los errores
+removeLogs();
