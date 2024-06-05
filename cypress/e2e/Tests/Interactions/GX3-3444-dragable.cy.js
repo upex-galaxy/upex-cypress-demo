08aba14d58db2211236febd2c1cb38633f1aeeb5
LucasSebastianCasco <lucassebaacasco@gmail.com>
2024-06-05 18:26:59 -0300

    GX3-3444 Se finaliza con todos los test

--- cypress/e2e/Tests/Interactions/GX3-3444-dragable.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-3444-dragable.cy.js
@@ -1,21 +1,20 @@
 import { pageDragable } from "../../../support/pages/GX3-3444-Dragable.page"
 
-describe('',()=>{
+describe('GX3-3444 | ToolsQA | Interactions | Dragabble (Drag and Drop)',()=>{
 	beforeEach('',()=>{
 		cy.visit('https://demoqa.com/dragabble')
 		cy.url().should('contain','dragabble')
 	})
 
-	it('',()=>{
+	it('GX3443 | TC01 Validate move dragme box',()=>{
 		const deltaX = Cypress._.random(0, 200)
 		const deltaY = Cypress._.random(-20, 100)
 		pageDragable.moveDragMe(deltaX, deltaY)
-		
 		pageDragable.get.dragMeBtn().should('have.css', 'left' , `${deltaX}px`)
 		pageDragable.get.dragMeBtn().should('have.css', 'top' , `${deltaY}px`)
 	})
 
-	it('Axis Restricted only x',()=>{
+	it('GX3443 | TC02 validate move Axis Restricted only x',()=>{
 		const deltaX = Cypress._.random(-30 ,150)
 		const deltaY= 0
 		pageDragable.clickAxisTab();
@@ -24,7 +23,7 @@ describe('',()=>{
 		
 	})
 
-	it('Axis Restricted only y',()=>{
+	it('GX3443 | TC03 Validate move Axis Restricted only y',()=>{
 		const deltaY = Cypress._.random(-30 ,150)
 		const deltaX=0
 		pageDragable.clickAxisTab();
@@ -32,12 +31,11 @@ describe('',()=>{
 		pageDragable.get.onlyYBtn().should('have.css','top', `${deltaY}px`)
 	})
 
-	it('Container Restricted move within box',()=>{
+	it('GX3443 | TC04 Validate move Container Restricted move within box',()=>{
 		const deltaX=Cypress._.random(0,403)
 		const deltaY=Cypress._.random(0,105)
 		pageDragable.clickOncontainerTab()
 		pageDragable.moveWBox(deltaX, deltaY)
-		//tengo que tomar el valor de x pero sin las comillas solo el numero, guardarlo y compararlo con deltax
 		pageDragable.get.withinBox().then(($el)=>{
 			const newLeft=$el.css('left').replace(/'/g, '')
 			const newTop=$el.css('top').replace(/'/g, '')
@@ -46,18 +44,12 @@ describe('',()=>{
 		})
 	})
 
-	it.only('',()=>{
+	it('GX3443 | TC05 Validate move contained within my parent',()=>{
 		const deltaX = Cypress._.random(0,13)
 		const deltaY= Cypress._.random(-1,85)
-		console.log(deltaX,deltaY)
 		pageDragable.clickOncontainerTab()
-		pageDragable.moveParent()
-		pageDragable.get.withinParent().then(($el)=>{
-			const valueLeft = $el.css('left')
-			console.log(valueLeft) 
-		})
-		//pageDragable.get.withinParent().should('have.css','left', `${deltaX}px`)
-		//pageDragable.get.withinParent().should('have.css','top', `${deltaY}px`)
-		
+		pageDragable.moveParent(deltaX, deltaY)
+		pageDragable.get.withinParent().should('have.css','left', `${deltaX}px`)
+		pageDragable.get.withinParent().should('have.css','top', `${deltaY}px`)	
 	})
 })
