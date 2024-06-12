1a8aaab8618d2077b138e1ffd3835a2c2edbc4ec
MMargoHC <margoth.honorioc@gmail.com>
2024-06-12 16:37:23 -0400

    Todos los casos de pruebas han sido realizados

--- cypress/e2e/Tests/Elements/GX3-3651-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3651-Buttons.cy.js
@@ -4,9 +4,22 @@ describe('GX3-3651|ToolsQA | Elements | Buttons', () => {
 		cy.url().should('include', 'buttons');
 	});
 
-	it('3652 | TC1: Validar hacer click en el button “Double click”', () => {
+	it('3652 | TC1: Validar hacer double click en el button “Double click Me” y se muestre el mensaje del button', () => {
 		cy.get('#doubleClickBtn').dblclick();
 		cy.get('p#doubleClickMessage').should('have.text', 'You have done a double click');
 	});
-	
+
+	it('3652 | TC2: Validar hacer right click en el button “Right Click Me" y se muestre el mensaje del button', () => {
+		cy.get('#rightClickBtn').rightclick();
+		cy.get('#rightClickMessage').should('have.text', 'You have done a right click');
+	});
+	it('3652 | TC3: Validar hacer click en el button “Click Me"y se muestre el mensaje del button', () => {
+		cy.get('button').eq(3).click();
+		cy.get('#dynamicClickMessage').should('have.text', 'You have done a dynamic click');
+	});
+
+	it('3652 | TC4: Validar hacer click en el button “Double click Me” y NO se muestre el mensaje del button', () => {
+		cy.get('#doubleClickBtn').click();
+		cy.get('#doubleClickMessage').should('not.exist');
+	});
 });
