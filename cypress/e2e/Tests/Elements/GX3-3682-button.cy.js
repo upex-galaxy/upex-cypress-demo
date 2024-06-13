1175ae0189fb5c9694d0676485a36331c8b8711d
PabloCz9 <pablotest098@gmail.com>
2024-06-13 16:55:26 -0300

    TC1 done

--- cypress/e2e/Tests/Elements/GX3-3682-button.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3682-button.cy.js
@@ -2,9 +2,14 @@ describe('⚡️ToolsQA | Elements | Buttons', () => {
 
 	beforeEach('PRC: Estar situado en la pagina de DemoQa buttons', () => {
 		cy.visit('https://demoqa.com/buttons');
+		cy.url().should('contains','/buttons');
 	});
 
 	it('3683 | TC1: Validar hacer click en el boton “Double Click Me“', () => {
-		expect(1).to.equal(1);
+		cy.get('[id="doubleClickBtn"]').dblclick();
+		// usando asercion (expect)
+		cy.get('#doubleClickMessage').then(($elementButton) => {
+			expect($elementButton).to.have.text('You have done a double click');
+		});
 	});
 });
