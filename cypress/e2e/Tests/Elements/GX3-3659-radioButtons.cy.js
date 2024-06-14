9bd7ca3b95e6cde379e4cb1b2e60c5752e870759
Marlovx <marlovxesquivel@gmail.com>
2024-06-14 12:57:01 -0600

    GX3-3659 Se modificó TC01 y TC02 agregando un force

--- cypress/e2e/Tests/Elements/GX3-3659-radioButtons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3659-radioButtons.cy.js
@@ -5,11 +5,11 @@ describe ('ToolsQA | Elements | Radio Buttons',() => {
 		cy.viewport(1000, 660);
 	});
 	it('3660 TC01 | Validar Que funcione el Radio Button "Yes" correctamente', () => {
-		cy.get('#yesRadio').click();
+		cy.get('#yesRadio').click({ force: true });
 		cy.get('.text-success').should('have.text','Yes');
 	});
 	it('3660 TC02 | Validar Que funcione el Radio Button "Impressive" correctamente', () => {
-		cy.get('#impressiveRadio').click();
+		cy.get('#impressiveRadio').click({ force: true });
 		cy.get('.text-success').should('have.text','Impressive');
 	});
 	it('3660 TC03 | Validar que el Radio Button "No" esté disabled', () => {
