3cb51ddb32a4cd2a5855b26dda21c5cb793c53c3
MMargoHC <margoth.honorioc@gmail.com>
2024-06-20 19:16:07 -0400

    GX3-3810: Diseño de los TC3 y TC4

--- cypress/e2e/Tests/Elements/GX3-3810-dynamicProperties.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3810-dynamicProperties.cy.js
@@ -7,10 +7,19 @@ describe('GX3-3810 |ToolsQA | Elements | Dynamic Properties',()=>{
 		cy.get('.col-12.mt-4.col-md-6 p').should('have.text','This text has random Id');
 
 	})
-	it('3810 | TC2: Validar que el elemento "Will enable 5 secconds" tenga un "Dynamic ID" en el dom y se habilite después de 5 seg.',()=>{
-		cy.get('#enableAfter',{timeout: 10000}).should('be.visible')
+	it('3810 | TC2: Validar que el elemento "Will enable 5 secconds" debe afirmarse antes y despues de que se habilite',()=>{
+		//cy.get('#enableAfter').should ('have.class','mt-4 btn')
+		cy.get('#enableAfter',{timeout: 5000}).should('be.visible')
 		//cy.wait(8000)
 		//cy.get('#enableAfter').should('be.visible')
 		
+	})
+	it('3810 | TC3: Validar que el elemento "color change" debe afirmarse antes y despues de cambiar de color',()=>{
+		cy.get('#colorChange').should ('have.class','btn-primary')
+		cy.get('#colorChange',{timeout: 5000}).should ('have.class','text-danger')
+	})
+	it('3810 | TC4: Validar que el elemento "Visible after 5 seconds" debe afirmarse antes y despues de que sea visible ',()=>{
+		//cy.get('#visibleAfter').should ('have.class','mt-4 btn btn-primary')
+		cy.get('#visibleAfter',{timeout: 5000}).should ('be.visible')
 	})
 });
