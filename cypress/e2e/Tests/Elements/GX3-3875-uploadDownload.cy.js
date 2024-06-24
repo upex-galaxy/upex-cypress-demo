e1b2b84e8f796c7e4ca4ce80b237672d2a4e2b49
MMargoHC <margoth.honorioc@gmail.com>
2024-06-24 17:11:30 -0400

    GX3-3875: Agregando validaciones porque si no me dara pablo un chanclazo

--- cypress/e2e/Tests/Elements/GX3-3875-uploadDownload.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3875-uploadDownload.cy.js
@@ -6,9 +6,13 @@ describe('GX-3 3875 | ToolsQA | Elements | Upload and Download', () => {
 		});
 	it('3876 | TC1: Validar descargar un archivo cuando se hace click sobre el button "Download"', () => {
 		uploadDownloadPage.clickDownloadButton();
+		cy.readFile('cypress/downloads/sampleFile.jpeg').should('have.length.greaterThan',3000)
+		cy.readFile('cypress/downloads/sampleFile.jpeg').should('exist')
+
 	});
 
 	it('3876 | TC2: Validar elejir un archivo cuando se hace click sobre el button "Choose file"', () => {
 		uploadDownloadPage.selectFileInput();
+		cy.get('#uploadedFilePath').should('exist')
 	});
 });
