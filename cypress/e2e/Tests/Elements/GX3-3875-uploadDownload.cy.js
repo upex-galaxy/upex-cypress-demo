fdfd6a6eebf0aefdefa4c937db45954fbb259dc4
MMargoHC <margoth.honorioc@gmail.com>
2024-06-23 23:45:21 -0400

    GX3-3875: Se agrego el TC2

--- cypress/e2e/Tests/Elements/GX3-3875-uploadDownload.cy.js
+++ cypress/e2e/Tests/Elements/GX3-3875-uploadDownload.cy.js
@@ -1,13 +1,14 @@
 import { uploadDownloadPage } from '@pages/GX3-3875-uploadDownload.page.js';
 describe('GX-3 3875 | ToolsQA | Elements | Upload and Download', () => {
-	beforeEach('PRC:',() => {
+	beforeEach('PRC: El usuario debe estar situado en la pagina de Demo QA',() => {
 			cy.visit('https://demoqa.com/upload-download');
+			cy.url().should('contain','upload-download')
 		});
-	it('1', () => {
+	it('3876 | TC1: Validar descargar un archivo cuando se hace click sobre el button "Download"', () => {
 		uploadDownloadPage.clickDownloadButton();
 	});
 
-	it('2', () => {
-		cy.get('#uploadFile').selectFile('cypress/fixtures/images/upexlogo.png')
+	it('3876 | TC2: Validar elejir un archivo cuando se hace click sobre el button "Choose file"', () => {
+		uploadDownloadPage.selectFileInput();
 	});
 });
--- cypress/support/pages/GX3-3875-uploadDownload.page.js
+++ cypress/support/pages/GX3-3875-uploadDownload.page.js
@@ -1,11 +1,18 @@
 class UploadDownload {
 	get = {
-		downloadButton: ()=> cy.get ('#downloadButton')
+		downloadButton: ()=> cy.get ('#downloadButton'),
+		uploadFile: ()=> cy.get('#uploadFile')
 	}
 	
 	clickDownloadButton (){
 		this.get.downloadButton().click()
 	}
+
+	selectFileInput (){
+		this.get.uploadFile().selectFile('cypress/fixtures/images/upexlogo.png')
+	}
 }
+
+//class DownloadFile
 
 export const uploadDownloadPage = new UploadDownload();
