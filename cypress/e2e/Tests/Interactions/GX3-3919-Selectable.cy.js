e9b21482808000592f470a3e5b4c0dec7c80bd1c
MMargoHC <margoth.honorioc@gmail.com>
2024-06-27 16:04:49 -0400

    GX3-3919: Diseño de las pruebas TC5 y TC6

--- cypress/e2e/Tests/Interactions/GX3-3919-Selectable.cy.js
+++ cypress/e2e/Tests/Interactions/GX3-3919-Selectable.cy.js
@@ -9,8 +9,7 @@ describe('GX-3 3920 |ToolsQA | Interactions | Selectable', () => {
 	});
 	it('3920 | TC2: Validar que solo se pueda mostrar una pestaña a la vez', () => {
 		selectablePage.get.listTab().should('have.attr', 'aria-selected', 'true');
-		selectablePage.clickListButton();
-		selectablePage.get.listTab().should('have.attr', 'aria-selected', 'true');
+		selectablePage.get.gridButton().should('have.attr', 'aria-selected', 'false');
 	});
 	it('3920 | TC3: Validar que los elementos de "List" se puedan seleccionar', () => {
 		selectablePage.clickListButton();
@@ -19,12 +18,37 @@ describe('GX-3 3920 |ToolsQA | Interactions | Selectable', () => {
 		selectablePage.get.buttonMorbi().should('have.class', 'active');
 		selectablePage.get.buttonPorta().should('have.class', 'active');
 	});
-	it.only('3920 | TC4: Validar que los elementos de "List" se puedan deseleccionar', () => {
+	it('3920 | TC4: Validar que los elementos de "List" se puedan deseleccionar', () => {
 		selectablePage.clickListButton();
 		selectablePage.clickListButton();
 		selectablePage.get.buttonCras().should('not.have.class', 'active');
 		selectablePage.get.buttonDapibus().should('not.have.class', 'active');
 		selectablePage.get.buttonMorbi().should('not.have.class', 'active');
 		selectablePage.get.buttonPorta().should('not.have.class', 'active');
+	});
+	it('3920 | TC5: Validar que los elementos de "Grid" se puedan seleccionar', () => {
+		selectablePage.clickGridButton();
+		selectablePage.get.oneButton().should('have.class', 'active');
+		selectablePage.get.twoButton().should('have.class', 'active');
+		selectablePage.get.threeButton().should('have.class', 'active');
+		selectablePage.get.fourButton().should('have.class', 'active');
+		selectablePage.get.fiveButton().should('have.class', 'active');
+		selectablePage.get.sixButton().should('have.class', 'active');
+		selectablePage.get.sevenButton().should('have.class', 'active');
+		selectablePage.get.eightButton().should('have.class', 'active');
+		selectablePage.get.nineButton().should('have.class', 'active');
+	});
+	it('3920 | TC6: Validar que los elementos de "Grid" se puedan deseleccionar', () => {
+		selectablePage.clickGridButton();
+		selectablePage.clickGridButton();
+		selectablePage.get.oneButton().should('not.have.class', 'active');
+		selectablePage.get.twoButton().should('not.have.class', 'active');
+		selectablePage.get.threeButton().should('not.have.class', 'active');
+		selectablePage.get.fourButton().should('not.have.class', 'active');
+		selectablePage.get.fiveButton().should('not.have.class', 'active');
+		selectablePage.get.sixButton().should('not.have.class', 'active');
+		selectablePage.get.sevenButton().should('not.have.class', 'active');
+		selectablePage.get.eightButton().should('not.have.class', 'active');
+		selectablePage.get.nineButton().should('not.have.class', 'active');
 	});
 });
--- cypress/support/pages/GX3-3919-Selectable.page.js
+++ cypress/support/pages/GX3-3919-Selectable.page.js
@@ -4,7 +4,19 @@ class ListElement {
 		buttonCras: ()=> cy.contains('Cras justo odio'),
 		buttonDapibus: ()=> cy.contains('Dapibus ac facilisis in'),
 		buttonMorbi: ()=> cy.contains('Morbi leo risus'),
-		buttonPorta: ()=> cy.contains('Porta ac consectetur ac')
+		buttonPorta: ()=> cy.contains('Porta ac consectetur ac'),
+
+        gridButton:()=>cy.get('#demo-tab-grid'),
+		oneButton:()=> cy.contains('One'),
+		twoButton: ()=> cy.contains ('Two'),
+		threeButton: ()=> cy.contains('Three'),
+		fourButton: ()=> cy.contains('Four'),
+		fiveButton: ()=> cy.contains('Five'),
+		sixButton: ()=> cy.contains('Six'),
+		sevenButton: ()=> cy.contains('Seven'),
+		eightButton: ()=> cy.contains('Eight'),
+		nineButton: ()=> cy.contains('Nine')
+
 
 		
 	}
@@ -13,7 +25,20 @@ class ListElement {
 		this.get.buttonDapibus().click()
 		this.get.buttonMorbi().click()
 		this.get.buttonPorta().click()
-	}
+          }
+	clickGridButton(){
+		this.get.gridButton().click()
+		this.get.oneButton().click()
+		this.get.twoButton().click()
+		this.get.threeButton().click()
+		this.get.fourButton().click()
+		this.get.fiveButton().click()
+		this.get.sixButton().click()
+		this.get.sevenButton().click()
+		this.get.eightButton().click()
+		this.get.nineButton().click()
+		}
+	
 }
 
 export const selectablePage = new ListElement();
