3991c4228827305a3124f45395f7d2b0f45fd2a0
Dana3444 <dianahigueras@hotmail.com>
2024-07-01 17:56:53 +0200

    GX3-3981 Actualizacion nueva version de Sanity y cambio test a typescript

--- cypress/e2e/Tests/Checkout/GX3-3981-Booking-Checkout.cy.ts
+++ cypress/e2e/Tests/Checkout/GX3-3981-Booking-Checkout.cy.ts
@@ -1,4 +1,4 @@
-describe('GX3-3981|[Automation] SpaceBeyond | Booking | Book a Destination in Checkout', () => {
+describe('GX3-3981|Automation SpaceBeyond | Booking | Book a Destination in Checkout', () => {
 	beforeEach('El usuario debería estar situado la url de login', () => {
 		cy.visit('https://demo.testim.io/login');
 		//cy.url().should('have.text','login');
