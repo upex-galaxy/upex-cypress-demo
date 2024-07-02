794fd7e30c6fa794f203280340310d11d620e5b4
Dana3444 <dianahigueras@hotmail.com>
2024-07-02 22:24:51 +0200

    GX3-3981 Cambio de endpoind en Test

--- cypress/e2e/Tests/Checkout/GX3-3981-Booking-Checkout.cy.ts
+++ cypress/e2e/Tests/Checkout/GX3-3981-Booking-Checkout.cy.ts
@@ -3,4 +3,8 @@ describe('GX3-3981|Automation SpaceBeyond | Booking | Book a Destination in Chec
 		cy.visit('https://demo.testim.io/login');
 		cy.url().should('have.text','login');
 	});
+
+	it('TC01 usuario debería estar logeado', () => {
+		cy.get('.Login__headline-1___qo4Tz').should('have.text','login');
+	});
 });
