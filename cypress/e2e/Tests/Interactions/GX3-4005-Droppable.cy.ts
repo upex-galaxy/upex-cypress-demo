209cc0e5f2980dc8b75f72b91eb40d745b58e2cf
MMargoHC <margoth.honorioc@gmail.com>
2024-07-04 16:53:48 -0400

    GX3-4005: Se realizo los TC2 y TC3

--- cypress/e2e/Tests/Interactions/GX3-4005-Droppable.cy.ts
+++ cypress/e2e/Tests/Interactions/GX3-4005-Droppable.cy.ts
@@ -1,11 +1,11 @@
-import { droppablePage } from '@pages/GX3-4005-Droppable.Page.ts';
+import { droppablePage } from '@pages/GX3-4005-Droppable.Page';
 describe('GX3 4006 | ToolsQA | Interactions | Droppable', () => {
 	beforeEach('PRC: El usuario debe estar situado en la pagina de Demo QA', () => {
 		cy.visit('https://demoqa.com/droppable');
 		cy.url().should('contain', 'droppable');
 	});
 	it('4006 | TC1: Validar que las pestañas "Simple", "Accept", "Prevent Propogation" y "Revert Draggable" sean visibles , donde “Simple” se muestra de forma predeterminada', () => {
-		//droppablePage.get.simpleTab().should('have.attr','true');
+
 		droppablePage.get.simpleTab().should('be.visible');
 		droppablePage.get.accceptTab().should('be.visible');
 		droppablePage.get.preventPropogationTab().should('be.visible');
@@ -15,5 +15,19 @@ describe('GX3 4006 | ToolsQA | Interactions | Droppable', () => {
 		droppablePage.get.preventPropogationTab().should('have.attr','aria-selected','false');
 		droppablePage.get.revertableTab().should('have.attr','aria-selected','false');
 	});
-	
+	it('4006 | TC2: Validar que solo se muestra una pestaña a la vez',() => {
+		droppablePage.clickAcceptTab();
+		droppablePage.get.accceptTab().should('have.class','active');
+		droppablePage.clickPreventPropogationTab();
+		droppablePage.get.preventPropogationTab().should('have.class','active');
+		droppablePage.clickRevertableTab();
+		droppablePage.get.revertableTab().should('have.class','active');
+
+	});
+	it('4006 | TC3: Validar que "Drag me" es arrastrado a "Drop Here" y se muestra el texto "Dropped!"',() => {
+		droppablePage.dragdropTc1();
+		droppablePage.get.droppableAction().should('have.text','Dropped!');
+		droppablePage.get.droppableAction().should('have.class','ui-state-highlight');
+
+	});
 });
--- cypress/support/pages/GX3-4005-Droppable.page.ts
+++ cypress/support/pages/GX3-4005-Droppable.page.ts
@@ -3,9 +3,30 @@ class DroppableElement {
 		simpleTab: () => cy.get('#droppableExample-tab-simple'),
 		accceptTab: () => cy.get('#droppableExample-tab-accept'),
 		preventPropogationTab: () => cy.get('#droppableExample-tab-preventPropogation'),
-		revertableTab:() => cy.get('#droppableExample-tab-revertable')
+		revertableTab:() => cy.get('#droppableExample-tab-revertable'),
+		//TC3
+		dragmeBox:() => cy.get('#draggable'),
+		droppableAction:() => cy.get('#droppable:first'),
+		dragcontainer: () => cy.get('#simpleDropContainer')
 
 	};
+	clickAcceptTab() {
+		this.get.accceptTab().click();
+	}
+	clickPreventPropogationTab() {
+		this.get.preventPropogationTab().click();
+	}
+	clickRevertableTab() {
+		this.get.revertableTab().click();
+	}
+
+	dragdropTc1() {
+		const deltaOX = Cypress._.random(350,380);
+		this.get.dragmeBox().move({ deltaX: deltaOX, deltaY: 57 });
+
+		//this.get.dragmeBox().drag('#droppable:first',{ force:true });
+	}
+
 }
 
 export const droppablePage = new DroppableElement();
