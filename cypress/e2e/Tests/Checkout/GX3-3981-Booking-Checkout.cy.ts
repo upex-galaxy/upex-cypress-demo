8885502615a861b620c2889ba1a2573c198b41c7
Dana3444 <dianahigueras@hotmail.com>
2024-07-05 20:40:08 +0200

    "GX3-3981. Se crea fixture. Modificacion del Data y test"

--- cypress/e2e/Tests/Checkout/GX3-3981-Booking-Checkout.cy.ts
+++ cypress/e2e/Tests/Checkout/GX3-3981-Booking-Checkout.cy.ts
@@ -1,10 +1,24 @@
+import { spaceLoginPage } from'@pages/GX3-3981-Booking-Checkout.Page';
+import json from'@data/Elements/GX3-3981-Booking-Checkout.json';
+
 describe('GX3-3981|Automation SpaceBeyond | Booking | Book a Destination in Checkout', () => {
 	beforeEach('El usuario debería estar situado la url de login', () => {
-		cy.visit('https://demo.testim.io/login');
-		cy.url().should('have.text','login');
+		cy.visit('https://demo.testim.io/');
+		cy.url().should('contain','testim');
+		//cy.url().should('include','testim');
+		cy.contains('button', 'Log in').click();
+		cy.url().should('include','login');
 	});
 
 	it('TC01 usuario debería estar logeado', () => {
-		cy.get('.Login__headline-1___qo4Tz').should('have.text','login');
+		expect(true).to.be.true;
+		//spaceLoginPage.loginButtonClick;
+		//cy.contains('button', 'Log in').click();
+		//cy.get('#login').contains('Log in').parent().get('button');
+		//spaceLoginPage.usernameInput();
+		//spaceLoginPage.loginUsernamePrueba().type('hola');
+		cy.get('#login').contains('Username').type(json.data.userameValid);
+		spaceLoginPage.loginButtonClick().should('have text','LOG IN');
+		//inner Text "LOG IN"
 	});
 });
--- cypress/support/pages/GX3-3981-Booking-Checkout.Page.ts
+++ cypress/support/pages/GX3-3981-Booking-Checkout.Page.ts
@@ -2,10 +2,17 @@
 class SpaceLoginPage {
 	loginForm: () => Cypress.Chainable<JQuery<HTMLElement>>;
 	usernameInput: () => Cypress.Chainable<JQuery<HTMLInputElement>>;
+	passwordInput: () => Cypress.Chainable<JQuery<HTMLInputElement>>;
+	loginUsername: () => Cypress.Chainable<JQuery<HTMLInputElement>>;
+	loginButtonClick: () => Cypress.Chainable<JQuery<HTMLButtonElement>>;
+	loginButton: () => cy.get('button').eq(10).click();
 
 	constructor() {
 		this.loginForm = () => cy.get('#login');
+		this.loginUsername = () => cy.get('#login').contains('Username').parent().get('input');
 		this.usernameInput = () => this.loginForm().contains('Username').parent().get('input');
+		this.passwordInput = () => this.loginForm().contains('Password').parent().get('input');
+		this.loginButtonClick = () => cy.get('button').eq(10).click();
 	}
 }
 
