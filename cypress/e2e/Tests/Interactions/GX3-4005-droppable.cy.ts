6d91fcb6deb5741cbfc09513b7297d295b365f54
MMargoHC <margoth.honorioc@gmail.com>
2024-07-07 16:55:46 -0400

    GX3-4005: Ejecucion de los TC3,4,5,6 y TC7

--- cypress/e2e/Tests/Interactions/GX3-4005-droppable.cy.ts
+++ cypress/e2e/Tests/Interactions/GX3-4005-droppable.cy.ts
@@ -5,33 +5,52 @@ describe('GX3 4006 | ToolsQA | Interactions | Droppable', () => {
 		cy.url().should('contain', 'droppable');
 	});
 	it('4006 | TC1: Validar que las pestañas "Simple", "Accept", "Prevent Propogation" y "Revert Draggable" sean visibles , donde “Simple” se muestra de forma predeterminada', () => {
-
 		droppaBlePage.get.simpleTab().should('be.visible');
 		droppaBlePage.get.accceptTab().should('be.visible');
 		droppaBlePage.get.preventPropogationTab().should('be.visible');
 		droppaBlePage.get.revertableTab().should('be.visible');
-		droppaBlePage.get.simpleTab().should('have.attr','aria-selected','true');
-		droppaBlePage.get.accceptTab().should('have.attr','aria-selected','false');
-		droppaBlePage.get.preventPropogationTab().should('have.attr','aria-selected','false');
-		droppaBlePage.get.revertableTab().should('have.attr','aria-selected','false');
+		droppaBlePage.get.simpleTab().should('have.attr', 'aria-selected', 'true');
+		droppaBlePage.get.accceptTab().should('have.attr', 'aria-selected', 'false');
+		droppaBlePage.get.preventPropogationTab().should('have.attr', 'aria-selected', 'false');
+		droppaBlePage.get.revertableTab().should('have.attr', 'aria-selected', 'false');
 	});
-	it('4006 | TC2: Validar que solo se muestra una pestaña a la vez',() => {
+	it('4006 | TC2: Validar que solo se muestra una pestaña a la vez', () => {
 		droppaBlePage.clickAcceptTab();
-		droppaBlePage.get.accceptTab().should('have.class','active');
+		droppaBlePage.get.accceptTab().should('have.class', 'active');
 		droppaBlePage.clickPreventPropogationTab();
-		droppaBlePage.get.preventPropogationTab().should('have.class','active');
+		droppaBlePage.get.preventPropogationTab().should('have.class', 'active');
 		droppaBlePage.clickRevertableTab();
-		droppaBlePage.get.revertableTab().should('have.class','active');
-
+		droppaBlePage.get.revertableTab().should('have.class', 'active');
 	});
-	it('4006 | TC3: Validar que "Drag me" es arrastrado a "Drop Here" y se muestra el texto "Dropped!"',() => {
-		droppaBlePage.dragdropTc1();
-		droppaBlePage.get.droppableAction().should('have.text','Dropped!');
-		droppaBlePage.get.droppableAction().should('have.class','ui-state-highlight');
-
+	it('4006 | TC3: Validar que "Drag me" es arrastrado a "Drop Here" y se muestra el texto "Dropped!"', () => {
+		droppaBlePage.simpleDrop();
+		cy.get('@simpleDropBox').should('have.text', 'Dropped!');
+		cy.get('@simpleDropBox').should('have.class', 'ui-state-highlight');
 	});
-	it.only('4006 | TC4: Validar que "Acepptable"  es arrastrado a "Drop Here" y se visualiza el Text “Dropped!” ',() => {
+	it('4006 | TC4: Validar que "Acepptable"  es arrastrado a "Drop Here" y se visualiza el Text “Dropped!” ', () => {
 		droppaBlePage.clickAcceptTab();
+		droppaBlePage.acceptableDrop();
+		cy.get('@acceptDropBox').should('have.class', 'ui-state-highlight');
+		cy.get('@acceptDropBox').should('have.text', 'Dropped!');
+		//droppaBlePage.get.droppableAction().eq(1).should('have.text', 'Dropped!');
+		//droppaBlePage.get.containerDrop()
+	});
 
+	it('4006 | TC5: Validar "Not Acepptable" es arrastrado a "Drop Here" y no se visualice ningun cambio', () => {
+		droppaBlePage.clickAcceptTab();
+		droppaBlePage.notAcceptableDrop();
+		cy.get('@notAcceptableDropBox').should('have.text', 'Drop here');
+	});
+
+	it('4006 | TC6: Validar que "Drag me" se arrastre a "Outer Droppable" ', () => {
+		droppaBlePage.clickPreventPropogationTab();
+		droppaBlePage.preventPropogationOuterDrop();
+		cy.get('@pLabelInNotGredyDrop').should('have.text', 'Dropped!');
+	});
+
+	it('4006 | TC7: Validar que "Drag me" se arrastre a "Inner Droppable (not greedy)" ', () => {
+		droppaBlePage.clickPreventPropogationTab();
+		droppaBlePage.preventPropogationDrop();
+		cy.get('@ppDropBox').should('have.text', 'Dropped!');
 	});
 });
--- cypress/support/pages/GX3-4005-DroppaBle.Page.ts
+++ cypress/support/pages/GX3-4005-DroppaBle.Page.ts
@@ -3,15 +3,23 @@ class DroppableElement {
 		simpleTab: () => cy.get('#droppableExample-tab-simple'),
 		accceptTab: () => cy.get('#droppableExample-tab-accept'),
 		preventPropogationTab: () => cy.get('#droppableExample-tab-preventPropogation'),
-		revertableTab:() => cy.get('#droppableExample-tab-revertable'),
+		revertableTab: () => cy.get('#droppableExample-tab-revertable'),
 		//TC3
-		dragmeBox:() => cy.get('#draggable'),
-		droppableAction:() => cy.get('#droppable:first'),
-		dragcontainer: () => cy.get('#simpleDropContainer')
+		dragmeBox: () => cy.get('#draggable'),
+		droppableAction: () => cy.get('#droppable'),
+		simpleDropContainer: () => cy.get('#simpleDropContainer'),
 		//TC4
-		//acceptableDropTc
-
+		acceptableBox: () => cy.get('#acceptable'),
+		acceptDropContainer: () => cy.get('#acceptDropContainer'),
+		containerDrop: () => cy.get('#droppable'),
+		notAcceptableBox: () => cy.get('#notAcceptable'),
+		//TC5
+		dragBox: () => cy.get('#dragBox'),
+		notGreedyDrop: () => cy.get('#notGreedyDropBox'),
+		preventPropogationContainer: () => cy.get('#ppDropContainer'),
+		notGreedyInnerDrop: () => cy.get('#notGreedyInnerDropBox'),
 	};
+
 	clickAcceptTab() {
 		this.get.accceptTab().click();
 	}
@@ -22,12 +30,48 @@ class DroppableElement {
 		this.get.revertableTab().click();
 	}
 
-	dragdropTc1() {
-		const deltaOX = Cypress._.random(350,380);
+	simpleDrop() {
+		const deltaOX = Cypress._.random(350, 380);
 		this.get.dragmeBox().move({ deltaX: deltaOX, deltaY: 57 });
+		this.get.simpleDropContainer().within(() => {
+			this.get.containerDrop().as('simpleDropBox');
+		});
 
 		//this.get.dragmeBox().drag('#droppable:first',{ force:true });
 	}
+	acceptableDrop() {
+		const deltaA = Cypress._.random(350, 380);
+		this.get.acceptableBox().move({ deltaX: deltaA, deltaY: 55 });
+		this.get.acceptDropContainer().within(() => {
+			this.get.containerDrop().as('acceptDropBox');
+		});
+	}
+	notAcceptableDrop() {
+		const deltaA = Cypress._.random(345, 355);
+		this.get.notAcceptableBox().move({ deltaX: deltaA, deltaY: 5 });
+		this.get.acceptDropContainer().within(() => {
+			this.get.containerDrop().as('notAcceptableDropBox');
+		});
+	}
+	preventPropogationOuterDrop() {
+		const deltaA = Cypress._.random(355, 365);
+		this.get.dragBox().move({ deltaX: deltaA, deltaY: 5 });
+		this.get.preventPropogationContainer().within(() => {
+			this.get.notGreedyDrop().as('ppOuterDropBox');
+			this.get.notGreedyDrop().within(() => {
+				cy.get('p').eq(0).as('pLabelInNotGredyDrop'); //should('have.text','Dropped!')
 
+			});
+		});
+	}
+
+	preventPropogationDrop() {
+		const deltaA = Cypress._.random(355, 365);
+		this.get.dragBox().move({ deltaX: deltaA, deltaY: 96 });
+		this.get.preventPropogationContainer().within(() => {
+			this.get.notGreedyInnerDrop().as('ppDropBox');
+		});
+	}
 }
+
 export const droppaBlePage = new DroppableElement();
