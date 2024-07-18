966bff4cd170620e70c949b12166597a203b560e
MMargoHC <margoth.honorioc@gmail.com>
2024-07-18 12:52:24 -0400

    GX3-4098: Se ejecuto las pruebas TC1 TC2 y TC3

--- cypress/e2e/Tests/Alerts/GX3-4098-FrameWindowAlerts.cy.ts
+++ cypress/e2e/Tests/Alerts/GX3-4098-FrameWindowAlerts.cy.ts
@@ -5,7 +5,21 @@ describe('GX3 4098 |ToolsQA | Alert-Frame-Window | Alerts', () => {
 		cy.url().should('contain', 'alerts');
 	});
 	it('4099 | TC1: Validate the first "click me" button. An alert with the text "You clicked the button" must appear.', () => {
-		//cy.get('alertButton').shoul
-	});
+		alertPage.actionAlertButton();
+		cy.on('window:alert', (str) => expect(str).to.equal('You clicked a button'));
 
+	});
+	it('4099 | TC2: Validate the second "click me" button. An alert must appear 5 seconds after the click.', () => {
+		alertPage.timerAlert();
+		cy.on('window:alert', (str) => expect(str).to.equal('This alert appeared after 5 seconds'));
+		//cy.get.alertTimerButton({ timeout: 5000 }).should('contain.text','This alert appeared after 5 seconds');
+
+	});
+	it('4099 | TC3: Validate the third "click me" button. If the "OK" option is selected, a “You selected OK” confirmation message must appear.', () => {
+		alertPage.confirmAlert();
+		 cy.on('window:confirm', (str) => {expect(str).to.equal('Do you confirm action?');
+			return true;
+		});
+		alertPage.get.confirmResult().should('have.text','You selected Ok');
+	});
 });
--- cypress/support/pages/GX3-4098-AlertFrameWindow.Page.ts
+++ cypress/support/pages/GX3-4098-AlertFrameWindow.Page.ts
@@ -1,4 +1,28 @@
 class AlertFrameWindow {
+	get={
+		//TC1
+		alertButton:() => cy.get('#alertButton'),
+		//TC2
+		timerAlertButton: () => cy.get('#timerAlertButton'),
+		//TC3
+		confirmButton: () => cy.get('#confirmButton'),
+		confirmResult: () => cy.get('#confirmResult'),
+
+	};
+	actionAlertButton() {
+		this.get.alertButton().click();
+		const condition = true;
+		expect(condition).to.be.true;
+
+	};
+
+	timerAlert() {
+		this.get.timerAlertButton().click();
+	}
+	confirmAlert() {
+		this.get.confirmButton().click();
+
+	}
 
 }
 export const alertPage = new AlertFrameWindow();
