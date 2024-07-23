fc841e4259c72dd62c472fbf29f3b01a5cbea9e1
JanetziJackiewicz <Janetzi.Jackiewicz@gmail.com>
2024-07-23 17:05:23 -0300

    termino de realizar los tc se trabajo con el grid

--- cypress/support/pages/GX3-4288-selectable.Page.js
+++
@@ -1,9 +0,0 @@
-class Selectable {
-	get itemsList() {
-		cy.get('#verticalListContainer li');
-	}
-	get countItemsList(){
-		this.itemsList.then($listItems => $listItems.length))
-	};
-};
-export const SelectablePage = new Selectable()
