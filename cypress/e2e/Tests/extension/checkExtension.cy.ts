19bc6b7a84749759ed51a964015476de0c0f0c2b
kenlyr <kenlyrojas96@gmail.com>
2024-08-14 22:37:29 -0500

    Managed all files to be organized

--- cypress/e2e/Tests/extension/checkExtension.cy.ts
+++ cypress/e2e/Tests/extension/checkExtension.cy.ts
@@ -4,7 +4,7 @@ describe('UX-345 | Check Extension', () => {
 
 	// TEST CASE:
 	it('UX-6785 | TC1: Validar que la extension adBlock haga bien', () => {
-		cy.visit('/radio-button'); // baseUrl
+		cy.visit('https://demoqa.com/radio-button'); // baseUrl
 		cy.get('input[id="yesRadio"]').check({ force: true });
 		cy.get('.text-success').should('have.text', 'Yes');
 		cy.reload();
