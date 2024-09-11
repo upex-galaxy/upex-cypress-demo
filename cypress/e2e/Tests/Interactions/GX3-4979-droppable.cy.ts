5a9eb5aa6981471dc22e8abe5f69cd92debcb1be
Franco-Carrera <carrerafranco825@gmail.com>
2024-09-11 15:07:31 -0300

    test: (GX3-4979) Intentar arreglar problema de CI (Opción 2)

--- cypress/e2e/Tests/Interactions/GX3-4979-droppable.cy.ts
+++ cypress/e2e/Tests/Interactions/GX3-4979-droppable.cy.ts
@@ -1,9 +1,11 @@
 import { droppablePage } from "@pages/GX3-4979-droppable.Page"
+const {endpoint} = Cypress.env("endpoints")
 
 describe("ToolsQA | Interactions | Droppable", () => {
 	beforeEach('El usuario debe visitar el endpoint mencionado debajo', ()=>{
-		cy.visit("/droppable/")
-		cy.url().should('contain', '/droppable')
+		cy.visit(endpoint);
+		cy.url().should('contain', '/droppable');
+		cy.wait(2000);
 	});  
 
 	it("4979 | TC1: Validar hacer Drag and Drop en el Tab 'Simple'", () => {
