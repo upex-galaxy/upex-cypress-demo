9e0c673b1c7fcfa0db641f90ead5427fa8e5b593
Franco-Carrera <carrerafranco825@gmail.com>
2024-09-11 14:45:27 -0300

    test: (GX3-4979) Intentar arreglar problema de CI (Opción 1) y adjuntar docu

--- cypress/e2e/Tests/Interactions/GX3-4979-droppable.cy.ts
+++ cypress/e2e/Tests/Interactions/GX3-4979-droppable.cy.ts
@@ -2,7 +2,7 @@ import { droppablePage } from "@pages/GX3-4979-droppable.Page"
 
 describe("ToolsQA | Interactions | Droppable", () => {
 	beforeEach('El usuario debe visitar el endpoint mencionado debajo', ()=>{
-		cy.visit("/droppable")
+		cy.visit("/droppable/")
 		cy.url().should('contain', '/droppable')
 	});  
 
@@ -13,13 +13,5 @@ describe("ToolsQA | Interactions | Droppable", () => {
 			force: true,
 		})
 		droppablePage.droppableParagraph().should('have.text', 'Dropped!');
-	
-	/**Fórmula aquí Debajo**/
-	/*	cy.get("#draggable").drag("#draggable", {
-			source: {x: -280, y: -50},
-			target: {position: "right"},
-			force: true,
-		})
-	*/
 	})
 })
