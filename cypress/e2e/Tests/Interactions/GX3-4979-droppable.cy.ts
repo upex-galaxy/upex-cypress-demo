aa573088e584a05964823408952e208ffb701243
Franco-Carrera <carrerafranco825@gmail.com>
2024-09-13 13:22:11 -0300

    chrome: (GX3-4979) Colocar baseUrl en el tsconfig.json

--- cypress/e2e/Tests/Interactions/GX3-4979-droppable.cy.ts
+++ cypress/e2e/Tests/Interactions/GX3-4979-droppable.cy.ts
@@ -1,4 +1,4 @@
-import { droppablePage } from "@pages/GX3-4979-droppable.Page"
+import { droppablePage } from "../../../support/pages/GX3-4979-droppable.Page";
 
 describe("ToolsQA | Interactions | Droppable", () => {
 	beforeEach('El usuario debe visitar el endpoint mencionado debajo', ()=>{
@@ -14,4 +14,28 @@ describe("ToolsQA | Interactions | Droppable", () => {
 		})
 		droppablePage.droppableParagraph().should('have.text', 'Dropped!');
 	})
+	/*	it("4979 | TC2: Validar hacer Drag and Drop en el Tab 'Accept' con el 'Acceptable' area", () => {
+
+	})
+	it("4979 | TC3: Validar hacer Drag and Drop en el Tab 'Accept' con el 'Not Acceptable' area", () => {
+
+	})
+	it("4979 | TC4: Validar hacer Drag and Drop en el Tab 'Propogation' hacia el 'First Outer Droppable'", () => {
+
+	})
+   it("4979 | TC5: Validar hacer Drag and Drop en el Tab 'Propogation' hacia el área 'not greedy'", () => {
+
+   })
+   it("4979 | TC6: Validar hacer Drag and Drop en el Tab 'Propogation' hacia el área 'greedy'", () => {
+
+   })
+    it("4979 | TC7: Validar hacer Drag and Drop en el Tab 'Revert' con el 'Will Revert' area hacia el 'Drop Here'", () => {
+
+   })
+   it("4979 | TC8: Validar hacer Drag and Drop permanente en el Tab 'Revert' con el 'Not Revert' area hacia el 'Drop Here'", () => {
+
+   })
+   it("4979 | TC9: Validar NO hacer Drag and Drop en el Tab 'Simple' al mover el 'Drag Me' area fuera del 'Drop here'", () => {
+	
+	})*/
 })
