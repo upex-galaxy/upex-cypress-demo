c6ae95c221ac9f57ed4aa4de7469fcd90aa12c24
DaryelinRossell <daryelin.rossell@gmail.com>
2024-10-07 21:25:12 -0400

    test: (GX3-5300) add validations for all fields

--- cypress/e2e/Tests/Forms/GX3-5300-PracticeForm.cy.js
+++ cypress/e2e/Tests/Forms/GX3-5300-PracticeForm.cy.js
@@ -13,17 +13,50 @@ describe('GX3-5300 | ToolsQA | Forms | Practice Form', () => {
 		const email = faker.internet.email();
 		const mobileNumber = faker.finance.pin(8);
 		const currentAddress = faker.location.direction();
+		const randomNumber = faker.number.int({ min: 0, max: 2 });
+		const letter = faker.helpers.arrayElement(['a', 'c', 'd', 'e', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'y']);
+		const randomDay = faker.datatype.number({ min: 1, max: 28 });
+		let formattedDay;
+		if (randomDay < 10) {
+			formattedDay = '0' + randomDay;
+		} else {
+			formattedDay = String(randomDay);
+		}
+		const randomMonth = faker.helpers.arrayElement(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
+		const randomYear = faker.datatype.number({ min: 1950, max: 2023 });
+		const stringYear = randomYear.toString();
+		const dateAssert = formattedDay + ' ' + randomMonth + ' ' + stringYear;
+		const letterState = faker.helpers.arrayElement(['a', 'c', 'd', 'e', 'h', 'n', 'p', 'r', 's', 't', 'u', 'y']);
+		const letterCity = faker.helpers.arrayElement(['a', 'd', 'e', 'g', 'h', 'i', 'l', 'n', 'o', 'r', 'u']);
 
 		formPage.typeFirstName(firstName);
 		formPage.typeLastName(lastName);
 		formPage.typeEmail(email);
 		formPage.typeMobileNumber(mobileNumber);
 		formPage.typeCurrentAddress(currentAddress);
+		formPage.selectGender(randomNumber);
+		formPage.selectHobbies(randomNumber);
+		formPage.selectSubjects(letter);
+		formPage.selectPicture();
+		formPage.selectMonth(randomMonth);
+		formPage.selectYear(stringYear);
+		formPage.selectDay(randomDay);
+		formPage.selectState(letterState);
+		formPage.selectCity(letterCity);
+		formPage.clickSubmit();
 
 		formPage.get.inputFirstName().should('have.value', firstName);
 		formPage.get.inputLastName().should('have.value', lastName);
 		formPage.get.inputEmail('have.value', email);
 		formPage.get.inputMobileNumber().should('have.value', mobileNumber);
 		formPage.get.inputCurrentAddress().should('have.value', currentAddress);
+		formPage.get.radioButtonGenderSelect().should('be.checked', randomNumber);
+		formPage.get.checksHobbiesSelect().should('be.checked', randomNumber);
+		formPage.get.inputSubject().should('contain', letter);
+		formPage.get.selectPicture().should('have.prop', 'files');
+		formPage.get.inputDate().should('have.value', dateAssert);
+		formPage.get.selectableState().should('contain', letterState);
+		formPage.get.selectableCity().should('contain', letterCity);
+		formPage.get.submitAssert().should('have.class', 'modal-content');
 	});
 });
--- cypress/support/pages/GX3-5300-practiceForm.Page.js
+++ cypress/support/pages/GX3-5300-practiceForm.Page.js
@@ -3,22 +3,25 @@ class PracticeForm {
 		inputFirstName: () => cy.get('#firstName'),
 		inputLastName: () => cy.get('#lastName'),
 		inputEmail: () => cy.get('#userEmail'),
-		genderMale: () => cy.get('#gender-radio-1'),
-		genderFemale: () => cy.get('#gender-radio-2'),
-		genderOther: () => cy.get('#gender-radio-3'),
+		radioButtonGender: () => cy.get('div[class*="custom-radio"]'),
+		radioButtonGenderSelect: () => cy.get('input[type="radio"]'),
 		inputMobileNumber: () => cy.get('#userNumber'),
-		dateDay: () => cy.get('.react-datepicker__day'),
-		dateMonth: () => cy.get('.class="react-datepicker__month-select"'),
-		dateYear: () => cy.get('.react-datepicker__year-select'),
+		inputDate: () => cy.get('#dateOfBirthInput'),
+		selectMonth: () => cy.get('.react-datepicker__month-select'),
+		selectYear: () => cy.get('.react-datepicker__year-select'),
+		selectDay: () => cy.get('.react-datepicker__day'),
 		inputSubject: () => cy.get('#subjectsContainer'),
-		hobbiesSports: () => cy.get('#hobbies-checkbox-1'),
-		hobbiesReading: () => cy.get('#hobbies-checkbox-2'),
-		hobbiesMusic: () => cy.get('#hobbies-checkbox-3'),
+		clickInSubjects: () => cy.get('#react-select-2-option-0'),
+		checkHobbies: () => cy.get('.custom-checkbox'),
+		checksHobbiesSelect: () => cy.get('input[type="checkbox"]'),
 		selectPicture: () => cy.get('#uploadPicture'),
 		inputCurrentAddress: () => cy.get('#currentAddress'),
-		selectState: () => cy.get('#state'),
-		selectCity: () => cy.get('#city'),
-		button: () => cy.get('submit')
+		selectableState: () => cy.get('#state'),
+		clickState: () => cy.get('#react-select-3-option-1'),
+		selectableCity: () => cy.get('#city'),
+		clickCity: () => cy.get('#city div[id*=react-select]'),
+		buttonSubmit: () => cy.get('#submit'),
+		submitAssert: () => cy.get('.modal-content')
 	};
 
 	typeFirstName(firstName) {
@@ -35,6 +38,49 @@ class PracticeForm {
 	}
 	typeCurrentAddress(currentAddress) {
 		this.get.inputCurrentAddress().type(currentAddress);
+	}
+	selectGender(randomNumber) {
+		this.get.radioButtonGender().eq(randomNumber).click();
+	}
+	selectHobbies(randomNumber) {
+		this.get.checkHobbies().eq(randomNumber).click();
+	}
+	selectSubjects(letter) {
+		this.get.inputSubject().type(letter);
+		this.get.clickInSubjects().click();
+	}
+	selectPicture() {
+		this.get.selectPicture().click();
+		this.get.selectPicture().selectFile('cypress/fixtures/images/upexgalaxy.gif');
+	}
+	selectMonth(randomMonth) {
+		this.get.inputDate().click();
+		this.get
+			.selectMonth()
+			.contains(randomMonth)
+			.then($option => {
+				const value = $option.val();
+				this.get.selectMonth().select(value);
+			});
+	}
+	selectYear(stringYear) {
+		this.get.inputDate().click();
+		this.get.selectYear().select(stringYear);
+	}
+	selectDay(randomDay) {
+		this.get.inputDate().click();
+		this.get.selectDay().contains(randomDay).click();
+	}
+	selectState(letterState) {
+		this.get.selectableState().type(letterState);
+		this.get.clickState().click();
+	}
+	selectCity(letterCity) {
+		this.get.selectableCity().type(letterCity);
+		this.get.clickCity().eq(0).click();
+	}
+	clickSubmit() {
+		this.get.buttonSubmit().click();
 	}
 }
 export const formPage = new PracticeForm();
