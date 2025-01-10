feba8ec96c02be5f43674cc8f858d129d046f017
Mili915 <milagrosgonzalez.esp@gmail.com>
2025-01-10 21:05:31 +0100

    GX3-5976 Terminado caso de prueba en estado PASS

--- cypress/e2e/Tests/Elements/GX3-5976-TexBox.cy.ts
+++ cypress/e2e/Tests/Elements/GX3-5976-TexBox.cy.ts
@@ -1,22 +1,25 @@
 describe('GX3-5976 ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 	beforeEach('PRC Abrir la url de Tex Box de Tools QA', () => {
 		cy.visit('https://demoqa.com/text-box');
-		cy.url().should('contain', 'Text-Box');
+		cy.url().should('contain', 'text-box');
 	});
 
-	it('TC01 Validar el registro de usuario ingresando data valida', () => {
+	it.only('TC01 Validar el registro de usuario ingresando data valida', () => {
 		cy.get('input#userName').type('Milagros');
 		cy.get('input#userName').should('have.value', 'Milagros');
-
-		cy.get('input#userEmail["type"="email"]').should('exist').and('be.visible').and('be.enabled');
 		cy.get('input#userEmail').type('mila@gmail.com');
-		cy.get('textarea#currentAddress').should('exist').and('be.visible').and('be.enabled');
+		cy.get('input#userEmail').should('have.value', 'mila@gmail.com');
 		cy.get('textarea#currentAddress').type('Barcelona');
-		cy.get('textarea#permanentAddress').should('be.enabled').and('exist').and('be.visible');
+		cy.get('textarea#currentAddress').should('have.value', 'Barcelona');
 		cy.get('textarea#permanentAddress').type('España');
+		cy.get('textarea#permanentAddress').should('have.value', 'España');
 
 		cy.get('button#submit').click();
 		cy.get('#output').should('be.visible');
+		cy.get('#name').should('contain', 'Milagros');
+		cy.get('#email').should('contain', 'mila@gmail.com');
+		cy.get('#output #currentAddress').should('contain', 'Barcelona');
+		cy.get('#output #permanentAddress').should('contain', 'España');
 	});
 
 	it('TC02 Validar No registro ingresando caracteres alfanumericos en campo full Name', () => {
