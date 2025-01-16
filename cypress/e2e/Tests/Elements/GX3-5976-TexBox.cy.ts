f5f7883cb3be34c08dfcaf1534540893f46f39ed
Mili915 <milagrosgonzalez.esp@gmail.com>
2025-01-16 16:58:27 +0100

    GX3-5976 Terminada la historia de Usuario, Gracias Dani

--- cypress/e2e/Tests/Elements/GX3-5976-TexBox.cy.ts
+++ cypress/e2e/Tests/Elements/GX3-5976-TexBox.cy.ts
@@ -22,15 +22,12 @@ describe('GX3-5976 ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 			cy.get('p#permanentAddress.mb-1').should('contain.text', carpeta.permanentAddress.permanentAddressValido);
 		});
 	});
-
-	it.only('TC02 Validar No registro dejando campos vacíos', () => {
+	it('TC02 Validar NO registro de usuario dejando campos vacios', () => {
 		cy.fixture('data/Elements/GX3-5976-texBox.json').then(carpeta => {
-			cy.get('#userName').type(carpeta.userName.userNameVacio);
-			cy.get('#userName').should('have.value', carpeta.userName.userNameVacio);
-			cy.get('#currentAddress').type(carpeta.currentAddress.currentAddressVacio);
-			cy.get('#currentAddress').should('have.value', carpeta.currentAddress.currentAddressVacio);
-			cy.get('#permanentAddress').type(carpeta.permanentAddress.permanentAddressVacio);
-			cy.get('#permanentAddress').should('have.value', carpeta.permanentAddress.permanentAddressVacio);
+			cy.get('#userName').should('not.have.value');
+			cy.get('#userEmail').should('not.have.value');
+			cy.get('#currentAddress').should('not.have.value');
+			cy.get('#permanentAddress').should('not.have.value');
 
 			cy.get('button#submit').click();
 			cy.get('#output').should('not.be.visible');
@@ -38,14 +35,53 @@ describe('GX3-5976 ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 	});
 
 	it('TC03 Validar que se muestre mensaje en rojo cuando ingresa data con formato invalido en capo Email (@)', () => {
-		cy.get('input#userEmail').type('email');
-		cy.get('input#userEmail').should('have.value', 'email');
-		cy.get('button#submit').click();
+		cy.fixture('data/Elements/GX3-5976-texBox.json').then(carpeta => {
+			cy.get('#userName').type(carpeta.userName.userNameValido2);
+			cy.get('#userName').should('have.value', carpeta.userName.userNameValido2);
+			cy.get('#userEmail').type(carpeta.userEmail.userEmailAtSign);
+			cy.get('#userEmail').should('have.value', carpeta.userEmail.userEmailAtSign);
+			cy.get('#currentAddress').type(carpeta.currentAddress.currentAddressValido2);
+			cy.get('#currentAddress').should('have.value', carpeta.currentAddress.currentAddressValido2);
+			cy.get('#permanentAddress').type(carpeta.permanentAddress.permanentAddressValido2);
+			cy.get('#permanentAddress').should('have.value', carpeta.permanentAddress.permanentAddressValido2);
 
-		cy.get('input#userEmail').should('have.class', 'field-error');
+			cy.get('button#submit').click();
+			cy.get('#userEmail').should('have.class', 'field-error');
+			cy.get('#output').should('not.be.visible');
+		});
+	});
 
-		// cy.get('.field-error').should('have.css', 'border', '1px solid red');
-		// no funciona porque no se puede validar el color del borde
-		// validar con propiedades CSS no es recomendable, ya que puede cambiar dependiendo del navegador
+	it('TC04 Validar que se muestre mensaje en rojo cuando ingresa data con formato invalido en capo Email (.)', () => {
+		cy.fixture('data/Elements/GX3-5976-texBox.json').then(carpeta => {
+			cy.get('#userName').type(carpeta.userName.userNameValido3);
+			cy.get('#userName').should('have.value', carpeta.userName.userNameValido3);
+			cy.get('#userEmail').type(carpeta.userEmail.userEmailSinPunto);
+			cy.get('#userEmail').should('have.value', carpeta.userEmail.userEmailSinPunto);
+			cy.get('#currentAddress').type(carpeta.currentAddress.currentAddressValido3);
+			cy.get('#currentAddress').should('have.value', carpeta.currentAddress.currentAddressValido3);
+			cy.get('#permanentAddress').type(carpeta.permanentAddress.permanentAddressValido3);
+			cy.get('#permanentAddress').should('have.value', carpeta.permanentAddress.permanentAddressValido3);
+
+			cy.get('button#submit').click();
+			cy.get('#userEmail').should('have.class', 'field-error');
+			cy.get('#output').should('not.be.visible');
+		});
+	});
+
+	it.only('TC05 Validar que se muestre mensaje en rojo cuando ingresa data con formato invalido en capo Email (com)', () => {
+		cy.fixture('data/Elements/GX3-5976-texBox.json').then(carpeta => {
+			cy.get('#userName').type(carpeta.userName.userNameValido4);
+			cy.get('#userName').should('have.value', carpeta.userName.userNameValido4);
+			cy.get('#userEmail').type(carpeta.userEmail.userEmailSinCom);
+			cy.get('#userEmail').should('have.value', carpeta.userEmail.userEmailSinCom);
+			cy.get('#currentAddress').type(carpeta.currentAddress.currentAddressValido4);
+			cy.get('#currentAddress').should('have.value', carpeta.currentAddress.currentAddressValido4);
+			cy.get('#permanentAddress').type(carpeta.permanentAddress.permanentAddressValido4);
+			cy.get('#permanentAddress').should('have.value', carpeta.permanentAddress.permanentAddressValido4);
+
+			cy.get('button#submit').click();
+			cy.get('#userEmail').should('have.class', 'field-error');
+			cy.get('#output').should('not.be.visible');
+		});
 	});
 });
