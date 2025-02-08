ed8d9e6c465c4efee5a303f2736c0a816ff23dea
Nicolas <nicolasborruso92@gmail.com>
2025-02-08 13:13:14 +0100

    Aqui estan las pruebas de Buttons oficiales

--- cypress/e2e/Tests/Elements/GX3-6057-buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-6057-buttons.cy.js
@@ -5,17 +5,17 @@ describe('GX3-6057 | ToolsQA | Elements | Buttons', () => {
 	});
 	it('6063 | TC1: Validar que al hacer “Double Click “ al botón debería desplegarse un mensaje que diga “You have done a double click”', () => {
 		cy.get('#doubleClickBtn').dblclick();
-		cy.get('[id="doubleClickMessage"]').should('contains.text','You have done a double click').and('to.be.visible')
+		cy.get('[id="doubleClickMessage"]').should('contain.text','You have done a double click').and('be.visible')
 	});
 
 	it('6063 | TC2: Validar que al hacer “Right Click” al botón debería desplegarse un mensaje que diga “You have done a right click”', () => {
-		cy.get('id="rightClickBtn"').rightClickBtn();
-		cy.get('[id="rightClickMessage"]').should('be.a.text').and('to.be.visible')
+		cy.get('#rightClickBtn').rightclick();
+		cy.get('[id="rightClickMessage"]').should('contain.text','You have done a right click').and('be.visible')
 
 	})
 
 	it('6063 | TC3: Validar que al hacer “Click” al botón debería desplegarse un mensaje que diga “You have done a dynamic click”', () =>{
-		cy.get('type="button"').eq(3).click()
-		cy.get('id="dynamicClickMessage"').should('be.a.text').and('to.be.visible')
+		cy.get('[type="button"]').eq(3).click()
+		cy.get('[id="dynamicClickMessage"]').should('contain.text','You have done a dynamic click').and('be.visible')
 	})
 });
