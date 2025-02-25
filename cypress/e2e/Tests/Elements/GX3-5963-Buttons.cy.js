0e8fc84c4675d80c80ffe000e35a84a39f27a43e
Luis Hernandez <hernandezluis.umc@gmail.com>
2025-02-24 23:43:46 -0300

    GX3-6095 test(elements): All tests have passed and the feature is working as expected

--- cypress/e2e/Tests/Elements/GX3-5963-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5963-Buttons.cy.js
@@ -1,22 +1,47 @@
-describe('GX3 - 5963 - tools - qa - elements - buttons', () => {
-	beforeEach('El usuario debe estar situdo en la pagina de Demo QA', () => {
-		cy.visit('https://demoqa.com/buttons');
-		cy.url().should('include', 'buttons');
-		cy.get('h1.text-center').should('have.text', 'Buttons');
+describe('Pruebas con sesión en Cypress', () => {
+	before(() => {
+		cy.session('login', () => {
+			cy.visit('https://example.com/login');
+			cy.get('input[name="username"]').type('usuario');
+			cy.get('input[name="password"]').type('contraseña');
+			cy.get('button[type="submit"]').click();
+		});
 	});
 
-	it('Validar hacer doble click en Buttons Bouble Click Me', () => {
-		cy.get('#doubleClickBtn').dblclick();
-		cy.get('#doubleClickMessage').should('have.text', 'You have done a double click');
+	beforeEach(() => {
+		cy.session('login');
 	});
 
-	it('Validar hacer click derecho en Buttons Right Click Me', () => {
-		cy.get('#rightClickBtn').rightclick();
-		cy.get('#rightClickMessage').should('have.text', 'You have done a right click');
+	it('debería acceder a la página de inicio', () => {
+		cy.visit('https://example.com/inicio');
+		cy.contains('Bienvenido, usuario').should('be.visible');
 	});
 
-	it('Validar hacer click en buttons Click Me y visualizacion de mensaje', () => {
-		cy.get('.btn.btn-primary').eq(2).click();
-		cy.get('#dynamicClickMessage').should('have.text', 'You have done a dynamic click');
+	it('debería acceder a la página de perfil', () => {
+		cy.visit('https://example.com/perfil');
+		cy.contains('Perfil de usuario').should('be.visible');
 	});
 });
+
+// describe('GX3 - 5963 - tools - qa - elements - buttons', () => {
+// 	beforeEach('El usuario debe estar situdo en la pagina de Demo QA', () => {
+// 		cy.visit('https://demoqa.com/buttons');
+// 		cy.url().should('include', 'buttons');
+// 		cy.get('h1.text-center').should('have.text', 'Buttons');
+// 	});
+
+// 	it('Validar hacer doble click en Buttons Bouble Click Me', () => {
+// 		cy.get('#doubleClickBtn').dblclick();
+// 		cy.get('#doubleClickMessage').should('have.text', 'You have done a double click');
+// 	});
+
+// 	it('Validar hacer click derecho en Buttons Right Click Me', () => {
+// 		cy.get('#rightClickBtn').rightclick();
+// 		cy.get('#rightClickMessage').should('have.text', 'You have done a right click');
+// 	});
+
+// 	it('Validar hacer click en buttons Click Me y visualizacion de mensaje', () => {
+// 		cy.get('.btn.btn-primary').eq(2).click();
+// 		cy.get('#dynamicClickMessage').should('have.text', 'You have done a dynamic click');
+// 	});
+// });
---
+++ cypress/e2e/Tests/Elements/GX3-6095_ToolsQA_Elements_Buttons.cy.js
@@ -0,0 +1,48 @@
+describe('GX3-6095 | ToolsQA | Elements | Buttons', () => {
+	beforeEach('PRC: The user should be on the URL https://demoqa.com/buttons', () => {
+		cy.visit('https://demoqa.com/buttons');
+		cy.contains('Buttons').should('be.visible');
+		cy.url().should('include', '/buttons');
+		cy.location('protocol').should('eq', 'https:');
+		cy.location('hostname').should('eq', 'demoqa.com');
+		cy.request('https://demoqa.com/buttons').its('status').should('eq', 200);
+	});
+	it('GX3-6095 | TC1: Validate “Click Me” Button is working successfully. (HP)', () => {
+		cy.get("[type = 'button'].btn.btn-primary").eq(2).click();
+		cy.get('#dynamicClickMessage').should('be.visible').and('have.text', 'You have done a dynamic click');
+	});
+
+	it('GX3-6095 | TC2: Validate “Double Click Me” Button is working successfully. (HP)', () => {
+		cy.get('#doubleClickBtn').dblclick();
+		cy.get('#doubleClickMessage').should('be.visible').and('have.text', 'You have done a double click');
+	});
+
+	it('GX3-6095 | TC3: Validate “Right Click Me” Button is working successfully. (HP)', () => {
+		cy.get('#rightClickBtn').rightclick();
+		cy.get('#rightClickMessage').should('be.visible').and('have.text', 'You have done a right click');
+	});
+
+	it('GX3-6095 | TC4: Validate that no results are returned with different actions on the “Double Click Me” Button', () => {
+		cy.get("[type = 'button'].btn.btn-primary").eq(2).should('be.visible').dblclick();
+		cy.get("[type = 'button'].btn.btn-primary").eq(2).should('be.visible').rightclick();
+		cy.get('#dynamicClickMessage').should('be.visible').and('have.text', 'You have done a dynamic click');
+		cy.get('#doubleClickMessage').should('not.exist');
+		cy.get('#rightClickMessage').should('not.exist');
+	});
+
+	it('GX3-6095 | TC5: Validate that no results are returned with different actions on the “Double Click Me” Button', () => {
+		cy.get('#doubleClickBtn').should('be.visible').click();
+		cy.get('#doubleClickBtn').should('be.visible').rightclick();
+		cy.get('#dynamicClickMessage').should('not.exist');
+		cy.get('#doubleClickMessage').should('not.exist');
+		cy.get('#rightClickMessage').should('not.exist');
+	});
+
+	it('GX3-6095 | TC6: Validate that no results are returned with different actions on the “Right Click Me” Button', () => {
+		cy.get('#rightClickBtn', { timeout: 90000 }).click();
+		cy.get('#rightClickBtn').dblclick();
+		cy.get('#dynamicClickMessage').should('not.exist');
+		cy.get('#doubleClickMessage').should('not.exist');
+		cy.get('#rightClickMessage').should('not.exist');
+	});
+});
