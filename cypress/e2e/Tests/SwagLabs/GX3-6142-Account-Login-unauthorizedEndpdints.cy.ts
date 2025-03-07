3ee0d3b6aa1e62816745b20c7382995e89ad8d88
Luis Hernandez <hernandezluis.umc@gmail.com>
2025-03-07 15:48:03 -0300

    test: (GX3-6142) add tests for feature 'No Authentication Access'

--- cypress/e2e/Tests/SwagLabs/GX3-6142-Account-Login-unauthorizedEndpdints.cy.ts
+++ cypress/e2e/Tests/SwagLabs/GX3-6142-Account-Login-unauthorizedEndpdints.cy.ts
@@ -1,7 +1,7 @@
 import loginSaucePage from '@pages/GX3-6142-Account-Login_and_access.Page';
 
 const dataTests = require('../../../fixtures/data/SwagLabs/GX3-6142-SwagLabs-login.json');
-// const dataUrls = require('../../../fixtures/data/SwagLabs/GX3-6142-SwagLabs-UnauthorizedAccess.json');
+const dataUrls = require('../../../fixtures/data/SwagLabs/GX3-6142-SwagLabs-UnauthorizedAccess.json');
 
 describe('GX3-6142: SwagLabs login', () => {
 	beforeEach('Preconditions: User must be on login Page', () => {
@@ -26,4 +26,14 @@ describe('GX3-6142: SwagLabs login', () => {
 	});
 });
 
-// describe('GX3-6142: SwagLabs Unauthorized Access', () => {});
+describe('GX3-6142: SwagLabs Unauthorized Access', () => {
+	dataUrls.forEach((dataUrl: { tc: string; url: string; errorMessage: string }) => {
+		it(dataUrl.tc, () => {
+			cy.visit(`${dataUrl.url}`, { failOnStatusCode: false });
+			cy.url().then(url => {
+				expect(url).to.be.equal('https://www.saucedemo.com/');
+			});
+			loginSaucePage.elements.errorMessage().should('be.visible').and('have.text', `${dataUrl.errorMessage}`);
+		});
+	});
+});
