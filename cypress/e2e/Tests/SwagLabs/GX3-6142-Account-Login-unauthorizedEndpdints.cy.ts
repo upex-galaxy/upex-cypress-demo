aecdb31f5d2dae9b49517409a8c30e3dd837fbd1
Luis Hernandez <hernandezluis.umc@gmail.com>
2025-03-07 15:45:21 -0300

    test: (GX3-6142) add tests for feature 'Account-Login'

--- cypress/e2e/Tests/SwagLabs/GX3-6142-Account-Login-unauthorizedEndpdints.cy.ts
+++ cypress/e2e/Tests/SwagLabs/GX3-6142-Account-Login-unauthorizedEndpdints.cy.ts
@@ -1,8 +1,29 @@
-import loginSaucPage from '@pages/GX3-6142-Account-Login_and_access.Page';
+import loginSaucePage from '@pages/GX3-6142-Account-Login_and_access.Page';
 
 const dataTests = require('../../../fixtures/data/SwagLabs/GX3-6142-SwagLabs-login.json');
-const dataUrls = require('../../../fixtures/data/SwagLabs/GX3-6142-SwagLabs-UnauthorizedAccess.json');
+// const dataUrls = require('../../../fixtures/data/SwagLabs/GX3-6142-SwagLabs-UnauthorizedAccess.json');
 
-describe('GX3-71927: SwagLabs login', () => {});
+describe('GX3-6142: SwagLabs login', () => {
+	beforeEach('Preconditions: User must be on login Page', () => {
+		cy.visit('https://www.saucedemo.com/');
+		cy.contains('Swag Labs');
+	});
+	dataTests.forEach((dataTest: { tc: string; username: string; password: string; errorMessage: string }) => {
+		it(dataTest.tc, () => {
+			if (dataTest.username !== null) {
+				loginSaucePage.typeUsername(dataTest.username);
+			}
+			if (dataTest.password !== null) {
+				loginSaucePage.typePassword(dataTest.password);
+			}
+			loginSaucePage.clickLoginButton();
+			if (dataTest.errorMessage === null) {
+				cy.url().should('include', '/inventory.html');
+			} else {
+				loginSaucePage.elements.errorMessage().should('be.visible').and('have.text', `${dataTest.errorMessage}`);
+			}
+		});
+	});
+});
 
-describe('GX3-71927: SwagLabs Unauthorized Access', () => {});
+// describe('GX3-6142: SwagLabs Unauthorized Access', () => {});
