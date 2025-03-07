f128292dc0d4cb7e37933457ecddfe1817e47ff9
Luis Hernandez <hernandezluis.umc@gmail.com>
2025-03-07 15:38:35 -0300

    feat: (GX3-6142) add features to be validated in e2e tests file

---
+++ cypress/e2e/Tests/SwagLabs/GX3-6142-Account-Login-unauthorizedEndpdints.cy.ts
@@ -0,0 +1,8 @@
+import loginSaucPage from '@pages/GX3-6142-Account-Login_and_access.Page';
+
+const dataTests = require('../../../fixtures/data/SwagLabs/GX3-6142-SwagLabs-login.json');
+const dataUrls = require('../../../fixtures/data/SwagLabs/GX3-6142-SwagLabs-UnauthorizedAccess.json');
+
+describe('GX3-71927: SwagLabs login', () => {});
+
+describe('GX3-71927: SwagLabs Unauthorized Access', () => {});
