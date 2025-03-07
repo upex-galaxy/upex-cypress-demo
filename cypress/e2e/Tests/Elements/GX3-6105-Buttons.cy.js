ad095d54e2f1d69d5bd7298821c8efa721775bb9
Diego <diegocaldera.adr@gmail.com>
2025-03-07 19:20:34 -0300

    GX-6105 Test(Elements/Buttons): {Modifiqué la nomenclatura de los TC y corre localigizadores de los TC3 y TC6 }

--- cypress/e2e/Tests/Elements/GX3-6105-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-6105-Buttons.cy.js
@@ -1,36 +1,36 @@
 describe('GX3-6105 TOOLSQA ELEMENTS BUTTONS', () => {
 	beforeEach('PRC: The user should be on the URL https://demoqa.com/buttons', () => {
 		cy.visit('https://demoqa.com/buttons');
-		cy.contains('Buttons').should('be.visible');
-		cy.url().should('include', '/buttons');
-		cy.location('protocol').should('eq', 'https:');
-		cy.location('hostname').should('eq', 'demoqa.com');
-		cy.request('https://demoqa.com/buttons').its('status').should('eq', 200);
+		//cy.contains('Buttons').should('be.visible');
+		//cy.url().should('include', '/buttons');
+		//cy.location('protocol').should('eq', 'https:');
+		//cy.location('hostname').should('eq', 'demoqa.com');
+		//cy.request('https://demoqa.com/buttons').its('status').should('eq', 200);
 	});
 
-	it('US GX3-6105 | TC 1 | VALIDAR COMPORTAMIENTO DEL BOTÓN DOUBLE CLICK', () => {
-		cy.get('#doubleClickBtn', { timeout: 10000 }).should('be.visible').and('be.enabled').dblclick();
+	it('US GX3-6105 | TC 1 | VALIDAR HACER CLICK EN EL BOTÓN "DOUBLE CLICK"', () => {
+		cy.get('#doubleClickBtn').should('be.visible').and('be.enabled').dblclick();
 	});
 
-	it('US GX3-6105|TC 2 | VALIDAR COMPORTAMIENTO DEL BOTÓN RIGHT CLICK', () => {
-		cy.get('button#rightClickBtn', { timeout: 10000 }).should('be.visible').and('be.enabled').rightclick();
+	it('US GX3-6105|TC 2 | VALIDAR HACER CLICK EN EL BOTÓN "RIGHT CLICK"', () => {
+		cy.get('button#rightClickBtn').should('be.visible').and('be.enabled').rightclick();
 	});
 
-	it('US GX3-6105 | TC 3 | VALIDAR COMPORTAMIENTO DEL BOTÓN CLICK ME', () => {
-		cy.get('button.btn-primary').eq(2).should('be.visible').and('be.enabled').click();
+	it('US GX3-6105 | TC 3 | VALIDAR HACER CLICK EN EL BOTÓN "CLICK ME"', () => {
+		cy.get('.btn.btn-primary:not(#doubleClickBtn):not(#rightClickBtn)').should('be.visible').and('be.enabled').click();
 	});
 
-	it('US GX3-6105 | TC 4 | VALIDAR MENSAJE DE BOTON DOUBLE CLICK ', () => {
-		cy.get('#doubleClickBtn', { timeout: 10000 }).should('be.visible').and('be.enabled').dblclick();
+	it('US GX3-6105 | TC 4 | VALIDAR QUE SE MUESTRE EL MENSAJE AL HACER CLICK EN EL BOTON "DOUBLE CLICK" ', () => {
+		cy.get('#doubleClickBtn').should('be.visible').and('be.enabled').dblclick();
 		cy.get('#doubleClickMessage').should('be.visible').and('contain', 'You have done a double click');
 	});
-	it('US GX3-6105 | TC 5 | VALIDAR MENSAJE DE BOTON RIGHT CLICK ', () => {
-		cy.get('button#rightClickBtn', { timeout: 10000 }).should('be.visible').and('be.enabled').rightclick();
+	it('US GX3-6105 | TC 5 | VALIDAR QUE SE MUESTRE EL MENSAJE AL HACER CLICK EN BOTON "RIGHT CLICK" ', () => {
+		cy.get('button#rightClickBtn').should('be.visible').and('be.enabled').rightclick();
 		cy.get('p[id=rightClickMessage]').should('be.visible').and('contain', 'You have done a right click');
 	});
 
-	it('US GX3-6105 | TC 6 | VALIDAR MENSAJE DE BOTON DOUBLE CLICK ', () => {
-		cy.get('button.btn-primary').eq(2).should('be.visible').and('be.enabled').click();
-		cy.get('p#dynamicClickMessage', { timeout: 10000 }).should('contain', 'You have done a dynamic click');
+	it('US GX3-6105 | TC 6 | VALIDAR QUE SE MUESTRE MENSAJE DE "BOTON  "CLICK ME" ', () => {
+		cy.get('.btn.btn-primary:not(#doubleClickBtn):not(#rightClickBtn)').should('be.visible').and('be.enabled').click();
+		cy.get('p#dynamicClickMessage').should('contain', 'You have done a dynamic click');
 	});
 });
