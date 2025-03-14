95b1b5d882050f7b81be8c4ebed2dbf9137e81fc
Diego <diegocaldera.adr@gmail.com>
2025-03-13 22:50:06 -0300

    GX-6175 Test(Elements/Buttons): { Mejoré la estructura y legibilidad de los casos de prueba }

--- cypress/e2e/Tests/Elements/GX3-6175-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-6175-Buttons.cy.js
@@ -3,28 +3,28 @@ describe('GX3-6175 TOOLSQA ELEMENTS BUTTONS', () => {
 		cy.visit('https://demoqa.com/buttons');
 	});
 
-	it('US GX3-6175 | TC 1 | VALIDAR HACER CLICK EN EL BOTON  DOUBLE CLICK', () => {
+	it('US GX3-6175 | TC 1 | VALIDAR QUE BOTON  "DOUBLE CLICK" FUNCIONA CORRECTAMENTE ', () => {
 		cy.get('#doubleClickBtn').should('be.visible').and('be.enabled').dblclick();
 	});
 
-	it('US GX3-6175|TC 2 | VALIDAR HACER CLICK EN EL BOTÓN  RIGHT CLICK', () => {
+	it('US GX3-6175|TC 2 | VALIDAR QUE BOTON "RIGHT CLICK" FUNCIONA CORRECTAMENTE ', () => {
 		cy.get('button#rightClickBtn').should('be.visible').and('be.enabled').rightclick();
 	});
 
-	it('US GX3-6175 | TC 3 | VALIDAR HACER CLICK EN EL BOTÓN  CLICK ME', () => {
+	it('US GX3-6175 | TC 3 | VALIDAR QUE BOTON "CLICK ME" FUNCIONA CORRECTAMENTE', () => {
 		cy.get('.btn.btn-primary:not(#doubleClickBtn):not(#rightClickBtn)').should('be.visible').and('be.enabled').click();
 	});
 
-	it('US GX3-6175 | TC 4 | VALIDAR QUE SE MUESTRE EL MENSAJE AL HACER CLICK EN EL BOTON  DOUBLE CLICK  ', () => {
+	it('US GX3-6175 | TC 4 | VALIDAR QUE SE MUESTRE EL MENSAJE AL HACER CLICK EN EL BOTON  "DOUBLE CLICK"  ', () => {
 		cy.get('#doubleClickBtn').should('be.visible').and('be.enabled').dblclick();
 		cy.get('#doubleClickMessage').should('be.visible').and('contain', 'You have done a double click');
 	});
-	it('US GX3-6175 | TC 5 | VALIDAR QUE SE MUESTRE EL MENSAJE AL HACER CLICK EN BOTON RIGHT CLICK ', () => {
+	it('US GX3-6175 | TC 5 | VALIDAR QUE SE MUESTRE EL MENSAJE AL HACER RIGHT CLICK EN "BOTON RIGHT CLICK" ', () => {
 		cy.get('button#rightClickBtn').should('be.visible').and('be.enabled').rightclick();
 		cy.get('p[id=rightClickMessage]').should('be.visible').and('contain', 'You have done a right click');
 	});
 
-	it('US GX3-6175 | TC 6 | VALIDAR QUE SE MUESTRE MENSAJE DE BOTON  CLICK ME ', () => {
+	it('US GX3-6175 | TC 6 | VALIDAR QUE SE MUESTRE MENSAJE  AL HACER CLICK EN BOTON "CLICK ME" ', () => {
 		cy.get('.btn.btn-primary:not(#doubleClickBtn):not(#rightClickBtn)').should('be.visible').and('be.enabled').click();
 		cy.get('p#dynamicClickMessage').should('contain', 'You have done a dynamic click');
 	});
